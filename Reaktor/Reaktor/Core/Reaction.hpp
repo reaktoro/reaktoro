79c355a07ae8b405e789fcab745935e44c942ce8
allanmulin <allanmulin@gmail.com>
2014-09-16 16:06:08 +0200

    Removed ReactionVector type.

--- Reaktor/Reaktor/Core/Reaction.hpp
+++ Reaktor/Reaktor/Core/Reaction.hpp
@@ -91,9 +91,6 @@ private:
     std::unique_ptr<Impl> pimpl;
 };
 
-/// Define a type for an ordered collection of Reaction instances
-typedef std::vector<Reaction> ReactionVector;
-
 /// Outputs the Reaction instance
 auto operator<<(std::ostream& out, const Reaction& reaction) -> std::ostream&;
 
