8d695f8f7a5a6aa741ffacb61246ce01bda45024
allanmulin <allanmulin@gmail.com>
2014-09-29 16:42:55 +0200

    Implemented test cases for Patition class and its utility methods.

--- tests/Core/TestPartition.cpp
+++ tests/Core/TestPartition.cpp
@@ -17,11 +17,334 @@
 
 #include "TestPartition.hpp"
 
+// Reaktor includes
+#include <Reaktor/Reaktor.hpp>
+
 namespace Reaktor {
+
+auto createMultiphase() -> Multiphase
+{
+    std::vector<Species> aqueous_species(3);
+    aqueous_species[0].setName("H2O");
+    aqueous_species[0].setElements({"H", "O"});
+    aqueous_species[0].setElementAtoms({2, 1});
+
+    aqueous_species[1].setName("H+");
+    aqueous_species[1].setElements({"H"});
+    aqueous_species[1].setElementAtoms({1});
+
+    aqueous_species[2].setName("OH-");
+    aqueous_species[2].setElements({"H", "O"});
+    aqueous_species[2].setElementAtoms({1, 1});
+
+    std::vector<Species> gaseous_species(2);
+    gaseous_species[0].setName("H2O(g)");
+    gaseous_species[0].setElements({"H", "O"});
+    gaseous_species[0].setElementAtoms({2, 1});
+
+    gaseous_species[1].setName("CO2(g)");
+    gaseous_species[1].setElements({"C", "O"});
+    gaseous_species[1].setElementAtoms({1, 2});
+
+    std::vector<Phase> phases(2);
+    phases[0].setName("Aqueous");
+    phases[1].setName("Gaseous");
+    phases[0].setSpecies(aqueous_species);
+    phases[1].setSpecies(gaseous_species);
+    Multiphase multiphase(phases);
+    return multiphase;
+}
+
+auto test_Partition() -> void
+{
+    Indices iequilibrium = {0, 1, 2};
+    Indices ikinetic = {3, 4};
+    Indices iinert = {5};
+    Partition partition(iequilibrium, ikinetic, iinert);
+    ASSERT_EQUAL(iequilibrium, partition.equilibriumSpeciesIndices());
+    ASSERT_EQUAL(ikinetic, partition.kineticSpeciesIndices());
+    ASSERT_EQUAL(iinert, partition.inertSpeciesIndices());
+}
+
+auto test_allEquilibrium() -> void
+{
+    Multiphase multiphase = createMultiphase();
+    Partition partition = Partition::allEquilibrium(multiphase);
+    Indices iequilibrium = range(5u);
+    ASSERT_EQUAL(iequilibrium, partition.equilibriumSpeciesIndices());
+    ASSERT(partition.kineticSpeciesIndices().empty());
+    ASSERT(partition.inertSpeciesIndices().empty());
+}
+
+auto test_allKinetic() -> void
+{
+    Multiphase multiphase = createMultiphase();
+    Partition partition = Partition::allKinetic(multiphase);
+    Indices ikinetic = range(5u);
+    ASSERT_EQUAL(ikinetic, partition.kineticSpeciesIndices());
+    ASSERT(partition.equilibriumSpeciesIndices().empty());
+    ASSERT(partition.inertSpeciesIndices().empty());
+}
+
+auto test_allEquilibriumExcept() -> void
+{
+    Indices iequilibrium = {0, 1, 3};
+    Indices ikinetic = {2, 4};
+    Indices iinert = {5};
+    Multiphase multiphase = createMultiphase();
+    Partition partition = Partition::allEquilibriumExcept(multiphase, ikinetic, iinert);
+    ASSERT_EQUAL(iequilibrium, partition.equilibriumSpeciesIndices());
+    ASSERT_EQUAL(ikinetic, partition.kineticSpeciesIndices());
+    ASSERT_EQUAL(iinert, partition.inertSpeciesIndices());
+}
+
+auto test_allKineticExcept() -> void
+{
+    Indices iequilibrium = {0, 1, 3};
+    Indices ikinetic = {2, 4};
+    Indices iinert = {5};
+    Multiphase multiphase = createMultiphase();
+    Partition partition = Partition::allKineticExcept(multiphase, iequilibrium, iinert);
+    ASSERT_EQUAL(iequilibrium, partition.equilibriumSpeciesIndices());
+    ASSERT_EQUAL(ikinetic, partition.kineticSpeciesIndices());
+    ASSERT_EQUAL(iinert, partition.inertSpeciesIndices());
+}
+
+auto test_numSpecies() -> void
+{
+    Partition partition({0,1,2}, {3,4}, {5});
+    ASSERT_EQUAL(6, numSpecies(partition));
+}
+
+auto test_numEquilibriumSpecies() -> void
+{
+    Partition partition({0,1,2}, {3,4}, {5});
+    ASSERT_EQUAL(3, numEquilibriumSpecies(partition));
+}
+
+auto test_numKineticSpecies() -> void
+{
+    Partition partition({0,1,2}, {3,4}, {5});
+    ASSERT_EQUAL(2, numKineticSpecies(partition));
+}
+
+auto test_numInertSpecies() -> void
+{
+    Partition partition({0,1,2}, {3,4}, {5});
+    ASSERT_EQUAL(1, numInertSpecies(partition));
+}
+
+auto test_elementIndicesInEquilibriumSpecies() -> void
+{
+    Multiphase multiphase = createMultiphase();
+    Partition partition({0,1,2}, {4}, {3});
+    const Index iH = elementIndex(multiphase, "H");
+    const Index iO = elementIndex(multiphase, "O");
+    Indices expected = {iH, iO};
+    Indices actual = elementIndicesInEquilibriumSpecies(multiphase, partition);
+    ASSERT(equal(expected, actual));
+}
+
+auto test_elementIndicesInKineticSpecies() -> void
+{
+    Multiphase multiphase = createMultiphase();
+    Partition partition({0,1,2}, {4}, {3});
+    const Index iO = elementIndex(multiphase, "O");
+    const Index iC = elementIndex(multiphase, "C");
+    Indices expected = {iC, iO};
+    Indices actual = elementIndicesInKineticSpecies(multiphase, partition);
+    ASSERT(equal(expected, actual));
+}
+
+auto test_elementIndicesInInertSpecies() -> void
+{
+    Multiphase multiphase = createMultiphase();
+    Partition partition({0,1,2}, {4}, {3});
+    const Index iH = elementIndex(multiphase, "H");
+    const Index iO = elementIndex(multiphase, "O");
+    Indices expected = {iH, iO};
+    Indices actual = elementIndicesInInertSpecies(multiphase, partition);
+    ASSERT(equal(expected, actual));
+}
+
+auto test_phaseIndicesWithEquilibriumSpecies() -> void
+{
+    Multiphase multiphase = createMultiphase();
+    Partition partition({0,1,2}, {4}, {3});
+    Indices expected = {0};
+    Indices actual = phaseIndicesWithEquilibriumSpecies(multiphase, partition);
+    ASSERT(equal(expected, actual));
+}
+
+auto test_phaseIndicesWithKineticSpecies() -> void
+{
+    Multiphase multiphase = createMultiphase();
+    Partition partition({0,1,2}, {4}, {3});
+    Indices expected = {1};
+    Indices actual = phaseIndicesWithKineticSpecies(multiphase, partition);
+    ASSERT(equal(expected, actual));
+}
+
+auto test_phaseIndicesWithInertSpecies() -> void
+{
+    Multiphase multiphase = createMultiphase();
+    Partition partition({0,1,2}, {4}, {3});
+    Indices expected = {1};
+    Indices actual = phaseIndicesWithInertSpecies(multiphase, partition);
+    ASSERT(equal(expected, actual));
+}
+
+#define ASSERT_EQUAL_VECTOR(expected, actual) ASSERT(arma::norm(expected - actual) < 1.e-16)
+#define ASSERT_EQUAL_MATRIX(expected, actual) ASSERT(arma::norm(expected - actual) < 1.e-16)
+
+auto test_equilibriumRows() -> void
+{
+    Partition partition({0,1,2}, {4}, {3});
+    Vector vec = {2.0, 3.0, 4.0, 5.0, 6.0};
+    Vector expected = {2.0, 3.0, 4.0};
+    Vector actual = equilibriumRows(partition, vec);
+    ASSERT_EQUAL_VECTOR(expected, actual);
+}
+
+auto test_kineticRows() -> void
+{
+    Partition partition({0,1,2}, {4}, {3});
+    Vector vec = {2.0, 3.0, 4.0, 5.0, 6.0};
+    Vector expected = {6.0};
+    Vector actual = kineticRows(partition, vec);
+    ASSERT_EQUAL_VECTOR(expected, actual);
+}
+
+auto test_inertRows() -> void
+{
+    Partition partition({0,1,2}, {4}, {3});
+    Vector vec = {2.0, 3.0, 4.0, 5.0, 6.0};
+    Vector expected = {5.0};
+    Vector actual = inertRows(partition, vec);
+    ASSERT_EQUAL_VECTOR(expected, actual);
+}
+
+auto test_equilibriumCols() -> void
+{
+    Partition partition({0,1,2}, {4}, {3});
+    Matrix mat = arma::randn<Matrix>(3, 5);
+    Matrix expected = mat.cols(arma::uvec{0,1,2});
+    Matrix actual = equilibriumCols(partition, mat);
+    ASSERT_EQUAL_MATRIX(expected, actual);
+}
+
+auto test_kineticCols() -> void
+{
+    Partition partition({0,1,2}, {4}, {3});
+    Matrix mat = arma::randn<Matrix>(3, 5);
+    Matrix expected = mat.cols(arma::uvec{4});
+    Matrix actual = kineticCols(partition, mat);
+    ASSERT_EQUAL_MATRIX(expected, actual);
+}
+
+auto test_inertCols() -> void
+{
+    Partition partition({0,1,2}, {4}, {3});
+    Matrix mat = arma::randn<Matrix>(3, 5);
+    Matrix expected = mat.cols(arma::uvec{3});
+    Matrix actual = inertCols(partition, mat);
+    ASSERT_EQUAL_MATRIX(expected, actual);
+}
+
+auto test_equilibriumRowsCols() -> void
+{
+    Partition partition({0,1,2}, {4}, {3});
+    Matrix mat = arma::randn<Matrix>(5, 5);
+    Matrix expected = mat.submat(arma::uvec{0,1,2}, arma::uvec{0,1,2});
+    Matrix actual = equilibriumRowsCols(partition, mat);
+    ASSERT_EQUAL_MATRIX(expected, actual);
+}
+
+auto test_kineticRowsCols() -> void
+{
+    Partition partition({0,1,2}, {4}, {3});
+    Matrix mat = arma::randn<Matrix>(5, 5);
+    Matrix expected = mat.submat(arma::uvec{4}, arma::uvec{4});
+    Matrix actual = kineticRowsCols(partition, mat);
+    ASSERT_EQUAL_MATRIX(expected, actual);
+}
+
+auto test_inertRowsCols() -> void
+{
+    Partition partition({0,1,2}, {4}, {3});
+    Matrix mat = arma::randn<Matrix>(5, 5);
+    Matrix expected = mat.submat(arma::uvec{3}, arma::uvec{3});
+    Matrix actual = inertRowsCols(partition, mat);
+    ASSERT_EQUAL_MATRIX(expected, actual);
+}
+
+auto test_equilibriumFormulaMatrix() -> void
+{
+    Multiphase multiphase = createMultiphase();
+    Partition partition({0,1,2}, {4}, {3});
+    const Index iH = elementIndex(multiphase, "H");
+    const Index iO = elementIndex(multiphase, "O");
+    Matrix mat = formulaMatrix(multiphase);
+    Matrix expected = mat.submat(arma::uvec{iH, iO}, arma::uvec{0,1,2});
+    Matrix actual = equilibriumFormulaMatrix(multiphase, partition, mat);
+    ASSERT_EQUAL_MATRIX(expected, actual);
+}
+
+auto test_kineticFormulaMatrix() -> void
+{
+    Multiphase multiphase = createMultiphase();
+    Partition partition({0,1,2}, {4}, {3});
+    const Index iC = elementIndex(multiphase, "C");
+    const Index iO = elementIndex(multiphase, "O");
+    Matrix mat = formulaMatrix(multiphase);
+    Matrix expected = mat.submat(arma::uvec{iC, iO}, arma::uvec{4});
+    Matrix actual = kineticFormulaMatrix(multiphase, partition, mat);
+    ASSERT_EQUAL_MATRIX(expected, actual);
+}
+
+auto test_inertFormulaMatrix() -> void
+{
+    Multiphase multiphase = createMultiphase();
+    Partition partition({0,1,2}, {4}, {3});
+    const Index iH = elementIndex(multiphase, "H");
+    const Index iO = elementIndex(multiphase, "O");
+    Matrix mat = formulaMatrix(multiphase);
+    Matrix expected = mat.submat(arma::uvec{iH, iO}, arma::uvec{3});
+    Matrix actual = inertFormulaMatrix(multiphase, partition, mat);
+    ASSERT_EQUAL_MATRIX(expected, actual);
+}
 
 auto testSuitePartition() -> cute::suite
 {
     cute::suite s;
+
+    s += CUTE(test_Partition);
+    s += CUTE(test_allEquilibrium);
+    s += CUTE(test_allKinetic);
+    s += CUTE(test_allEquilibriumExcept);
+    s += CUTE(test_allKineticExcept);
+    s += CUTE(test_numSpecies);
+    s += CUTE(test_numEquilibriumSpecies);
+    s += CUTE(test_numKineticSpecies);
+    s += CUTE(test_numInertSpecies);
+    s += CUTE(test_elementIndicesInEquilibriumSpecies);
+    s += CUTE(test_elementIndicesInKineticSpecies);
+    s += CUTE(test_elementIndicesInInertSpecies);
+    s += CUTE(test_phaseIndicesWithEquilibriumSpecies);
+    s += CUTE(test_phaseIndicesWithKineticSpecies);
+    s += CUTE(test_phaseIndicesWithInertSpecies);
+    s += CUTE(test_equilibriumRows);
+    s += CUTE(test_kineticRows);
+    s += CUTE(test_inertRows);
+    s += CUTE(test_equilibriumCols);
+    s += CUTE(test_kineticCols);
+    s += CUTE(test_inertCols);
+    s += CUTE(test_equilibriumRowsCols);
+    s += CUTE(test_kineticRowsCols);
+    s += CUTE(test_inertRowsCols);
+    s += CUTE(test_equilibriumFormulaMatrix);
+    s += CUTE(test_kineticFormulaMatrix);
+    s += CUTE(test_inertFormulaMatrix);
 
     return s;
 }
