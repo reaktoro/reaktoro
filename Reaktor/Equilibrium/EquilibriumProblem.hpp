e9287535ee8be15f11acccb54b789ae5c5206a8a
Allan Leal <allan.leal@psi.ch>
2014-12-08 13:31:27 +0100

    Implemented class EquilibriumProblem.

--- Reaktor/Equilibrium/EquilibriumProblem.hpp
+++ Reaktor/Equilibrium/EquilibriumProblem.hpp
@@ -26,33 +26,65 @@
 namespace Reaktor {
 
 // Forward declarations
-class Multiphase;
+class ChemicalSystem;
 class Partition;
 
+/// A type that defines an equilibrium problem
 class EquilibriumProblem
 {
 public:
-    explicit EquilibriumProblem(const Multiphase& multiphase);
+    /// Construct an EquilibriumProblem instance
+    explicit EquilibriumProblem(const ChemicalSystem& system);
 
-    EquilibriumProblem(const Multiphase& multiphase, const Partition& partition);
+    /// Construct an EquilibriumProblem instance with partitioning information
+    EquilibriumProblem(const ChemicalSystem& system, const Partition& partition);
 
+    /// Construct a copy of a EquilibriumProblem instance
     EquilibriumProblem(const EquilibriumProblem& other);
 
-    ~EquilibriumProblem();
+    /// Destroy this EquilibriumProblem instance
+    virtual ~EquilibriumProblem();
 
+    /// Assign an EquilibriumProblem instance to this
     auto operator=(EquilibriumProblem other) -> EquilibriumProblem&;
 
-    auto setTemperature(double value) -> EquilibriumProblem&;
+    /// Set the temperature for the equilibrium calculation (in units of K)
+    /// By default, the temperature is 298.15 K.
+    auto setTemperature(double val) -> EquilibriumProblem&;
 
-    auto setPressure(double value) -> EquilibriumProblem&;
+    /// Set the pressure for the equilibrium calculation (in units of Pa)
+    /// By default, the pressure is 10<sup>5</sup> Pa.
+    auto setPressure(double val) -> EquilibriumProblem&;
 
+    /// Set the electrical charge for the equilibrium calculation (in units of mol)
+    /// By default, the electrical charge is zero.
+    auto setCharge(double val) -> EquilibriumProblem&;
+
+    /// Set the amounts of the elements for the equilibrium calculation (in units of mol)
     auto setElementAmounts(const Vector& b) -> EquilibriumProblem&;
 
+    /// Get the temperature for the equilibrium calculation (in units of K)
     auto temperature() const -> double;
 
+    /// Get the pressure for the equilibrium calculation (in units of Pa)
     auto pressure() const -> double;
 
+    /// Get the electrical charge for the equilibrium calculation (in units of mol)
+    auto charge() const -> double;
+
+    /// Get the amounts of the elements for the equilibrium calculation (in units of mol)
     auto elementAmounts() const -> const Vector&;
+
+    /// Get the amounts of the linearly independent components for the equilibrium calculation (in units of mol)
+    /// The components in a ChemicalSystem comprise of elements and charge that are linearly independent.
+    /// @see ChemicalSystem::components
+    auto componentAmounts() const -> const Vector&;
+
+    /// Get a reference to the ChemicalSystem instance used to create this EquilibriumProblem instance
+    auto system() const -> const ChemicalSystem&;
+
+    /// Get a reference to the Partition instance used to create this EquilibriumProblem instance
+    auto partition() const -> const Partition&;
 
 private:
     class Impl;
