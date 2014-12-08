92a4a1765c36f01297a628c85f1b80572820e889
Allan Leal <allan.leal@psi.ch>
2014-12-08 13:31:27 +0100

    Implemented class EquilibriumProblem.

--- Reaktor/Equilibrium/EquilibriumProblem.cpp
+++ Reaktor/Equilibrium/EquilibriumProblem.cpp
@@ -17,11 +17,116 @@
 
 #include "EquilibriumProblem.hpp"
 
-// C++ includes
-
 // Reaktor includes
+#include <Reaktor/Core/ChemicalSystem.hpp>
 #include <Reaktor/Core/Partition.hpp>
 
 namespace Reaktor {
+
+struct EquilibriumProblem::Impl
+{
+    /// The reference to the ChemicalSystem instance
+    const ChemicalSystem& system;
+
+    /// The reference to the Partition instance
+    const Partition& partition;
+
+    /// The temperature for the equilibrium problem (in units of K)
+    double T;
+
+    /// The pressure for the equilibrium problem (in units of Pa)
+    double P;
+
+    /// The electrical charge for the equilibrium problem (in units of mol)
+    double charge;
+
+    /// The amounts of the elements for the equilibrium problem (in units of mol)
+    Vector b;
+
+    /// The amounts of the components for the equilibrium problem (in units of mol)
+    Vector bc;
+
+    /// Construct a EquilibriumProblem::Impl instance
+    Impl(const ChemicalSystem& system)
+    : Impl(system, Partition::allEquilibrium(system))
+    {}
+
+    /// Construct a EquilibriumProblem::Impl instance
+    Impl(const ChemicalSystem& system, const Partition& partition)
+    : system(system), partition(partition),
+      T(298.15), P(1e5), charge(0), b(arma::zeros(system.components().size())), bc
+    {}
+};
+
+EquilibriumProblem::EquilibriumProblem(const ChemicalSystem& system)
+: pimpl(new Impl(system))
+{}
+
+EquilibriumProblem::EquilibriumProblem(const EquilibriumProblem& other)
+: pimpl(new Impl(*other.pimpl))
+{}
+
+EquilibriumProblem::~EquilibriumProblem()
+{}
+
+auto EquilibriumProblem::operator=(EquilibriumProblem other) -> EquilibriumProblem&
+{
+    pimpl = std::move(other.pimpl);
+    return *this;
+}
+
+auto EquilibriumProblem::setTemperature(double val) -> EquilibriumProblem&
+{
+    pimpl->T = val;
+    return *this;
+}
+
+auto EquilibriumProblem::setPressure(double val) -> EquilibriumProblem&
+{
+    pimpl->P = val;
+    return *this;
+}
+
+auto EquilibriumProblem::setElementAmounts(const Vector& b) -> EquilibriumProblem&
+{
+    pimpl->b = b;
+    return *this;
+}
+
+auto EquilibriumProblem::setCharge(double val) -> EquilibriumProblem&
+{
+    pimpl->charge = val;
+    return *this;
+}
+
+auto EquilibriumProblem::temperature() const -> double
+{
+    return pimpl->T;
+}
+
+auto EquilibriumProblem::pressure() const -> double
+{
+    return pimpl->P;
+}
+
+auto EquilibriumProblem::charge() const -> double
+{
+    return pimpl->charge;
+}
+
+auto EquilibriumProblem::elementAmounts() const -> const Vector&
+{
+    return pimpl->b;
+}
+
+auto EquilibriumProblem::system() const -> const ChemicalSystem&
+{
+    return pimpl->system;
+}
+
+auto EquilibriumProblem::partition() const -> const Partition&
+{
+    return pimpl->partition;
+}
 
 } // namespace Reaktor
