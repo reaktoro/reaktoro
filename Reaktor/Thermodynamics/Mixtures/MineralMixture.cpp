79f101544f81de8e8d4b0eb08d875f35b70e9f8e
Allan Leal <allanmulin@gmail.com>
2015-03-18 15:20:49 +0100

    Before merging.

---
+++ Reaktor/Thermodynamics/Mixtures/MineralMixture.cpp
@@ -0,0 +1,37 @@
+// Reaktor is a C++ library for computational reaction modelling.
+//
+// Copyright (C) 2014 Allan Leal
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#include "MineralMixture.hpp"
+
+namespace Reaktor {
+
+MineralMixture::MineralMixture()
+: GeneralMixture<MineralSpecies>()
+{}
+
+MineralMixture::MineralMixture(const std::vector<MineralSpecies>& species)
+: GeneralMixture<MineralSpecies>(species)
+{}
+
+MineralMixture::MineralMixture(const MineralSpecies& species)
+: GeneralMixture<MineralSpecies>({species})
+{}
+
+MineralMixture::~MineralMixture()
+{}
+
+} // namespace Reaktor
