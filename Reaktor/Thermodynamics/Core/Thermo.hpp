0ed936020ecd0c85fba15d351c97ad2e4322c1d9
Allan Leal <allanmulin@gmail.com>
2015-03-25 23:37:36 +0100

    Implemented the Thermo class to calculate thermodynamic properties of species with memoization for improved performance.
    Former-commit-id: 78e83448ebec90f3e3721c6b880218c6083e975e

--- Reaktor/Thermodynamics/Core/Thermo.hpp
+++ Reaktor/Thermodynamics/Core/Thermo.hpp
@@ -1,98 +1,106 @@
-//// Reaktor is a C++ library for computational reaction modelling.
-////
-//// Copyright (C) 2014 Allan Leal
-////
-//// This program is free software: you can redistribute it and/or modify
-//// it under the terms of the GNU General Public License as published by
-//// the Free Software Foundation, either version 3 of the License, or
-//// (at your option) any later version.
-////
-//// This program is distributed in the hope that it will be useful,
-//// but WITHOUT ANY WARRANTY; without even the implied warranty of
-//// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-//// GNU General Public License for more details.
-////
-//// You should have received a copy of the GNU General Public License
-//// along with this program. If not, see <http://www.gnu.org/licenses/>.
+// Reaktor is a C++ library for computational reaction modelling.
 //
-//#pragma once
+// Copyright (C) 2014 Allan Leal
 //
-//// C++ includes
-//#include <vector>
-//#include <string>
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 //
-//namespace Reaktor {
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
 //
-//// Forward declarations
-//class Database;
-//class AqueousSpecies;
-//class GaseousSpecies;
-//class MineralSpecies;
-//class SpeciesThermoProperties;
-//class BilinearInterpolator;
-//
-//class Thermo
-//{
-//public:
-//    explicit Thermo(const Database& database);
-//
-//    auto setTemperatures(const std::vector<double>& temperatures) -> void;
-//
-//    auto setPressures(const std::vector<double>& pressures) -> void;
-//
-//    auto database() const -> const Database&;
-//
-//    auto temperatures() const -> const std::vector<double>&;
-//
-//    auto pressures() const -> const std::vector<double>&;
-//
-//    auto standardGibbsEnergy(const AqueousSpecies& species) const -> ThermoScalarFunction;
-//
-//    auto standardHelmholtzEnergy(const AqueousSpecies& species) const -> ThermoScalarFunction;
-//
-//    auto standardInternalEnergy(const AqueousSpecies& species) const -> ThermoScalarFunction;
-//
-//    auto standardEnthalpy(const AqueousSpecies& species) const -> ThermoScalarFunction;
-//
-//    auto standardEntropy(const AqueousSpecies& species) const -> ThermoScalarFunction;
-//
-//    auto standardVolume(const AqueousSpecies& species) const -> ThermoScalarFunction;
-//
-//    auto thermoDataHKF(const AqueousSpecies& species) const -> SpeciesThermoProperties;
-//
-//    auto thermoDataHKF(const GaseousSpecies& species) const -> SpeciesThermoProperties;
-//
-//    auto thermoDataHKF(const MineralSpecies& species) const -> SpeciesThermoProperties;
-//
-//    auto thermoDataReaction(const AqueousSpecies& species) const -> SpeciesThermoProperties;
-//
-//    auto thermoDataReaction(const GaseousSpecies& species) const -> SpeciesThermoProperties;
-//
-//    auto thermoDataReaction(const MineralSpecies& species) const -> SpeciesThermoProperties;
-//
-//    auto thermoDataInterpolated(const AqueousSpecies& species) const -> SpeciesThermoProperties;
-//
-//    auto thermoDataInterpolated(const GaseousSpecies& species) const -> SpeciesThermoProperties;
-//
-//    auto thermoDataInterpolated(const MineralSpecies& species) const -> SpeciesThermoProperties;
-//
-//    auto thermoData(const std::string& species) const -> SpeciesThermoProperties;
-//
-//    auto thermoData(const AqueousSpecies& species) const -> SpeciesThermoProperties;
-//
-//    auto thermoData(const GaseousSpecies& species) const -> SpeciesThermoProperties;
-//
-//    auto thermoData(const MineralSpecies& species) const -> SpeciesThermoProperties;
-//
-//private:
-//    /// The database instance
-//    const Database& database$;
-//
-//    /// The temperatures for the interpolation (in units of K)
-//    std::vector<double> temperatures$;
-//
-//    /// The pressures for the interpolation (in units of Pa)
-//    std::vector<double> pressures$;
-//};
-//
-//} // namespace Reaktor
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+// C++ includes
+#include <string>
+#include <memory>
+
+namespace Reaktor {
+
+// Forward declarations
+class Database;
+class ThermoScalar;
+struct SpeciesThermoState;
+struct WaterThermoState;
+
+/// A type to calculate thermodynamic properties of chemical species
+class Thermo
+{
+public:
+    /// Construct a Thermo instance with given Database instance
+    explicit Thermo(const Database& database);
+
+    /// Calculate the apparent standard molar Gibbs free energy of a species (in units of J/mol).
+    /// @param T The temperature value (in units of K)
+    /// @param P The pressure value (in units of Pa)
+    /// @param species The name of the species
+    auto standardGibbsEnergy(double T, double P, std::string species) -> ThermoScalar;
+
+    /// Calculate the apparent standard molar Helmholtz free energy of a species (in units of J/mol).
+    /// @param T The temperature value (in units of K)
+    /// @param P The pressure value (in units of Pa)
+    /// @param species The name of the species
+    auto standardHelmholtzEnergy(double T, double P, std::string species) -> ThermoScalar;
+
+    /// Calculate the apparent standard molar internal energy of a species (in units of J/mol).
+    /// @param T The temperature value (in units of K)
+    /// @param P The pressure value (in units of Pa)
+    /// @param species The name of the species
+    auto standardInternalEnergy(double T, double P, std::string species) -> ThermoScalar;
+
+    /// Calculate the apparent standard molar enthalpy of a species (in units of J/mol).
+    /// @param T The temperature value (in units of K)
+    /// @param P The pressure value (in units of Pa)
+    /// @param species The name of the species
+    auto standardEnthalpy(double T, double P, std::string species) -> ThermoScalar;
+
+    /// Calculate the standard molar entropies of a species (in units of J/K).
+    /// @param T The temperature value (in units of K)
+    /// @param P The pressure value (in units of Pa)
+    /// @param species The name of the species
+    auto standardEntropy(double T, double P, std::string species) -> ThermoScalar;
+
+    /// Calculate the standard molar volumes of a species (in units of m3/mol).
+    /// @param T The temperature value (in units of K)
+    /// @param P The pressure value (in units of Pa)
+    /// @param species The name of the species
+    auto standardVolume(double T, double P, std::string species) -> ThermoScalar;
+
+    /// Calculate the standard molar isobaric heat capacity of a species (in units of J/(mol*K)).
+    /// @param T The temperature value (in units of K)
+    /// @param P The pressure value (in units of Pa)
+    /// @param species The name of the species
+    auto standardHeatCapacity(double T, double P, std::string species) -> ThermoScalar;
+
+    /// Calculate the thermodynamic state of an aqueous species using the HKF model.
+    /// @param T The temperature value (in units of K)
+    /// @param P The pressure value (in units of Pa)
+    /// @param species The name of the species
+    /// @see SpeciesThermoState
+    auto speciesThermoStateHKF(double T, double P, std::string species) -> SpeciesThermoState;
+
+    /// Calculate the thermodynamic state of water using the Haar-Gallagher-Kell (1984) equation of state.
+    /// @param T The temperature of water (in units of K)
+    /// @param P The pressure of water (in units of Pa)
+    /// @see WaterThermoState
+    auto waterThermoStateHGK(double T, double P) -> WaterThermoState;
+
+    /// Calculate the thermodynamic state of water using the Wagner and Pruss (1995) equation of state.
+    /// @param T The temperature of water (in units of K)
+    /// @param P The pressure of water (in units of Pa)
+    /// @see WaterThermoState
+    auto waterThermoStateWagnerPruss(double T, double P) -> WaterThermoState;
+
+private:
+    struct Impl;
+
+    std::shared_ptr<Impl> pimpl;
+};
+
+} // namespace Reaktor
