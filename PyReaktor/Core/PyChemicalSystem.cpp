a7ccdc5faa87b1e4822e8b006e0d4cd38f2b45c5
Allan Leal <allan.leal@psi.ch>
2015-03-27 12:43:44 +0100

    Fixed python interface to Multiphase and ChemicalSystem classes.
    Former-commit-id: 97d1ee7469321103582223a71718e2d07a49903e

--- PyReaktor/Core/PyChemicalSystem.cpp
+++ PyReaktor/Core/PyChemicalSystem.cpp
@@ -23,6 +23,7 @@
 namespace py = boost::python;
 
 // Reaktor includes
+#include <Reaktor/Core/Multiphase.hpp>
 #include <Reaktor/Core/ChemicalSystem.hpp>
 #include <Reaktor/Interfaces/Gems.hpp>
 #include <Reaktor/Interfaces/Phreeqx.hpp>
@@ -53,76 +54,12 @@ auto export_ChemicalSystem() -> void
         .def_readwrite("phase_to_elements", &Connectivity::phase_to_elements)
         ;
 
-    auto element1 = static_cast<const Element&(ChemicalSystem::*)(Index) const>(&ChemicalSystem::element);
-    auto element2 = static_cast<const Element&(ChemicalSystem::*)(std::string) const>(&ChemicalSystem::element);
-
-    auto species1 = static_cast<const std::vector<Species>&(ChemicalSystem::*)() const>(&ChemicalSystem::species);
-    auto species2 = static_cast<const Species&(ChemicalSystem::*)(Index) const>(&ChemicalSystem::species);
-    auto species3 = static_cast<const Species&(ChemicalSystem::*)(std::string) const>(&ChemicalSystem::species);
-
-    auto phase1 = static_cast<const Phase&(ChemicalSystem::*)(Index) const>(&ChemicalSystem::phase);
-    auto phase2 = static_cast<const Phase&(ChemicalSystem::*)(std::string) const>(&ChemicalSystem::phase);
-
-    auto indicesElementsInSpecies1 = static_cast<Indices(ChemicalSystem::*)(Index) const>(&ChemicalSystem::indicesElementsInSpecies);
-    auto indicesElementsInSpecies2 = static_cast<Indices(ChemicalSystem::*)(const Indices&) const>(&ChemicalSystem::indicesElementsInSpecies);
-
-    py::class_<ChemicalSystem>("ChemicalSystem")
+    py::class_<ChemicalSystem, py::bases<Multiphase>>("ChemicalSystem")
         .def(py::init<>())
-        .def(py::init<const std::vector<Phase>&>())
-        .def(py::init<const std::vector<Phase>&, const ChemicalModels&>())
+        .def(py::init<const Multiphase&>())
         .def("__init__", py::make_constructor(createChemicalSystemGems))
         .def("__init__", py::make_constructor(createChemicalSystemPhreeqx))
-        .def("numElements", &ChemicalSystem::numElements)
-        .def("numSpecies", &ChemicalSystem::numSpecies)
-        .def("numSpeciesInPhase", &ChemicalSystem::numSpeciesInPhase)
-        .def("numPhases", &ChemicalSystem::numPhases)
-        .def("elements", &ChemicalSystem::elements, py::return_value_policy<py::copy_const_reference>())
-        .def("species", species1, py::return_value_policy<py::copy_const_reference>())
-        .def("phases", &ChemicalSystem::phases, py::return_value_policy<py::copy_const_reference>())
-        .def("formulaMatrix", &ChemicalSystem::formulaMatrix, py::return_value_policy<py::copy_const_reference>())
         .def("connectivity", &ChemicalSystem::connectivity, py::return_value_policy<py::copy_const_reference>())
-        .def("element", element1, py::return_value_policy<py::copy_const_reference>())
-        .def("element", element2, py::return_value_policy<py::copy_const_reference>())
-        .def("species", species2, py::return_value_policy<py::copy_const_reference>())
-        .def("species", species3, py::return_value_policy<py::copy_const_reference>())
-        .def("phase", phase1, py::return_value_policy<py::copy_const_reference>())
-        .def("phase", phase2, py::return_value_policy<py::copy_const_reference>())
-        .def("indexElement", &ChemicalSystem::indexElement)
-        .def("indexElementWithError", &ChemicalSystem::indexElementWithError)
-        .def("indexSpecies", &ChemicalSystem::indexSpecies)
-        .def("indexSpeciesWithError", &ChemicalSystem::indexSpeciesWithError)
-        .def("indexPhase", &ChemicalSystem::indexPhase)
-        .def("indexPhaseWithError", &ChemicalSystem::indexPhaseWithError)
-        .def("indexPhaseWithSpecies", &ChemicalSystem::indexPhaseWithSpecies)
-        .def("indicesElements", &ChemicalSystem::indicesElements)
-        .def("indicesSpecies", &ChemicalSystem::indicesSpecies)
-        .def("indicesPhases", &ChemicalSystem::indicesPhases)
-        .def("indicesPhasesWithSpecies", &ChemicalSystem::indicesPhasesWithSpecies)
-        .def("indicesElementsInSpecies", indicesElementsInSpecies1)
-        .def("indicesElementsInSpecies", indicesElementsInSpecies2)
-        .def("indexFirstSpeciesInPhase", &ChemicalSystem::indexFirstSpeciesInPhase)
-        .def("standardGibbsEnergies", &ChemicalSystem::standardGibbsEnergies)
-        .def("standardEnthalpies", &ChemicalSystem::standardEnthalpies)
-        .def("standardHelmholtzEnergies", &ChemicalSystem::standardHelmholtzEnergies)
-        .def("standardEntropies", &ChemicalSystem::standardEntropies)
-        .def("standardVolumes", &ChemicalSystem::standardVolumes)
-        .def("standardInternalEnergies", &ChemicalSystem::standardInternalEnergies)
-        .def("standardHeatCapacities", &ChemicalSystem::standardHeatCapacities)
-        .def("concentrations", &ChemicalSystem::concentrations)
-        .def("activityCoefficients", &ChemicalSystem::activityCoefficients)
-        .def("activities", &ChemicalSystem::activities)
-        .def("chemicalPotentials", &ChemicalSystem::chemicalPotentials)
-        .def("phaseMolarVolumes", &ChemicalSystem::phaseMolarVolumes)
-        .def("phaseDensities", &ChemicalSystem::phaseDensities)
-        .def("phaseMolarAmounts", &ChemicalSystem::phaseMolarAmounts)
-        .def("phaseMassAmounts", &ChemicalSystem::phaseMassAmounts)
-        .def("phaseVolumes", &ChemicalSystem::phaseVolumes)
-        .def("elementAmounts", &ChemicalSystem::elementAmounts)
-        .def("elementAmountsInPhase", &ChemicalSystem::elementAmountsInPhase)
-        .def("elementAmountsInSpecies", &ChemicalSystem::elementAmountsInSpecies)
-        .def("elementAmount", &ChemicalSystem::elementAmount)
-        .def("elementAmountInPhase", &ChemicalSystem::elementAmountInPhase)
-        .def("elementAmountsInSpecies", &ChemicalSystem::elementAmountsInSpecies)
         .def(py::self_ns::str(py::self_ns::self));
         ;
 }
