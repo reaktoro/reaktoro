de66c3fa103977af538a4210c5ce1ade549f64f4
Allan Leal <allanmulin@gmail.com>
2015-04-10 16:17:32 -0300

    Added new operators for ThermoScalar.
    Former-commit-id: 517c9cf331e24b53d279e11c0a54215011f2c724

--- Reaktor/Common/ThermoScalar.cpp
+++ Reaktor/Common/ThermoScalar.cpp
@@ -118,9 +118,66 @@ auto operator*(const ThermoScalar& l, double scalar) -> ThermoScalar
     return scalar * l;
 }
 
+auto operator*(const ThermoScalar& l, const ThermoScalar& r) -> ThermoScalar
+{
+    ThermoScalar res;
+    res.val = l.val * r.val;
+    res.ddt = l.ddt * r.val + l.val * r.ddt;
+    res.ddp = l.ddp * r.val + l.val * r.ddp;
+    return res;
+}
+
+auto operator/(double scalar, const ThermoScalar& r) -> ThermoScalar
+{
+    const double factor = -scalar/(r.val * r.val);
+    ThermoScalar res;
+    res.val = scalar/r.val;
+    res.ddt = factor * r.ddt;
+    res.ddp = factor * r.ddp;
+    return res;
+}
+
 auto operator/(const ThermoScalar& l, double scalar) -> ThermoScalar
 {
     return (1.0/scalar) * l;
+}
+
+auto operator/(const ThermoScalar& l, const ThermoScalar& r) -> ThermoScalar
+{
+    const double factor = 1.0/(r.val * r.val);
+    ThermoScalar res;
+    res.val = l.val / r.val;
+    res.ddt = (r.val * l.ddt - l.val * r.ddt) * factor;
+    res.ddp = (r.val * l.ddp - l.val * r.ddp) * factor;
+    return res;
+}
+
+auto pow(const ThermoScalar& a, double power) -> ThermoScalar
+{
+    ThermoScalar b;
+    b.val = std::pow(a.val, power);
+    b.ddt = power * b.val * a.ddt/a.val;
+    b.ddp = power * b.val * a.ddp/a.val;
+    return b;
+}
+
+auto exp(const ThermoScalar& a) -> ThermoScalar
+{
+    ThermoScalar b;
+    b.val = std::exp(a.val);
+    b.ddt = b.val * a.ddt;
+    b.ddp = b.val * a.ddp;
+    return b;
+}
+
+auto log(const ThermoScalar& a) -> ThermoScalar
+{
+    const double factor = 1.0/a.val;
+    ThermoScalar b;
+    b.val = std::log(a.val);
+    b.ddt = factor * a.ddt;
+    b.ddp = factor * a.ddp;
+    return b;
 }
 
 } // namespace Reaktor
