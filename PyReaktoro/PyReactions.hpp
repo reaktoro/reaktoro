8d034fe0ae3d54a62c071de2af44efa8ee9939e0
Allan Leal <allanmulin@gmail.com>
2015-04-21 11:14:06 -0300

    Implemented python bindings for MineralReaction, MineralMechanism, MineralCatalyst classes.

---
+++ PyReaktoro/PyReactions.hpp
@@ -0,0 +1,34 @@
+// Reaktoro is a C++ library for computational reaction modelling.
+//
+// Copyright (C) 2014 Allan Leal
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+// PyReaktoro includes
+#include <PyReaktoro/Reactions/PyMineralCatalyst.hpp>
+#include <PyReaktoro/Reactions/PyMineralMechanism.hpp>
+#include <PyReaktoro/Reactions/PyMineralReaction.hpp>
+
+namespace Reaktoro {
+
+inline auto export_Reactions() -> void
+{
+    export_MineralCatalyst();
+    export_MineralMechanism();
+    export_MineralReaction();
+}
+
+} // namespace Reaktoro
