843e16a8ec944c9d39c54543ca7b42677ed45814
Allan Leal <allan.leal@psi.ch>
2015-04-29 10:36:47 +0200

    Fixed include directive.
    Former-commit-id: c311ce74b84ca06abd5ef07a27e18eae0cb3bbe8

--- PyReaktoro/Core/PyChemicalPlot.cpp
+++ PyReaktoro/Core/PyChemicalPlot.cpp
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-#include "PyPartition.hpp"
+#include "PyChemicalPlot.hpp"
 
 // Boost includes
 #include <boost/python.hpp>
