0bec0e94b59fcb665b6f6f4abeeecf3594284f83
Allan Leal <allan.leal@psi.ch>
2015-05-29 16:26:15 +0200

    Implemented Brent's method for single-variable minimization problems.

--- Reaktoro/Optimization/Utils.cpp
+++ Reaktoro/Optimization/Utils.cpp
@@ -92,7 +92,7 @@ auto bfgs() -> std::function<Matrix(const Vector&, const Vector&)>
     return f;
 }
 
-auto minimizeGoldenSectionSearch(const std::function<double(double)>& f, double a, double b, double tol) -> double
+auto minimizeGoldenSectionSearch(const std::function<double(double)>& f, double tol) -> double
 {
     //---------------------------------------------------------------
     // Reference: http://en.wikipedia.org/wiki/Golden_section_search
@@ -101,10 +101,13 @@ auto minimizeGoldenSectionSearch(const std::function<double(double)>& f, double 
     // The golden ratio
     const double phi = 0.61803398875;
 
-    double c = b - phi*(b - a);
-    double d = a + phi*(b - a);
+    double a = 0.0;
+    double b = 1.0;
 
-    while(std::abs(c - d) > tol*(std::abs(a) + std::abs(b)))
+    double c = 1 - phi;
+    double d = phi;
+
+    while(std::abs(c - d) > tol)
     {
         double fc = f(c);
         double fd = f(d);
@@ -124,6 +127,136 @@ auto minimizeGoldenSectionSearch(const std::function<double(double)>& f, double 
     }
 
     return (b + a)/2.0;
+}
+
+
+auto minimizeGoldenSectionSearch(const std::function<double(double)>& f, double a, double b, double tol) -> double
+{
+    auto g = [=](double x)
+    {
+        return f(a + x*(b - a));
+    };
+
+    const double xmin = minimizeGoldenSectionSearch(g, tol);
+    return a + xmin*(b - a);
+}
+
+auto minimizeBrent(const std::function<double(double)>& f, double min, double max, double tolerance, unsigned maxiters) -> double
+{
+    //-------------------------------------------------------------------
+    // The code below was adapted from boost library, found at header
+    // boost/math/tools/minima.hpp under the name brent_find_minima.
+    //-------------------------------------------------------------------
+    double x; // minima so far
+    double w; // second best point
+    double v; // previous value of w
+    double u; // most recent evaluation point
+    double delta; // The distance moved in the last step
+    double delta2; // The distance moved in the step before last
+    double fu, fv, fw, fx; // function evaluations at u, v, w, x
+    double mid; // midpoint of min and max
+    double fract1, fract2; // minimal relative movement in x
+
+    const double golden = 0.3819660;// golden ratio, don't need too much precision here!
+
+    x = w = v = max;
+    fw = fv = fx = f(x);
+    delta2 = delta = 0;
+
+    unsigned count = maxiters;
+
+    do
+    {
+        // get midpoint
+        mid = (min + max)/2;
+        // work out if we're done already:
+        fract1 = tolerance * fabs(x) + tolerance / 4;
+        fract2 = 2 * fract1;
+        if(fabs(x - mid) <= (fract2 - (max - min) / 2))
+        break;
+
+        if(fabs(delta2) > fract1)
+        {
+            // try and construct a parabolic fit:
+            double r = (x - w) * (fx - fv);
+            double q = (x - v) * (fx - fw);
+            double p = (x - v) * q - (x - w) * r;
+            q = 2 * (q - r);
+            if(q > 0)
+            p = -p;
+            q = fabs(q);
+            double td = delta2;
+            delta2 = delta;
+            // determine whether a parabolic step is acceptible or not:
+            if((fabs(p) >= fabs(q * td / 2)) || (p <= q * (min - x)) || (p >= q * (max - x)))
+            {
+                // nope, try golden section instead
+                delta2 = (x >= mid) ? min - x : max - x;
+                delta = golden * delta2;
+            }
+            else
+            {
+                // when, parabolic fit:
+                delta = p / q;
+                u = x + delta;
+                if(((u - min) < fract2) || ((max- u) < fract2))
+                delta = (mid - x) < 0 ? -fabs(fract1) : fabs(fract1);
+            }
+        }
+        else
+        {
+            // golden section:
+            delta2 = (x >= mid) ? min - x : max - x;
+            delta = golden * delta2;
+        }
+        // update current position:
+        u = (fabs(delta) >= fract1) ? double(x + delta) : (delta > 0 ? double(x + fabs(fract1)) : double(x - fabs(fract1)));
+        fu = f(u);
+        if(fu <= fx)
+        {
+            // good new point is an improvement!
+            // update brackets:
+            if(u >= x)
+            min = x;
+            else
+            max = x;
+            // update control points:
+            v = w;
+            w = x;
+            x = u;
+            fv = fw;
+            fw = fx;
+            fx = fu;
+        }
+        else
+        {
+            // Oh dear, point u is worse than what we have already,
+            // even so it *must* be better than one of our endpoints:
+            if(u < x)
+            min = u;
+            else
+            max = u;
+            if((fu <= fw) || (w == x))
+            {
+                // however it is at least second best:
+                v = w;
+                w = u;
+                fv = fw;
+                fw = fu;
+            }
+            else if((fu <= fv) || (v == x) || (v == w))
+            {
+                // third best:
+                v = u;
+                fv = fu;
+            }
+        }
+
+    } while(--count);
+
+    maxiters -= count;
+
+    return x;
 }
 
 } // namespace Reaktoro
