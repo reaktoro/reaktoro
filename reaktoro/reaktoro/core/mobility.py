091eda1bba297afc240cfdfd93817b02310b5eb9
Allan Leal <allan.leal@psi.ch>
2015-06-11 08:44:24 +0200

    Mobility class now uses the new implementation of Connectivity class.

--- reaktoro/reaktoro/core/mobility.py
+++ reaktoro/reaktoro/core/mobility.py
@@ -1,19 +1,19 @@
-
+from reaktoro.core import *
 
 class Mobility:
     def __init__(self, system):
         self.system = system
+        self.connectivity = Connectivity(self.system)
         self._ifluid_phases  = []
         self._isolid_phases  = range(system.numPhases())
         self._ifluid_species = []
         self._isolid_species = range(system.numSpecies())
 
     def setFluidPhases(self, fluid_phases):
-        phase_to_species = self.system.connectivity().phase_to_species
         for phase_name in fluid_phases:
             iphase = self.system.indexPhaseWithError(phase_name)
             self._ifluid_phases.append(iphase)
-            self._ifluid_species += phase_to_species[iphase]
+            self._ifluid_species += self.connectivity.indicesSpeciesInPhase(iphase)
         prange = range(self.system.numPhases())
         srange = range(self.system.numSpecies())
         self._isolid_phases = set(prange) - set(self._ifluid_phases)
