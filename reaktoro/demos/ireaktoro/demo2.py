727d6adb102a92b8e92f2805c5ec7e2f32a0f43a
Allan Leal <allan.leal@psi.ch>
2015-06-18 13:23:58 +0200

    Added a new demo that uses the EquilibriumPath command block.

---
+++ reaktoro/demos/ireaktoro/demo2.py
@@ -0,0 +1,42 @@
+import sys
+sys.path.append('/home/leal_a/git/Reaktoro/reaktoro/')
+from reaktoro.interpreter.ireaktoro import interpret
+
+input = """
+ChemicalSystem:
+    Database: supcrt98.xml
+    AqueousPhase:
+        Species: H2O(l) H+ OH- HCO3- CO2(aq) Ca+2 Cl- Na+ O2(aq) H2(aq)
+    GaeousPhase:
+        Species: H2O(g) CO2(g)
+    MineralPhases: Calcite
+
+Equilibrium State1:
+    Temperature: 60 celsius
+    Pressure: 150 bar
+    Mixture:
+        H2O: 1 kg
+        NaCl: 1 umol
+        HCl: 1 umol
+        O2: 1 umol
+        CaCO3: 10 mol
+
+Equilibrium State2:
+    Temperature: 60 celsius
+    Pressure: 150 bar
+    Mixture:
+        H2O: 1 kg
+        NaCl: 1 umol
+        HCl: 1 mol
+        O2: 1 umol
+        CaCO3: 10 mol
+
+EquilibriumPath:
+    From: State1
+    To: State2
+    Plot 1:
+        x: t
+        y: m[Ca] pH
+"""
+
+interpret(input)
