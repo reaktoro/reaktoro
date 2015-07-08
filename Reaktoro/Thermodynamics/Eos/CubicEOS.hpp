934c90b4bdb8b5d8a8f9a30cc5ccc86173e51430
Allan Leal <allan.leal@psi.ch>
2015-07-08 16:53:46 +0200

    Still implementing CubicEOS - going home.

--- Reaktoro/Thermodynamics/Eos/CubicEOS.hpp
+++ Reaktoro/Thermodynamics/Eos/CubicEOS.hpp
@@ -43,17 +43,11 @@ public:
 
     struct Result
     {
-        /// The residual partial molar volumes of the species (in units m3/mol)
-        ChemicalVector residual_partial_molar_volumes;
-
-        /// The residual partial molar Gibbs energies of the species (in units J/mol)
-        ChemicalVector residual_partial_molar_gibbs_energies;
-
-        /// The residual partial molar enthalpies of the species (in units J/mol)
-        ChemicalVector residual_partial_molar_enthalpies;
-
         /// The molar volume of the phase (in units of m3/mol).
         ChemicalScalar molar_volume;
+
+        /// The residual molar Gibbs energy of the phase (in units of J/mol).
+        ChemicalScalar residual_molar_gibbs_energy;
 
         /// The residual molar enthalpy of the phase (in units of J/mol).
         ChemicalScalar residual_molar_enthalpy;
@@ -66,9 +60,6 @@ public:
 
         /// The fugacity coefficients of the species in the phase.
         ChemicalVector fugacity_coefficients;
-
-        /// The residual molar Gibbs energy of the phase (in units of J/mol).
-        ChemicalScalar residual_molar_gibbs_energy;
     };
 
     /// Construct a CubicEOS instance with given number of species.
