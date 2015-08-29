562c2578a0ab5aeb76120d5c3528d8613438af43
unknown <leal_a@PC11230.psi.ch>
2015-08-29 23:03:16 +0200

    Fixed include directive.

--- python/PyReaktoro/Thermodynamics/PyThermo.cpp
+++ python/PyReaktoro/Thermodynamics/PyThermo.cpp
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-#include "PyDatabase.hpp"
+#include "PyThermo.hpp"
 
 // Boost includes
 #include <boost/python.hpp>
