eb4e2d23495529eb2ae1f765e64a8811aeb1ace2
Allan Leal <allanmulin@gmail.com>
2015-11-14 17:23:49 +0100

    Several improvements in IpNewton that results in more stable and efficient calculations.

--- Reaktoro/Optimization/OptimumSolverIpNewton.cpp
+++ Reaktoro/Optimization/OptimumSolverIpNewton.cpp
@@ -17,13 +17,9 @@
 
 #include "OptimumSolverIpNewton.hpp"
 
-// Eigen includes
-#include <Reaktoro/Eigen/Dense>
-
 // Reaktoro includes
 #include <Reaktoro/Common/Exception.hpp>
 #include <Reaktoro/Common/Outputter.hpp>
-#include <Reaktoro/Common/SetUtils.hpp>
 #include <Reaktoro/Common/TimeUtils.hpp>
 #include <Reaktoro/Math/MathUtils.hpp>
 #include <Reaktoro/Optimization/KktSolver.hpp>
@@ -40,7 +36,7 @@ struct OptimumSolverIpNewton::Impl
     KktVector rhs;
     KktSolution sol;
     KktSolver kkt;
-
+    Vector xtrial;
     Outputter outputter;
 
     auto solve(const OptimumProblem& problem, OptimumState& state, const OptimumOptions& options) -> OptimumResult;
@@ -67,17 +63,28 @@ auto OptimumSolverIpNewton::Impl::solve(const OptimumProblem& problem, OptimumSt
     auto& z = state.z;
     auto& f = state.f;
 
+    // The number of variables and equality constraints
     const auto& A = problem.A;
     const auto& b = problem.b;
+    const auto& n = problem.A.cols();
+    const auto& m = problem.A.rows();
 
-    Vector h;
+    // Define auxiliary references to general options
+    const auto tol = options.tolerance;
+    const auto maxiters = options.max_iterations;
 
-    // Define some auxiliary references to parameters
-    const auto& n         = problem.A.cols();
-    const auto& m         = problem.A.rows();
-    const auto& tolerance = options.tolerance;
-    const auto& mu        = options.ipnewton.mu;
-    const auto& tau       = options.ipnewton.tau;
+    // Define some auxiliary references to IpNewton parameters
+    const auto mu = options.ipnewton.mu;
+    const auto tau = options.ipnewton.tau;
+
+    // Define some auxiliary references to result variables
+    auto& error = result.error;
+    auto& iterations = result.iterations;
+    auto& succeeded = result.succeeded = false;
+
+    // The regularization parameters delta and gamma
+    auto gamma = options.regularization.gamma;
+    auto delta = options.regularization.delta;
 
     // Ensure the initial guesses for `x` and `y` have adequate dimensions
     if(x.size() != n) x = zeros(n);
@@ -91,45 +98,39 @@ auto OptimumSolverIpNewton::Impl::solve(const OptimumProblem& problem, OptimumSt
     // The transpose representation of matrix `A`
     const auto At = tr(A);
 
-    // The alpha step sizes used to restric the steps inside the feasible domain
-    double alphax, alphaz, alpha;
+    // The KKT matrix
+    KktMatrix lhs(f.hessian, A, x, z, gamma, delta);
 
     // The optimality, feasibility, centrality and total error variables
-    double errorf, errorh, errorc, error;
+    double errorf, errorh, errorc;
 
     // The function that outputs the header and initial state of the solution
-    auto output_header = [&]()
+    auto output_initial_state = [&]()
     {
         if(!options.output.active) return;
 
-        outputter.addEntry("iter");
+        outputter.addEntry("Iteration");
         outputter.addEntries(options.output.xprefix, n, options.output.xnames);
         outputter.addEntries(options.output.yprefix, m, options.output.ynames);
         outputter.addEntries(options.output.zprefix, n, options.output.znames);
+        outputter.addEntries("r", n, options.output.xnames);
         outputter.addEntry("f(x)");
-        outputter.addEntry("h(x)");
-        outputter.addEntry("errorf");
-        outputter.addEntry("errorh");
-        outputter.addEntry("errorc");
-        outputter.addEntry("error");
-        outputter.addEntry("alpha");
-        outputter.addEntry("alphax");
-        outputter.addEntry("alphaz");
+        outputter.addEntry("Error");
+        outputter.addEntry("Optimality");
+        outputter.addEntry("Feasibility");
+        outputter.addEntry("Centrality");
 
         outputter.outputHeader();
-        outputter.addValue(result.iterations);
+        outputter.addValue(iterations);
         outputter.addValues(x);
         outputter.addValues(y);
         outputter.addValues(z);
+        outputter.addValues(rhs.rx);
         outputter.addValue(f.val);
-        outputter.addValue(norminf(h));
-        outputter.addValue("---");
-        outputter.addValue("---");
-        outputter.addValue("---");
-        outputter.addValue("---");
-        outputter.addValue("---");
-        outputter.addValue("---");
-        outputter.addValue("---");
+        outputter.addValue(error);
+        outputter.addValue(errorf);
+        outputter.addValue(errorh);
+        outputter.addValue(errorc);
         outputter.outputState();
     };
 
@@ -138,129 +139,160 @@ auto OptimumSolverIpNewton::Impl::solve(const OptimumProblem& problem, OptimumSt
     {
         if(!options.output.active) return;
 
-        outputter.addValue(result.iterations);
+        outputter.addValue(iterations);
         outputter.addValues(x);
         outputter.addValues(y);
         outputter.addValues(z);
+        outputter.addValues(rhs.rx);
         outputter.addValue(f.val);
-        outputter.addValue(norminf(h));
+        outputter.addValue(error);
         outputter.addValue(errorf);
         outputter.addValue(errorh);
         outputter.addValue(errorc);
-        outputter.addValue(error);
-        outputter.addValue(alpha);
-        outputter.addValue(alphax);
-        outputter.addValue(alphaz);
         outputter.outputState();
     };
 
-    // The function that updates the objective and constraint state
-    auto update_state = [&]()
+    // Return true if the result of a calculation failed
+    auto failed = [&](bool succeeded)
     {
-        f = problem.objective(x);
-        h = A*x - b;
+        return !succeeded;
     };
 
-    // Return true if function `update_state` failed
-    auto update_state_failed = [&]()
+    // The function that computes the current error norms
+    auto update_residuals = [&]()
     {
-        const bool f_finite = std::isfinite(f.val);
-        const bool g_finite = f.grad.allFinite();
-        const bool all_finite = f_finite && g_finite;
-        return !all_finite;
+        // Compute the right-hand side vectors of the KKT equation
+        rhs.rx.noalias() = -(f.grad - At*y - z + gamma*gamma*ones(n));
+        rhs.ry.noalias() = -(A*x + delta*delta*y - b);
+        rhs.rz.noalias() = -(x % z - mu);
+
+        // Calculate the optimality, feasibility and centrality errors
+        errorf = norminf(rhs.rx);
+        errorh = norminf(rhs.ry);
+        errorc = norminf(rhs.rz);
+        error = std::max({errorf, errorh, errorc});
+    };
+
+    // The function that initialize the state of some variables
+    auto initialize = [&]()
+    {
+        // Initialize xtrial
+        xtrial.resize(n);
+
+        // Evaluate the objective function
+        f = problem.objective(x);
+
+        // Update the residuals of the calculation
+        update_residuals();
     };
 
     // The function that computes the Newton step
     auto compute_newton_step = [&]()
     {
-        KktMatrix lhs{f.hessian, A, x, z};
-
+        // Update the decomposition of the KKT matrix with update Hessian matrix
         kkt.decompose(lhs);
 
-        // Compute the right-hand side vectors of the KKT equation
-        rhs.rx.noalias() = -(f.grad - At*y - z);
-        rhs.ry.noalias() = -(h);
-        rhs.rz.noalias() = -(x % z - mu);
-
-        // Compute `dx` and `dy` by solving the KKT equation
+        // Compute `dx`, `dy`, `dz` by solving the KKT equation
         kkt.solve(rhs, sol);
 
         // Update the time spent in linear systems
         result.time_linear_systems += kkt.result().time_solve;
         result.time_linear_systems += kkt.result().time_decompose;
-    };
 
-    // Return true if the function `compute_newton_step` failed
-    auto compute_newton_step_failed = [&]()
-    {
-        const bool dx_finite = sol.dx.allFinite();
-        const bool dy_finite = sol.dy.allFinite();
-        const bool dz_finite = sol.dz.allFinite();
-        const bool all_finite = dx_finite && dy_finite && dz_finite;
-        return !all_finite;
+        // Perform emergency Newton step calculation as long as steps contains NaN or INF values
+        while(!kkt.result().succeeded)
+        {
+            // Increase the value of the regularization parameter delta
+            delta = std::max(delta * 100, 1e-8);
+
+            // Return false if the calculation did not succeeded
+            if(delta > 1e-2) return false;
+
+            // Update the residual of the feasibility conditition
+            rhs.ry -= -delta*delta*y;
+
+            // Update the decomposition of the KKT matrix with update Hessian matrix
+            kkt.decompose(lhs);
+
+            // Compute `dx`, `dy`, `dz` by solving the KKT equation
+            kkt.solve(rhs, sol);
+
+            // Update the time spent in linear systems
+            result.time_linear_systems += kkt.result().time_solve;
+            result.time_linear_systems += kkt.result().time_decompose;
+        }
+
+        // Return true if he calculation succeeded
+        return true;
     };
 
     // The function that performs an update in the iterates
     auto update_iterates = [&]()
     {
-        alphax = fractionToTheBoundary(x, sol.dx, tau);
-        alphaz = fractionToTheBoundary(z, sol.dz, tau);
-        alpha  = std::min(alphax, alphaz);
+        // Initialize the step length factor
+        double alpha = 1.0;
 
-        if(options.ipnewton.uniform_newton_step)
+        // The number of tentatives to find a trial iterate that results in finite objective result
+        unsigned tentatives = 0;
+
+        // Repeat until a suitable xtrial iterate if found such that f(xtrial) is finite
+        for(; tentatives < 6; ++tentatives)
         {
-            x += alpha * sol.dx;
-            y += alpha * sol.dy;
-            z += alpha * sol.dz;
-        }
-        else
-        {
-            x += alphax * sol.dx;
-            y += sol.dy;
-            z += alphaz * sol.dz;
-        }
-    };
+            // Calculate the current trial iterate for x
+            for(int i = 0; i < n; ++i)
+                xtrial[i] = (x[i] + alpha*sol.dx[i] > 0.0) ?
+                    x[i] + alpha*sol.dx[i] : x[i]*(1.0 - alpha*tau);
 
-    // The function that computes the current error norms
-    auto update_errors = [&]()
-    {
-        // Calculate the optimality, feasibility and centrality errors
-        errorf = norminf(f.grad - At*y - z);
-        errorh = norminf(h);
-        errorc = norminf(x%z - mu);
+            // Evaluate the objective function at the trial iterate
+            f = problem.objective(xtrial);
 
-        // Calculate the maximum error
-        error = std::max({errorf, errorh, errorc});
-        result.error = error;
+            // Leave the loop if f(xtrial) is finite
+            if(isfinite(f))
+                break;
+
+            // Decrease alpha in a hope that a shorter step results f(xtrial) finite
+            alpha *= 0.1;
+        }
+
+        // Return false if xtrial could not be found s.t. f(xtrial) is finite
+        if(tentatives == 6)
+            return false;
+
+        // Update the iterate x from xtrial
+        x = xtrial;
+
+        // Update the z-Lagrange multipliers
+        for(int i = 0; i < n; ++i)
+            z[i] += (z[i] + alpha*sol.dz[i] > 0.0) ?
+                alpha*sol.dz[i] : -alpha*tau * z[i];
+
+        // Update the y-Lagrange multipliers
+        y += sol.dy;
+
+        // Return true as found xtrial results in finite f(xtrial)
+        return true;
     };
 
     auto converged = [&]()
     {
-        if(error < tolerance)
-        {
-            result.succeeded = true;
-            return true;
-        }
-        return false;
+        return error < tol;
     };
 
-    update_state();
-    output_header();
+    initialize();
+    output_initial_state();
 
-    do
+    for(iterations = 1; iterations <= maxiters && !succeeded; ++iterations)
     {
-        ++result.iterations; if(result.iterations > options.max_iterations) break;
-        compute_newton_step();
-        if(compute_newton_step_failed())
+        if(failed(compute_newton_step()))
             break;
-        update_iterates();
-        update_state();
-        if(update_state_failed())
+        if(failed(update_iterates()))
             break;
-        update_errors();
+        update_residuals();
         output_state();
-    } while(!converged());
+        succeeded = converged();
+    }
 
+    // Output a final header
     outputter.outputHeader();
 
     // Finish timing the calculation
