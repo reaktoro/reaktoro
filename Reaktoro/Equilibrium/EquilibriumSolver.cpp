b79e1b51caf94631d6d1f5c4f63dc1bf22cf003d
Allan Leal <allanmulin@gmail.com>
2015-11-14 17:25:47 +0100

    Set up of Gibbs energy minimization problem assumes matrix A is rational.

--- Reaktoro/Equilibrium/EquilibriumSolver.cpp
+++ Reaktoro/Equilibrium/EquilibriumSolver.cpp
@@ -161,6 +161,10 @@ struct EquilibriumSolver::Impl
             // Initialize the names of the dual variables `z`
             znames = xnames;
         }
+
+        // Set a non-zero value to the maximum denominator in the regularized balance matrix
+        if(optimum_options.max_denominator == 0)
+            optimum_options.max_denominator = 1e6;
     }
 
     /// Update the OptimumProblem instance with given EquilibriumProblem and ChemicalState instances
