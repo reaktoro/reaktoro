d18f50280447ea8d1f890d2c72d8c484ba0a104a
Allan Leal <allanmulin@gmail.com>
2015-11-25 08:23:30 +0100

    Update python wrappers with addtion of new classes/methods.
    Former-commit-id: da7cdc60e3a0e8a31b4d5db74f52c19a01cf61cc

--- ReaktoroPython/PyReaktoro/Core/PyChemicalSensitivity.cpp
+++ ReaktoroPython/PyReaktoro/Core/PyChemicalSensitivity.cpp
@@ -8,33 +8,31 @@
 // (at your option) any later version.
 //
 // This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY without even the implied warranty of
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-#include "PyPhreeqc.hpp"
+#include "PyChemicalSensitivity.hpp"
 
 // Boost includes
 #include <boost/python.hpp>
 namespace py = boost::python;
 
 // Reaktoro includes
-#include <Reaktoro/Interfaces/Interface.hpp>
-#include <Reaktoro/Interfaces/Phreeqc.hpp>
+#include <Reaktoro/Core/ChemicalSensitivity.hpp>
 
 namespace Reaktoro {
 
-auto export_Phreeqc() -> void
+auto export_ChemicalSensitivity() -> void
 {
-    py::class_<Phreeqc, py::bases<Interface>>("Phreeqc")
-        .def(py::init<>())
-        .def(py::init<std::string>())
-        .def("load", &Phreeqc::load)
-        .def("execute", &Phreeqc::execute)
-        .def("reset", &Phreeqc::reset)
+    py::class_<ChemicalSensitivity>("ChemicalSensitivity")
+        .def_readwrite("dndT", &ChemicalSensitivity::dndT)
+        .def_readwrite("dndP", &ChemicalSensitivity::dndP)
+        .def_readwrite("dndb", &ChemicalSensitivity::dndb)
+        .def_readwrite("dndt", &ChemicalSensitivity::dndt)
         ;
 }
 
