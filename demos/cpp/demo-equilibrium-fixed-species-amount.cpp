3e909bb0b265f57814a60b202c17d20c6f5b6a5a
Allan Leal <allanmulin@gmail.com>
2016-02-04 15:20:27 +0100

    Finished implementaton of IpBounds with linear inequality constraints.
    Former-commit-id: fb39bb84860631d852012b445b92acd239b9e1f9

--- demos/cpp/demo-equilibrium-fixed-species-amount.cpp
+++ demos/cpp/demo-equilibrium-fixed-species-amount.cpp
@@ -34,22 +34,22 @@ int main()
     EquilibriumProblem problem(system);
     problem.add("H2O", 1, "kg");
     problem.add("NaCl", 0.1, "mol");
-//    problem.add("CaCO3", 10.0, "mol");
+    problem.add("CaCO3", 10.0, "mol");
 //    problem.add("HCl", 2.0, "mol");
 //    problem.add("CO2", 10, "moles");
-    problem.setSpeciesAmount("Calcite", 10, "moles");
+//    problem.setSpeciesAmount("Calcite", 10, "moles");
 //    problem.setSpeciesAmount("CO2(g)", 10, "moles");
 //    problem.setSpeciesAmount("H2O(l)", 55, "moles");
 //    problem.setPhaseAmount("Aqueous", 60, "moles", "H2O");
 //    problem.setPhaseAmount("Aqueous", 60, "moles");
 //    problem.setPhaseAmount("Calcite", 10, "moles");
-//    problem.pH(3.0, "HCl");
+    problem.pH(3.0, "HCl");
 //    problem.pH(11.0, "HCl", "NaOH");
 //    problem.setPhaseVolume("Aqueous", 0.5, "m3");
 //    problem.setPhaseVolume("Calcite", 0.5, "m3");
 
     EquilibriumOptions options;
-    options.optimum.output.active = true;
+//    options.optimum.output.active = true;
     options.hessian = GibbsHessian::Exact;
 
 //    ChemicalState state = equilibrate(problem, "output=true");
