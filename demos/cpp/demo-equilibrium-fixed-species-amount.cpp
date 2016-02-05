0904fc27b63d07eb9c600216c90710113ccbb13e
Allan Leal <allanmulin@gmail.com>
2016-02-05 15:11:27 +0100

    ...

--- demos/cpp/demo-equilibrium-fixed-species-amount.cpp
+++ demos/cpp/demo-equilibrium-fixed-species-amount.cpp
@@ -17,9 +17,43 @@
 
 #include <Reaktoro/Reaktoro.hpp>
 using namespace Reaktoro;
-
 int main()
 {
+//    double n1 = 3.0;
+//    double n2 = 6.0;
+//
+//    ChemicalVector u(2, 2), v(2, 2);
+//    u.val << n1*n2, n1+n2;
+//    u.ddn << n2, n1, 1, 1;
+//    v.val << n1*n1, n2*n2;
+//    v.ddn << 2*n1, 0, 0, 2*n2;
+//
+//    ChemicalVector t = u % v;
+//    ChemicalScalar s = sum(u % v);
+//
+//    ChemicalVector texp(2, 2);
+//    texp.val << n1*n1*n1*n2, (n1+n2)*n2*n2;
+//    texp.ddn << 3*n1*n1*n2, n1*n1*n1, n2*n2, 2*n2*(n1+n2)+n2*n2;
+//
+//    ChemicalScalar sexp(2);
+//    sexp.val = n1*n1*n1*n2 + n2*n2*(n1+n2);
+//    sexp.ddn << 3*n1*n1*n2 + n2*n2, n1*n1*n1 + 2*n2*(n1+n2)+n2*n2;
+//
+//    std::cout << "t.val(n) = \n" << t.val << std::endl;
+//    std::cout << "t.val(e) = \n" << texp.val << std::endl;
+//
+//    std::cout << "t.ddn(n) = \n" << t.ddn << std::endl;
+//    std::cout << "t.ddn(e) = \n" << texp.ddn << std::endl;
+//
+//    std::cout << "s.val(n) = \n" << s.val << std::endl;
+//    std::cout << "s.val(e) = \n" << sexp.val << std::endl;
+//
+//    std::cout << "s.ddn(n) = \n" << s.ddn << std::endl;
+//    std::cout << "s.ddn(e) = \n" << sexp.ddn << std::endl;
+//
+//    return 0;
+
+
     Database database("databases/supcrt/supcrt98.xml");
 
     ChemicalEditor editor(database);
@@ -36,7 +70,7 @@ int main()
     problem.add("NaCl", 0.1, "mol");
 //    problem.add("CaCO3", 10.0, "mol");
 //    problem.add("HCl", 2.0, "mol");
-    problem.add("CO2", 1, "umol");
+//    problem.add("CO2", 1, "umol");
 //    problem.setSpeciesAmount("Calcite", 10, "moles");
 //    problem.setSpeciesAmount("CO2(g)", 10, "moles");
 //    problem.setSpeciesAmount("H2O(l)", 55, "moles");
@@ -45,11 +79,19 @@ int main()
 //        .titrateWith("1 kg H2O; 1 mol NaCl");
 //    problem.setPhaseAmount("Calcite", 10, "moles");
 //    problem.setPhaseAmount("Gaseous", 10, "moles");
-    problem.pH(4.0, "CO2");
+//    problem.pH(4.0).titrateWith("CO2");
+//    problem.pH(4.0).titrateWithEither("HCl", "NaOH");
+//    problem.setPhaseVolume("Aqueous", 0.5. "m3").
+//        titrateWith("1 kg H2O; 1 mol NaCl");
+
 //    problem.pH(3.0, "HCl");
 //    problem.pH(3.0, "HCl", "NaOH");
-//    problem.setPhaseVolume("Aqueous", 0.5, "m3");
-//    problem.setPhaseVolume("Calcite", 0.5, "m3");
+    problem.setPhaseVolume("Gaseous", 0.5, "m3", "(1:mol:CO2)");
+    problem.setPhaseVolume("Aqueous", 0.5, "m3", "(1:kg:H2O)(0.1:mol:NaCl)");
+//    problem.setPhaseVolume("Aqueous", 0.5, "m3", "H2O");
+//    problem.setPhaseAmount("Aqueous", 100, "mol", "H2O");
+//    problem.setPhaseAmount("Aqueous", 100, "mol", "(1:kg:H2O)(0.1:mol:NaCl)");
+//    problem.setPhaseVolume("Calcite", 0.5, "m3", "CaCO3");
 
     EquilibriumOptions options;
     options.optimum.output.active = true;
