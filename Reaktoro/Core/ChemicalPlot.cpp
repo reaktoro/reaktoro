d9b5e0919ef4b60bf37c86e17d7ea2a1691a0ac3
Allan Leal <allanmulin@gmail.com>
2016-03-01 08:46:10 +0100

    Removed single quotes from legend items.

--- Reaktoro/Core/ChemicalPlot.cpp
+++ Reaktoro/Core/ChemicalPlot.cpp
@@ -197,9 +197,9 @@ struct ChemicalPlot::Impl
         plotfile << config;
 
         // Define a Gnuplot variable that is a list of titles for the curves
-        plotfile << "titles = \""; // e.g., titles = "'legend1' 'legend2' 'legend3'"
+        plotfile << "titles = \""; // e.g., titles = "legend1 legend2 legend3"
         for(unsigned i = 0; i < legend.size(); ++i)
-            plotfile << (i == 0 ? "" : " ") << "'" << legend[i] << "'";
+            plotfile << (i == 0 ? "" : " ") << legend[i];
         plotfile << "\"\n" << std::endl;
 
         // On Windows, use the `dir` command on the data file to check its state.
