de5fe7062b87fd2745e362a02c112f3a99931595
Allan Leal <allanmulin@gmail.com>
2016-04-11 19:28:56 +0200

    Implemented demo for ChemicalComposition class.

---
+++ demos/cpp/demo-chemicalcomposition.cpp
@@ -0,0 +1,49 @@
+// Reaktoro is a unified framework for modeling chemically reactive systems.
+//
+// Copyright (C) 2014-2015 Allan Leal
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#include <Reaktoro/Reaktoro.hpp>
+using namespace Reaktoro;
+
+int main()
+{
+    ChemicalEditor editor;
+    editor.addAqueousPhase("H O Na Cl C Ca Mg Si");
+    editor.addGaseousPhase("H2O(g) CO2(g)");
+    editor.addMineralPhase("Calcite");
+    editor.addMineralPhase("Magnesite");
+    editor.addMineralPhase("Dolomite");
+    editor.addMineralPhase("Quartz");
+
+    ChemicalSystem system(editor);
+
+    EquilibriumOptions options;
+    options.hessian = GibbsHessian::Exact;
+
+    ChemicalComposition composition(system);
+    composition.setTemperature(100, "celsius");
+    composition.setPressure(60, "bar");
+    composition.setAqueousFluid("1 molal NaCl");
+    composition.setGaseousFluid("0.90 CO2; 0.10 H2O");
+    composition.setSolid("0.10 Calcite; 0.05 Magnesite; 0.05 Dolomite; 0.80 Quartz");
+    composition.setAqueousSaturation(0.80);
+    composition.setGaseousSaturation(0.20);
+    composition.setPorosity(0.3);
+
+    ChemicalState state = equilibrate(composition, options);
+
+    std::cout << state << std::endl;
+}
