9a4275de6fc794b2fd7766095eea3ba8002fb206
Allan Leal <allanmulin@gmail.com>
2016-06-08 14:55:48 +0200

    Adds missing options in the python wrapper for OptimumOptions.
    Former-commit-id: 325e9e522ea37e71b424c225d63b5edd3de591d6

--- ReaktoroPython/PyReaktoro/Optimization/PyOptimumOptions.cpp
+++ ReaktoroPython/PyReaktoro/Optimization/PyOptimumOptions.cpp
@@ -28,25 +28,30 @@ namespace Reaktoro {
 
 auto export_OptimumOptions() -> void
 {
+    py::enum_<StepMode>("StepMode")
+        .value("Conservative", StepMode::Conservative)
+        .value("Aggressive", StepMode::Aggressive)
+        ;
+
+    py::class_<OptimumParamsActNewton>("OptimumParamsActNewton")
+        .def_readwrite("threshold", &OptimumParamsActNewton::threshold)
+        ;
+
     py::class_<OptimumParamsIpNewton>("OptimumParamsIpNewton")
         .def_readwrite("mu", &OptimumParamsIpNewton::mu)
         .def_readwrite("tau", &OptimumParamsIpNewton::tau)
+        .def_readwrite("step", &OptimumParamsIpNewton::step)
+        ;
+
+    py::class_<OptimumParamsIpActive>("OptimumParamsIpActive")
+        .def_readwrite("epsilon", &OptimumParamsIpActive::epsilon)
+        .def_readwrite("tau", &OptimumParamsIpActive::tau)
         ;
 
     py::class_<OptimumParamsIpAction>("OptimumParamsIpAction")
         .def_readwrite("mu", &OptimumParamsIpAction::mu)
         .def_readwrite("tau", &OptimumParamsIpAction::tau)
-        ;
-
-    py::class_<OptimumParamsKarpov>("OptimumParamsKarpov")
-        .def_readwrite("line_search_max_iterations", &OptimumParamsKarpov::line_search_max_iterations)
-        .def_readwrite("line_search_wolfe", &OptimumParamsKarpov::line_search_wolfe)
-        .def_readwrite("tau_feasible", &OptimumParamsKarpov::tau_feasible)
-        .def_readwrite("tau_descent", &OptimumParamsKarpov::tau_descent)
-        .def_readwrite("feasibility_tolerance", &OptimumParamsKarpov::feasibility_tolerance)
-        .def_readwrite("negative_dual_tolerance", &OptimumParamsKarpov::negative_dual_tolerance)
-        .def_readwrite("active_to_inactive", &OptimumParamsKarpov::active_to_inactive)
-        .def_readwrite("use_kkt_solver", &OptimumParamsKarpov::use_kkt_solver)
+        .def_readwrite("step", &OptimumParamsIpAction::step)
         ;
 
     py::class_<OptimumParamsIpOpt>("OptimumParamsIpOpt")
@@ -70,6 +75,17 @@ auto export_OptimumOptions() -> void
         .def_readwrite("scaling", &OptimumParamsIpOpt::scaling)
         ;
 
+    py::class_<OptimumParamsKarpov>("OptimumParamsKarpov")
+        .def_readwrite("line_search_max_iterations", &OptimumParamsKarpov::line_search_max_iterations)
+        .def_readwrite("line_search_wolfe", &OptimumParamsKarpov::line_search_wolfe)
+        .def_readwrite("tau_feasible", &OptimumParamsKarpov::tau_feasible)
+        .def_readwrite("tau_descent", &OptimumParamsKarpov::tau_descent)
+        .def_readwrite("feasibility_tolerance", &OptimumParamsKarpov::feasibility_tolerance)
+        .def_readwrite("negative_dual_tolerance", &OptimumParamsKarpov::negative_dual_tolerance)
+        .def_readwrite("active_to_inactive", &OptimumParamsKarpov::active_to_inactive)
+        .def_readwrite("use_kkt_solver", &OptimumParamsKarpov::use_kkt_solver)
+        ;
+
     py::class_<OptimumOutputOptions, py::bases<OutputterOptions>>("OptimumOutput")
         .def_readwrite("xprefix", &OptimumOutputOptions::xprefix)
         .def_readwrite("yprefix", &OptimumOutputOptions::yprefix)
@@ -79,14 +95,29 @@ auto export_OptimumOptions() -> void
         .def_readwrite("znames", &OptimumOutputOptions::znames)
         ;
 
+    py::class_<RegularizerOptions>("RegularizerOptions")
+        .def_readwrite("echelonize", &RegularizerOptions::echelonize)
+        .def_readwrite("max_denominator", &RegularizerOptions::max_denominator)
+        ;
+
+    py::class_<OptimumParamsRegularization, py::bases<RegularizerOptions>>("OptimumParamsRegularization")
+        .def_readwrite("delta", &OptimumParamsRegularization::delta)
+        .def_readwrite("gamma", &OptimumParamsRegularization::gamma)
+        ;
+
     py::class_<OptimumOptions>("OptimumOptions")
         .def_readwrite("tolerance", &OptimumOptions::tolerance)
         .def_readwrite("tolerancex", &OptimumOptions::tolerancex)
         .def_readwrite("tolerancef", &OptimumOptions::tolerancef)
         .def_readwrite("max_iterations", &OptimumOptions::max_iterations)
         .def_readwrite("output", &OptimumOptions::output)
+        .def_readwrite("actnewton", &OptimumOptions::actnewton)
+        .def_readwrite("ipaction", &OptimumOptions::ipaction)
         .def_readwrite("ipopt", &OptimumOptions::ipopt)
         .def_readwrite("ipnewton", &OptimumOptions::ipnewton)
+        .def_readwrite("ipactive", &OptimumOptions::ipactive)
+        .def_readwrite("karpov", &OptimumOptions::karpov)
+        .def_readwrite("regularization", &OptimumOptions::regularization)
         ;
 }
 
