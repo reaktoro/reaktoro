d1e8bfa8f880b94b60cf119914a8800cd1aa8587
Allan Leal <allanmulin@gmail.com>
2016-06-09 13:58:42 +0200

    Adds missing values for ln_activity_constants.

--- Reaktoro/Thermodynamics/Models/AqueousChemicalModelIdeal.cpp
+++ Reaktoro/Thermodynamics/Models/AqueousChemicalModelIdeal.cpp
@@ -32,9 +32,24 @@ auto aqueousChemicalModelIdeal(const AqueousMixture& mixture) -> PhaseChemicalMo
         // Calculate state of the mixture
         const AqueousMixtureState state = mixture.state(T, P, n);
 
+        // The ln of water molar fraction
+        ChemicalScalar ln_xw = log(state.x[iH2O]);
+
+        // The result of the phase chemical model
         PhaseChemicalModelResult res(nspecies);
-        res.ln_activities = log(state.m);
-        res.ln_activities[iH2O] = log(state.x[iH2O]);
+
+        // Set the activity coefficients of the aqueous species
+        res.ln_activity_coefficients = ln_xw;
+        res.ln_activity_coefficients[iH2O] = 0.0;
+
+        // Set the activities of the aqueous species
+        res.ln_activities = res.ln_activity_coefficients + log(state.m);
+        res.ln_activities[iH2O] = ln_xw;
+
+        // Set the activity constants of the aqueous species
+        res.ln_activity_constants = std::log(55.508472);
+        res.ln_activity_constants[iH2O] = 0.0;
+
         return res;
     };
 
