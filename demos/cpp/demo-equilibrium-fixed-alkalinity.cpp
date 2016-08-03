a551c6a6723921f54bae3d995e26ae78d07fc573
Allan Leal <allanmulin@gmail.com>
2016-08-03 23:06:54 +0200

    Added a new demo file that calculates equilibrium with constrained alkalinity.
    Former-commit-id: e11328b20fc2a5850df99cf5c09ce40ad6b21840

---
+++ demos/cpp/demo-equilibrium-fixed-alkalinity.cpp
@@ -0,0 +1,40 @@
+// Reaktoro is a unified framework for modeling chemically reactive systems.
+//
+// Copyright (C) 2014-2015 Allan Leal
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#include <Reaktoro/Reaktoro.hpp>
+using namespace Reaktoro;
+
+int main()
+{
+    ChemicalEditor editor;
+    editor.addAqueousPhase("H2O NaCl CaCO3");
+    editor.addGaseousPhase("H2O(g) CO2(g)");
+    editor.addMineralPhase("Calcite");
+
+    ChemicalSystem system(editor);
+
+    EquilibriumInverseProblem problem(system);
+    problem.add("H2O", 1, "kg");
+    problem.add("NaCl", 0.1, "mol");
+    problem.fixSpeciesMass("Calcite", 100, "g");
+    problem.fixSpeciesAmount("CO2(g)", 1.0, "mol");
+    problem.alkalinity(25.0, "meq/L", "HCO3-");
+
+    EquilibriumState state = equilibrate(problem);
+
+    std::cout << state << std::endl;
+}
