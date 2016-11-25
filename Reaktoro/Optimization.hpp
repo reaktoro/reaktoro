d3b808cd0acc2e714470cb3ce757eec320a0dab1
Allan Leal <allanmulin@gmail.com>
2016-11-25 16:31:40 +0100

    Implemented header files for all modules.

---
+++ Reaktoro/Optimization.hpp
@@ -0,0 +1,38 @@
+// Reaktoro is a unified framework for modeling chemically reactive systems.
+//
+// Copyright (C) 2014-2017 Allan Leal
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+#include <Reaktoro/Optimization/Filter.hpp>
+#include <Reaktoro/Optimization/Hessian.hpp>
+#include <Reaktoro/Optimization/Jacobian.hpp>
+#include <Reaktoro/Optimization/KktSolver.hpp>
+#include <Reaktoro/Optimization/NonlinearSolver.hpp>
+#include <Reaktoro/Optimization/OptimumOptions.hpp>
+#include <Reaktoro/Optimization/OptimumProblem.hpp>
+#include <Reaktoro/Optimization/OptimumResult.hpp>
+#include <Reaktoro/Optimization/OptimumSolver.hpp>
+#include <Reaktoro/Optimization/OptimumSolverIpActive.hpp>
+#include <Reaktoro/Optimization/OptimumSolverIpBounds.hpp>
+#include <Reaktoro/Optimization/OptimumSolverIpFeasible.hpp>
+#include <Reaktoro/Optimization/OptimumSolverIpNewton.hpp>
+#include <Reaktoro/Optimization/OptimumSolverIpOpt.hpp>
+#include <Reaktoro/Optimization/OptimumSolverKarpov.hpp>
+#include <Reaktoro/Optimization/OptimumSolverSimplex.hpp>
+#include <Reaktoro/Optimization/OptimumState.hpp>
+#include <Reaktoro/Optimization/Regularizer.hpp>
+#include <Reaktoro/Optimization/Utils.hpp>
---
+++ Reaktoro/Thermodynamics.hpp
@@ -0,0 +1,55 @@
+// Reaktoro is a unified framework for modeling chemically reactive systems.
+//
+// Copyright (C) 2014-2017 Allan Leal
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+#include <Reaktoro/Thermodynamics/Activity/AqueousActivityModel.hpp>
+#include <Reaktoro/Thermodynamics/Activity/AqueousActivityModelDrummondCO2.hpp>
+#include <Reaktoro/Thermodynamics/Activity/AqueousActivityModelDuanSunCO2.hpp>
+#include <Reaktoro/Thermodynamics/Activity/AqueousActivityModelRumpfCO2.hpp>
+#include <Reaktoro/Thermodynamics/Activity/AqueousActivityModelSetschenow.hpp>
+#include <Reaktoro/Thermodynamics/Core/ChemicalEditor.hpp>
+#include <Reaktoro/Thermodynamics/Core/Database.hpp>
+#include <Reaktoro/Thermodynamics/Core/Thermo.hpp>
+#include <Reaktoro/Thermodynamics/EOS/CubicEOS.hpp>
+#include <Reaktoro/Thermodynamics/Mixtures/AqueousMixture.hpp>
+#include <Reaktoro/Thermodynamics/Mixtures/GaseousMixture.hpp>
+#include <Reaktoro/Thermodynamics/Mixtures/GeneralMixture.hpp>
+#include <Reaktoro/Thermodynamics/Mixtures/MineralMixture.hpp>
+#include <Reaktoro/Thermodynamics/Models/SpeciesElectroState.hpp>
+#include <Reaktoro/Thermodynamics/Models/SpeciesElectroStateHKF.hpp>
+#include <Reaktoro/Thermodynamics/Models/SpeciesThermoState.hpp>
+#include <Reaktoro/Thermodynamics/Models/SpeciesThermoStateHKF.hpp>
+#include <Reaktoro/Thermodynamics/Phases/AqueousPhase.hpp>
+#include <Reaktoro/Thermodynamics/Phases/GaseousPhase.hpp>
+#include <Reaktoro/Thermodynamics/Phases/MineralPhase.hpp>
+#include <Reaktoro/Thermodynamics/Reactions/MineralCatalyst.hpp>
+#include <Reaktoro/Thermodynamics/Reactions/MineralMechanism.hpp>
+#include <Reaktoro/Thermodynamics/Reactions/MineralReaction.hpp>
+#include <Reaktoro/Thermodynamics/Species/AqueousSpecies.hpp>
+#include <Reaktoro/Thermodynamics/Species/GaseousSpecies.hpp>
+#include <Reaktoro/Thermodynamics/Species/MineralSpecies.hpp>
+#include <Reaktoro/Thermodynamics/Species/ThermoData.hpp>
+#include <Reaktoro/Thermodynamics/Water/WaterConstants.hpp>
+#include <Reaktoro/Thermodynamics/Water/WaterElectroState.hpp>
+#include <Reaktoro/Thermodynamics/Water/WaterElectroStateJohnsonNorton.hpp>
+#include <Reaktoro/Thermodynamics/Water/WaterHelmholtzState.hpp>
+#include <Reaktoro/Thermodynamics/Water/WaterHelmholtzStateHGK.hpp>
+#include <Reaktoro/Thermodynamics/Water/WaterHelmholtzStateWagnerPruss.hpp>
+#include <Reaktoro/Thermodynamics/Water/WaterThermoState.hpp>
+#include <Reaktoro/Thermodynamics/Water/WaterThermoStateUtils.hpp>
+#include <Reaktoro/Thermodynamics/Water/WaterUtils.hpp>
