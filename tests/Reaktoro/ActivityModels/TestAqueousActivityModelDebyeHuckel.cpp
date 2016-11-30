96f0b20f1ee84409ce79bbb3f3ba03eaee50f748
Allan Leal <allanmulin@gmail.com>
2016-11-30 12:02:45 +0100

    Implemented demo and test for Debye-Huckel model.

--- tests/Reaktoro/ActivityModels/TestAqueousActivityModelDebyeHuckel.cpp
+++ tests/Reaktoro/ActivityModels/TestAqueousActivityModelDebyeHuckel.cpp
@@ -21,33 +21,157 @@
 #include <Reaktoro/Reaktoro.hpp>
 using namespace Reaktoro;
 
-auto createAqueousMixture() -> AqueousPhase
-{
-    ChemicalEditor editor;
-    editor.addAqueousPhase("H2O(l) H+ OH- Na+ Cl- Ca++ HCO3- CO2(aq) CO3--")
-        .setChemicalModelDebyeHuckel();
+const Database db("supcrt98");
 
-    ChemicalSystem system;
-}
-
-TEST_CASE("TestAqueousActivityModelDebyeHuckel")
+TEST_CASE("Electrolyte Solution: NaCl")
 {
-    ChemicalEditor editor;
-    editor.addAqueousPhase("H2O(l) H+ OH- Na+ Cl- Ca++ HCO3- CO2(aq) CO3--")
+    ChemicalEditor editor(db);
+    editor.addAqueousPhase("H2O(l) H+ OH- Na+ Cl-")
         .setChemicalModelDebyeHuckel();
 
     ChemicalSystem system(editor);
 
     ChemicalState state(system);
     state.setSpeciesAmount("H2O(l)", 1.0/waterMolarMass);
-    state.setSpeciesAmount("H+", 1.0e-4);
-    state.setSpeciesAmount("OH-", 1.0e-10);
+    state.setSpeciesAmount("H+", 1.0e-7);
+    state.setSpeciesAmount("OH-", 1.0e-7);
     state.setSpeciesAmount("Na+", 0.5);
     state.setSpeciesAmount("Cl-", 0.5);
-    state.setSpeciesAmount("HCO3-", 0.1001);
-    state.setSpeciesAmount("CO2(aq)", 0.4);
-    state.setSpeciesAmount("CO3--", 1.0e-6);
 
     ChemicalProperties properties = state.properties();
 
+    const Vector ln_g = properties.lnActivityCoefficients().val;
+
+    const double ln10 = std::log(10.0);
+    const double I = 0.5;
+    const double A = 0.51137763214615395;
+    const double B = 0.3287812566783076;
+    const double nwo = 55.508472036052972;
+
+    std::map<std::string, double> a = {
+        {"H+", 9.0}, {"OH-", 3.5}, {"Na+", 4.08}, {"Cl-", 3.63}
+    };
+
+    std::map<std::string, double> b = {
+        {"H+", 0.0}, {"OH-", 0.0}, {"Na+",  0.082}, {"Cl-", 0.017}
+    };
+
+    Vector ln_g_expected(5);
+    ln_g_expected[1] = ln10 * (-A*std::sqrt(I)/(1+B*a["H+"] *std::sqrt(I)) + b["H+"]*I);
+    ln_g_expected[2] = ln10 * (-A*std::sqrt(I)/(1+B*a["OH-"]*std::sqrt(I)) + b["OH-"]*I);
+    ln_g_expected[3] = ln10 * (-A*std::sqrt(I)/(1+B*a["Na+"]*std::sqrt(I)) + b["Na+"]*I);
+    ln_g_expected[4] = ln10 * (-A*std::sqrt(I)/(1+B*a["Cl-"]*std::sqrt(I)) + b["Cl-"]*I);
+
+    CHECK(ln_g[1] == approx(ln_g_expected[1]));
+    CHECK(ln_g[2] == approx(ln_g_expected[2]));
+    CHECK(ln_g[3] == approx(ln_g_expected[3]));
+    CHECK(ln_g[4] == approx(ln_g_expected[4]));
 }
+
+//TEST_CASE("Electrolyte Solution: KCl")
+//{
+//    DebyeHuckelParams db;
+//
+//    ChemicalEditor editor(db);
+//    editor.addAqueousPhase("H2O(l) H+ OH- Na+ Cl- Ca++ HCO3- CO2(aq) CO3--")
+//        .setChemicalModelDebyeHuckel();
+//
+//    ChemicalSystem system(editor);
+//
+//    ChemicalState state(system);
+//    state.setSpeciesAmount("H2O(l)", 1.0/waterMolarMass);
+//    state.setSpeciesAmount("H+", 1.0e-4);
+//    state.setSpeciesAmount("OH-", 1.0e-10);
+//    state.setSpeciesAmount("Na+", 0.5);
+//    state.setSpeciesAmount("Cl-", 0.5);
+//    state.setSpeciesAmount("HCO3-", 0.1001);
+//    state.setSpeciesAmount("CO2(aq)", 0.4);
+//    state.setSpeciesAmount("CO3--", 1.0e-6);
+//
+//    ChemicalProperties properties = state.properties();
+//
+//    const double I = 0.55010200005;
+//    const double A = 0.51137763214615395;
+//    const double B = 0.3287812566783076;
+//    const double nwo = 55.508472036052972;
+//
+//    const std::map<std::string, double> aion = {
+//        {"H+", 9.0}, {"OH-", 3.5}, {"Na+", 4.08}, {"Cl-", 3.63},
+//    };
+//
+//    const std::map<std::string, double> bion = {
+//        {"H+", 0.0}, {"OH-", 0.0}, {"Na+",  0.082}, {"Cl-", 0.017}, {"HCO3-", 5.4}
+//    };
+//
+//}
+//
+//TEST_CASE("Electrolyte Solution: CaCl2")
+//{
+//    DebyeHuckelParams db;
+//
+//    ChemicalEditor editor(db);
+//    editor.addAqueousPhase("H2O(l) H+ OH- Ca++ Cl-")
+//        .setChemicalModelDebyeHuckel();
+//
+//    ChemicalSystem system(editor);
+//
+//    ChemicalState state(system);
+//    state.setSpeciesAmount("H2O(l)", 1.0/waterMolarMass);
+//    state.setSpeciesAmount("H+", 1.0e-7);
+//    state.setSpeciesAmount("OH-", 1.0e-7);
+//    state.setSpeciesAmount("Ca++", 0.1);
+//    state.setSpeciesAmount("Cl-", 0.2);
+//
+//    ChemicalProperties properties = state.properties();
+//
+//    const double I = 0.5*(0.1*4 + 0.2*1);
+//    const double A = 0.51137763214615395;
+//    const double B = 0.3287812566783076;
+//    const double nwo = 55.508472036052972;
+//
+//    const std::map<std::string, double> aion = {
+//        {"H+", 9.0}, {"OH-", 3.5}, {"Na+", 4.08}, {"Cl-", 3.63},
+//    };
+//
+//    const std::map<std::string, double> bion = {
+//        {"H+", 0.0}, {"OH-", 0.0}, {"Na+",  0.082}, {"Cl-", 0.017}, {"HCO3-", 5.4}
+//    };
+//
+//}
+//
+//TEST_CASE("Electrolyte Solution: CO2")
+//{
+//    DebyeHuckelParams db;
+//
+//    ChemicalEditor editor(db);
+//    editor.addAqueousPhase("H2O(l) H+ OH- Na+ Cl- HCO3- CO2(aq) CO3--")
+//        .setChemicalModelDebyeHuckel();
+//
+//    ChemicalSystem system(editor);
+//
+//    ChemicalState state(system);
+//    state.setSpeciesAmount("H2O(l)", 1.0/waterMolarMass);
+//    state.setSpeciesAmount("H+", 1.0e-4);
+//    state.setSpeciesAmount("OH-", 1.0e-10);
+//    state.setSpeciesAmount("Na+", 0.5);
+//    state.setSpeciesAmount("Cl-", 0.5);
+//    state.setSpeciesAmount("HCO3-", 0.1001);
+//    state.setSpeciesAmount("CO2(aq)", 0.4);
+//    state.setSpeciesAmount("CO3--", 1.0e-6);
+//
+//    ChemicalProperties properties = state.properties();
+//
+//    const double I = 0.55010200005;
+//    const double A = 0.51137763214615395;
+//    const double B = 0.3287812566783076;
+//    const double nwo = 55.508472036052972;
+//
+//    const std::map<std::string, double> aion = {
+//        {"H+", 9.0}, {"OH-", 3.5}, {"Na+", 4.08}, {"Cl-", 3.63},
+//    };
+//
+//    const std::map<std::string, double> bion = {
+//        {"H+", 0.0}, {"OH-", 0.0}, {"Na+",  0.082}, {"Cl-", 0.017}, {"HCO3-", 5.4}
+//    };
+//
+//}
