8dcdd914b65392479f1a6a4ac1b98d34b0b2b335
Allan Leal <allanmulin@gmail.com>
2017-02-27 14:12:29 +0100

    Implemeted methods to set individual element amounts, including charge.

--- ReaktoroPython/PyReaktoro/Equilibrium/PyEquilibriumProblem.cpp
+++ ReaktoroPython/PyReaktoro/Equilibrium/PyEquilibriumProblem.cpp
@@ -34,6 +34,9 @@ auto export_EquilibriumProblem() -> void
     auto setElementAmounts1 = static_cast<EquilibriumProblem&(EquilibriumProblem::*)(const Vector&)>(&EquilibriumProblem::setElementAmounts);
     auto setElementAmounts2 = static_cast<EquilibriumProblem&(EquilibriumProblem::*)(double)>(&EquilibriumProblem::setElementAmounts);
 
+    auto setElementAmount1 = static_cast<EquilibriumProblem&(EquilibriumProblem::*)(Index, double)>(&EquilibriumProblem::setElementAmount);
+    auto setElementAmount2 = static_cast<EquilibriumProblem&(EquilibriumProblem::*)(std::string, double)>(&EquilibriumProblem::setElementAmount);
+
     auto setTemperature1 = static_cast<EquilibriumProblem&(EquilibriumProblem::*)(double)>(&EquilibriumProblem::setTemperature);
     auto setTemperature2 = static_cast<EquilibriumProblem&(EquilibriumProblem::*)(double, std::string)>(&EquilibriumProblem::setTemperature);
 
@@ -53,6 +56,9 @@ auto export_EquilibriumProblem() -> void
         .def("setPressure", setPressure2, py::return_internal_reference<>())
         .def("setElementAmounts", setElementAmounts1, py::return_internal_reference<>())
         .def("setElementAmounts", setElementAmounts2, py::return_internal_reference<>())
+        .def("setElementAmount", setElementAmount1, py::return_internal_reference<>())
+        .def("setElementAmount", setElementAmount2, py::return_internal_reference<>())
+        .def("setElectricalCharge", &EquilibriumProblem::setElectricalCharge, py::return_internal_reference<>())
         .def("add", add1, py::return_internal_reference<>())
         .def("add", add2, py::return_internal_reference<>())
         .def("addCompound", &EquilibriumProblem::addCompound, py::return_internal_reference<>())
