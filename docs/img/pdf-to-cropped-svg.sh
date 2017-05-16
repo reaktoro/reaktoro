60e949c8c5872199a03f4a97e795800d9652373a
Allan Leal <allanmulin@gmail.com>
2017-05-16 15:32:11 +0200

    Improved documentation by spliting into web and cpp.

--- docs/img/pdf-to-cropped-svg.sh
+++ docs/img/pdf-to-cropped-svg.sh
@@ -4,4 +4,4 @@
 #  - Inkscape: https://inkscape.org/sk/doc/inkscape-man.html
 #  - pdfcrop: http://alexsleat.co.uk/2011/01/25/using-pdfcrop-to-remove-white-margins-ubuntu/
 
-for f in *.pdf; do pdfcrop $f $f; inkscape $f --export-text-to-path --export-plain-svg=${f%.pdf}.svg; done
+for f in "$@"; do pdfcrop $f $f; inkscape $f --export-text-to-path --export-plain-svg=${f%.pdf}.svg; done
