7700f5dde9627fbffcc6b44c08895509ba16fdb2
Allan Leal <allanmulin@gmail.com>
2017-05-24 16:19:27 +0200

    Implemented c++ and python demo using GEMS.
    Former-commit-id: e4e36c33a46e328f78f10d6e4a8649b8eade54d7

---
+++ demos/python/demo-equilibrium-gems.py
@@ -0,0 +1,48 @@
+# Reaktoro is a unified framework for modeling chemically reactive systems.
+#
+# Copyright (C) 2014-2015 Allan Leal
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+from reaktoro import *
+
+# **Note:**
+# This demo should be executed on the same directory where the script is located.
+# Example:
+# ~~~
+# cd Reaktoro/demos/python
+# python demo-equilibrium-gems.py
+# ~~~
+
+# Use an exported project file from GEMS to initialize a Gems object,
+gems = Gems("../resources/gems/CalciteBC-dat.lst")
+
+# and then use it to construct the ChemicalSystem object.
+system = ChemicalSystem(gems)
+
+# Create a ChemicalState object that contains the temperature, pressure,
+# and amounts of species stored in the exported GEMS file.
+state = EquilibriumState(gems.state(system))
+
+# Output the equilibrium state calculated by GEMS to a file.
+state.output("state-gems2.txt")
+
+# Perturb the equilibrium state calculated by GEMS
+state.setSpeciesAmount("CO2@", 0.1)
+
+# and then equilibrate the modified chemical state using Reaktoro's methods.
+equilibrate(state)
+
+# Output the updated equilibrium state to a file.
+state.output("state-gems-updated2.txt")
