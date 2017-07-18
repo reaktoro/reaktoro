0fb80b5252afd360afcad222f801ab1710d189ab
Allan Leal <allanmulin@gmail.com>
2017-07-18 13:37:07 +0200

    Added code to extend Reaktoro classes with usefull python methods.

--- python/reaktoro/__init__.py
+++ python/reaktoro/__init__.py
@@ -1,1 +1,13 @@
 from PyReaktoro import *
+
+import numpy
+
+#==============================================================================
+# Extending Reaktoro classes with handy methods 
+#==============================================================================
+
+#------------------------------------------------------------------------------
+# Extending class ChemicalOutput
+#------------------------------------------------------------------------------
+# Define a method to convert the file data into an array.
+ChemicalOutput.array = lambda self: numpy.loadtxt(self.filename(), skiprows=1)
