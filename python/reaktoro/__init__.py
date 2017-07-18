bab9813d2d85b3f79f20f852cbb79b7934c3d811
Allan Leal <allanmulin@gmail.com>
2017-07-18 15:08:07 +0200

    Added ChemicalOutput::headings and ChemicalOutput::quantities methods.
    Former-commit-id: 9a61e154ae9314bcc5e4210d9a9046f58f166701

--- python/reaktoro/__init__.py
+++ python/reaktoro/__init__.py
@@ -11,3 +11,13 @@ import numpy
 #------------------------------------------------------------------------------
 # Define a method to convert the file data into an array.
 ChemicalOutput.array = lambda self: numpy.loadtxt(self.filename(), skiprows=1)
+
+
+# Define a method to convert the data in the file into a dictionary, with the 
+# headings being the keys, and the column data as the values.
+def _ChemicalOutput_dict(self):
+    data = numpy.loadtxt(self.filename(), skiprows=1)
+    columns = [ data[:, i] for i in xrange(data.shape[1]) ]
+    return { heading:column for heading,column in zip(self.headings(), columns) } 
+
+ChemicalOutput.dict = _ChemicalOutput_dict
