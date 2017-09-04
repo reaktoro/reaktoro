860e18692446ca4f484185df9f3613f89ac4aa61
Allan Leal <allanmulin@gmail.com>
2017-09-04 16:16:05 +0200

    Removed comment on implictly_convertible

--- python/PyReaktoro/Common/PyAutoDiff.cpp
+++ python/PyReaktoro/Common/PyAutoDiff.cpp
@@ -89,7 +89,6 @@ auto export_Temperature() -> void
         .def(py::init<double>())
         ;
 
-//    py::implicitly_convertible<Temperature, double>();
     py::implicitly_convertible<double, Temperature>();
 }
 
@@ -100,7 +99,6 @@ auto export_Pressure() -> void
         .def(py::init<double>())
         ;
 
-//    py::implicitly_convertible<Pressure, double>();
     py::implicitly_convertible<double, Pressure>();
 }
 
