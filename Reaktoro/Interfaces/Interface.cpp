68fd220bbead9c8155ee2bcbba73ffbab0976736
Allan Leal <allanmulin@gmail.com>
2018-03-02 08:43:55 +0100

    Prevent zero amounts when converting Interface state to ChemicalState.
    Former-commit-id: 75b729fcfd5c9934a346f5cf2fe4385ccc941e18

--- Reaktoro/Interfaces/Interface.cpp
+++ Reaktoro/Interfaces/Interface.cpp
@@ -193,7 +193,9 @@ auto Interface::state(const ChemicalSystem& system) const -> ChemicalState
     ChemicalState state(system);
     state.setTemperature(temperature());
     state.setPressure(pressure());
-    state.setSpeciesAmounts(speciesAmounts());
+    Vector n = speciesAmounts();
+    n = (n.array() > 0.0).select(n, 1e-20);
+    state.setSpeciesAmounts(n);
     return state;
 }
 
