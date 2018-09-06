020e5a77e209ab7a52a83d57268f7893ada8ae6d
Alexandre Barbosa Bruno <abarbosa@esss.co>
2018-09-06 16:00:52 -0300

    move test_integration_demo_equilibrium_co2_brine, test_integration_demo_equilibrium_co2_brine_custom, test_integration_demo_equilibrium_co2_brine_using_equilibriumsolver to test_equilibrium_calculation.py and add num_regression

---
+++ tests/ReaktoroIntegrationTests/test_equilibrium_calculation.py
@@ -0,0 +1,149 @@
+import pytest
+import io
+import math
+import numpy as np
+
+from PyReaktoro import *
+from pytest_regressions.plugin import num_regression
+
+def stateDict(state):
+    #TODO - add pH, pE, Eh, alk -- no pybind
+    properties = state.properties() 
+    system = state.system()
+    
+    phase_masses = state.properties().phaseMasses().val
+    phase_volumes = state.properties().phaseVolumes().val
+    
+    checkOutPut = {
+        'number of phases': np.asarray([system.numPhases()]),
+        'temperature': np.asarray([state.temperature()]),
+        'pressure': np.asarray([state.pressure()]),
+        'molar fraction': np.asarray(state.properties().moleFractions().val),
+        #'lnActivity Coefficient' : np.asarray(state.properties().lnActivityCoefficients().val),
+        'Activity' : np.asarray(state.properties().chemicalPotentials().val),
+        'phase moles' : np.asarray(state.properties().phaseAmounts().val),
+        'phases masses' : np.asarray(state.properties().phaseMasses().val),
+        'phase molar volumes' : np.asarray(state.properties().phaseMolarVolumes().val),
+        'phase volumes' : np.asarray(state.properties().phaseVolumes().val),
+        'phase volume fraction' : np.asarray(phase_volumes/sum(phase_volumes)),
+        'phase densities' : np.asarray(phase_masses/phase_volumes),
+        'phase stability indices' : np.asarray(state.phaseStabilityIndices()),
+        'species amounts' : np.asarray(state.speciesAmounts())
+        }
+    
+    for i in range(0, system.numElements()):
+        checkOutPut[system.element(i).name()+' amount'] = np.asarray([state.elementAmount(i)]) 
+    
+    return checkOutPut
+
+@pytest.fixture
+def problemSetupH2O_CO2_NaCl_Halite_60C_300P():
+    '''
+    Build a problem with 1kg of H2O, 100g of CO2 and 0.1mol of NaCl 
+    at 60ºC and 300 bar 
+    '''
+    database = Database(b"supcrt98.xml")
+    
+    editor = ChemicalEditor(database)
+    editor.addAqueousPhase(b"H2O NaCl CO2")
+    editor.addGaseousPhase([b"H2O(g)", b"CO2(g)"])
+    editor.addMineralPhase(b"Halite")
+    
+    system = ChemicalSystem(editor)
+    
+    problem = EquilibriumProblem(system)
+    problem.add(b"H2O", 1, b"kg")
+    problem.add(b"CO2", 100, b"g")
+    problem.add(b"NaCl", 0.1, b"mol")
+    problem.setTemperature(60, b"celsius")
+    problem.setPressure(300, b"bar")
+    
+    return problem
+
+@pytest.fixture
+def problemSetupH2O_CO2_NaCl_Halite_dissolved_60C_300P():
+    '''
+    Build a problem with H2O, H+, Na+, Cl-, HCO3-, CO2(aq), CO3-- and 
+    Halite at 60ºC and 300 bar, defining their chemical species 
+    '''
+    editor = ChemicalEditor()
+    editor.addAqueousPhase([b"H2O(l)", b"H+", b"OH-", b"Na+", b"Cl-", b"HCO3-", b"CO2(aq)", b"CO3--"]) \
+        .setActivityModelDrummondCO2()
+    editor.addGaseousPhase([b"H2O(g)", b"CO2(g)"]). \
+        setChemicalModelSpycherPruessEnnis()
+    editor.addMineralPhase(b"Halite")
+
+    system = ChemicalSystem(editor)
+
+    problem = EquilibriumProblem(system)
+    problem.add(b"H2O", 1, b"kg")
+    problem.add(b"CO2", 100, b"g")
+    problem.add(b"NaCl", 1, b"mol")
+    problem.setTemperature(60, b"celsius")
+    problem.setPressure(300, b"bar")
+    
+    return problem
+
+
+@pytest.mark.parametrize('problemSetup',
+    [
+        (
+            pytest.lazy_fixture('problemSetupH2O_CO2_NaCl_Halite_60C_300P')
+        ),
+        (
+            pytest.lazy_fixture('problemSetupH2O_CO2_NaCl_Halite_dissolved_60C_300P')
+        )
+    ],
+    ids=['problem with H2O, CO2, NaCl and Halite at 60C and 300 bar',
+         'problem with H2O, CO2, NaCl and Halite already dissolved at 60C and 300 bar'
+         ]
+    )
+def test_equilibrium_calculation_H2O_NaCl_CO2(
+    problemSetup,
+    num_regression):
+    '''
+    checks equilibrium state calculation for given problem setup 
+    '''    
+    
+    state = equilibrate(problemSetup)
+    
+    output = stateDict(state)
+    
+    num_regression.check(output, 
+                         default_tolerance=dict(atol=1e-7, rtol=1e-18))
+
+def test_equilibrium_calculation_H2O_NaCl_CO2_using_equilibriumsolver(
+    problemSetupH2O_CO2_NaCl_Halite_60C_300P,
+    num_regression):
+    '''
+    calculates multiples equilibrium state for the given setup problem
+    '''    
+    system = problemSetupH2O_CO2_NaCl_Halite_60C_300P.system()
+    
+    solver = EquilibriumSolver(system)
+    
+    state = ChemicalState(system)
+    
+    solver.solve(state, 
+                problemSetupH2O_CO2_NaCl_Halite_60C_300P.temperature(),
+                problemSetupH2O_CO2_NaCl_Halite_60C_300P.pressure(),
+                problemSetupH2O_CO2_NaCl_Halite_60C_300P.elementAmounts())
+    
+    outputState1 = stateDict(state)
+    
+    num_regression.check(outputState1, 
+                        basename="test_equilibrium_calculation_H2O_NaCl_CO2_using_equilibriumsolver_state1.txt",
+                        tolerances=None, 
+                        default_tolerance=dict(atol=1e-7, rtol=1e-18))
+     
+    solver.solve(state,
+                 problemSetupH2O_CO2_NaCl_Halite_60C_300P.temperature() + 10.0,
+                 problemSetupH2O_CO2_NaCl_Halite_60C_300P.pressure(),
+                 problemSetupH2O_CO2_NaCl_Halite_60C_300P.elementAmounts())
+    
+    outputState2 = stateDict(state)
+    
+    num_regression.check(outputState2, 
+                        basename="test_equilibrium_calculation_H2O_NaCl_CO2_using_equilibriumsolver_state2.txt",
+                        tolerances=None,  
+                        default_tolerance=dict(atol=1e-7, rtol=1e-18))    
--- tests/_tests/test_integration.py
+++ tests/_tests/test_integration.py
@@ -4,120 +4,7 @@ from numpy import *
 from PyReaktoro import *
 from pytest_regressions.plugin import file_regression
 
-#TODO: try to use num_regression
-def test_integration_demo_equilibrium_co2_brine(file_regression):
-    database = Database(b"supcrt98.xml")
-
-    editor = ChemicalEditor(database)#
-    editor.addAqueousPhase(b"H2O NaCl CO2")
-    editor.addGaseousPhase([b"H2O(g)", b"CO2(g)"])
-    editor.addMineralPhase(b"Halite")
-
-    system = ChemicalSystem(editor)
-
-    problem = EquilibriumProblem(system)
-    problem.setTemperature(60, b"celsius")
-    problem.setPressure(300, b"bar")
-    problem.add(b"H2O", 1, b"kg")
-    problem.add(b"CO2", 100, b"g")
-    problem.add(b"NaCl", 0.1, b"mol")
-    
-    state = equilibrate(problem)
-    
-    state.output("state.txt")
-    
-    with io.open('state.txt', 'r') as file:
-        result = file.read()
-        file_regression.check(result)
-
-#TODO: try to use num_regression 
-@pytest.mark.xfail(reason='RES-9')       
-def test_integration_demo_equilibrium_co2_brine_custom(file_regression):
-    editor = ChemicalEditor()
-    editor.addAqueousPhase([b"H2O(l)", b"H+", b"OH-", b"Na+", b"Cl-", b"HCO3-", b"CO2(aq)", b"CO3--"]) \
-        .setActivityModelDrummondCO2()
-    editor.addGaseousPhase([b"H2O(g)", b"CO2(g)"]). \
-        setChemicalModelSpycherPruessEnnis()
-    editor.addMineralPhase(b"Halite")
-
-    system = ChemicalSystem(editor)
-
-    problem = EquilibriumProblem(system)
-    problem.setTemperature(60, b"celsius")
-    problem.setPressure(300, b"bar")
-    problem.add(b"H2O", 1, b"kg")
-    problem.add(b"CO2", 100, b"g")
-    problem.add(b"NaCl", 1, b"mol")
-
-    state = equilibrate(problem)
-    
-    state.output("state2.txt")
-    
-    with io.open('state2.txt', 'r') as file:
-        result = file.read()
-        file_regression.check(result)
-
-#TODO: try to use num_regression        
-def test_integration_demo_equilibrium_co2_brine_using_equilibriumsolver(file_regression):
-   # Create an object of Database class to use in the initialization of the
-   # chemical system.
-   database = Database("supcrt98.xml")
-
-   # Define which phases and species the chemical system should have using a
-   # ChemicalEditor object.
-   editor = ChemicalEditor(database)
-   editor.addAqueousPhase(b"H2O NaCl CO2")
-   editor.addGaseousPhase([b"H2O(g)", b"CO2(g)"])
-   editor.addMineralPhase(b"Halite")
-
-   # Initialize the chemical system using the definition provided to the
-   # chemical editor.
-   system = ChemicalSystem(editor)
-
-   # Define an equilibrium problem with given temperature, pressure, and
-   # amounts of compounds.
-   problem = EquilibriumProblem(system)
-   problem.setTemperature(60, b"celsius")
-   problem.setPressure(300, b"bar")
-   problem.add(b"H2O", 1, b"kg")
-   problem.add(b"CO2", 100, b"g")
-   problem.add(b"NaCl", 0.1, b"mol")
-
-   # Get the temperature, pressure, and mole amounts of the elements.
-   T = problem.temperature()
-   P = problem.pressure()
-   b = problem.elementAmounts()
-    
-   # Create an object of EquilibriumSolver class that can be reused many times.
-   solver = EquilibriumSolver(system)
-   
-   # Create an object of ChemicalState class to store the equilibrium
-   # state of the system.
-   state = ChemicalState(system)
-   
-   # Solve the equilibrium state with given (T, P, b) inputs.
-   solver.solve(state, T, P, b)
-    
-   # Print the calculated chemical equilibrium state.
-   state.output("state.txt")
-    
-   with io.open('state.txt', 'r') as file:
-       result = file.read()
-       file.close()
-       
-   # Calculate the new equilibrium state when temperature is increased.
-   # Use the previous equilibrium state as an initial guess for improved
-   # performance.
-   solver.solve(state, T + 10.0, P, b)
-
-   # Print the new calculated chemical equilibrium state.
-   state.output("state.txt")
-   with io.open('state.txt', 'r') as file:
-       result = result + file.read()
-       file.close()
-   
-   file_regression.check(result)
-
+#TODO: add documentation
 #TODO: try to use num_regression
 @pytest.mark.xfail(reason='RES-9')
 def test_demo_equilibrium_fixed_alkalinity(file_regression):
@@ -143,6 +30,8 @@ def test_demo_equilibrium_fixed_alkalinity(file_regression):
         result = file.read()
         file_regression.check(result)
 
+#TODO: add documentation
+#TODO: try to use num_regression
 def test_demo_equilibrium_fixed_amount(file_regression):
     editor = ChemicalEditor()
     editor.addAqueousPhase(b"H2O NaCl CaCO3")
@@ -165,6 +54,8 @@ def test_demo_equilibrium_fixed_amount(file_regression):
         result = file.read()
         file_regression.check(result)
         
+#TODO: add documentation
+#TODO: try to use num_regression
 def test_demo_equilibrium_fixed_ph_activity_amount(file_regression):
     editor = ChemicalEditor()
     editor.addAqueousPhase(b"H O Na Cl Ca Mg C")
@@ -187,8 +78,10 @@ def test_demo_equilibrium_fixed_ph_activity_amount(file_regression):
     
     with io.open("state.txt", "r") as file:
         result = file.read()
-        file_regression.check(result)    
-
+        file_regression.check(result)   
+         
+#TODO: add documentation
+#TODO: try to use num_regression
 def test_demo_equilibrium_fixed_ph_co2(file_regression):
     editor = ChemicalEditor()
     editor.addAqueousPhase(b"H O Na Cl Ca Mg C")
@@ -210,7 +103,9 @@ def test_demo_equilibrium_fixed_ph_co2(file_regression):
     with io.open("state.txt", "r") as file:
         result = file.read()
         file_regression.check(result)
-
+        
+#TODO: add documentation
+#TODO: try to use num_regression
 def test_demo_equilibrium_fixed_ph_hcl_naoh(file_regression):
     editor = ChemicalEditor()
     editor.addAqueousPhase(b"H O Na Cl Ca C")
@@ -232,6 +127,8 @@ def test_demo_equilibrium_fixed_ph_hcl_naoh(file_regression):
         result = file.read()
         file_regression.check(result)
 
+#TODO: add documentation
+#TODO: try to use num_regression
 @pytest.mark.xfail(reason='RES-10')
 def test_demo_equilibrium_fixed_phase_volume(file_regression):
     editor = ChemicalEditor()
@@ -254,8 +151,10 @@ def test_demo_equilibrium_fixed_phase_volume(file_regression):
 
     with io.open("state.txt", "r") as file:
         result = file.read()
-        file_regression.check(result)    
-
+        file_regression.check(result)
+            
+#TODO: add documentation
+#TODO: try to use num_regression
 def test_demo_equilibrium_iron_nh3(file_regression):
     database = Database("supcrt98.xml")
 
@@ -280,7 +179,8 @@ def test_demo_equilibrium_iron_nh3(file_regression):
         result = file.read()
         file_regression.check(result)
         
-    
+#TODO: add documentation
+#TODO: try to use num_regression    
 @pytest.mark.xfail(reason='RES-10')
 def test_demo_equilibriumpath_calcite_hcl(file_regression):
     editor = ChemicalEditor()
@@ -366,7 +266,8 @@ def test_demo_equilibriumpath_calcite_hcl(file_regression):
        
     file_regression.check(result)
 
-
+#TODO: add documentation
+#TODO: try to use num_regression
 def test_demo_equilibriumpath_co2(file_regression):
     editor = ChemicalEditor()
     editor.addAqueousPhase(b"H O C Na Cl")
@@ -419,7 +320,9 @@ def test_demo_equilibriumpath_co2(file_regression):
        file.close()
     
     file_regression.check(result)
-    
+
+#TODO: add documentation
+#TODO: try to use num_regression    
 def test_demo_kineticpath_calcite_hcl(file_regression):
     editor = ChemicalEditor()
     editor.addAqueousPhase(b"H2O HCl CaCO3")
@@ -477,7 +380,8 @@ def test_demo_kineticpath_calcite_hcl(file_regression):
     
     file_regression.check(result)
     
-
+#TODO: add documentation
+#TODO: try to use num_regression
 def test_demo_kineticpath_carbonates_co2(file_regression):
     editor = ChemicalEditor()
     editor.addAqueousPhase(b"H2O NaCl CaCO3 MgCO3")
@@ -577,7 +481,8 @@ def test_demo_kineticpath_carbonates_co2(file_regression):
        
     file_regression.check(result) 
 
-    
+#TODO: add documentation
+#TODO: try to use num_regression    
 def test_demo_thermodynamic_properties_of_species_using_supcrt_database(file_regression):
     # Create a Database object loaded with SUPCRT98 database file.
     database = Database("supcrt98.xml")
@@ -626,7 +531,9 @@ def test_demo_thermodynamic_properties_of_species_using_supcrt_database(file_reg
         file.close()
     
     file_regression.check(result)  
-
+    
+#TODO: add documentation
+#TODO: try to use num_regression
 def test_demo_thermo_properties(file_regression):
     # Create a Database object loaded with SUPCRT database file.
     database = Database("supcrt98.xml")
@@ -662,7 +569,8 @@ def test_demo_thermo_properties(file_regression):
     
     file_regression.check(result)    
      
-#TODO - add a test based on demo-water-properties.py
+#TODO: add documentation
+#TODO: try to use num_regression
 def test_demo_water_properties(file_regression):
     T, P = 298.15, 1e5
 
