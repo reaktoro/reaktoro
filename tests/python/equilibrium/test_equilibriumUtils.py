4eaed872f100cca956b57919b71f5c2694cfeb07
Alexandre Barbosa Bruno <abarbosa@esss.co>
2018-09-13 15:06:31 -0300

    move equilibriumsolver test to test_equilibriumSolver.py

--- tests/python/equilibrium/test_equilibriumUtils.py
+++ tests/python/equilibrium/test_equilibriumUtils.py
@@ -6,7 +6,7 @@ from pytest_regressions.plugin import num_regression
 from _pytest.fixtures import fixture
 
 #PyReaktoro
-from equilibriumProblems import *
+from problemsSetup import *
 from PyReaktoro import *
 
 def stateDict(state):
@@ -41,42 +41,6 @@ def stateDict(state):
     return checkOutPut
 
 
- 
-def test_equilibrium_calculation_using_equilibriumsolver(
-    equilibriumProblemSetupH2O_CO2_NaCl_Halite_60C_300P,
-    num_regression):
-    '''
-    calculates multiples equilibrium state for the given setup problem
-    '''    
-    system = equilibriumProblemSetupH2O_CO2_NaCl_Halite_60C_300P.system()
-      
-    solver = EquilibriumSolver(system)
-      
-    state = ChemicalState(system)
-      
-    solver.solve(state, 
-                equilibriumProblemSetupH2O_CO2_NaCl_Halite_60C_300P.temperature(),
-                equilibriumProblemSetupH2O_CO2_NaCl_Halite_60C_300P.pressure(),
-                equilibriumProblemSetupH2O_CO2_NaCl_Halite_60C_300P.elementAmounts())
-      
-    outputState1 = stateDict(state)
-      
-    num_regression.check(outputState1, 
-                        basename="test_equilibrium_calculation_H2O_NaCl_CO2_using_equilibriumsolver_state1.txt",
-                        tolerances=None, 
-                        default_tolerance=dict(atol=1e-7, rtol=1e-18))
-       
-    solver.solve(state,
-                 equilibriumProblemSetupH2O_CO2_NaCl_Halite_60C_300P.temperature() + 10.0,
-                 equilibriumProblemSetupH2O_CO2_NaCl_Halite_60C_300P.pressure(),
-                 equilibriumProblemSetupH2O_CO2_NaCl_Halite_60C_300P.elementAmounts())
-      
-    outputState2 = stateDict(state)
-      
-    num_regression.check(outputState2, 
-                        basename="test_equilibrium_calculation_H2O_NaCl_CO2_using_equilibriumsolver_state2.txt",
-                        tolerances=None,  
-                        default_tolerance=dict(atol=1e-7, rtol=1e-18))
       
 # #TODO: move problem setup to equilibriumProblems after fix xfail
 #@pytest.mark.skip(reason="RES-9, some times is doesn't converge, throwing an error")
