b98911d00cc90213fa4215c7002aaa8671948a79
Allan Leal <allanmulin@gmail.com>
2018-11-26 10:55:25 -0200

    Improved logic and BOOST_ROOT now set to CONDA_PREFIX

--- cmake/CondaRecognizer.cmake
+++ cmake/CondaRecognizer.cmake
@@ -1,9 +1,17 @@
 # This cmake module determines if a conda environment is active.
-# If so, CMAKE_INSTALL_PREFIX is set to the environmental variable CONDA_PREFIX.
+# Note: CMAKE_INSTALL_PREFIX is set to the environment variable CONDA_PREFIX if a conda environment is found active.
 # This automatic behavior can be overriden by manually specifying a different CMAKE_INSTALL_PREFIX.
 
-# Check if user didn't override CMAKE_INSTALL_PREFIX and a conda environment is active
-if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT and DEFINED ENV{CONDA_PREFIX})
+# Check if a conda environment is active
+if(DEFINED ENV{CONDA_PREFIX})
+    # Show that conda env has been recognized
     message(STATUS "Conda environment recognized in $ENV{CONDA_PREFIX}")
-    set(CMAKE_INSTALL_PREFIX $ENV{CONDA_PREFIX})
+
+    # Set CMAKE_INSTALL_PREFIX to CONDA_PREFIX if not specified by the user
+    if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
+        set(CMAKE_INSTALL_PREFIX $ENV{CONDA_PREFIX})
+    endif()
+
+    # Set BOOST_ROOT to CONDA_PREFIX to ensure conda's boost version is used
+    set(BOOST_ROOT $ENV{CONDA_PREFIX})
 endif()
