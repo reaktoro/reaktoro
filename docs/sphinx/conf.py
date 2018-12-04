aee94f55fd46c021fc2db176ec65845ff7b1a705
Allan Leal <allan.leal@erdw.ethz.ch>
2018-12-04 15:43:58 +0100

    Using readthedocs theme with sphinx.

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -16,6 +16,8 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+# Import the readthedocs theme python package
+import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -67,7 +69,7 @@ language = None
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = None
+pygments_style = 'xcode'
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -75,7 +77,8 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'nature'
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
