725cd24f193a473a0d0c3f30f8d8a7de12f32920
Allan Leal <allan.leal@erdw.ethz.ch>
2018-12-13 09:51:03 +0100

    Added a demo that demonstrates use of find_package(Reaktoro).

---
+++ demos/cmake/reaktoro-app/main.cpp
@@ -0,0 +1,25 @@
+#include <Reaktoro/Reaktoro.hpp>
+using namespace Reaktoro;
+
+int main()
+{
+    Database database("supcrt98.xml");
+
+    ChemicalEditor editor(database);
+    editor.addAqueousPhase("H O C Na Cl");
+    editor.addGaseousPhase({"H2O(g)", "CO2(g)"});
+    editor.addMineralPhase("Halite");
+
+    ChemicalSystem system(editor);
+
+    EquilibriumProblem problem(system);
+    problem.setTemperature(60, "celsius");
+    problem.setPressure(300, "bar");
+    problem.add("H2O", 1, "kg");
+    problem.add("CO2", 100, "g");
+    problem.add("NaCl", 0.1, "mol");
+
+    ChemicalState state = equilibrate(problem);
+
+    state.output("state-cmake-example.txt");
+}
