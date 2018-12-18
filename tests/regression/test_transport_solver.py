69c7bc212e075cb8ff93ac5e778b143fd6fc7ae0
Alexandre Barbosa Bruno <abarbosa@esss.co>
2018-12-18 14:11:24 -0200

    add test for transport solver

---
+++ tests/regression/test_transport_solver.py
@@ -0,0 +1,136 @@
+import numpy as np
+import pytest
+import reaktoro as rk
+
+from collections import namedtuple
+
+#a and b values of a linear source: q = a*x+b
+source_parameters = namedtuple("source_parameters", ["a", "b"])
+
+@pytest.mark.parametrize(
+    "source_parameters",
+    [
+        (source_parameters(0, 0)),
+        (source_parameters(0, 1)),
+        (source_parameters(1, 1)),
+    ],
+    ids=[
+        "diffusion-source rate q=0",
+        "diffusion-source rate q=1",
+        "diffusion-source rate q=x+1",
+        ],
+    )
+def test_transport_solver_diffusion(source_parameters, num_regression):
+    '''
+    """
+    A test to check the solution of a advection-diffusion equation with v = 0
+    Eq: 
+        du/dt + v du/dx = D d²u/dx² + q
+    
+    The result were compared with the following analytic solution and got
+    near results when we increase the number of cells. We decided to compare with
+    numerical solve to save some computational time. 
+    
+    analytic_u = -((a*x**3)/(6*D)) - ((b*x**2)/(2*D)) + ((a*x*xr**2)/(2*D)) + ((b*x*xr)/(D)) + ul
+    
+    @param source_parameters
+        a tuple that has values of a and b coefficient of a source term
+        that behaves as q a*x+b
+    '''
+    a = source_parameters.a
+    b = source_parameters.b
+    D = 0.0002
+    v = 0
+    ul = 1
+    dt = 100
+    num_steps = 1000
+    num_cells = 10
+    xl = 0
+    xr = 1.0
+    
+    mesh = rk.Mesh(num_cells, xl, xr)
+    
+    x = mesh.xcells()
+    q = a*x + b
+    
+    transp_solver = rk.TransportSolver()
+    
+    transp_solver.setMesh(mesh)
+    transp_solver.setVelocity(v)
+    transp_solver.setDiffusionCoeff(D)
+    transp_solver.setBoundaryValue(ul)
+    transp_solver.setTimeStep(dt)
+      
+    transp_solver.initialize()
+      
+    numerical_u = np.zeros(num_cells)
+    transp_solver.step(numerical_u, q)
+      
+    for i in range(0,num_steps):
+        transp_solver.step(numerical_u, q)
+    
+    num_regression.check({'u': numerical_u})
+
+@pytest.mark.parametrize(
+    "source_parameters",
+    [
+        (source_parameters(0, 0)),
+        (source_parameters(0, 1)),
+        (source_parameters(1, 1)),
+    ],
+    ids=[
+        "advection-with source rate q=0",
+        "advection-with source rate q=1",
+        "advection-with source rate q=x+1",
+        ],
+    )
+def test_transport_solver_advection(source_parameters, num_regression):
+    '''
+    """
+    A test to check the solution of a advection-diffusion equation with D = 0
+    Eq: 
+        du/dt + v du/dx = D d²u/dx² + q
+    
+    The result were compared with the following analytic solution and got
+    near results when we increase the number of cells. We decided to compare with
+    numerical solve to save some computational time. 
+    
+    analytic_u = (a*x**2)/(2*v) + (b*x)/(v) + ul
+    
+    @param source_parameters
+        a tuple that has values of "a" and "b" coefficients of a source term
+        that behaves as q=a*x+b
+    '''
+    a = source_parameters.a
+    b = source_parameters.b
+    D = 0
+    v = 1
+    ul = 1 
+    dt = 0.01
+    num_steps = 5000
+    num_cells = 10
+    xl = 0
+    xr = 1.0
+    
+    mesh = rk.Mesh(num_cells, xl, xr)
+    
+    x = mesh.xcells()
+    q = a*x + b
+    
+    transp_solver = rk.TransportSolver()
+    
+    transp_solver.setMesh(mesh)
+    transp_solver.setVelocity(v)
+    transp_solver.setDiffusionCoeff(D)
+    transp_solver.setBoundaryValue(ul)
+    transp_solver.setTimeStep(dt)
+      
+    transp_solver.initialize()
+      
+    numerical_u = np.zeros(num_cells)
+    transp_solver.step(numerical_u, q)
+      
+    for i in range(0,num_steps):
+        transp_solver.step(numerical_u, q)
+    
+    num_regression.check({'u': numerical_u})
