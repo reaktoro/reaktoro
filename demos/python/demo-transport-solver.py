330468343832fbd2f96b2d1b00863148be29cfbc
Allan Leal <allanmulin@gmail.com>
2018-12-19 12:44:01 +0100

    Some renaming in the demo
    I made some changes in the names of the variables. I did not test if this file still works (probably should). Some import packages were removed because they were not needed with the changes (e.g., `copy`)

--- demos/python/demo-transport-solver.py
+++ demos/python/demo-transport-solver.py
@@ -1,8 +1,6 @@
-import copy
-import matplotlib
+import reaktoro as rkt
 import matplotlib.pyplot as plt
 import numpy as np
-import reaktoro as rk
 
 # Auxiliary time related constants
 second = 1
@@ -16,40 +14,44 @@ nsteps = 225      # the number of steps in the transport simulation
 ncells = 100      # the number of cells in the discretization
 xl = 0.0          # the x-coordinate of the left boundary
 xr = 100.0        # the x-coordinate of the right boundary
-D  = 1.0e-9       # the diffusion coefficinet (in units of m2/s)
-v  = 1.0/day      # the fluid pore velocity (in units of m/s)
+D  = 1.0e-9       # the diffusion coefficient (in units of m2/s)
+v  = 1.0/day      # the velocity (in units of m/s)
 dt = 0.5*day      # the time step (in units of s)
-ul = 1            # amount at the left boundary
+ul = 1            # concentration at the left boundary (mol/m3)
 
-mesh = rk.Mesh(ncells, xl, xr)
+mesh = rkt.Mesh(ncells, xl, xr)
 
 x = mesh.xcells()
  
-ts = rk.TransportSolver()
-ts.setMesh(mesh)
-ts.setVelocity(v)
-ts.setDiffusionCoeff(D)
-ts.setBoundaryValue(ul)
-ts.setTimeStep(dt)
+transport = rkt.TransportSolver()
+transport.setMesh(mesh)
+transport.setVelocity(v)
+transport.setDiffusionCoeff(D)
+transport.setBoundaryValue(ul)
+transport.setTimeStep(dt)
       
-ts.initialize()
+transport.initialize()
   
-numerical_u = np.zeros(ncells)
+u = np.zeros(ncells)
 
-amounts = []
-times = []
+useries = []
+tseries = []
 
 for i in range(1,nsteps):
-    if (i%25 == 0):
-        amounts.append(copy.deepcopy(numerical_u))
-        times.append(i*dt/day)
-    ts.step(numerical_u)
+
+    # Record at every 25 steps the current time and concentration
+    if i % 25 == 0:
+        tseries.append(i * dt / day)
+        useries.append(u.copy())
+
+    # Perform one time step
+    transport.step(u)
 
 fig, ax = plt.subplots(figsize=(10, 20))    
-ax.set(xlabel='x [m]', ylabel='amount [mol]')
+ax.set(xlabel='x [m]', ylabel='u [mol/m3]')
 
 for i in range(0,len(times)):
-    ax.plot(x, amounts[i], label='{} days'.format(times[i]))
+    ax.plot(x, useries[i], label='{} days'.format(tseries[i]))
 
 legend = ax.legend(fontsize='x-large')
 
