1ff36c7e8ad3309aa26162ac3ce6700859b4bb4a
Allan Leal <allanmulin@gmail.com>
2018-12-19 22:33:42 -0200

    pybind11 is now stored in thirdparty for convenience

--- cmake/ReaktoroFindDeps.cmake
+++ cmake/ReaktoroFindDeps.cmake
@@ -8,11 +8,14 @@ if(REAKTORO_BUILD_PYTHON)
         find_package(PythonInterp REQUIRED) # PYTHON_EXECUTABLE gets set here
     endif()
 
-    find_package(pybind11 REQUIRED)
+    find_package(pybind11 REQUIRED HINTS ${PROJECT_SOURCE_DIR}/thirdparty/pybind11)
 
     if(NOT pybind11_FOUND)
         message(WARNING "Could not find pybind11 - the Python module `reaktoro` will not be built.")
         set(REAKTORO_BUILD_PYTHON OFF)
+    else()
+        message(STATUS "Found pybind11 v${pybind11_VERSION}: ${pybind11_INCLUDE_DIRS}")
     endif()
+
 endif()
 
