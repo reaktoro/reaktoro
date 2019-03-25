01a28c8f7dc13f2fd79972859d8de3d8775eaba3
Alexandre Bruno <alexandrebruno@Alexandres-MacBook-Air.local>
2019-03-24 21:52:31 -0300

    uncomment tests

--- tests/regression/test_equilibrium_solver.py
+++ tests/regression/test_equilibrium_solver.py
@@ -166,54 +166,54 @@ def test_equilibrium_solver_solve_overload_3(setup, state_regression):
 @pytest.mark.parametrize(
     "setup",
     [
-        #(
-        #    pytest.lazy_fixture(
-        #        "equilibrium_problem_with_h2o_co2_nacl_halite_60C_300bar"
-        #    )
-        #),
+        (
+            pytest.lazy_fixture(
+                "equilibrium_problem_with_h2o_co2_nacl_halite_60C_300bar"
+            )
+        ),
         (
             pytest.lazy_fixture(
                 "equilibrium_problem_with_h2o_co2_nacl_halite_dissolved_60C_300bar"
             )
         ),
-        #(pytest.lazy_fixture("equilibrium_problem_with_h2o_feoh2_feoh3_nh3_magnetite")),
-        #(
-        #    pytest.lazy_fixture(
-        #        "equilibrium_inverse_with_h_o_na_cl_ca_mg_c_fixed_amount_and_activity"
-        #    )
-        #),
-        #(pytest.lazy_fixture("equilibrium_inverse_with_h_o_na_cl_ca_mg_c_defined_ph")),
-        #(
-        #    pytest.lazy_fixture(
-        #        "equilibrium_inverse_with_h_o_na_cl_ca_c_calcite_ph_and_fixed_amounts"
-        #    )
-        #),
-        #(
-        #    pytest.lazy_fixture(
-        #        "equilibrium_inverse_with_h2o_nacl_caco3_calcilte_and_fixed_mass"
-        #    )
-        #),
-        #(
-        #    pytest.lazy_fixture(
-        #        "equilibrium_inverse_with_h2o_nacl_caco3_co2_fixed_mass_amount_and_alkalinity"
-        #    )
-        #),
-        #(
-        #    pytest.lazy_fixture(
-        #        "equilibrium_inverse_with_h2o_nacl_caco3_co2_calcite_fixed_phase_volume"
-        #    )
-        #),
+        (pytest.lazy_fixture("equilibrium_problem_with_h2o_feoh2_feoh3_nh3_magnetite")),
+        (
+            pytest.lazy_fixture(
+                "equilibrium_inverse_with_h_o_na_cl_ca_mg_c_fixed_amount_and_activity"
+            )
+        ),
+        (pytest.lazy_fixture("equilibrium_inverse_with_h_o_na_cl_ca_mg_c_defined_ph")),
+        (
+            pytest.lazy_fixture(
+                "equilibrium_inverse_with_h_o_na_cl_ca_c_calcite_ph_and_fixed_amounts"
+            )
+        ),
+        (
+            pytest.lazy_fixture(
+                "equilibrium_inverse_with_h2o_nacl_caco3_calcilte_and_fixed_mass"
+            )
+        ),
+        (
+            pytest.lazy_fixture(
+                "equilibrium_inverse_with_h2o_nacl_caco3_co2_fixed_mass_amount_and_alkalinity"
+            )
+        ),
+        (
+            pytest.lazy_fixture(
+                "equilibrium_inverse_with_h2o_nacl_caco3_co2_calcite_fixed_phase_volume"
+            )
+        ),
     ],
     ids=[
-        #"Eq Prob-H2O CO2 NaCl and Halite 60C 300bar",
+        "Eq Prob-H2O CO2 NaCl and Halite 60C 300bar",
         "Eq Prob-H2O CO2 NaCl and Halite already dissolved 60C 300bar",
-        #"Eq Prob-H2O Fe(OH)2 Fe(OH)3 NH3 and Magnetite",
-        #"InvEq Prob-H O Na Cl Ca Mg C with fixed amount and activity",
-        #"InvEq Prob-H O Na Cl Ca Mg C with defined pH",
-        #"InvEq Prob-H O Na Cl Ca C Calcite with defined pH and fixed amount",
-        #"InvEq Prob-H2O CO2 NaCl CaCO3 Calcite with fixed species mass",
-        #"InvEq Prob-H2O NaCl CaCO3 CO2 with fixed mass amount and alkalinity",
-        #"InvEq Prob-H2H NaCl CaCO3 CO2 Calcite with fixed phase volume",
+        "Eq Prob-H2O Fe(OH)2 Fe(OH)3 NH3 and Magnetite",
+        "InvEq Prob-H O Na Cl Ca Mg C with fixed amount and activity",
+        "InvEq Prob-H O Na Cl Ca Mg C with defined pH",
+        "InvEq Prob-H O Na Cl Ca C Calcite with defined pH and fixed amount",
+        "InvEq Prob-H2O CO2 NaCl CaCO3 Calcite with fixed species mass",
+        "InvEq Prob-H2O NaCl CaCO3 CO2 with fixed mass amount and alkalinity",
+        "InvEq Prob-H2H NaCl CaCO3 CO2 Calcite with fixed phase volume",
     ],
 )
 def test_equilibrium_solver_approx_overload_1(setup, state_regression):
