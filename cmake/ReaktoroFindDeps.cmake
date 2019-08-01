d42ca25b6bf3a7e7b8a5fc98164a3a9d0b4d5799
Allan Leal <allanmulin@gmail.com>
2019-08-01 14:24:34 +0200

    Using json lib from conda-forge

--- cmake/ReaktoroFindDeps.cmake
+++ cmake/ReaktoroFindDeps.cmake
@@ -1,13 +1,14 @@
-# Find Boost library
 find_package(Boost REQUIRED)
+find_package(nlohmann_json 3.6.1 REQUIRED)
 
-# Find pybind11 library (if needed)
 if(REAKTORO_BUILD_PYTHON)
-    find_package(pybind11 REQUIRED)
+
+    find_package(pybind11 2.3.0 REQUIRED)
     if(NOT pybind11_FOUND)
         message(WARNING "Could not find pybind11 - the Python module `reaktoro` will not be built.")
         set(REAKTORO_BUILD_PYTHON OFF)
     else()
         message(STATUS "Found pybind11 v${pybind11_VERSION}: ${pybind11_INCLUDE_DIRS}")
     endif()
+
 endif()
