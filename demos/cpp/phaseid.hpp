d90593a498b744cccfaabc7b0f9041726549eb71
Alexandre Barbosa Bruno <abarbosa@esss.co>
2019-08-06 14:12:35 -0300

    remove  oil demo and unused binds

--- demos/cpp/phaseid.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-enum phaseIdentificationMethod {
-	VolumeMethod, CriticalPointMethods, IsothermalCompressibilityMethods, workanalisys, workanalisysPengRobinson, Gibbs_residual_based ,NoMethod,
-};
