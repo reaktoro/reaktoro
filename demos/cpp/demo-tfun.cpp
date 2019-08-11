3af3718bf5cca5acba94b1813079cadbb330d425
dmiron <mirondanro@yahoo.com>
2019-08-11 19:25:12 +0200

    Output time of initialization, properties calculations, comparison with direct ThemroFun calculations

--- demos/cpp/demo-tfun.cpp
+++ demos/cpp/demo-tfun.cpp
@@ -5,8 +5,12 @@ using namespace Reaktoro;
 
 int main()
 {
-    ThermoFun::Database database("databases/thermofun/aq17.json");
     Time start = time();
+    ThermoFun::Database database("aq17.json");
+    double time_parse_db = elapsed(start);
+
+    start = time();
+    Time startT = time();
     ChemicalEditor editor(database);
     editor.setTemperatures({650}, "celsius");
     editor.setPressures({2000}, "bar");
@@ -25,11 +29,17 @@ int main()
     editor.addMineralPhase("Andalusite");
     editor.addMineralPhase("Quartz");
 
-    ChemicalSystem system(editor);
-
-    double time_initialization = elapsed(start);
+    double time_initialization_editor = elapsed(start);
 
     start = time();
+
+    ChemicalSystem system(editor);
+
+    double time_initialization_system = elapsed(start);
+
+    start = time();
+
+    std::cout << "solving " << std::endl;
 
     EquilibriumProblem problem(system);
     problem.add("H2O", 1, "kg");
@@ -55,17 +65,60 @@ int main()
 
     double time_solve = elapsed(start);
 
+    double time_total = elapsed(startT);
+
     state.output("result.txt");
 
     start = time();
 
     waterDensityWagnerPruss(Temperature(650+273), Pressure(2000e5), StateOfMatter::Liquid);
 
+    ThermoFun::ThermoEngine engine(database);
+    ThermoFun::ThermoEngine engine2(database);
+    ThermoFun::ThermoBatch batch(database);
+    batch.setSolventSymbol("H2O@");
+
     double time_watereos = elapsed(start);
 
-    std::cout << "time_initialization = " << time_initialization << std::endl;
+    start = time();
+    double P = 2000e5;
+    engine.propertiesSolvent(650+273, P, "H2O@");
+    double time_watereos_fun = elapsed(start);
+
+    start = time();
+    P = 2000e5;
+    engine.electroPropertiesSolvent(650+273, P, "H2O@");
+    double time_waterelectro_fun = elapsed(start);
+
+    start = time();
+    P = 2000e5;
+    engine.propertiesSolvent(650+273, P, "H2O@");
+    double time_watereos_fun2 = elapsed(start);
+
+ //   batch.thermoPropertiesSubstance(650+273, P, {"H2O@", "Al(OH)3@"},  {"gibbs_energy","entropy", "volume", "enthalpy"});
+    start = time();
+    P = 2000e5;
+    batch.thermoPropertiesSubstance(650+273, P, {"Al(OH)2+", "Al(OH)3@", "Al(OH)4-", "Al+3", "AlH3SiO4+2", "AlOH+2", "Cl-", "H+", "H2@", "H2O@", "HCl@", "HSiO3-", "K+", "KAlO2@", "KCl@", "KOH@", "Na+", "NaAl(OH)4@", "NaCl@", "NaHSiO3@", "NaOH@", "O2@", "OH-", "SiO2@"}, {"gibbs_energy"});
+
+    engine.propertiesSolvent(650+273, P, "H2O@");
+    double time_species_fun = elapsed(start);
+
+    start =time();
+
+    engine2.thermoPropertiesSubstance(650+273, P, "Al(OH)2+");
+    double time_Alspecies_fun = elapsed(start);
+
+    std::cout << "time_parse_db = " << time_parse_db << std::endl;
+    std::cout << "time_initialization_editor = " << time_initialization_editor << std::endl;
+    std::cout << "time_initialization_system = " << time_initialization_system << std::endl;
     std::cout << "time_solve = " << time_solve << std::endl;
     std::cout << "time_watereos = " << time_watereos << std::endl;
+    std::cout << "time_watereos_fun = " << time_watereos_fun << std::endl;
+    std::cout << "time_waterelectro_fun = " << time_waterelectro_fun << std::endl;
+    std::cout << "time_watereos_fun called 2nd time = " << time_watereos_fun2 << std::endl;
+    std::cout << "time_aq_species_fun = " << time_species_fun << std::endl;
+    std::cout << "time_total = " << time_total << std::endl;
+    std::cout << "time_Alspecies_fun = " << time_Alspecies_fun << std::endl;
 }
 
 
