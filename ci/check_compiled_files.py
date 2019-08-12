556f9314391331d6758c6cd5179a245206ae04e4
Tadeu Manoel <tadeu@esss.co>
2019-08-12 10:27:11 -0300

    Add macOS support in check_compiled_files.py script

--- ci/check_compiled_files.py
+++ ci/check_compiled_files.py
@@ -9,10 +9,15 @@ artifacts = (Path(__file__).parent / '../artifacts').absolute()
 if sys.platform.startswith('linux'):
     assert (artifacts / 'lib/libReaktoro.so').is_file()
     assert (artifacts / 'include/Reaktoro/Reaktoro.hpp').is_file()
-else:
+elif sys.platform == 'win32':
     assert (artifacts / 'lib/Reaktoro.lib').is_file()
     assert (artifacts / 'bin/Reaktoro.dll').is_file()
     assert (artifacts / 'include/Reaktoro/Reaktoro.hpp').is_file()
+elif sys.platform == 'darwin':
+    assert (artifacts / 'lib/libReaktoro.dylib').is_file()
+    assert (artifacts / 'include/Reaktoro/Reaktoro.hpp').is_file()
+else:
+    assert False, f'Unsupported platform: {sys.platform}'
 
 import reaktoro
 assert hasattr(reaktoro, 'Reaction')
