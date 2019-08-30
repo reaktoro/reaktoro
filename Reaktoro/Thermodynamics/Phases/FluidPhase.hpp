99ab5faf0fb6357c7fddd06770f1395e416a5fee
Allan Leal <allanmulin@gmail.com>
2019-08-30 09:34:28 +0200

    Remove deprecated notice.

--- Reaktoro/Thermodynamics/Phases/FluidPhase.hpp
+++ Reaktoro/Thermodynamics/Phases/FluidPhase.hpp
@@ -44,35 +44,27 @@ public:
     /// Set the chemical model of the phase with the ideal gas equation of state.
     /// This model only supports a gaseous phase. Using it in a FluidPhase that is not a
     /// PhaseType::Gas will result in a runtime error.
-    auto setChemicalModelIdeal()->FluidPhase&;
+    auto setChemicalModelIdeal() -> FluidPhase&;
 
-    /// This function is deprecated in favor of setChemicalModelCubicEOS.
-    ///
     /// Set the chemical model of the phase with the van der Waals equation of state.
-    /// 
-    /// Reference: *van der Waals, J.D. (1910). Nobel Lectures in Physics. pp. 254-265*.
-    auto setChemicalModelVanDerWaals()->FluidPhase&;
-
-    /// This function is deprecated in favor of setChemicalModelCubicEOS.
     ///
+    /// Reference: *van der Waals, J.D. (1910). Nobel Lectures in Physics. pp. 254-265*.
+    auto setChemicalModelVanDerWaals() -> FluidPhase&;
+
     /// Set the chemical model of the phase with the Redlich-Kwong equation of state.
     ///
     /// Reference: *Redlich, O., Kwong, J.N.S. (1949). On The Thermodynamics of Solutions. Chem. Rev. 44(1) 233–244*.
-    auto setChemicalModelRedlichKwong()->FluidPhase&;
+    auto setChemicalModelRedlichKwong() -> FluidPhase&;
 
-    /// This function is deprecated in favor of setChemicalModelCubicEOS.
-    ///
     /// Set the chemical model of the phase with the Soave-Redlich-Kwong equation of state.
     ///
     /// Reference: *Soave, G. (1972). Equilibrium constants from a modified Redlich-Kwong equation of state, Chem. Eng. Sci., 27, 1197-1203*.
-    auto setChemicalModelSoaveRedlichKwong()->FluidPhase&;
+    auto setChemicalModelSoaveRedlichKwong() -> FluidPhase&;
 
-    /// This function is deprecated in favor of setChemicalModelCubicEOS.
-    ///
     /// Set the chemical model of the phase with the Peng-Robinson equation of state.
     ///
     /// Reference: *Peng, D.Y., Robinson, D.B. (1976). A New Two-Constant Equation of State. Industrial and Engineering Chemistry: Fundamentals 15: 59–64*.
-    auto setChemicalModelPengRobinson(CubicEOS::Params params = {})->FluidPhase&;
+    auto setChemicalModelPengRobinson(CubicEOS::Params params = {}) -> FluidPhase&;
 
     /// Set the chemical model of the phase with a Cubic equation of state. The specific type
     /// can be passed as a parameter (the default in Peng-Robinson).
@@ -82,7 +74,7 @@ public:
     ///     *Soave, G. (1972). Equilibrium constants from a modified Redlich-Kwong equation of state, Chem. Eng. Sci., 27, 1197-1203*.
     ///     *Redlich, O., Kwong, J.N.S. (1949). On The Thermodynamics of Solutions. Chem. Rev. 44(1) 233-244*.
     ///     *van der Waals, J.D. (1910). Nobel Lectures in Physics. pp. 254-265*.
-    auto setChemicalModelCubicEOS(CubicEOS::Params params = {})->FluidPhase&;
+    auto setChemicalModelCubicEOS(CubicEOS::Params params = {}) -> FluidPhase&;
 
     /// Set the chemical model of the phase with the Spycher et al. (2003) equation of state.
     /// This model only supports the gaseous species `H2O(g)` and `CO2(g)`. Any other species
@@ -91,7 +83,7 @@ public:
     /// Reference: *Spycher, N., Pruess, K., Ennis-King, J. (2003). CO2-H2O mixtures in the
     /// geological sequestration of CO2. I. Assessment and calculation of mutual solubilities from 12 to 100°C
     /// and up to 600 bar. Geochimica et Cosmochimica Acta, 67(16), 3015–3031*.
-    auto setChemicalModelSpycherPruessEnnis()->FluidPhase&;
+    auto setChemicalModelSpycherPruessEnnis() -> FluidPhase&;
 
     /// Set the chemical model of the phase with the Spycher and Reed (1988) equation of state.
     /// This model only supports the gaseous species `H2O(g)`, `CO2(g)`, and CH4(g). Any other
@@ -101,7 +93,7 @@ public:
     /// CH4, H2O and of H2O--CO2--CH4 mixtures: A virial equation treatment for
     /// moderate pressures and temperatures applicable to calculations of
     /// hydrothermal boiling. Geochimica et Cosmochimica Acta, 52(3), 739–749*.
-    auto setChemicalModelSpycherReed()->FluidPhase&;
+    auto setChemicalModelSpycherReed() -> FluidPhase&;
 
     /// Return a const reference of the FluidMixture instance
     auto mixture() const -> const FluidMixture&;
