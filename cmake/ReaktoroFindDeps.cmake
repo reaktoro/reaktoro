b2d805691aee9e73a11487236e877be34cb81e48
Allan Leal <allan.leal@erdw.ethz.ch>
2019-09-02 10:54:04 +0200

    Ensure pybind11 really used c++17

--- cmake/ReaktoroFindDeps.cmake
+++ cmake/ReaktoroFindDeps.cmake
@@ -3,6 +3,7 @@ find_package(Boost REQUIRED)
 
 # Find pybind11 library (if needed)
 if(REAKTORO_BUILD_PYTHON)
+    set(PYBIND11_CPP_STANDARD -std=c++17)  # Ensure pybind11 really uses c++17
     find_package(pybind11 REQUIRED)
     if(NOT pybind11_FOUND)
         message(WARNING "Could not find pybind11 - the Python module `reaktoro` will not be built.")
