84a5dd44e9c85bdd92175531c278c8879bdc3ce3
Alexandre Barbosa Bruno <abarbosa@esss.co>
2019-09-02 13:37:53 -0300

    change demo from std::filesystem to boost::filesystem

--- demos/cpp/demo-transport-scaveging.cpp
+++ demos/cpp/demo-transport-scaveging.cpp
@@ -16,13 +16,13 @@
 // along with this library. If not, see <http://www.gnu.org/licenses/>.
 
 #include <Reaktoro/Reaktoro.hpp>
-#include <experimental/filesystem>
+#include "boost/filesystem.hpp"
 
 using namespace Reaktoro;
 
 int main()
 {
-    std::experimental::filesystem::create_directory("results_demo_transport_and_scaveging");
+    boost::filesystem::create_directory("results_demo_transport_and_scaveging");
 
     auto second = 1;
     auto minute = 60 * second;
