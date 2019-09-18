f5a3dfceea01504544fa6ba3bdd79a2bfe92af73
Armando Aguiar de Souza Cruz Neto <acruz@esss.co>
2019-09-18 08:16:31 -0300

    Add partial molar volumes on Chemical Properties (#117)
    * add partial molar volume
    
    * add tests on Chemical Properties for partial molar volumes
    
    * Update Reaktoro/Core/ChemicalProperties.hpp

--- Reaktoro/Core/ChemicalProperties-test.py
+++ Reaktoro/Core/ChemicalProperties-test.py
@@ -19,7 +19,7 @@
 import pytest
 
 import numpy as np
-from reaktoro import ChemicalProperties
+from reaktoro import ChemicalProperties, PhaseType
 
 
 def test_chemical_properties_subvolume(chemical_properties):
@@ -36,3 +36,34 @@ def test_chemical_properties_not_updated(chemical_system):
     with pytest.raises(RuntimeError, match=r"Cannot proceed with method ChemicalProperties::update."):
         chemical_properties.update(np.array([55, 1e-7, 1e-7, 0.1, 0.5, 0.01, 1.0, 0.001, 1.0]))
 
+
+def test_chemical_properties_standard_partial_volumes(chemical_system, chemical_properties):
+    standard_partial_volumes = chemical_properties.standardPartialMolarVolumes().val
+    for i in range(0, len(standard_partial_volumes)):
+        name = chemical_system.species(i).name()
+        if name == 'H+':
+            assert standard_partial_volumes[i] == 0.0
+        else:
+            assert standard_partial_volumes[i] > 0.0
+
+    only_updated_by_temperature_and_pressure = ChemicalProperties(chemical_system)
+    only_updated_by_temperature_and_pressure.update(chemical_properties.temperature().val, chemical_properties.pressure().val)
+    for pVol1, pVol2 in zip(only_updated_by_temperature_and_pressure.standardPartialMolarVolumes().val, standard_partial_volumes):
+        assert pVol1 == pVol2
+
+
+def test_chemical_properties_partial_volumes(chemical_system, chemical_properties):
+    partial_volumes = chemical_properties.partialMolarVolumes().val
+    for phase in chemical_system.phases():
+        phase_type = phase.type()
+        for species in phase.species():
+            index = chemical_system.indexSpecies(species.name())
+            if phase_type != PhaseType.Gas:
+                assert partial_volumes[index] == 0.0
+            else:
+                assert partial_volumes[index] > 0.0
+
+    only_updated_by_temperature_and_pressure = ChemicalProperties(chemical_system)
+    only_updated_by_temperature_and_pressure.update(chemical_properties.temperature().val, chemical_properties.pressure().val)
+    for pVol in only_updated_by_temperature_and_pressure.partialMolarVolumes().val:
+        assert pVol == 0.0
