cec3ce18f05af87caed3bd68a6188d566e4de4ad
Allan Leal <allan.leal@erdw.ethz.ch>
2019-10-26 08:43:24 +0200

    Update py bindings of EquilibtiumProblem following minor API changes

--- python/PyReaktoro/Equilibrium/PyEquilibriumProblem.cpp
+++ python/PyReaktoro/Equilibrium/PyEquilibriumProblem.cpp
@@ -27,40 +27,40 @@ namespace Reaktoro {
 
 void exportEquilibriumProblem(py::module& m)
 {
-    auto setElementAmounts1 = static_cast<EquilibriumProblem&(EquilibriumProblem::*)(VectorConstRef)>(&EquilibriumProblem::setElementAmounts);
-    auto setElementAmounts2 = static_cast<EquilibriumProblem&(EquilibriumProblem::*)(double)>(&EquilibriumProblem::setElementAmounts);
+    auto setElementAmounts1 = static_cast<void(EquilibriumProblem::*)(VectorConstRef)>(&EquilibriumProblem::setElementAmounts);
+    auto setElementAmounts2 = static_cast<void(EquilibriumProblem::*)(double)>(&EquilibriumProblem::setElementAmounts);
 
-    auto setElementAmount1 = static_cast<EquilibriumProblem&(EquilibriumProblem::*)(Index, double)>(&EquilibriumProblem::setElementAmount);
-    auto setElementAmount2 = static_cast<EquilibriumProblem&(EquilibriumProblem::*)(std::string, double)>(&EquilibriumProblem::setElementAmount);
+    auto setElementAmount1 = static_cast<void(EquilibriumProblem::*)(Index, double)>(&EquilibriumProblem::setElementAmount);
+    auto setElementAmount2 = static_cast<void(EquilibriumProblem::*)(std::string, double)>(&EquilibriumProblem::setElementAmount);
 
-    auto setTemperature1 = static_cast<EquilibriumProblem&(EquilibriumProblem::*)(double)>(&EquilibriumProblem::setTemperature);
-    auto setTemperature2 = static_cast<EquilibriumProblem&(EquilibriumProblem::*)(double, std::string)>(&EquilibriumProblem::setTemperature);
+    auto setTemperature1 = static_cast<void(EquilibriumProblem::*)(double)>(&EquilibriumProblem::setTemperature);
+    auto setTemperature2 = static_cast<void(EquilibriumProblem::*)(double, std::string)>(&EquilibriumProblem::setTemperature);
 
-    auto setPressure1 = static_cast<EquilibriumProblem&(EquilibriumProblem::*)(double)>(&EquilibriumProblem::setPressure);
-    auto setPressure2 = static_cast<EquilibriumProblem&(EquilibriumProblem::*)(double, std::string)>(&EquilibriumProblem::setPressure);
+    auto setPressure1 = static_cast<void(EquilibriumProblem::*)(double)>(&EquilibriumProblem::setPressure);
+    auto setPressure2 = static_cast<void(EquilibriumProblem::*)(double, std::string)>(&EquilibriumProblem::setPressure);
 
-    auto add1 = static_cast<EquilibriumProblem&(EquilibriumProblem::*)(std::string, double, std::string)>(&EquilibriumProblem::add);
-    auto add2 = static_cast<EquilibriumProblem&(EquilibriumProblem::*)(const ChemicalState&)>(&EquilibriumProblem::add);
+    auto add1 = static_cast<void(EquilibriumProblem::*)(std::string, double, std::string)>(&EquilibriumProblem::add);
+    auto add2 = static_cast<void(EquilibriumProblem::*)(const ChemicalState&)>(&EquilibriumProblem::add);
 
     py::class_<EquilibriumProblem>(m, "EquilibriumProblem")
         .def(py::init<const ChemicalSystem&>())
         .def(py::init<const Partition&>())
         .def(py::init<const EquilibriumProblem&>())
-        .def("setPartition", &EquilibriumProblem::setPartition, py::return_value_policy::reference_internal)
-        .def("setTemperature", setTemperature1, py::return_value_policy::reference_internal)
-        .def("setTemperature", setTemperature2, py::return_value_policy::reference_internal)
-        .def("setPressure", setPressure1, py::return_value_policy::reference_internal)
-        .def("setPressure", setPressure2, py::return_value_policy::reference_internal)
-        .def("setElementAmounts", setElementAmounts1, py::return_value_policy::reference_internal)
-        .def("setElementAmounts", setElementAmounts2, py::return_value_policy::reference_internal)
-        .def("setElementAmount", setElementAmount1, py::return_value_policy::reference_internal)
-        .def("setElementAmount", setElementAmount2, py::return_value_policy::reference_internal)
-        .def("setElectricalCharge", &EquilibriumProblem::setElectricalCharge, py::return_value_policy::reference_internal)
-        .def("add", add1, py::return_value_policy::reference_internal)
-        .def("add", add2, py::return_value_policy::reference_internal)
-        .def("addCompound", &EquilibriumProblem::addCompound, py::return_value_policy::reference_internal)
-        .def("addSpecies", &EquilibriumProblem::addSpecies, py::return_value_policy::reference_internal)
-        .def("addState", &EquilibriumProblem::addState, py::return_value_policy::reference_internal)
+        .def("setPartition", &EquilibriumProblem::setPartition)
+        .def("setTemperature", setTemperature1)
+        .def("setTemperature", setTemperature2)
+        .def("setPressure", setPressure1)
+        .def("setPressure", setPressure2)
+        .def("setElementAmounts", setElementAmounts1)
+        .def("setElementAmounts", setElementAmounts2)
+        .def("setElementAmount", setElementAmount1)
+        .def("setElementAmount", setElementAmount2)
+        .def("setElectricalCharge", &EquilibriumProblem::setElectricalCharge)
+        .def("add", add1)
+        .def("add", add2)
+        .def("addCompound", &EquilibriumProblem::addCompound)
+        .def("addSpecies", &EquilibriumProblem::addSpecies)
+        .def("addState", &EquilibriumProblem::addState)
         .def("system", &EquilibriumProblem::system, py::return_value_policy::reference_internal)
         .def("partition", &EquilibriumProblem::partition, py::return_value_policy::reference_internal)
         .def("temperature", &EquilibriumProblem::temperature)
