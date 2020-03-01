7c620bf9fdd5203dade4c6157f126bec2bb7af7d
Diego Tavares Volpatto <diego@esss.co>
2020-03-01 19:55:31 -0300

    Add pandas DataFrame extension and refactor for extensions

---
+++ python/reaktoro/_chemicaloutput.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd
+from reaktoro.PyReaktoro import ChemicalOutput
+
+
+def _to_array(self):
+    """
+    Define a method to convert the file data into an array.
+
+    :return:
+        An numpy array with data from ChemicalOutput.
+    :rtype numpy.ndarray:
+    """
+    output_array = lambda self: np.loadtxt(self.filename(), skiprows=1)
+    return output_array
+
+
+def _to_dict(self):
+    """
+    Define a method to convert the ChemicalOutput data in the file into a dictionary,
+    with the headings being the keys, and the column data as the values.
+
+    :return:
+        A dict with ChemicalOutput properties from result file.
+    :rtype dict:
+    """
+    data = np.loadtxt(self.filename(), skiprows=1)
+    columns = [data[:, i] for i in range(data.shape[1])]
+    return {heading: column for heading, column in zip(self.headings(), columns)}
+
+
+def _to_dataframe(self):
+    """
+    Convert ChemicalOutput data into a pandas DataFrame.
+
+    :return:
+        A pandas DataFrame with ChemicalOutput data.
+    :rtype pd.DataFrame:
+    """
+    output_dataframe = pd.DataFrame(self.dict())
+    return output_dataframe
+
+
+ChemicalOutput.array = _to_array
+ChemicalOutput.dict = _to_dict
+ChemicalOutput.DataFrame = _to_dataframe
