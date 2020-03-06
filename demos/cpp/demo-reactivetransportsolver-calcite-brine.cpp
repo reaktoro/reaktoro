dee36a70e87105f78be4c50aca5632ff7274d18a
Allan Leal <allanmulin@gmail.com>
2020-03-06 13:15:39 +0100

    Implement error control based on major species

--- demos/cpp/demo-reactivetransportsolver-calcite-brine.cpp
+++ demos/cpp/demo-reactivetransportsolver-calcite-brine.cpp
@@ -52,6 +52,8 @@ struct Params
     bool track_statistics;
     double smart_equlibrium_reltol;
     double smart_equlibrium_abstol;
+    double amount_fraction_cutoff;
+    double mole_fraction_cutoff;
 
     std::string activity_model;
 
@@ -134,6 +136,8 @@ int main()
     // Define parameters of the equilibrium solvers
     params.smart_equlibrium_reltol = 0.005;
     params.smart_equlibrium_abstol = 1e-8;
+    params.amount_fraction_cutoff = 1e-14;
+    params.mole_fraction_cutoff = 1e-14;
     params.activity_model = "hkf";
     //params.activity_model = "pitzer";
     params.track_statistics = true;
@@ -184,6 +188,9 @@ auto runReactiveTransport(const Params& params, Results& results) -> void
     SmartEquilibriumOptions smart_equilibrium_options;
     smart_equilibrium_options.reltol = params.smart_equlibrium_reltol;
     smart_equilibrium_options.abstol = params.smart_equlibrium_abstol;
+
+    smart_equilibrium_options.amount_fraction_cutoff = params.amount_fraction_cutoff;
+    smart_equilibrium_options.mole_fraction_cutoff = params.mole_fraction_cutoff;
 
     // Step **: Construct the chemical system with its phases and species (using ChemicalEditor)
     ChemicalEditor editor;
