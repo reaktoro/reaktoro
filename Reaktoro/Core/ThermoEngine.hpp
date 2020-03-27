e510c98be2e8f4b6d697b765a686907ded693ba3
Allan Leal <allanmulin@gmail.com>
2020-03-27 08:59:37 +0100

    ThermoEngine is no longer a pure abstract base class.

--- Reaktoro/Core/ThermoEngine.hpp
+++ Reaktoro/Core/ThermoEngine.hpp
@@ -18,67 +18,38 @@
 #pragma once
 
 // Reaktoro includes
-#include <Reaktoro/Common/ThermoScalar.hpp>
 #include <Reaktoro/Core/Database.hpp>
+#include <Reaktoro/Core/StandardThermoModel.hpp>
 
 namespace Reaktoro {
 
-// Forward declarations
-class Species;
-
-/// A type to store the standard thermodynamic properties of a single species.
-struct StandardThermoProps
-{
-    /// The standard molar Gibbs energy @f$G^{\circ}@f$ of the species (in unit of J/mol)
-    ThermoScalar G0;
-
-    /// The standard molar Helmholtz energy @f$A^{\circ}@f$ of the species (in unit of J/mol)
-    ThermoScalar A0;
-
-    /// The standard molar internal energy @f$U^{\circ}@f$ of the species (in unit of J/mol)
-    ThermoScalar U0;
-
-    /// The standard molar enthalpy @f$H^{\circ}@f$ of the species (in unit of J/mol)
-    ThermoScalar H0;
-
-    /// The standard molar entropy @f$S^{\circ}@f$ of the species (in unit of J/K)
-    ThermoScalar S0;
-
-    /// The standard molar volume @f$V^{\circ}@f$ of the species (in unit of m3/mol)
-    ThermoScalar V0;
-
-    /// The standard molar isobaric heat capacity @f$C_{P}^{\circ}@f$ of the species (in unit of J/(mol·K))
-    ThermoScalar Cp0;
-
-    /// The standard molar isochoric heat capacity @f$C_{V}^{\circ}@f$ of the species (in unit of J/(mol·K))
-    ThermoScalar Cv0;
-};
-
-/// A base type defining the interface for calculation of standard thermodynamic properties.
+/// The thermodynamic engine base class for standard thermodynamic properties calculation.
 class ThermoEngine
 {
 public:
     /// Construct a ThermoEngine object.
-    explicit ThermoEngine(const Database& database);
+    /// @param db The thermodynamic database
+    /// @param model The standard thermodynamic model function
+    ThermoEngine(const Database& db, const StandardThermoModelFn& model);
 
-    /// Return the database in this thermodynamic engine.
+    /// Return the database of this thermodynamic engine.
     auto database() const -> const Database&;
+
+    /// Return the standard thermodynamic model function of this thermodynamic engine.
+    auto standardThermoModelFn() const -> const StandardThermoModelFn&;
 
     /// Return the standard thermodynamic properties of a species at given temperature and pressure.
     /// @param T The temperature for the calculation (in unit of K)
     /// @param P The pressure for the calculation (in unit of Pa)
     /// @param species The species object.
-    virtual auto standardThermoProps(Temperature T, Pressure P, const Species& species) const -> StandardThermoProps = 0;
-
-    /// Return the standard thermodynamic properties of multiple species at given temperature and pressure.
-    /// @param T The temperature for the calculation (in unit of K)
-    /// @param P The pressure for the calculation (in unit of Pa)
-    /// @param species The species object.
-    virtual auto standardThermoProps(Temperature T, Pressure P, const std::vector<Species>& species) const -> std::vector<StandardThermoProps>;
+    auto standardThermoProps(Temperature T, Pressure P, const Species& species) const -> StandardThermoProps;
 
 private:
-    /// The thermodynamic database
+    /// The thermodynamic database of this thermodynamic engine
     Database db;
+
+    /// The standard thermodynamic model function of this thermodynamic engine
+    StandardThermoModelFn model;
 };
 
 } // namespace Reaktoro
