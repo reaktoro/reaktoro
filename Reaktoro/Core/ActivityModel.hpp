4b4ed2873b7d69ded9eaa43a4b05233d94a2eb5e
Allan Leal <allanmulin@gmail.com>
2020-03-27 08:58:27 +0100

    Add ActivitModel header file.

---
+++ Reaktoro/Core/ActivityModel.hpp
@@ -0,0 +1,61 @@
+// Reaktoro is a unified framework for modeling chemically reactive systems.
+//
+// Copyright (C) 2014-2018 Allan Leal
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library. If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+// C++ includes
+#include <functional>
+
+// Reaktoro includes
+#include <Reaktoro/Common/ChemicalScalar.hpp>
+#include <Reaktoro/Common/ChemicalVector.hpp>
+
+namespace Reaktoro {
+
+/// The activity and excess thermodynamic properties of a phase.
+/// @see ActivityModelFn, StandardThermoModelFn, StandardThermoProps
+struct ActivityProps
+{
+    /// The activity coefficients (natural log) of the species in the phase.
+    ChemicalVector ln_activity_coefficients;
+
+    /// The activities (natural log) of the species in the phase.
+    ChemicalVector ln_activities;
+
+    /// The partial molar volumes of the species in the phase (in units of m3/mol).
+    ChemicalVector partial_molar_volumes;
+
+    /// The molar volume of the phase (in units of m3/mol).
+    ChemicalScalar molar_volume;
+
+    /// The excess molar Gibbs energy of the phase (in units of J/mol).
+    ChemicalScalar excess_molar_gibbs_energy;
+
+    /// The excess molar enthalpy of the phase (in units of J/mol).
+    ChemicalScalar excess_molar_enthalpy;
+
+    /// The excess molar isobaric heat capacity of the phase (in units of J/(mol*K)).
+    ChemicalScalar excess_molar_heat_capacity_cp;
+
+    /// The excess molar isochoric heat capacity of the phase (in units of J/(mol*K)).
+    ChemicalScalar excess_molar_heat_capacity_cv;
+};
+
+/// The function type for the activity model of a phase.
+using ActivityModelFn = std::function<ActivityProps(Temperature, Pressure, VectorConstRef)>;
+
+} // namespace Reaktoro
