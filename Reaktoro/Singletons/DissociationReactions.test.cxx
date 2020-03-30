89fff370a919c71ad25de7c3b2a470d224c1ad18
Allan Leal <allanmulin@gmail.com>
2020-03-30 16:36:01 +0200

    Implemented singleton DissociationReactions and its tests

---
+++ Reaktoro/Singletons/DissociationReactions.test.cxx
@@ -0,0 +1,41 @@
+// Reaktoro is a unified framework for modeling chemically reactive systems.
+//
+// Copyright (C) 2014-2018 Allan Leal
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library. If not, see <http://www.gnu.org/licenses/>.
+
+// Catch includes
+#include <catch2/catch.hpp>
+
+// Reaktoro includes
+#include <Reaktoro/Singletons/DissociationReactions.hpp>
+using namespace Reaktoro;
+
+TEST_CASE("Testing DissociationReactions", "[DissociationReactions]")
+{
+    REQUIRE(DissociationReactions::size() == DissociationReactions::reactions().size());
+
+    REQUIRE(DissociationReactions::get("NaCl").has_value());
+    REQUIRE(DissociationReactions::get("CaCO3").has_value());
+    REQUIRE(DissociationReactions::get("Ca(CO3)").has_value());
+
+    DissociationReactions::append({ "NaKCl2", {{1, "Na+"}, {1, "K+"}, {2, "Cl-"}} });
+
+    REQUIRE(DissociationReactions::get("NaKClCl2").has_value());
+
+    DissociationReactions::append({ "NaCl", {} }); // Remove ions in NaCl dissociation reactions
+
+    REQUIRE(DissociationReactions::get("NaCl").has_value());
+    REQUIRE(DissociationReactions::get("NaCl").value().ions.size() == 0);
+}
