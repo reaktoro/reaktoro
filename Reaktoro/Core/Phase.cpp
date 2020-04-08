b88787e5fc65ce24dc7e13e0438e4d22f8da41c4
Allan Leal <allan.leal@erdw.ethz.ch>
2020-04-08 17:49:28 +0200

    Phase now has method props to compute ChemicalPropsPhase object

--- Reaktoro/Core/Phase.cpp
+++ Reaktoro/Core/Phase.cpp
@@ -18,13 +18,21 @@
 #include "Phase.hpp"
 
 // Reaktoro includes
-#include <Reaktoro/Common/Constants.hpp>
+#include <Reaktoro/Common/Algorithms.hpp>
 #include <Reaktoro/Common/Exception.hpp>
-#include <Reaktoro/Common/SetUtils.hpp>
-#include <Reaktoro/Common/StringUtils.hpp>
-#include <Reaktoro/Core/Utils.hpp>
 
 namespace Reaktoro {
+namespace detail {
+
+/// Return the molar masses of the species
+auto molarMasses(const SpeciesList& species)
+{
+    ArrayXd molar_masses(species.size());
+    transform(species, molar_masses, [](auto&& s) { return s.molarMass(); });
+    return molar_masses;
+}
+
+} // namespace detail
 
 struct Phase::Impl
 {
@@ -42,14 +50,115 @@ struct Phase::Impl
 
     /// The activity model of the phase.
     ActivityModelFn activity_model_fn;
+
+    /// The molar masses of the species in the phase.
+    ArrayXd species_molar_masses;
+
+    /// Evaluate the mole fractions of the species in the phase.
+    auto evalMoleFractions(ChemicalPropsPhaseRef props, ArrayXrConstRef n) const -> void
+    {
+        auto&& data = props.data();
+        auto& amount = data.amount;
+        auto& x = data.x;
+        amount = n.sum();
+        const auto size = species.size();
+        assert(x.size() == size);
+        if(amount == 0.0)
+            x = (size == 1) ? 1.0 : 0.0;
+        else x = n/amount;
+    }
+
+    /// Evaluate the activity properties of the phase.
+    auto evalActivityProps(ChemicalPropsPhaseRef props) const -> void
+    {
+        auto&& data  = props.data();
+
+        auto& T      = data.T;
+        auto& P      = data.P;
+        auto& amount = data.amount;
+        auto& x      = data.x;
+        auto& Vex    = data.Vex;
+        auto& VexT   = data.VexT;
+        auto& VexP   = data.VexP;
+        auto& Gex    = data.Gex;
+        auto& Hex    = data.Hex;
+        auto& Cpex   = data.Cpex;
+        auto& Cvex   = data.Cvex;
+        auto& ln_g   = data.ln_g;
+        auto& ln_a   = data.ln_a;
+
+        const auto size = species.size();
+        assert(ln_g.size() == size);
+        assert(ln_a.size() == size);
+
+        if(amount == 0.0)
+        {
+            Vex = VexT = VexP = Gex = Hex = Cpex = Cvex = 0.0;
+            ln_g = ln_a = 0.0;
+        }
+        else
+        {
+            activity_model_fn({Vex, VexT, VexP, Gex, Hex, Cpex, Cvex, ln_g, ln_a }, T, P, x);
+        }
+    }
+
+    /// Evaluate the standard thermodynamic properties of the species in the phase.
+    auto evalStandardThermoProps(ChemicalPropsPhaseRef props) const -> void
+    {
+        auto&& data  = props.data();
+        auto& T      = data.T;
+        auto& P      = data.P;
+        auto& G0     = data.G0;
+        auto& H0     = data.H0;
+        auto& V0     = data.V0;
+        auto& Cp0    = data.Cp0;
+        auto& Cv0    = data.Cv0;
+
+        const auto size = species.size();
+        assert(G0.size() == size);
+        assert(H0.size() == size);
+        assert(V0.size() == size);
+        assert(Cp0.size() == size);
+        assert(Cv0.size() == size);
+
+        StandardThermoProps stdprops;
+        for(auto i = 0; i < size; ++i)
+        {
+            stdprops = standard_thermo_model_fn(T, P, species[i]);
+            G0[i]  = stdprops.G0;
+            H0[i]  = stdprops.H0;
+            V0[i]  = stdprops.V0;
+            Cp0[i] = stdprops.Cp0;
+            Cv0[i] = stdprops.Cv0;
+        }
+    }
+
+    /// Evaluate the chemical properties of the phase.
+    auto eval(ChemicalPropsPhaseRef props, real T, real P, ArrayXrConstRef n) const -> void
+    {
+        auto&& data = props.data();
+
+        const auto updatingT = data.T != T;
+        const auto updatingP = data.P != P;
+        const auto updatingN = (data.n != n).all();
+
+        if(updatingT || updatingP)
+        {
+            data.T = T;
+            data.P = P;
+            evalStandardThermoProps(props);
+        }
+        if(updatingT || updatingP || updatingN)
+        {
+            data.n = n;
+            evalMoleFractions(props, n);
+            evalActivityProps(props);
+        }
+    }
 };
 
 Phase::Phase()
 : pimpl(new Impl())
-{}
-
-Phase::Phase(const Phase& other)
-: pimpl(new Impl(*other.pimpl))
 {}
 
 auto Phase::withName(std::string name) -> Phase
@@ -63,6 +172,7 @@ auto Phase::withSpecies(SpeciesList species) -> Phase
 {
     Phase copy = clone();
     copy.pimpl->species = std::move(species);
+    copy.pimpl->species_molar_masses = detail::molarMasses(copy.pimpl->species);
     return copy;
 }
 
@@ -117,32 +227,16 @@ auto Phase::activityModel() const -> const ActivityModelFn&
     return pimpl->activity_model_fn;
 }
 
-auto Phase::activityProps(real T, real P, VectorXrConstRef n) const -> ActivityProps
+auto Phase::props(real T, real P, ArrayXrConstRef n) const -> ChemicalPropsPhase
 {
-    assert(activityModel());
-    ActivityProps props;
-    props.ln_g.resize(species().size());
-    props.ln_a.resize(species().size());
-    activityModel()(props, T, P, n);
-    return props;
-}
-
-auto Phase::standardThermoProps(real T, real P, Index ispecies) const -> StandardThermoProps
-{
-    assert(standardThermoModel());
-    return standardThermoModel()(T, P, species(ispecies));
-}
-
-auto Phase::props(real T, real P, VectorXrConstRef n) const -> ChemicalPropsPhase
-{
-    ChemicalPropsPhase res;
+    ChemicalPropsPhase res(species().size());
     eval(res, T, P, n);
     return res;
 }
 
-auto Phase::eval(ChemicalPropsPhaseRef props, real T, real P, VectorXrConstRef n) const -> void
+auto Phase::eval(ChemicalPropsPhaseRef props, real T, real P, ArrayXrConstRef n) const -> void
 {
-
+    pimpl->eval(props, T, P, n);
 }
 
 auto Phase::clone() const -> Phase
