eed94b8f264579a62927bbdb8a75efa4bcd6f737
Allan Leal <allan.leal@erdw.ethz.ch>
2020-04-09 10:56:57 +0200

    Implemented ChemicalProps class, containing all chemical properties of phases and species in a chemical system

---
+++ Reaktoro/Core/ChemicalProps.cpp
@@ -0,0 +1,196 @@
+// Reaktoro is a unified framework for modeling chemically reactive phases.
+//
+// Copyright (C) 2014-2018 Allan Leal
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library. If not, see <http://www.gnu.org/licenses/>.
+
+#include "ChemicalProps.hpp"
+
+// Reaktoro includes
+#include <Reaktoro/Core/ChemicalPropsPhase.hpp>
+
+namespace Reaktoro {
+
+ChemicalProps::ChemicalProps(const ChemicalSystem& system)
+: sys(system)
+{
+    const auto numspecies = sys.species().size();
+    const auto numphases = sys.phases().size();
+
+    props.n      = ArrayXr::Zero(numspecies);
+    props.amount = ArrayXr::Zero(numphases);
+    props.x      = ArrayXr::Zero(numspecies);
+    props.G0     = ArrayXr::Zero(numspecies);
+    props.H0     = ArrayXr::Zero(numspecies);
+    props.V0     = ArrayXr::Zero(numspecies);
+    props.Cp0    = ArrayXr::Zero(numspecies);
+    props.Cv0    = ArrayXr::Zero(numspecies);
+    props.Vex    = ArrayXr::Zero(numphases);
+    props.VexT   = ArrayXr::Zero(numphases);
+    props.VexP   = ArrayXr::Zero(numphases);
+    props.Gex    = ArrayXr::Zero(numphases);
+    props.Hex    = ArrayXr::Zero(numphases);
+    props.Cpex   = ArrayXr::Zero(numphases);
+    props.Cvex   = ArrayXr::Zero(numphases);
+    props.ln_g   = ArrayXr::Zero(numspecies);
+    props.ln_a   = ArrayXr::Zero(numspecies);
+}
+
+ChemicalProps::ChemicalProps(const ChemicalSystem& system, const ChemicalPropsData& data)
+: sys(system), props(data)
+{}
+
+auto ChemicalProps::system() const -> const ChemicalSystem&
+{    return sys;
+}
+
+auto ChemicalProps::data() const -> const ChemicalPropsData&
+{    return props;
+}
+
+auto ChemicalProps::phase(Index idx) const -> ChemicalPropsPhaseConstRef
+{
+    const auto phase = sys.phase(idx);
+    const auto begin = sys.indexFirstSpeciesInPhase(idx);
+    const auto size = phase.species().size();
+
+    return ChemicalPropsPhaseConstRef(phase, {
+        props.T,
+        props.P,
+        props.n.segment(begin, size),
+        props.amount[idx],
+        props.x.segment(begin, size),
+        props.G0.segment(begin, size),
+        props.H0.segment(begin, size),
+        props.V0.segment(begin, size),
+        props.Cp0.segment(begin, size),
+        props.Cv0.segment(begin, size),
+        props.Vex[idx],
+        props.VexT[idx],
+        props.VexP[idx],
+        props.Gex[idx],
+        props.Hex[idx],
+        props.Cpex[idx],
+        props.Cvex[idx],
+        props.ln_g.segment(begin, size),
+        props.ln_a.segment(begin, size)
+    });
+}
+
+auto ChemicalProps::phase(Index idx) -> ChemicalPropsPhaseRef
+{
+    const auto phase = sys.phase(idx);
+    const auto begin = sys.indexFirstSpeciesInPhase(idx);
+    const auto size = phase.species().size();
+
+    return ChemicalPropsPhaseRef(phase, {
+        props.T,
+        props.P,
+        props.n.segment(begin, size),
+        props.amount[idx],
+        props.x.segment(begin, size),
+        props.G0.segment(begin, size),
+        props.H0.segment(begin, size),
+        props.V0.segment(begin, size),
+        props.Cp0.segment(begin, size),
+        props.Cv0.segment(begin, size),
+        props.Vex[idx],
+        props.VexT[idx],
+        props.VexP[idx],
+        props.Gex[idx],
+        props.Hex[idx],
+        props.Cpex[idx],
+        props.Cvex[idx],
+        props.ln_g.segment(begin, size),
+        props.ln_a.segment(begin, size)
+    });
+}
+
+auto ChemicalProps::temperature() const -> real
+{
+    return props.T;
+}
+
+auto ChemicalProps::pressure() const -> real
+{
+    return props.P;
+}
+
+auto ChemicalProps::speciesAmounts() const -> ArrayXrConstRef
+{
+    return props.n;
+}
+
+auto ChemicalProps::moleFractions() const -> ArrayXrConstRef
+{
+    return props.x;
+}
+
+auto ChemicalProps::lnActivityCoefficients() const -> ArrayXrConstRef
+{
+    return props.ln_g;
+}
+
+auto ChemicalProps::lnActivities() const -> ArrayXrConstRef
+{
+    return props.ln_a;
+}
+
+auto ChemicalProps::chemicalPotentials() const -> ArrayXr
+{
+    const auto R = universalGasConstant;
+    return props.G0 + R*props.T * props.ln_a;
+}
+
+auto ChemicalProps::standardGibbsEnergies() const -> ArrayXrConstRef
+{
+    return props.G0;
+}
+
+auto ChemicalProps::standardEnthalpies() const -> ArrayXrConstRef
+{
+    return props.H0;
+}
+
+auto ChemicalProps::standardVolumes() const -> ArrayXrConstRef
+{
+    return props.V0;
+}
+
+auto ChemicalProps::standardEntropies() const -> ArrayXr
+{
+    return (props.H0 - props.G0)/props.T; // from G0 = H0 - T*S0
+}
+
+auto ChemicalProps::standardInternalEnergies() const -> ArrayXr
+{
+    return props.H0 - props.P * props.V0; // from H0 = U0 + P*V0
+}
+
+auto ChemicalProps::standardHelmholtzEnergies() const -> ArrayXr
+{
+    return props.G0 - props.P * props.V0; // from A0 = U0 - T*S0 = (H0 - P*V0) + (G0 - H0) = G0 - P*V0
+}
+
+auto ChemicalProps::standardHeatCapacitiesConstP() const -> ArrayXrConstRef
+{
+    return props.Cp0;
+}
+
+auto ChemicalProps::standardHeatCapacitiesConstV() const -> ArrayXrConstRef
+{
+    return props.Cv0;
+}
+
+} // namespace Reaktoro
---
+++ Reaktoro/Core/ChemicalProps.hpp
@@ -0,0 +1,164 @@
+// Reaktoro is a unified framework for modeling chemically reactive phases.
+//
+// Copyright (C) 2014-2018 Allan Leal
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library. If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+// Reaktoro includes
+#include <Reaktoro/Core/ChemicalSystem.hpp>
+#include <Reaktoro/Core/ChemicalPropsPhase.hpp>
+#include <Reaktoro/Math/Matrix.hpp>
+
+namespace Reaktoro {
+
+/// The primary chemical property data of the phases and species in a chemical system.
+/// @see ChemicalProps
+struct ChemicalPropsData
+{
+    /// The temperature of the system (in K).
+    real T = {};
+
+    /// The pressure of the system (in Pa).
+    real P = {};
+
+    /// The amounts of each species in the system (in mol).
+    ArrayXr n;
+
+    /// The sum of species amounts in each phase of the system (in mol).
+    ArrayXr amount;
+
+    /// The mole fractions of the species in the system (in mol/mol).
+    ArrayXr x;
+
+    /// The standard molar Gibbs energies of the species in the system (in J/mol).
+    ArrayXr G0;
+
+    /// The standard molar enthalpies of the species in the system (in J/mol).
+    ArrayXr H0;
+
+    /// The standard molar volumes of the species in the system (in m3/mol).
+    ArrayXr V0;
+
+    /// The standard molar isobaric heat capacities of the species in the system (in J/(mol·K)).
+    ArrayXr Cp0;
+
+    /// The standard molar isochoric heat capacities of the species in the system (in J/(mol·K)).
+    ArrayXr Cv0;
+
+    /// The excess molar volume of each phase in the system (in m3/mol).
+    ArrayXr Vex;
+
+    /// The temperature derivative at constant pressure of the excess molar volume of each phase in the system (in m3/(mol*K)).
+    ArrayXr VexT;
+
+    /// The pressure derivative at constant temperature of the excess molar volume of each phase in the system (in m3/(mol*Pa)).
+    ArrayXr VexP;
+
+    /// The excess molar Gibbs energy of each phase in the system (in units of J/mol).
+    ArrayXr Gex;
+
+    /// The excess molar enthalpy of each phase in the system (in units of J/mol).
+    ArrayXr Hex;
+
+    /// The excess molar isobaric heat capacity of each phase in the system (in units of J/(mol*K)).
+    ArrayXr Cpex;
+
+    /// The excess molar isochoric heat capacity of each phase in the system (in units of J/(mol*K)).
+    ArrayXr Cvex;
+
+    /// The activity coefficients (natural log) of the species in the system.
+    ArrayXr ln_g;
+
+    /// The activities (natural log) of the species in the system.
+    ArrayXr ln_a;
+};
+
+/// The chemical properties of the phases and species in a chemical system.
+class ChemicalProps
+{
+public:
+    /// Construct a ChemicalProps object.
+    explicit ChemicalProps(const ChemicalSystem& system);
+
+    /// Construct a ChemicalProps object.
+    ChemicalProps(const ChemicalSystem& system, const ChemicalPropsData& data);
+
+    /// Return the chemical system associated with these chemical properties.
+    auto system() const -> const ChemicalSystem&;
+
+    /// Return the primary chemical property data of the system from which others are calculated.
+    auto data() const -> const ChemicalPropsData&;
+
+    /// Return the chemical properties of a phase with given index.
+    auto phase(Index idx) const -> ChemicalPropsPhaseConstRef;
+
+    /// Return the chemical properties of a phase with given index.
+    auto phase(Index idx) -> ChemicalPropsPhaseRef;
+
+    /// Return the temperature of the system (in K).
+    auto temperature() const -> real;
+
+    /// Return the pressure of the system (in Pa).
+    auto pressure() const -> real;
+
+    /// Return the amounts of each species in the system (in mol).
+    auto speciesAmounts() const -> ArrayXrConstRef;
+
+    /// Return the mole fractions of the species in the system.
+    auto moleFractions() const -> ArrayXrConstRef;
+
+    /// Return the ln activity coefficients of the species in the system.
+    auto lnActivityCoefficients() const -> ArrayXrConstRef;
+
+    /// Return the ln activities of the species in the system.
+    auto lnActivities() const -> ArrayXrConstRef;
+
+    /// Return the chemical potentials of the species in the system (in J/mol).
+    auto chemicalPotentials() const -> ArrayXr;
+
+    /// Return the standard partial molar Gibbs energies of the species in the system (in J/mol).
+    auto standardGibbsEnergies() const -> ArrayXrConstRef;
+
+    /// Return the standard partial molar enthalpies of the species in the system (in J/mol).
+    auto standardEnthalpies() const -> ArrayXrConstRef;
+
+    /// Return the standard partial molar volumes of the species in the system (in m3/mol).
+    auto standardVolumes() const -> ArrayXrConstRef;
+
+    /// Return the standard partial molar entropies of the species in the system (in J/(mol*K)).
+    auto standardEntropies() const -> ArrayXr;
+
+    /// Return the standard partial molar internal energies of the species in the system (in J/mol).
+    auto standardInternalEnergies() const -> ArrayXr;
+
+    /// Return the standard partial molar Helmholtz energies of the species in the system (in J/mol).
+    auto standardHelmholtzEnergies() const -> ArrayXr;
+
+    /// Return the standard partial molar isobaric heat capacities of the species in the system (in J/(mol*K)).
+    auto standardHeatCapacitiesConstP() const -> ArrayXrConstRef;
+
+    /// Return the standard partial molar isochoric heat capacities of the species in the system (in J/(mol*K)).
+    auto standardHeatCapacitiesConstV() const -> ArrayXrConstRef;
+
+private:
+    /// The chemical system associated with these chemical properties.
+    ChemicalSystem sys;
+
+    /// The primary chemical property data of the system from which others are calculated.
+    ChemicalPropsData props;
+};
+
+} // namespace Reaktoro
