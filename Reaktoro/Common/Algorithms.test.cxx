435f18e9e737ea9539bd67ed3f4f930653b2686a
Allan Leal <allan.leal@erdw.ethz.ch>
2020-04-16 16:02:06 +0200

    Implemented tests for all methods in Algorithms + docs

--- Reaktoro/Common/Algorithms.test.cxx
+++ Reaktoro/Common/Algorithms.test.cxx
@@ -20,100 +20,130 @@
 
 // Reaktoro includes
 #include <Reaktoro/Common/Algorithms.hpp>
+#include <Reaktoro/Common/Types.hpp>
 using namespace Reaktoro;
 
 TEST_CASE("Testing Algorithms", "[Algorithms]")
 {
-    std::vector<int> nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    std::vector<int> res;
+    Vec<int> nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    Vec<int> res;
+    Strings strs;
 
-    // Test the function index(container, value)
+    //-------------------------------------------------------------------------
+    // TESTING METHOD: index
+    //-------------------------------------------------------------------------
     for(auto n : nums)
         REQUIRE( index(nums, n) == n - 1);
 
-    REQUIRE( index(nums, 1000) == -1 );
+    REQUIRE( index(nums, 1000) >= nums.size() );
 
-    // Test the function indexfn(container, pred)
+    //-------------------------------------------------------------------------
+    // TESTING METHOD: indexfn
+    //-------------------------------------------------------------------------
     for(auto n : nums)
-        REQUIRE( indexfn(nums, [=](auto x) { return x == n; } ) == n - 1);
+        REQUIRE( indexfn(nums, lambda(x, x == n)) == n - 1 );
 
-    REQUIRE( indexfn(nums, [=](auto x) { return x == 1000; } ) == -1 );
+    REQUIRE( indexfn(nums, lambda(x, x == 1000)) >= nums.size() );
 
-    // Test the filter function
-    res = filter(nums, [](auto x) { return x % 2 == 0; });
+    //-------------------------------------------------------------------------
+    // TESTING METHOD: filter
+    //-------------------------------------------------------------------------
+    res = filter(nums, lambda(x, x % 2 == 0));
+    REQUIRE( res == Vec<int>{2, 4, 6, 8} );
 
-    REQUIRE( res.size() == 4 );
-    REQUIRE( res[0] == 2 );
-    REQUIRE( res[1] == 4 );
-    REQUIRE( res[2] == 6 );
-    REQUIRE( res[3] == 8 );
+    //-------------------------------------------------------------------------
+    // TESTING METHOD: remove
+    //-------------------------------------------------------------------------
+    res = remove(nums, lambda(x, x % 2 == 1));
+    REQUIRE( res == Vec<int>{2, 4, 6, 8} );
 
-    // Test the remove function
-    res = remove(nums, [](auto x) { return x % 2 == 1; });
+    //-------------------------------------------------------------------------
+    // TESTING METHOD: unique
+    //-------------------------------------------------------------------------
+    res = unique(Vec<int>{ 3, 2, 3, 6, 8, 2, 5 });
+    REQUIRE( res == Vec<int>{ 2, 3, 5, 6, 8 } );
 
-    REQUIRE( res.size() == 4 );
-    REQUIRE( res[0] == 2 );
-    REQUIRE( res[1] == 4 );
-    REQUIRE( res[2] == 6 );
-    REQUIRE( res[3] == 8 );
+    //-------------------------------------------------------------------------
+    // TESTING METHOD: vectorize
+    //-------------------------------------------------------------------------
+    strs = vectorize(Map<String, int>{{"C", 1}, {"A", 2}, {"B", 3}}, lambda(x, x.first));
 
-    // Test the unique function
-    std::vector<int> notunique = { 3, 2, 3, 6, 8, 2, 5 };
+    REQUIRE( strs.size() == 3 );
+    REQUIRE( contains(strs, "A") );
+    REQUIRE( contains(strs, "B") );
+    REQUIRE( contains(strs, "C") );
 
-    res = unique(notunique);
-
-    REQUIRE( res.size() == 5 );
-    REQUIRE( res[0] == 2 );
-    REQUIRE( res[1] == 3 );
-    REQUIRE( res[2] == 5 );
-    REQUIRE( res[3] == 6 );
-    REQUIRE( res[4] == 8 );
-
-    // Test the vectorize function
-    std::unordered_map<std::string, int> map { {"C", 1}, {"A", 2}, {"B", 3} };
-
-    res = vectorize(map, lambda(x, x.second));
+    res = vectorize(Map<String, int>{{"C", 1}, {"A", 2}, {"B", 3}}, lambda(x, x.second));
 
     REQUIRE( res.size() == 3 );
     REQUIRE( contains(res, 1) );
     REQUIRE( contains(res, 2) );
     REQUIRE( contains(res, 3) );
 
-    // Test the merge function
-    std::vector<int> vec1 = { 3, 2, 1 };
-    std::vector<int> vec2 = { 5, 2, 3, 4 };
+    //-------------------------------------------------------------------------
+    // TESTING METHOD: merge
+    //-------------------------------------------------------------------------
+    res = merge(Vec<int>{ 3, 2, 1 }, Vec<int>{ 5, 2, 3, 4 });
+    REQUIRE( res == Vec<int>{1, 2, 3, 4, 5} );
 
-    res = merge(vec1, vec2);
+    res = merge(Vec<int>{ 5, 2, 3, 4 }, Vec<int>{ 3, 2, 1 } );
+    REQUIRE( res == Vec<int>{1, 2, 3, 4, 5} );
 
-    REQUIRE( res.size() == 5 );
-    REQUIRE( res[0] == 1 );
-    REQUIRE( res[1] == 2 );
-    REQUIRE( res[2] == 3 );
-    REQUIRE( res[3] == 4 );
-    REQUIRE( res[4] == 5 );
+    res = merge(Vec<int>{ 1, 2, 3, 4 }, Vec<int>{ 7, 6, 5 } );
+    REQUIRE( res == Vec<int>{1, 2, 3, 4, 5, 6, 7} );
 
-    // Test the contains function
-    REQUIRE( contains(nums, 3) );
-    REQUIRE_FALSE( contains(nums, 10) );
+    res = merge(Vec<int>{ 1, 2, 3, 4 }, Vec<int>{} );
+    REQUIRE( res == Vec<int>{1, 2, 3, 4} );
 
-    // Test the containsfn function
-    REQUIRE( containsfn(nums, [](auto x) { return x == 5; }) );
-    REQUIRE_FALSE( containsfn(nums, [](auto x) { return x == 13; }) );
+    res = merge(Vec<int>{}, Vec<int>{1, 2, 3, 4} );
+    REQUIRE( res == Vec<int>{1, 2, 3, 4} );
 
-    // Test the contained function
-    std::vector<int> a = {1, 2, 3, 4, 5};
-    std::vector<int> b = {2, 4};
-    std::vector<int> c = {4, 6};
-    std::vector<int> d = {5, 7};
-    std::vector<int> e = {};
+    res = merge(Vec<int>{}, Vec<int>{} );
+    REQUIRE( res == Vec<int>{} );
 
-    REQUIRE( contained(a, a) );
-    REQUIRE( contained(b, a) );
-    REQUIRE_FALSE( contained(c, a) );
+    //-------------------------------------------------------------------------
+    // TESTING METHOD: contains
+    //-------------------------------------------------------------------------
+    REQUIRE(  contains(Vec<int>{1, 2, 3}, 3) );
+    REQUIRE( !contains(Vec<int>{1, 2, 3}, 5) );
 
-    REQUIRE( disjoint(b, d) );
-    REQUIRE( disjoint(b, e) );
-    REQUIRE( disjoint(e, e) );
-    REQUIRE_FALSE( disjoint(b, b) );
-    REQUIRE_FALSE( disjoint(b, c) );
+    REQUIRE(  contains(Strings{"1", "2", "3"}, "3") );
+    REQUIRE( !contains(Strings{"1", "2", "3"}, "5") );
+
+    //-------------------------------------------------------------------------
+    // TESTING METHOD: containsfn
+    //-------------------------------------------------------------------------
+    REQUIRE(  containsfn(Vec<int>{1, 2, 5}, lambda(x, x == 5)) );
+    REQUIRE( !containsfn(Vec<int>{1, 2, 5}, lambda(x, x == 3)) );
+
+    //-------------------------------------------------------------------------
+    // TESTING METHOD: contained
+    //-------------------------------------------------------------------------
+    REQUIRE(  contained(Vec<int>{},        Vec<int>{1, 2, 3, 4, 5}) );
+    REQUIRE(  contained(Vec<int>{1},       Vec<int>{1, 2, 3, 4, 5}) );
+    REQUIRE(  contained(Vec<int>{5, 3},    Vec<int>{1, 2, 3, 4, 5}) );
+    REQUIRE(  contained(Vec<int>{2, 4, 5}, Vec<int>{1, 2, 3, 4, 5}) );
+    REQUIRE( !contained(Vec<int>{6},       Vec<int>{1, 2, 3, 4, 5}) );
+    REQUIRE( !contained(Vec<int>{2, 9, 5}, Vec<int>{1, 2, 3, 4, 5}) );
+
+    //-------------------------------------------------------------------------
+    // TESTING METHOD: disjoint
+    //-------------------------------------------------------------------------
+    REQUIRE(  disjoint(Vec<int>{},        Vec<int>{1, 2, 3, 4, 5}) );
+    REQUIRE(  disjoint(Vec<int>{6},       Vec<int>{1, 2, 3, 4, 5}) );
+    REQUIRE(  disjoint(Vec<int>{6, 8},    Vec<int>{1, 2, 3, 4, 5}) );
+    REQUIRE(  disjoint(Vec<int>{6, 8, 9}, Vec<int>{1, 2, 3, 4, 5}) );
+    REQUIRE( !disjoint(Vec<int>{4},       Vec<int>{1, 2, 3, 4, 5}) );
+    REQUIRE( !disjoint(Vec<int>{2, 1, 5}, Vec<int>{1, 2, 3, 4, 5}) );
+
+    //-------------------------------------------------------------------------
+    // TESTING METHOD: identical
+    //-------------------------------------------------------------------------
+    REQUIRE(  identical(Vec<int>{},        Vec<int>{}) );
+    REQUIRE(  identical(Vec<int>{6},       Vec<int>{6}) );
+    REQUIRE(  identical(Vec<int>{6, 8},    Vec<int>{8, 6}) );
+    REQUIRE(  identical(Vec<int>{6, 8, 9}, Vec<int>{9, 6, 8}) );
+    REQUIRE( !identical(Vec<int>{4},       Vec<int>{5}) );
+    REQUIRE( !identical(Vec<int>{2, 1, 5}, Vec<int>{2, 1, 6}) );
+    REQUIRE( !identical(Vec<int>{2, 1, 5}, Vec<int>{2, 1, 4, 5}) );
 }
