4fd9c2104d3e2b04f19750d566b52c9a717ba869
Allan Leal <allan.leal@erdw.ethz.ch>
2020-04-16 21:14:54 +0200

    Implemented tests for GenericPhase and GenericPhases in Phases module

--- Reaktoro/Core/Phases.test.cxx
+++ Reaktoro/Core/Phases.test.cxx
@@ -26,72 +26,249 @@ using namespace Reaktoro;
 
 TEST_CASE("Testing Phases", "[Phases]")
 {
-    StandardThermoPropsFn std_thermo_props_fn = [](real T, real P, const Species& species)
+    StandardThermoPropsFn standard_thermo_props_fn = [](real T, real P, const Species& species)
     {
-        return StandardThermoProps{ 1.0, 2.0, 3.0, 4.0, 5.0 }; // G0, H0, V0, Cp0, Cv0
+        return StandardThermoProps{};
     };
 
-    ActivityModel activitymodel = [](const SpeciesList& species)
+    ActivityModel activity_model = [](const SpeciesList& species)
     {
-        ActivityPropsFn fn = [](ActivityProps props, real T, real P, ArrayXrConstRef x)
-        {
-            props.Vex  = 123.0;
-            props.VexT = 123.0;
-            props.VexP = 123.0;
-            props.Gex  = 123.0;
-            props.Hex  = 123.0;
-            props.Cpex = 123.0;
-            props.Cvex = 123.0;
-            props.ln_g = 123.0;
-            props.ln_a = 123.0;
-        };
-
+        ActivityPropsFn fn = [](ActivityProps props, real T, real P, ArrayXrConstRef x) {};
         return fn;
     };
 
     Database db;
-    db.addSpecies(Species("H2O(aq)"));
-    db.addSpecies(Species("H+"));
-    db.addSpecies(Species("OH-"));
-    db.addSpecies(Species("Na+"));
-    db.addSpecies(Species("Cl-"));
+    db.addSpecies( Species("H2O(aq)")                            );
+    db.addSpecies( Species("H+")                                 );
+    db.addSpecies( Species("OH-")                                );
+    db.addSpecies( Species("H2(aq)")                             );
+    db.addSpecies( Species("O2(aq)")                             );
+    db.addSpecies( Species("Na+")                                );
+    db.addSpecies( Species("Cl-")                                );
+    db.addSpecies( Species("NaCl(aq)")                           );
+    db.addSpecies( Species("HCl(aq)")                            );
+    db.addSpecies( Species("NaOH(aq)")                           );
+    db.addSpecies( Species("Ca++")                               );
+    db.addSpecies( Species("Mg++")                               );
+    db.addSpecies( Species("CaCl2(aq)")                          );
+    db.addSpecies( Species("MgCl2(aq)")                          );
+    db.addSpecies( Species("SiO2(aq)")                           );
+    db.addSpecies( Species("NaCl(s)").withName("Halite")         );
+    db.addSpecies( Species("CaCO3(s)").withName("Calcite")       );
+    db.addSpecies( Species("MgCO3(s)").withName("Magnesite")     );
+    db.addSpecies( Species("CaMg(CO3)2(s)").withName("Dolomite") );
+    db.addSpecies( Species("SiO2(s)").withName("Quartz")         );
 
-    ThermoEngine engine(db, std_thermo_props_fn);
+    ThermoEngine engine(db, standard_thermo_props_fn);
 
-    GenericPhase genericphase("H2O(aq) H+ OH-");
-    genericphase.named("AqueousPhase");
-    genericphase.set(StateOfMatter::Liquid);
-    genericphase.set(AggregateState::Aqueous);
-    genericphase.set(activitymodel);
+    //=================================================================================================================
+    //-----------------------------------------------------------------------------------------------------------------
+    // TESTING CLASS: GenericPhase
+    //-----------------------------------------------------------------------------------------------------------------
+    //=================================================================================================================
+    SECTION("Testing GenericPhase::GenericPhase(const StringList&)")
+    {
+        GenericPhase genericphase("H2O(aq) H+ OH-");
+        genericphase.setName("AqueousSolution");
+        genericphase.setStateOfMatter(StateOfMatter::Liquid);
+        genericphase.setAggregateState(AggregateState::Aqueous);
+        genericphase.setActivityModel(activity_model);
 
-    Phase phase = genericphase.convert(engine, {"H", "O", "Na", "Cl"});
+        Phase phase = genericphase.convert(engine, {"H", "O", "C", "Na", "Cl", "Ca", "Mg", "Si"});
 
-    REQUIRE( phase.name() == "AqueousPhase" );
-    REQUIRE( phase.stateOfMatter() == StateOfMatter::Liquid );
-    REQUIRE( phase.species().size() == 3 );
-    REQUIRE( phase.species(0).name() == "H2O(aq)" );
-    REQUIRE( phase.species(1).name() == "H+"      );
-    REQUIRE( phase.species(2).name() == "OH-"     );
+        REQUIRE( phase.name() == "AqueousSolution" );
+        REQUIRE( phase.stateOfMatter() == StateOfMatter::Liquid );
+        REQUIRE( phase.species().size() == 3 );
+        REQUIRE( phase.species(0).name() == "H2O(aq)" );
+        REQUIRE( phase.species(1).name() == "H+"      );
+        REQUIRE( phase.species(2).name() == "OH-"     );
+        REQUIRE( phase.standardThermoPropsFn() );
+        REQUIRE( phase.activityPropsFn() );
+    }
 
-    auto thermoprops = phase.props(300.00, 10.0e5);
+    SECTION("Testing GenericPhase::GenericPhase(const Speciate&)")
+    {
+        GenericPhase genericphase(speciate("H O"));
+        genericphase.setName("AqueousSolution");
+        genericphase.setStateOfMatter(StateOfMatter::Liquid);
+        genericphase.setAggregateState(AggregateState::Aqueous);
+        genericphase.setActivityModel(activity_model);
 
-    REQUIRE( thermoprops.temperature() == 300.00 );
-    REQUIRE( thermoprops.pressure()    == 10.0e5 );
+        Phase phase = genericphase.convert(engine, {"H", "O", "C", "Na", "Cl", "Ca", "Mg", "Si"});
 
-    REQUIRE( (thermoprops.standardGibbsEnergies()        == 1.0).all() );
-    REQUIRE( (thermoprops.standardGibbsEnergies()        == 1.0).all() );
-    REQUIRE( (thermoprops.standardEnthalpies()           == 2.0).all() );
-    REQUIRE( (thermoprops.standardVolumes()              == 3.0).all() );
-    REQUIRE( (thermoprops.standardHeatCapacitiesConstP() == 4.0).all() );
-    REQUIRE( (thermoprops.standardHeatCapacitiesConstV() == 5.0).all() );
+        REQUIRE( phase.name() == "AqueousSolution" );
+        REQUIRE( phase.stateOfMatter() == StateOfMatter::Liquid );
+        REQUIRE( phase.species().size() == 5 );
+        REQUIRE( phase.species(0).name() == "H2O(aq)" );
+        REQUIRE( phase.species(1).name() == "H+"      );
+        REQUIRE( phase.species(2).name() == "OH-"     );
+        REQUIRE( phase.species(3).name() == "H2(aq)"  );
+        REQUIRE( phase.species(4).name() == "O2(aq)"  );
+        REQUIRE( phase.standardThermoPropsFn() );
+        REQUIRE( phase.activityPropsFn() );
+    }
 
-    ArrayXr n(3); n << 55.508, 1.0e-7, 1.0e-7;
+    SECTION("Testing GenericPhase::GenericPhase() with all elements during phase conversion process")
+    {
+        GenericPhase genericphase;
+        genericphase.setName("AqueousSolution");
+        genericphase.setStateOfMatter(StateOfMatter::Liquid);
+        genericphase.setAggregateState(AggregateState::Aqueous);
+        genericphase.setActivityModel(activity_model);
 
-    auto chemicalprops = phase.props(310.00, 11.0e5, n);
+        Phase phase = genericphase.convert(engine, {"H", "O", "C", "Na", "Cl", "Ca", "Mg", "Si"});
 
-    REQUIRE( chemicalprops.temperature() == 310.00 );
-    REQUIRE( chemicalprops.pressure()    == 11.0e5 );
+        REQUIRE( phase.name() == "AqueousSolution" );
+        REQUIRE( phase.stateOfMatter() == StateOfMatter::Liquid );
+        REQUIRE( phase.species().size() == 15 );
+        REQUIRE( phase.species(0).name()  == "H2O(aq)"   );
+        REQUIRE( phase.species(1).name()  == "H+"        );
+        REQUIRE( phase.species(2).name()  == "OH-"       );
+        REQUIRE( phase.species(3).name()  == "H2(aq)"    );
+        REQUIRE( phase.species(4).name()  == "O2(aq)"    );
+        REQUIRE( phase.species(5).name()  == "Na+"       );
+        REQUIRE( phase.species(6).name()  == "Cl-"       );
+        REQUIRE( phase.species(7).name()  == "NaCl(aq)"  );
+        REQUIRE( phase.species(8).name()  == "HCl(aq)"   );
+        REQUIRE( phase.species(9).name()  == "NaOH(aq)"  );
+        REQUIRE( phase.species(10).name() == "Ca++"      );
+        REQUIRE( phase.species(11).name() == "Mg++"      );
+        REQUIRE( phase.species(12).name() == "CaCl2(aq)" );
+        REQUIRE( phase.species(13).name() == "MgCl2(aq)" );
+        REQUIRE( phase.species(14).name() == "SiO2(aq)"  );
+        REQUIRE( phase.standardThermoPropsFn() );
+        REQUIRE( phase.activityPropsFn() );
+    }
 
-    REQUIRE( (chemicalprops.lnActivities()           == 123.0).all() );
-    REQUIRE( (chemicalprops.lnActivityCoefficients() == 123.0).all() );
+    SECTION("Testing GenericPhase::GenericPhase() with H, O, and Cl elements during phase conversion process")
+    {
+        GenericPhase genericphase;
+        genericphase.setName("AqueousSolution");
+        genericphase.setStateOfMatter(StateOfMatter::Liquid);
+        genericphase.setAggregateState(AggregateState::Aqueous);
+        genericphase.setActivityModel(activity_model);
+
+        Phase phase = genericphase.convert(engine, {"H", "O", "Cl"});
+
+        REQUIRE( phase.name() == "AqueousSolution" );
+        REQUIRE( phase.stateOfMatter() == StateOfMatter::Liquid );
+        REQUIRE( phase.species().size() == 7 );
+        REQUIRE( phase.species(0).name() == "H2O(aq)"  );
+        REQUIRE( phase.species(1).name() == "H+"       );
+        REQUIRE( phase.species(2).name() == "OH-"      );
+        REQUIRE( phase.species(3).name() == "H2(aq)"   );
+        REQUIRE( phase.species(4).name() == "O2(aq)"   );
+        REQUIRE( phase.species(5).name() == "Cl-"      );
+        REQUIRE( phase.species(6).name() == "HCl(aq)"  );
+        REQUIRE( phase.standardThermoPropsFn() );
+        REQUIRE( phase.activityPropsFn() );
+    }
+
+    //=================================================================================================================
+    //-----------------------------------------------------------------------------------------------------------------
+    // TESTING CLASS: GenericPhases
+    //-----------------------------------------------------------------------------------------------------------------
+    //=================================================================================================================
+    SECTION("Testing GenericPhases::GenericPhases(const StringList&)")
+    {
+        GenericPhases genericphases("Calcite Magnesite");
+        genericphases.setStateOfMatter(StateOfMatter::Solid);
+        genericphases.setAggregateState(AggregateState::Solid);
+        genericphases.setActivityModel(activity_model);
+
+        Vec<GenericPhase> phases = genericphases.convert(engine, {"H", "O", "C", "Na", "Cl", "Ca", "Mg", "Si"});
+
+        REQUIRE( phases.size() == 2 );
+
+        REQUIRE( phases[0].name() == "Calcite" );
+        REQUIRE( phases[0].stateOfMatter() == StateOfMatter::Solid );
+        REQUIRE( phases[0].aggregateState() == AggregateState::Solid );
+        REQUIRE( phases[0].activityModel() );
+
+        REQUIRE( phases[1].name() == "Magnesite" );
+        REQUIRE( phases[1].stateOfMatter() == StateOfMatter::Solid );
+        REQUIRE( phases[1].aggregateState() == AggregateState::Solid );
+        REQUIRE( phases[1].activityModel() );
+    }
+
+    SECTION("Testing GenericPhases::GenericPhases(const Speciate&)")
+    {
+        GenericPhases genericphases(speciate("Ca Mg C O"));
+        genericphases.setStateOfMatter(StateOfMatter::Solid);
+        genericphases.setAggregateState(AggregateState::Solid);
+        genericphases.setActivityModel(activity_model);
+
+        Vec<GenericPhase> phases = genericphases.convert(engine, {"H", "O", "C", "Na", "Cl", "Ca", "Mg", "Si"});
+
+        REQUIRE( phases.size() == 3 );
+
+        REQUIRE( phases[0].name() == "Calcite" );
+        REQUIRE( phases[0].stateOfMatter() == StateOfMatter::Solid );
+        REQUIRE( phases[0].aggregateState() == AggregateState::Solid );
+        REQUIRE( phases[0].activityModel() );
+
+        REQUIRE( phases[1].name() == "Magnesite" );
+        REQUIRE( phases[1].stateOfMatter() == StateOfMatter::Solid );
+        REQUIRE( phases[1].aggregateState() == AggregateState::Solid );
+        REQUIRE( phases[1].activityModel() );
+
+        REQUIRE( phases[2].name() == "Dolomite" );
+        REQUIRE( phases[2].stateOfMatter() == StateOfMatter::Solid );
+        REQUIRE( phases[2].aggregateState() == AggregateState::Solid );
+        REQUIRE( phases[2].activityModel() );
+    }
+
+    SECTION("Testing GenericPhases::GenericPhases() with all elements during phase conversion process")
+    {
+        GenericPhases genericphases;
+        genericphases.setStateOfMatter(StateOfMatter::Solid);
+        genericphases.setAggregateState(AggregateState::Solid);
+        genericphases.setActivityModel(activity_model);
+
+        Vec<GenericPhase> phases = genericphases.convert(engine, {"H", "O", "C", "Na", "Cl", "Ca", "Mg", "Si"});
+
+        REQUIRE( phases.size() == 5 );
+
+        REQUIRE( phases[0].name() == "Halite" );
+        REQUIRE( phases[0].stateOfMatter() == StateOfMatter::Solid );
+        REQUIRE( phases[0].aggregateState() == AggregateState::Solid );
+        REQUIRE( phases[0].activityModel() );
+
+        REQUIRE( phases[1].name() == "Calcite" );
+        REQUIRE( phases[1].stateOfMatter() == StateOfMatter::Solid );
+        REQUIRE( phases[1].aggregateState() == AggregateState::Solid );
+        REQUIRE( phases[1].activityModel() );
+
+        REQUIRE( phases[2].name() == "Magnesite" );
+        REQUIRE( phases[2].stateOfMatter() == StateOfMatter::Solid );
+        REQUIRE( phases[2].aggregateState() == AggregateState::Solid );
+        REQUIRE( phases[2].activityModel() );
+
+        REQUIRE( phases[3].name() == "Dolomite" );
+        REQUIRE( phases[3].stateOfMatter() == StateOfMatter::Solid );
+        REQUIRE( phases[3].aggregateState() == AggregateState::Solid );
+        REQUIRE( phases[3].activityModel() );
+
+        REQUIRE( phases[4].name() == "Quartz" );
+        REQUIRE( phases[4].stateOfMatter() == StateOfMatter::Solid );
+        REQUIRE( phases[4].aggregateState() == AggregateState::Solid );
+        REQUIRE( phases[4].activityModel() );
+    }
+
+    SECTION("Testing GenericPhases::GenericPhases() with Na and Cl elements during phase conversion process")
+    {
+        GenericPhases genericphases;
+        genericphases.setStateOfMatter(StateOfMatter::Solid);
+        genericphases.setAggregateState(AggregateState::Solid);
+        genericphases.setActivityModel(activity_model);
+
+        Vec<GenericPhase> phases = genericphases.convert(engine, {"Na", "Cl"});
+
+        REQUIRE( phases.size() == 1 );
+
+        REQUIRE( phases[0].name() == "Halite" );
+        REQUIRE( phases[0].stateOfMatter() == StateOfMatter::Solid );
+        REQUIRE( phases[0].aggregateState() == AggregateState::Solid );
+        REQUIRE( phases[0].activityModel() );
+    }
 }
