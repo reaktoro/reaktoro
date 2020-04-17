b377beea8d2aea7d1112496c1e536dedf8b96c20
Allan Leal <allan.leal@erdw.ethz.ch>
2020-04-17 09:39:16 +0200

    Phase class now ensures all species have common aggregate state

--- Reaktoro/Core/Phase.test.cxx
+++ Reaktoro/Core/Phase.test.cxx
@@ -51,23 +51,75 @@ TEST_CASE("Testing Phase", "[Phase]")
     };
 
     Phase phase;
-    phase = phase.withName("AqueousSolution");
-    phase = phase.withSpecies(SpeciesList("H2O(aq) H+ OH- H2(aq) O2(aq) CO2(aq) HCO3- CO3--"));
-    phase = phase.withStateOfMatter(StateOfMatter::Liquid);
     phase = phase.withStandardThermoPropsFn(std_thermo_props_fn);
     phase = phase.withActivityPropsFn(activity_props_fn);
 
-    REQUIRE( phase.name() == "AqueousSolution" );
-    REQUIRE( phase.stateOfMatter() == StateOfMatter::Liquid );
-    REQUIRE( phase.species().size() == 8 );
-    REQUIRE( phase.species(0).name() == "H2O(aq)" );
-    REQUIRE( phase.species(1).name() == "H+"      );
-    REQUIRE( phase.species(2).name() == "OH-"     );
-    REQUIRE( phase.species(3).name() == "H2(aq)"  );
-    REQUIRE( phase.species(4).name() == "O2(aq)"  );
-    REQUIRE( phase.species(5).name() == "CO2(aq)" );
-    REQUIRE( phase.species(6).name() == "HCO3-"   );
-    REQUIRE( phase.species(7).name() == "CO3--"   );
     REQUIRE( phase.standardThermoPropsFn() );
     REQUIRE( phase.activityPropsFn() );
+
+    SECTION("Testing PhasePhase::withSpecies with aqueous species")
+    {
+        phase = phase.withName("AqueousSolution");
+        phase = phase.withSpecies(SpeciesList("H2O(aq) H+ OH- H2(aq) O2(aq) CO2(aq) HCO3- CO3--"));
+        phase = phase.withStateOfMatter(StateOfMatter::Liquid);
+
+        REQUIRE( phase.name() == "AqueousSolution" );
+        REQUIRE( phase.stateOfMatter() == StateOfMatter::Liquid );
+        REQUIRE( phase.species().size() == 8 );
+        REQUIRE( phase.species(0).name() == "H2O(aq)" );
+        REQUIRE( phase.species(1).name() == "H+"      );
+        REQUIRE( phase.species(2).name() == "OH-"     );
+        REQUIRE( phase.species(3).name() == "H2(aq)"  );
+        REQUIRE( phase.species(4).name() == "O2(aq)"  );
+        REQUIRE( phase.species(5).name() == "CO2(aq)" );
+        REQUIRE( phase.species(6).name() == "HCO3-"   );
+        REQUIRE( phase.species(7).name() == "CO3--"   );
+    }
+
+    SECTION("Testing PhasePhase::withSpecies with gaseous species")
+    {
+        Phase phase;
+        phase = phase.withName("GaseousSolution");
+        phase = phase.withSpecies(SpeciesList("H2O(g) CO2(g) CH4(g) O2(g) H2(g)"));
+        phase = phase.withStateOfMatter(StateOfMatter::Gas);
+        phase = phase.withStandardThermoPropsFn(std_thermo_props_fn);
+        phase = phase.withActivityPropsFn(activity_props_fn);
+
+        REQUIRE( phase.name() == "GaseousSolution" );
+        REQUIRE( phase.stateOfMatter() == StateOfMatter::Gas );
+        REQUIRE( phase.species().size() == 5 );
+        REQUIRE( phase.species(0).name() == "H2O(g)" );
+        REQUIRE( phase.species(1).name() == "CO2(g)" );
+        REQUIRE( phase.species(2).name() == "CH4(g)" );
+        REQUIRE( phase.species(3).name() == "O2(g)"  );
+        REQUIRE( phase.species(4).name() == "H2(g)"  );
+    }
+
+    SECTION("Testing PhasePhase::withSpecies with a single mineral species")
+    {
+        Phase phase;
+        phase = phase.withName("Calcite");
+        phase = phase.withSpecies({ Species("CaCO3(s)").withName("Calcite") });
+        phase = phase.withStateOfMatter(StateOfMatter::Solid);
+
+        REQUIRE( phase.name() == "Calcite" );
+        REQUIRE( phase.stateOfMatter() == StateOfMatter::Solid );
+        REQUIRE( phase.species().size() == 1 );
+        REQUIRE( phase.species(0).name() == "Calcite" );
+    }
+
+    SECTION("Testing PhasePhase::withSpecies with species having uncommon aggregate states")
+    {
+        Phase phase;
+        phase = phase.withName("AqueousSolution");
+        phase = phase.withStateOfMatter(StateOfMatter::Liquid);
+
+        REQUIRE_THROWS( phase.withSpecies(SpeciesList("H2O H+ OH-")) );
+        // The aggregate state of H2O above cannot be inferred,
+        // so it takes the value AggregateState::Undefined.
+        // However, the aggregate state of the ionic species are
+        // inferred as AggregateState::Aqueous by default.
+        // Because of this mismatch in aggregate states, the above
+        // Phase::withSpecies call should raise a runtime error.
+    }
 }
