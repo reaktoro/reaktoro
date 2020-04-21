27c09e1b86a6915c8678055f6659a51012725f88
Svetlana Kyas <mtsveta@gmail.com>
2020-04-21 13:29:36 +0200

    Correction of the system created by selecting specific species

--- demos/cpp/demo-reactivetransport-scaveging.cpp
+++ demos/cpp/demo-reactivetransport-scaveging.cpp
@@ -127,10 +127,11 @@ int main()
     params.P = 1.01325;                      // the pressure (in units of bar)
 
     // Define parameters of the equilibrium solvers
-    params.smart_equlibrium_reltol = 0.001;
+    params.smart_equlibrium_reltol = 0.01;
     //params.activity_model = "hkf";
-    params.activity_model = "pitzer";
-    //params.activity_model = "dk";
+    //params.activity_model = "pitzer";
+    params.activity_model = "dk";
+    //params.activity_model = "dk-full";
 
     params.amount_fraction_cutoff = 1e-14;
     params.mole_fraction_cutoff = 1e-14;
@@ -142,8 +143,8 @@ int main()
     Results results;
 
     // Execute reactive transport with different solvers
-    params.use_smart_eqilibirum_solver = true; runReactiveTransport(params, results);
-    //params.use_smart_eqilibirum_solver = false; runReactiveTransport(params, results);
+    //params.use_smart_eqilibirum_solver = true; runReactiveTransport(params, results);
+    params.use_smart_eqilibirum_solver = false; runReactiveTransport(params, results);
 
     results.conventional_total = results.equilibrium_timing.solve;
     results.smart_total = results.smart_equilibrium_timing.solve;
@@ -202,44 +203,38 @@ auto runReactiveTransport(const Params& params, Results& results) -> void
 
     if(params.activity_model == "hkf"){
         // HKF full system
-        editor.addAqueousPhase({"H2O(l)",  "H+", "OH-", "HCO3-", "Mg(HCO3)+", "Ca(HCO3)+", "MgCO3(aq)",
-                                "CO3--", "CaCO3(aq)", "Ca++", "CaSO4(aq)", "CaOH+", "Cl-", "FeCl++",
-                                "FeCl2(aq)", "FeCl+", "Fe++", "FeOH+",  "FeOH++", "Fe+++", "H2(aq)", "K+",
-                                "KSO4-", "Mg++", "MgSO4(aq)", "MgCO3(aq)", "MgOH+", "Na+", "NaSO4-",
-                                "O2(aq)", "H2S(aq)", "HS-", "S5--", "S4--", "S3--", "S2--", "SO4--",
-                                "NaSO4-", "MgSO4(aq)", "CaSO4(aq)", "KSO4-", "HSO4-"});
+        editor.addAqueousPhase({"Ca(HCO3)+", "CO3--", "CaCO3(aq)", "Ca++", "CaSO4(aq)", "CaOH+", "Cl-",
+                                "FeCl++", "FeCl2(aq)", "FeCl+", "Fe++", "FeOH+",  "FeOH++", "Fe+++",
+                                "H2(aq)", "HSO4-", "H2S(aq)", "HS-", "H2O(l)",  "H+", "OH-", "HCO3-",
+                                "K+", "KSO4-",
+                                "Mg++", "MgSO4(aq)", "MgCO3(aq)", "MgOH+", "Mg(HCO3)+",
+                                "Na+", "NaSO4-",
+                                "O2(aq)",
+                                "S5--", "S4--", "S3--", "S2--", "SO4--"});
     }
     else if(params.activity_model == "pitzer"){
         // Pitzer full system
-        editor.addAqueousPhase({"H2O(l)",  "H+", "OH-",
-                                "HCO3-", "Mg(HCO3)+", "Ca(HCO3)+", "MgCO3(aq)",  "CO3--", "CaCO3(aq)" ,
-                                "Ca++", "CaSO4(aq)", "CaOH+",
-                                "Cl-", "FeCl++", "FeCl2(aq)", "FeCl+",
-                                "Fe++", "FeOH+",  "FeOH++", "Fe+++",
-                                "H2(aq)",
+        editor.addAqueousPhase({"Ca(HCO3)+", "CO3--", "CaCO3(aq)", "Ca++", "CaSO4(aq)", "CaOH+", "Cl-",
+                                "FeCl++", "FeCl2(aq)", "FeCl+", "Fe++", "FeOH+",  "FeOH++", "Fe+++",
+                                "H2(aq)", "HSO4-", "H2S(aq)", "HS-", "H2O(l)",  "H+", "OH-", "HCO3-",
                                 "K+", "KSO4-",
-                                "Mg++", "MgSO4(aq)", "MgCO3(aq)", "MgOH+",
+                                "Mg++", "MgSO4(aq)", "MgCO3(aq)", "MgOH+", "Mg(HCO3)+",
                                 "Na+", "NaSO4-",
                                 "O2(aq)",
-                                "H2S(aq)", "HS-", "S5--", "S4--", "S3--", "S2--",
-                                "SO4--", "NaSO4-", "MgSO4(aq)", "CaSO4(aq)", "KSO4-", "HSO4-"})
+                                "S5--", "S4--", "S3--", "S2--", "SO4--"})
                 .setChemicalModelPitzerHMW()
                 .setActivityModelDrummondCO2();
     }
     else if(params.activity_model == "dk"){
         // Debye-Huckel full system
-        editor.addAqueousPhase({"H2O(l)",  "H+", "OH-",
-                                "HCO3-", "Mg(HCO3)+", "Ca(HCO3)+", "MgCO3(aq)",  "CO3--", "CaCO3(aq)" ,
-                                "Ca++", "CaSO4(aq)", "CaOH+",
-                                "Cl-", "FeCl++", "FeCl2(aq)", "FeCl+",
-                                "Fe++", "FeOH+",  "FeOH++", "Fe+++",
-                                "H2(aq)",
+        editor.addAqueousPhase({"Ca(HCO3)+", "CO3--", "CaCO3(aq)", "Ca++", "CaSO4(aq)", "CaOH+", "Cl-",
+                                "FeCl++", "FeCl2(aq)", "FeCl+", "Fe++", "FeOH+",  "FeOH++", "Fe+++",
+                                "H2(aq)", "HSO4-", "H2S(aq)", "HS-", "H2O(l)",  "H+", "OH-", "HCO3-",
                                 "K+", "KSO4-",
-                                "Mg++", "MgSO4(aq)", "MgCO3(aq)", "MgOH+",
+                                "Mg++", "MgSO4(aq)", "MgCO3(aq)", "MgOH+", "Mg(HCO3)+",
                                 "Na+", "NaSO4-",
                                 "O2(aq)",
-                                "H2S(aq)", "HS-", "S5--", "S4--", "S3--", "S2--",
-                                "SO4--", "NaSO4-", "MgSO4(aq)", "CaSO4(aq)", "KSO4-", "HSO4-"})
+                                "S5--", "S4--", "S3--", "S2--", "SO4--"})
                 .setChemicalModelDebyeHuckel(dhModel);
     }
     else if(params.activity_model == "dk-full"){
@@ -471,4 +466,3 @@ auto outputConsole(const Params& params) -> void {
     std::cout << "activity model : " << params.activity_model << std::endl;
 
 }
-
