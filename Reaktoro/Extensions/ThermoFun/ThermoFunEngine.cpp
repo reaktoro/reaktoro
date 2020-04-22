fda6488d6621eb75ca1f654eb8a4943cec27b4b1
Allan Leal <allan.leal@erdw.ethz.ch>
2020-04-22 17:51:50 +0200

    Created extension ThermoFun

---
+++ Reaktoro/Extensions/ThermoFun/ThermoFunEngine.cpp
@@ -0,0 +1,88 @@
+// Reaktoro is a unified framework for modeling chemically reactive systems.
+//
+// Copyright (C) 2014-2020 Allan Leal
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library. If not, see <http://www.gnu.org/licenses/>.
+
+#include "ThermoFunEngine.hpp"
+
+// Reaktoro includes
+#include <Reaktoro/Common/Exception.hpp>
+
+// ThermoFun includes
+#include <ThermoFun/ThermoFun.h>
+
+namespace Reaktoro {
+namespace {
+
+/// Convert a SpeciesThermoState object into a StandardThermoProps one
+auto convertProps(const ThermoFun::ThermoPropertiesSubstance& other) -> StandardThermoProps
+{
+    StandardThermoProps converted;
+    converted.G0  = other.gibbs_energy.val;
+    converted.H0  = other.enthalpy.val;
+    converted.V0  = other.volume.val * 1.0e-05; // from J/bar to m3/mol
+    converted.Cp0 = other.heat_capacity_cp.val;
+    converted.Cv0 = other.heat_capacity_cv.val;
+    return converted;
+}
+
+} // namespace
+
+struct ThermoFunEngine::Impl
+{
+    /// The ThermoFun::ThermoEngine object.
+    mutable ThermoFun::ThermoEngine engine; // mutable needed because some methods in ThermoFun is not const
+
+    /// The ThermoFun::Database object.
+    ThermoFun::Database database;
+
+    /// Costruct a Impl object with given ThermoFun::Database object.
+    Impl(const ThermoFun::Database& database)
+    : engine(database), database(database)
+    {
+        // Set solvent symbol, the HGK, JN water solvent model are defined in this record
+        engine.setSolventSymbol("H2O@");
+    }
+
+    /// Return the standard thermodynamic properties of a chemical species with given name.
+    auto props(double T, double P, const String& species) const -> StandardThermoProps
+    {
+        if(database.containsSubstance(species))
+        {
+            const auto props = engine.thermoPropertiesSubstance(T, P, species);
+            return convertProps(props);
+        }
+        error("Cannot evaluate the standard thermodynamic properties of species with "
+            "name ", species, " because it is know available in the ThermoFun database.");
+        return {};
+    }
+};
+
+
+ThermoFunEngine::ThermoFunEngine(const ThermoFun::Database& database)
+: pimpl(new Impl(database))
+{}
+
+auto ThermoFunEngine::database() const -> const ThermoFun::Database&
+{
+    return pimpl->database;
+}
+
+auto ThermoFunEngine::props(real T, real P, const String& species) const -> StandardThermoProps
+{
+    return pimpl->props(T, P, species);
+}
+
+} // namespace Reaktoro
---
+++ Reaktoro/Extensions/ThermoFun/ThermoFunEngine.hpp
@@ -0,0 +1,49 @@
+// Reaktoro is a unified framework for modeling chemically reactive systems.
+//
+// Copyright (C) 2014-2020 Allan Leal
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library. If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+// Reaktoro includes
+#include <Reaktoro/Common/Types.hpp>
+#include <Reaktoro/Core/StandardThermoProps.hpp>
+
+// Forward declaration of ThermoFun classes
+namespace ThermoFun { class Database; }
+
+namespace Reaktoro {
+
+/// The class used for standard thermodynamic property calculations based on ThermoFun.
+/// @ingroup ThermoFunExtension
+class ThermoFunEngine
+{
+public:
+    /// Construct a ThermoFunEngine object with given database.
+    ThermoFunEngine(const ThermoFun::Database& database);
+
+    /// Return the ThermoFun::Database object.
+    auto database() const -> const ThermoFun::Database&;
+
+    /// Return the standard thermodynamic properties of a chemical species with given name.
+    auto props(real T, real P, const String& species) const -> StandardThermoProps;
+
+private:
+    struct Impl;
+
+    std::shared_ptr<Impl> pimpl;
+};
+
+} // namespace Reaktoro
