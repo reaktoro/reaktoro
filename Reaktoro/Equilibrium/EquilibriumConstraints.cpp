a92710f3fa36884d4e74de4b937078fd92e05dc2
Allan Leal <allan.leal@erdw.ethz.ch>
2020-05-08 11:35:53 +0200

    Fix header message

--- Reaktoro/Equilibrium/EquilibriumConstraints.cpp
+++ Reaktoro/Equilibrium/EquilibriumConstraints.cpp
@@ -1,11 +1,13 @@
-// This file is part of Reaktoro (https://reaktoro.org).
+// Reaktoro is a unified framework for modeling chemically reactive systems.
 //
-// Reaktoro is free software; you can redistribute it and/or
+// Copyright (C) 2014-2020 Allan Leal
+//
+// This library is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public
 // License as published by the Free Software Foundation; either
 // version 2.1 of the License, or (at your option) any later version.
 //
-// Reaktoro is distributed in the hope that it will be useful,
+// This library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 // Lesser General Public License for more details.
