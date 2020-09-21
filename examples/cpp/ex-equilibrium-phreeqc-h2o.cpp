3a0124c9a8279c5125462e13d63c54e18cee189a
Allan Leal <allan.leal@erdw.ethz.ch>
2020-09-21 11:35:54 +0200

    Added examples using phreeqc and supcrt databases

--- examples/cpp/ex-equilibrium-phreeqc-h2o.cpp
+++ examples/cpp/ex-equilibrium-phreeqc-h2o.cpp
@@ -21,8 +21,18 @@ using namespace Reaktoro;
 int main()
 {
     PhreeqcDatabase db("phreeqc.dat");
+    // PhreeqcDatabase db("pitzer.dat");
 
-    Phases phases(db,  AqueousSolution("H2O H+ OH- O2 H2"));
+    AqueousSolution aqueousphase(speciate("H O C Na Cl"));
+    // AqueousSolution aqueousphase("H2O H+ OH- HCO3- CO2");
+    aqueousphase.setActivityModel(ActivityModelHKF());
+
+    GaseousSolution gaseousphase("CO2(g)");
+    gaseousphase.setActivityModel(ActivityModelPengRobinson());
+
+    // Phases phases(db, AqueousSolution("H2O H+ OH- O2 H2"));
+    // Phases phases(db,  AqueousSolution("H2O H+ OH-"));
+    Phases phases(db, aqueousphase, gaseousphase);
 
     ChemicalSystem system(db, phases);
 
@@ -45,42 +55,93 @@ int main()
     double T = 25.0 + 273.15;
     double P = 1.0 * 1e5;
 
+    // const auto H2O = system.species(0);
+    // const auto Hp = system.species(1);
+    // const auto OHm = system.species(2);
+    // const auto O2 = system.species(3);
+    // const auto H2 = system.species(4);
+
+    // const auto u0_H2O = H2O.props(T, P).G0;
+    // const auto u0_Hp  = Hp.props(T, P).G0;
+    // const auto u0_OHm = OHm.props(T, P).G0;
+    // const auto u0_O2  = O2.props(T, P).G0;
+    // const auto u0_H2  = H2.props(T, P).G0;
+
+    // std::cout << "u0_H2O = " << u0_H2O << std::endl;
+    // std::cout << "u0_Hp  = " << u0_Hp << std::endl;
+    // std::cout << "u0_OHm = " << u0_OHm << std::endl;
+    // std::cout << "u0_O2  = " << u0_O2 << std::endl;
+    // std::cout << "u0_H2  = " << u0_H2 << std::endl;
+
     const auto H2O = system.species(0);
     const auto Hp = system.species(1);
     const auto OHm = system.species(2);
-    const auto O2 = system.species(3);
-    const auto H2 = system.species(4);
+    const auto HCO3m = system.species(3);
+    const auto CO2 = system.species(4);
+    const auto CO2g = system.species(5);
 
-    const auto u0_H2O = H2O.props(T, P).G0;
-    const auto u0_Hp  = Hp.props(T, P).G0;
-    const auto u0_OHm = OHm.props(T, P).G0;
-    const auto u0_O2  = O2.props(T, P).G0;
-    const auto u0_H2  = H2.props(T, P).G0;
+    const auto G0_H2O = H2O.props(T, P).G0;
+    const auto G0_Hp = Hp.props(T, P).G0;
+    const auto G0_OHm = OHm.props(T, P).G0;
+    const auto G0_HCO3m = HCO3m.props(T, P).G0;
+    const auto G0_CO2 = CO2.props(T, P).G0;
+    const auto G0_CO2g = CO2g.props(T, P).G0;
 
-    std::cout << "u0_H2O = " << u0_H2O << std::endl;
-    std::cout << "u0_Hp  = " << u0_Hp << std::endl;
-    std::cout << "u0_OHm = " << u0_OHm << std::endl;
-    std::cout << "u0_O2  = " << u0_O2 << std::endl;
-    std::cout << "u0_H2  = " << u0_H2 << std::endl;
+    std::cout << "G0_H2O   = " << G0_H2O << std::endl;
+    std::cout << "G0_Hp    = " << G0_Hp << std::endl;
+    std::cout << "G0_OHm   = " << G0_OHm << std::endl;
+    std::cout << "G0_HCO3m = " << G0_HCO3m << std::endl;
+    std::cout << "G0_CO2   = " << G0_CO2 << std::endl;
+    std::cout << "G0_CO2g  = " << G0_CO2g << std::endl;
+
+    const auto R = universalGasConstant;
+
+    std::cout << "lgK_CO2g  = " << -(G0_CO2g - G0_CO2)/(R*T*ln10) << std::endl;
 
 
 
-    // ChemicalState state(system);
-    // // state.setTemperature(60.0, "celsius");
-    // // state.setPressure(30.0, "bar");
-    // state.setTemperature(25.0, "celsius");
-    // state.setPressure(1.0, "bar");
+
+    ChemicalState state(system);
+    // state.setTemperature(60.0, "celsius");
+    // state.setPressure(30.0, "bar");
+    // state.setTemperature(60.0, "celsius");
+    // state.setPressure(100.0, "bar");
+    state.setTemperature(25.0, "celsius");
+    state.setPressure(1.0, "bar");
     // state.setSpeciesMass("H2O", 1.0, "kg");
+    state.setSpeciesAmount("H2O", 55.0, "mol");
+    state.setSpeciesAmount("CO2", 50.0, "mol");
+    state.setSpeciesAmount("Na+", 1.0, "mol");
+    state.setSpeciesAmount("Cl-", 1.0, "mol");
+    // state.setSpeciesAmount("O2", 1.0e-14, "mol"); // FIXME: Without this, the example currently fails.
+    // state.setSpeciesAmount("O2", 1.0e-16, "mol"); // FIXME: Without this, the example currently fails.
+    // state.setSpeciesAmount("O2", 1.0e-35, "mol"); // FIXME: Without this, the example currently fails.
+    // state.setSpeciesAmount("O2", 0.999999999e-39, "mol"); // FIXME: Without this, the example currently fails.
 
-    // EquilibriumOptions options;
-    // options.optima.output.active = true;
+    EquilibriumOptions options;
+    options.optima.output.active = true;
+    options.optima.max_iterations = 50;
+    // options.optima.kkt.method = Optima::SaddlePointMethod::Fullspace;
+    // options.optima.kkt.method = Optima::SaddlePointMethod::Nullspace;
+    options.optima.kkt.method = Optima::SaddlePointMethod::Rangespace;
+    options.optima.linesearch.trigger_when_current_error_is_greater_than_initial_error_by_factor = 100000000000.0;
+    options.optima.linesearch.trigger_when_current_error_is_greater_than_previous_error_by_factor = 100000000000.0;
 
-    // EquilibriumSolver solver(system);
-    // solver.setOptions(options);
+    EquilibriumSolver solver(system);
+    solver.setOptions(options);
 
-    // solver.solve(state);
+    solver.solve(state);
 
-    // std::cout << state.speciesAmounts() << std::endl;
+    const auto n = state.speciesAmounts();
+
+    for(auto i = 0; i < n.size(); ++i)
+    {
+        std::cout << std::setw(20) << system.species(i).name();
+        std::cout << std::setw(20) << n[i];
+        std::cout << std::endl;
+    }
+
+
 
     return 0;
 }
