d208e517970510a5a0cb44fbe39a9762aeef56f4
Allan Leal <allan.leal@erdw.ethz.ch>
2020-10-01 17:33:01 +0200

    Implemented python bindings and tests for all reaction thermo models.

---
+++ Reaktoro/Thermodynamics/Reactions/ReactionThermoModelAnalyticalPHREEQC.py.cxx
@@ -0,0 +1,30 @@
+// Reaktoro is a unified framework for modeling chemically reactive systems.
+//
+// Copyright (C) 2014-2020 Allan Leal
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library. If not, see <http://www.gnu.org/licenses/>.
+
+// pybind11 includes
+#include <pybind11/pybind11.h>
+#include <pybind11/functional.h>
+namespace py = pybind11;
+
+// Reaktoro includes
+#include <Reaktoro/Thermodynamics/Reactions/ReactionThermoModelAnalyticalPHREEQC.hpp>
+using namespace Reaktoro;
+
+void exportReactionThermoModelAnalyticalPHREEQC(py::module& m)
+{
+    m.def("ReactionThermoModelAnalyticalPHREEQC", ReactionThermoModelAnalyticalPHREEQC);
+}
