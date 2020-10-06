fff4a579076c2f194dd4ec442170809065ed8cf7
Allan Leal <allan.leal@erdw.ethz.ch>
2020-10-06 16:20:37 +0200

    Implemented and tested chain methods for Model class.

--- Reaktoro/Core/Model.test.cxx
+++ Reaktoro/Core/Model.test.cxx
@@ -189,3 +189,93 @@ TEST_CASE("Testing Model class", "[Model]")
         CHECK( res == 11.0 );
     }
 }
+
+
+TEST_CASE("Testing chain methods on Model objects", "[Model]")
+{
+    auto creatorfn0 = [](const Params& params)
+    {
+        const real A = params.get("A");
+        const real B = params.get("B");
+
+        return [=](real& res, real a, real b)
+        {
+            res += A*a*a + B*b*b + 2*a*b*A*B;
+        };
+    };
+
+    auto creatorfn1 = [](const Params& params)
+    {
+        const real A = params.get("A");
+        const real B = params.get("B");
+        const real C = params.get("C");
+
+        return [=](real& res, real a, real b)
+        {
+            res += B*a*a + A*b*b + 2*(a + b)*A*B*C;
+        };
+    };
+
+    Params params0;
+    params0.set("A", 1.0);
+    params0.set("B", 2.0);
+
+    Params params1;
+    params1.set("A", 3.0);
+    params1.set("B", 4.0);
+    params1.set("C", 5.0);
+
+    Params params;
+    params.set("0", params0);
+    params.set("1", params1);
+
+    Model<real(real, real)> model0(creatorfn0, params0);
+    Model<real(real, real)> model1(creatorfn1, params1);
+
+    auto model = chain(model0, model1);
+
+    CHECK( model.params().at("0").get("A").value() == 1.0 );
+    CHECK( model.params().at("0").get("B").value() == 2.0 );
+
+    CHECK( model.params().at("1").get("A").value() == 3.0 );
+    CHECK( model.params().at("1").get("B").value() == 4.0 );
+    CHECK( model.params().at("1").get("C").value() == 5.0 );
+
+    const auto a = 4.0;
+    const auto b = 7.0;
+
+    real xres = {};
+    creatorfn0(params0)(into(xres), a, b);
+    creatorfn1(params1)(into(xres), a, b);
+
+    const real res = model(a, b);
+
+    CHECK( res == Approx(xres) );
+
+    WHEN("Updating the parameters of a chained Model object...")
+    {
+        const auto a = 4.0;
+        const auto b = 7.0;
+
+        params0.set("A", 5.0);
+        params0.set("B", 6.0);
+
+        params1.set("A", 7.0);
+        params1.set("B", 8.0);
+        params1.set("C", 9.0);
+
+        Params params;
+        params.set("0", params0);
+        params.set("1", params1);
+
+        model = model.withParams(params);
+
+        real xres = {};
+        creatorfn0(params0)(into(xres), a, b);
+        creatorfn1(params1)(into(xres), a, b);
+
+        const real res = model(a, b);
+
+        CHECK( res == Approx(xres) );
+    }
+}
