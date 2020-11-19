2ea79f46e15c75724d7798a74a221fcde45d76b6
Svetlana Kyas <mtsveta@gmail.com>
2020-11-19 17:51:14 +0100

    Adding two different simulations for calcite dissolution with different surface areas

--- demos/cpp/demo-kineticpath-calcite-hcl-different-surface-area.cpp
+++ demos/cpp/demo-kineticpath-calcite-hcl-different-surface-area.cpp
@@ -43,6 +43,8 @@ int main()
 
     ChemicalState state = equilibrate(problem);
 
+    state.setSpeciesMass("Calcite", 100, "g");
+
     state.scalePhaseVolume("Aqueous", 1, "m3");
     state.scalePhaseVolume("Calcite", 1, "m3");
 
@@ -56,12 +58,24 @@ int main()
     plot1.legend("right center");
     plot1.title("Surface area is 1 m2");
 
+    ChemicalOutput output1 = path.output();
+    output1.add("t");
+    output1.add("pH");
+    output1.add("speciesMass(Calcite units=g)", "Calcite");
+    output1.add("speciesMolality(HCO3-)", "HCO3- [molal]");
+    output1.add("speciesMolality(Ca++)", "Ca++  [molal]");
+    output1.filename("path-sa-" +  std::to_string(1) + ".txt");
+
+    double initial_calcite = state.speciesAmount("Calcite");
+
     // The initial chemical state conditions for both kinetic path calculations below
     ChemicalState state01 = state;
     ChemicalState state02 = state;
 
     // Perform the kinetic path calculation with 1 m2 surface area for 100 minutes
-    path.solve(state01, 0, 100, "minutes");
+    path.solve(state01, 0, 30, "minutes");
+
+    double result1_calcite = state01.speciesAmount("Calcite");
 
     // Change the surface area of the mineral reaction to 10 m2
     reaction.setSurfaceArea(10, "m2");
@@ -74,6 +88,23 @@ int main()
     plot2.legend("right center");
     plot2.title("Surface area is 10 m2");
 
+
+    ChemicalOutput output2 = path.output();
+    output2.add("t");
+    output2.add("pH");
+    output2.add("speciesMass(Calcite units=g)", "Calcite");
+    output2.add("speciesMolality(HCO3-)", "HCO3- [molal]");
+    output2.add("speciesMolality(Ca++)", "Ca++  [molal]");
+    output2.filename("path-sa-" +  std::to_string(10) + ".txt");
+
+
     // Perform the kinetic path calculation with 10 m2 surface area for 100 minutes
-    path.solve(state02, 0, 100, "minutes");
+    path.solve(state02, 0, 30, "minutes");
+
+    double result2_calcite = state02.speciesAmount("Calcite");
+
+    std::cout << "dissol. Calcite, case 1: " << initial_calcite - result1_calcite << std::endl;
+    std::cout << "dissol. Calcite, case 2: " << initial_calcite - result2_calcite << std::endl;
+
+
 }
