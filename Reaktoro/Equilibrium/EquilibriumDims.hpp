3071a36b39569576833f1ea8eadadcb395962a1a
Allan Leal <>
2021-01-14 16:27:19 +0100

    Simplified EquilibriumDims class

--- Reaktoro/Equilibrium/EquilibriumDims.hpp
+++ Reaktoro/Equilibrium/EquilibriumDims.hpp
@@ -23,28 +23,42 @@
 namespace Reaktoro {
 
 // Forward declarations
-class EquilibriumConstraints;
+class EquilibriumSpecs;
 
-/// The dimensions of the variables in a constrained equilibrium problem.
-/// @see dims(const EquilibriumConstraints&)
+/// The dimensions of the variables in a chemical equilibrium problem.
+///
+/// In a chemical equilibrium problem, we are interested in computing the
+/// values of the following variables:
+///
+/// * @eq{n}, the vector with the amounts of the chemical species;
+/// * @eq{p}, the vector with the *p* control variables;
+/// * @eq{q}, the vector with the *q* control variables;
+///
+/// The *p* control variables are those variables introduced to enforce
+/// constraints of *equation type*. These are *temperature* and/or *pressure*, in
+/// case they are unknown, as well as the amounts of *explicit titrants* (the
+/// substances that we permit in/out of the system).
+///
+/// The *q* control variables are those variables introduced to enforce
+/// constraints of *chemical potential type*. These are *implicit titrants*
+/// (also substances allowed in/out of the system) whose amounts need to be
+/// found so that the chemical potential constraints are satisfied.
 struct EquilibriumDims
 {
-    Index Ne  = 0; ///< The number of chemical elements and electric charge in the chemical system.
-    Index Nn  = 0; ///< The number of species in the chemical system.
-    Index Npe = 0; ///< The number of equation constraints among the functional constraints.
-    Index Npp = 0; ///< The number of property preservation constraints among the functional constraints.
-    Index Np  = 0; ///< The number of functional constraints (`Np = Npe + Npp`).
-    Index Nq  = 0; ///< The number of chemical potential constraints.
-    Index Nir = 0; ///< The number of reactions prevented from reacting during the equilibrium calculation.
-    Index Ncv = 0; ///< The number of introduced control variables (must be equal to `Np + Nq`)
-    Index Nx  = 0; ///< The number of variables (`Nx = Nn + Np + Nq`).
-    Index Nc  = 0; ///< The number of components (`Nc = Ne + Nir`).
+    Index Ne = 0; ///< The number of elements in the chemical system.
+    Index Nc = 0; ///< The number of components in the chemical equilibrium problem (`Nc = Ne` if no inert reactions are specified).
+    Index Nn = 0; ///< The number of species in the chemical system.
+    Index Np = 0; ///< The number of *p* control variables (temperature, pressure, and amounts of explicit titrants when these are introduced unknowns).
+    Index Nq = 0; ///< The number of *q* control variables (the amounts of the implicit titrants when these are introduced unknowns).
+    Index Nt = 0; ///< The number of substances for which the chemical system is open to (the number of explicit and implicit titrants).
+    Index Nx = 0; ///< The number of variables *x* in *x = (n, q)* (equivalent to `Nn + Nq`).
+    Index Nu = 0; ///< The number of unknown variables in the chemical equilibrium problem (equivalent to `Nn + Np + Nq`).
 
     /// Construct a default EquilibriumDims object.
     EquilibriumDims() = default;
 
-    /// Construct an EquilibriumDims object with given equilibrium constraints.
-    explicit EquilibriumDims(const EquilibriumConstraints& constraints);
+    /// Construct an EquilibriumDims object with given equilibrium specifications.
+    explicit EquilibriumDims(const EquilibriumSpecs& specs);
 };
 
 } // namespace Reaktoro
