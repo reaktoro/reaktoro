7ac65efddae177307a7c579608c20f2917f0311d
Allan Leal <allanmulin@gmail.com>
2021-03-05 19:08:06 +0100

    Implemented Core/Utils

---
+++ Reaktoro/Core/Utils.hpp
@@ -0,0 +1,38 @@
+// Reaktoro is a unified framework for modeling chemically reactive systems.
+//
+// Copyright (C) 2014-2021 Allan Leal
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library. If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+// Reaktoro includes
+#include <Reaktoro/Common/Types.hpp>
+
+namespace Reaktoro {
+
+// Forward declarations
+class ChemicalSystem;
+
+namespace detail {
+
+/// Compute the amount of a species given a value in mass or amount unit.
+/// @param system The chemical system in which the species is.
+/// @param ispecies The index of the species in the chemical system.
+/// @param value The quantity value of the species.
+/// @param unit The quantity unit of the species.
+auto computeSpeciesAmount(const ChemicalSystem& system, Index ispecies, real value, const String& unit) -> real;
+
+} // namespace detail
+} // namespace Reaktoro
