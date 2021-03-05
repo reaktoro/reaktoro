e34f18ffa21fa94dc8a7d558cd2ff295b67531a3
Allan Leal <allanmulin@gmail.com>
2021-03-05 14:37:09 +0100

    Added file template for class

---
+++ resources/template/Class.cpp
@@ -0,0 +1,47 @@
+// Reaktoro is a unified framework for modeling chemically reactive systems.
+//
+// Copyright (C) 2014-2021 Allan Leal
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library. If not, see <http://www.gnu.org/licenses/>.
+
+#include "Class.hpp"
+
+namespace Reaktoro {
+
+struct Class::Impl
+{
+
+};
+
+Class::Class()
+: pimpl(new Impl())
+{
+}
+
+Class::Class(const Class& other)
+: pimpl(new Impl(*other.pimpl))
+{
+}
+
+Class::~Class()
+{
+}
+
+auto Class::operator=(Class other) -> Class&
+{
+    pimpl = std::move(other.pimpl);
+    return *this;
+}
+
+} // namespace Reaktoro
---
+++ resources/template/Class.hpp
@@ -0,0 +1,47 @@
+// Reaktoro is a unified framework for modeling chemically reactive systems.
+//
+// Copyright (C) 2014-2021 Allan Leal
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library. If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+// Reaktoro includes
+#include <Reaktoro/Common/Types.hpp>
+
+namespace Reaktoro {
+
+/// A class that still needs to be commented.
+class Class
+{
+public:
+    /// Construct a default Class object.
+    Class();
+
+    /// Construct a copy of a Class object.
+    Class(const Class& other);
+
+    /// Destroy this Class object.
+    ~Class();
+
+    /// Assign a copy of a Class object to this.
+    auto operator=(Class other) -> Class&;
+
+private:
+    struct Impl;
+
+    Ptr<Impl> pimpl;
+};
+
+} // namespace Reaktoro
