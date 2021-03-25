e4ea68ad883937bfee43fe67afda7a3e2f4c4cf2
Allan Leal <allanmulin@gmail.com>
2021-03-25 22:53:58 +0100

    Implemented tests for ReactionThermoModels to check Model::params and Model::serialize methos

--- Reaktoro/Models/ReactionThermoModelGemsLgK.test.cxx
+++ Reaktoro/Models/ReactionThermoModelGemsLgK.test.cxx
@@ -25,20 +25,24 @@ using namespace Reaktoro;
 
 TEST_CASE("Testing ReactionThermoModelGemsLgK class", "[ReactionThermoModelGemsLgK]")
 {
+    Param A0 = 1.0;
+    Param A1 = 2.0;
+    Param A2 = 3.0;
+    Param A3 = 4.0;
+    Param A4 = 5.0;
+    Param A5 = 6.0;
+    Param A6 = 7.0;
+    real  Pr = 7.0;
+
+    const auto model = ReactionThermoModelGemsLgK({A0, A1, A2, A3, A4, A5, A6, Pr});
+
+    //======================================================================
+    // Test method Model::operator()(T, P, dV0)
+    //======================================================================
+
     const auto T = 5.0;
     const auto P = 7.0;
     const auto dV0 = 9.0;
-
-    const auto A0 = 1.0;
-    const auto A1 = 2.0;
-    const auto A2 = 3.0;
-    const auto A3 = 4.0;
-    const auto A4 = 5.0;
-    const auto A5 = 6.0;
-    const auto A6 = 7.0;
-    const auto Pr = 7.0;
-
-    const auto model = ReactionThermoModelGemsLgK({A0, A1, A2, A3, A4, A5, A6, Pr});
 
     const auto R = universalGasConstant;
 
@@ -57,4 +61,38 @@ TEST_CASE("Testing ReactionThermoModelGemsLgK class", "[ReactionThermoModelGemsL
 
     CHECK( rprops.dG0 == Approx(dG0x) );
     CHECK( rprops.dH0 == Approx(dH0x) );
+
+    //======================================================================
+    // Test method Model::params()
+    //======================================================================
+
+    CHECK( model.params().size() == 7 );
+    CHECK( model.params()[0] == A0 );
+    CHECK( model.params()[1] == A1 );
+    CHECK( model.params()[2] == A2 );
+    CHECK( model.params()[3] == A3 );
+    CHECK( model.params()[4] == A4 );
+    CHECK( model.params()[5] == A5 );
+    CHECK( model.params()[6] == A6 );
+
+    //======================================================================
+    // Test method Model::serialize()
+    //======================================================================
+
+    yaml node;
+
+    node = model.serialize();
+    CHECK( double(node.at("GemsLgK").at("A0")) == A0 );
+    CHECK( double(node.at("GemsLgK").at("A1")) == A1 );
+    CHECK( double(node.at("GemsLgK").at("A2")) == A2 );
+    CHECK( double(node.at("GemsLgK").at("A3")) == A3 );
+    CHECK( double(node.at("GemsLgK").at("A4")) == A4 );
+    CHECK( double(node.at("GemsLgK").at("A5")) == A5 );
+    CHECK( double(node.at("GemsLgK").at("A6")) == A6 );
+    CHECK( double(node.at("GemsLgK").at("Pr")) == Pr );
+
+    A0 = 1234.0; // change value of Param object and check if new serialize call reflects this change
+
+    node = model.serialize();
+    CHECK( double(node.at("GemsLgK").at("A0")) == 1234.0 );
 }
