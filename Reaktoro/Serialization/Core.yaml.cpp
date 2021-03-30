db440e56a70d7b569d1f70807a4fe51823cb750c
Allan Leal <allanmulin@gmail.com>
2021-03-30 13:13:20 +0200

    A few more Core.yaml serialization operators implemented.

--- Reaktoro/Serialization/Core.yaml.cpp
+++ Reaktoro/Serialization/Core.yaml.cpp
@@ -22,6 +22,7 @@
 #include <Reaktoro/Core/ChemicalFormula.hpp>
 #include <Reaktoro/Core/ChemicalSystem.hpp>
 #include <Reaktoro/Core/Element.hpp>
+#include <Reaktoro/Core/FormationReaction.hpp>
 #include <Reaktoro/Core/Param.hpp>
 #include <Reaktoro/Core/Params.hpp>
 #include <Reaktoro/Core/Phase.hpp>
@@ -29,6 +30,22 @@
 #include <Reaktoro/Serialization/Common.yaml.hpp>
 
 namespace Reaktoro {
+
+//=====================================================================================================================
+
+auto operator<<(yaml& node, const AggregateState& obj) -> void
+{
+    std::stringstream ss;
+    ss << obj;
+    node = ss.str();
+}
+
+auto operator>>(const yaml& node, AggregateState& obj) -> void
+{
+    obj = parseAggregateState(node.as<std::string>());
+}
+
+//=====================================================================================================================
 
 auto operator<<(yaml& node, const ChemicalFormula& obj) -> void
 {
@@ -60,21 +77,44 @@ auto operator<<(yaml& node, const Element& obj) -> void
     node["Name"]              = obj.name();
     node["AtomicNumber"]      = obj.atomicNumber();
     node["AtomicWeight"]      = obj.atomicWeight();
-    node["MolarMass"]         = obj.molarMass();
     node["Electronegativity"] = obj.electronegativity();
     node["Tags"]              = obj.tags();
 }
 
 auto operator>>(const yaml& node, Element& obj) -> void
 {
-    Element::Args args;
-    node.at("Symbol").to(args.symbol);
-    node.at("Name").to(args.name);
-    node.at("AtomicNumber").to(args.atomic_number);
-    node.at("AtomicWeight").to(args.atomic_weight);
-    node.at("Electronegativity").to(args.electronegativity);
-    node.at("Tags").to(args.tags);
-    obj = Element(args);
+    Element::Attribs attribs;
+    node.required("Symbol", attribs.symbol);
+    node.required("Name", attribs.name);
+    node.required("AtomicNumber", attribs.atomic_number);
+    node.required("AtomicWeight", attribs.atomic_weight);
+    node.required("Electronegativity", attribs.electronegativity);
+    node.required("Tags", attribs.tags);
+    obj = Element(attribs);
+}
+
+//=====================================================================================================================
+
+auto operator<<(yaml& node, const ElementalComposition& obj) -> void
+{
+    errorif(true, "`auto operator<<(yaml& node, const ElementalComposition& obj) -> void` not implemented!");
+}
+
+auto operator>>(const yaml& node, ElementalComposition& obj) -> void
+{
+    errorif(true, "`auto operator>>(const yaml& node, ElementalComposition& obj) -> void` not implemented!");
+}
+
+//=====================================================================================================================
+
+auto operator<<(yaml& node, const FormationReaction& obj) -> void
+{
+    errorif(true, "`auto operator<<(yaml& node, const FormationReaction& obj) -> void` not implemented!");
+}
+
+auto operator>>(const yaml& node, FormationReaction& obj) -> void
+{
+    errorif(true, "`auto operator>>(const yaml& node, FormationReaction& obj) -> void` not implemented!");
 }
 
 //=====================================================================================================================
@@ -118,12 +158,22 @@ auto operator>>(const yaml& node, Phase& obj) -> void
 
 auto operator<<(yaml& node, const Species& obj) -> void
 {
-
+    errorif(true, "`auto operator<<(yaml& node, const Species& obj) -> void` not implemented!");
 }
 
 auto operator>>(const yaml& node, Species& obj) -> void
 {
-
+    Species::Attribs attribs;
+    node.optional("Name", attribs.name);
+    node.required("Formula", attribs.formula);
+    node.optional("Substance", attribs.substance);
+    node.optional("Elements", attribs.elements);
+    node.optional("Charge", attribs.charge);
+    node.optional("AggregateState", attribs.aggregate_state);
+    node.optional("FormationReaction", attribs.formation_reaction);
+    // node.optional("StandardThermoModel", attribs.std_thermo_model);
+    node.optional("Tags", attribs.tags);
+    obj = Species(attribs);
 }
 
 } // namespace YAML
