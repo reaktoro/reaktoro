b668695ba6a10680cd0922714e191f64817ed649
Allan Leal <allanmulin@gmail.com>
2021-06-15 08:10:03 +0200

    Implemented support class DatabaseDecoderYAML

---
+++ Reaktoro/Serialization/Support/DatabaseDecoderYAML.test.cxx
@@ -0,0 +1,101 @@
+// Reaktoro is a unified framework for modeling chemically reactive systems.
+//
+// Copyright (C) 2014-2021 Allan Leal
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library. If not, see <http://www.gnu.org/licenses/>.
+
+// Catch includes
+#include <catch2/catch.hpp>
+
+// Reaktoro includes
+#include <Reaktoro/Common/YAML.hpp>
+#include <Reaktoro/Core/Database.hpp>
+#include <Reaktoro/Serialization/Support/DatabaseDecoderYAML.hpp>
+using namespace Reaktoro;
+
+std::string doc = R"(
+Elements:
+  - Symbol: A
+    MolarMass: 1.0
+  - Symbol: B
+    MolarMass: 2.0
+Species:
+  - Name: A2B
+    Formula: A2B
+    Elements: 2:A 1:B
+    Charge: 0.0
+    AggregateState: Liquid
+    StandardThermoModel:
+      MaierKelley:
+        Gf: 1.234
+        Hf: 2.345
+        Sr: 0.0
+        Vr: 0.0
+        a: 0.0
+        b: 0.0
+        c: 0.0
+        Tmax: 0.0
+  - Name: A2B3
+    Formula: A2B3
+    Elements: 2:A 3:B
+    Charge: 1.0
+    AggregateState: Aqueous
+    StandardThermoModel:
+      HKF:
+        Gf: 0.1
+        Hf: 0.2
+        Sr: 0.3
+        a1: 0.4
+        a2: 0.5
+        a3: 0.6
+        a4: 0.7
+        c1: 0.8
+        c2: 0.9
+        wref: 1.0
+        charge: 1.0
+        Tmax: 1.2
+)";
+
+TEST_CASE("Testing DatabaseDecoderYAML class", "[DatabaseDecoderYAML]")
+{
+    yaml node(doc);
+
+    Database db = DatabaseDecoderYAML(node);
+
+    auto elements = db.elements();
+    auto species = db.species();
+
+    CHECK( elements.size() == 2 );
+
+    CHECK( elements[0].symbol() == "A" );
+    CHECK( elements[0].molarMass() == 1.0 );
+    CHECK( elements[1].symbol() == "B" );
+    CHECK( elements[1].molarMass() == 2.0 );
+
+    CHECK( species.size() == 2 );
+
+    CHECK( species[0].name() == "A2B" );
+    CHECK( species[0].formula() == "A2B" );
+    CHECK( species[0].elements().coefficient("A") == 2.0 );
+    CHECK( species[0].elements().coefficient("B") == 1.0 );
+    CHECK( species[0].aggregateState() == AggregateState::Liquid );
+    CHECK( species[0].standardThermoModel().serialize()["MaierKelley"].IsDefined() );
+
+    CHECK( species[1].name() == "A2B3" );
+    CHECK( species[1].formula() == "A2B3" );
+    CHECK( species[1].elements().coefficient("A") == 2.0 );
+    CHECK( species[1].elements().coefficient("B") == 3.0 );
+    CHECK( species[1].aggregateState() == AggregateState::Aqueous );
+    CHECK( species[1].standardThermoModel().serialize()["HKF"].IsDefined() );
+}
