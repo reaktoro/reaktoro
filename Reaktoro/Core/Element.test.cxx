84d55887b967d96f97dcf8f768604ebc291cf728
Allan Leal <allanmulin@gmail.com>
2021-06-16 12:29:24 +0200

    Improved Element(Element::Attribs) constructor and also checks for molar mass

--- Reaktoro/Core/Element.test.cxx
+++ Reaktoro/Core/Element.test.cxx
@@ -25,23 +25,48 @@ using namespace Reaktoro;
 
 TEST_CASE("Testing Element", "[Element]")
 {
-    Element element({ "H", 0.001007940, "Hydrogen" });
+    WHEN("using constructor Element()")
+    {
+        Element element;
 
-    REQUIRE(element.symbol() == "H");
-    REQUIRE(element.name() == "Hydrogen");
-    REQUIRE(element.molarMass() == 0.001007940);
+        element = element.withSymbol("Na");
+        CHECK(element.symbol() == "Na");
 
-    element = element.withSymbol("Na");
-    REQUIRE(element.symbol() == "Na");
+        element = element.withName("Sodium");
+        CHECK(element.name() == "Sodium");
 
-    element = element.withName("Sodium");
-    REQUIRE(element.name() == "Sodium");
+        element = element.withMolarMass(0.022989768);
+        CHECK(element.molarMass() == 0.022989768);
 
-    element = element.withMolarMass(0.022989768);
-    REQUIRE(element.molarMass() == 0.022989768);
+        element = element.withTags({"tag1", "tag2"});
+        CHECK(element.tags().size() == 2);
+        CHECK(contains(element.tags(), "tag1"));
+        CHECK(contains(element.tags(), "tag2"));
+    }
 
-    element = element.withTags({"tag1", "tag2"});
-    REQUIRE(element.tags().size() == 2);
-    REQUIRE(contains(element.tags(), "tag1"));
-    REQUIRE(contains(element.tags(), "tag2"));
+    WHEN("using constructor Element(Element::Attribs)")
+    {
+        Element element({ "H", 0.001007940, "Hydrogen" });
+
+        CHECK(element.symbol() == "H");
+        CHECK(element.name() == "Hydrogen");
+        CHECK(element.molarMass() == 0.001007940);
+
+        CHECK_NOTHROW( element.withMolarMass( 0.0) );
+        CHECK_THROWS( element.withMolarMass(-1.0) );
+    }
+
+    WHEN("using constructor Element(Element::Attribs) without all required attributes")
+    {
+        using Catch::Contains;
+
+        Element::Attribs attribs;
+
+        CHECK_THROWS_WITH(Element(attribs), Contains("Element::Attribs::symbol cannot be empty"));
+
+        attribs.symbol = "O";
+
+        attribs.molar_mass = 1.0;
+        CHECK_NOTHROW(Element(attribs));
+    }
 }
