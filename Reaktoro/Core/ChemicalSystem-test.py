a9d657b8d93e8e292e8b0997ca281d216ee818f5
Allan Leal <allanmulin@gmail.com>
2021-06-30 15:41:56 +0200

    Implemented python tests for ChemicalSystem

---
+++ Reaktoro/Core/ChemicalSystem-test.py
@@ -0,0 +1,62 @@
+# Reaktoro is a unified framework for modeling chemically reactive systems.
+#
+# Copyright (C) 2014-2021 Allan Leal
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this library. If not, see <http://www.gnu.org/licenses/>.
+
+
+from reaktoro import *
+import pytest
+
+
+def testChemicalSystem():
+    db = Database("supcrtbl.yaml")
+
+    phases = Phases(db)
+    phases.add( AqueousPhase("H2O(aq) H+ OH- Na+ Cl- Ca+2 Mg+2 HCO3- CO3-2 CO2(aq) SiO2(aq)") )
+    phases.add( GaseousPhase("H2O(g) CO2(g)") )
+    phases.add( MineralPhase("Halite") )
+    phases.add( MineralPhase("Calcite") )
+    phases.add( MineralPhase("Magnesite") )
+    phases.add( MineralPhase("Dolomite") )
+    phases.add( MineralPhase("Quartz") )
+
+    system = ChemicalSystem(phases)
+
+    assert system.database().elements().size() == db.elements().size()
+    assert system.database().species().size() == db.species().size()
+
+    assert system.elements().size() == 8
+    assert system.species().size() == 18
+    assert system.phases().size() == 7
+
+    assert system.element(0).symbol() == "H"
+    assert system.element(1).symbol() == "C"
+
+    assert system.species(0).name() == "H2O(aq)"
+    assert system.species(1).name() == "H+"
+
+    assert system.phase(0).name() == "AqueousPhase"
+    assert system.phase(1).name() == "GaseousPhase"
+
+    assert (system.formulaMatrix() ==
+        [[ 2,  1,  1,  0,  0,  0,  0,  1,  0,  0,  0,  2,  0,  0,  0,  0,  0,  0],
+         [ 0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  0,  0,  1,  0,  1,  1,  2,  0],
+         [ 1,  0,  1,  0,  0,  0,  0,  3,  3,  2,  2,  1,  2,  0,  3,  3,  6,  2],
+         [ 0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  0,  0,  0,  0],
+         [ 0,  0,  0,  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  0],
+         [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  1],
+         [ 0,  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0,  0,  1,  0,  0,  0,  0],
+         [ 0,  0,  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0,  0,  1,  0,  1,  0],
+         [ 0,  1, -1,  1, -1,  2,  2, -1, -2,  0,  0,  0,  0,  0,  0,  0,  0,  0]]).all()
