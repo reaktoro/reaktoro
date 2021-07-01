c72bf17331c05d52bca29b8fcd185c62fd23c975
Allan Leal <allanmulin@gmail.com>
2021-07-01 11:50:15 +0200

    Implemented tests for python interface of AggregateState

--- Reaktoro/Core/AggregateState[test].py
+++ Reaktoro/Core/AggregateState[test].py
@@ -20,6 +20,88 @@ from reaktoro import *
 import pytest
 
 
-# TODO Implement tests for the python bindings of component AggregateState in AggregateState[test].py
 def testAggregateState():
-    pass
+    assert parseAggregateState("g")   == AggregateState.Gas
+    assert parseAggregateState("l")   == AggregateState.Liquid
+    assert parseAggregateState("s")   == AggregateState.Solid
+    assert parseAggregateState("pl")  == AggregateState.Plasma
+    assert parseAggregateState("cd")  == AggregateState.CondensedPhase
+    assert parseAggregateState("fl")  == AggregateState.Fluid
+    assert parseAggregateState("lc")  == AggregateState.LiquidCrystal
+    assert parseAggregateState("cr")  == AggregateState.CrystallineSolid
+    assert parseAggregateState("am")  == AggregateState.AmorphousSolid
+    assert parseAggregateState("vit") == AggregateState.Vitreous
+    assert parseAggregateState("ads") == AggregateState.Adsorbed
+    assert parseAggregateState("mon") == AggregateState.Monomeric
+    assert parseAggregateState("pol") == AggregateState.Polymeric
+    assert parseAggregateState("ss")  == AggregateState.SolidSolution
+    assert parseAggregateState("ex")  == AggregateState.IonExchange
+    assert parseAggregateState("aq")  == AggregateState.Aqueous
+    assert parseAggregateState("xy")  == AggregateState.Undefined
+
+    assert parseAggregateState("Gas")              == AggregateState.Gas
+    assert parseAggregateState("Liquid")           == AggregateState.Liquid
+    assert parseAggregateState("Solid")            == AggregateState.Solid
+    assert parseAggregateState("Plasma")           == AggregateState.Plasma
+    assert parseAggregateState("CondensedPhase")   == AggregateState.CondensedPhase
+    assert parseAggregateState("Fluid")            == AggregateState.Fluid
+    assert parseAggregateState("LiquidCrystal")    == AggregateState.LiquidCrystal
+    assert parseAggregateState("CrystallineSolid") == AggregateState.CrystallineSolid
+    assert parseAggregateState("AmorphousSolid")   == AggregateState.AmorphousSolid
+    assert parseAggregateState("Vitreous")         == AggregateState.Vitreous
+    assert parseAggregateState("Adsorbed")         == AggregateState.Adsorbed
+    assert parseAggregateState("Monomeric")        == AggregateState.Monomeric
+    assert parseAggregateState("Polymeric")        == AggregateState.Polymeric
+    assert parseAggregateState("SolidSolution")    == AggregateState.SolidSolution
+    assert parseAggregateState("IonExchange")      == AggregateState.IonExchange
+    assert parseAggregateState("Aqueous")          == AggregateState.Aqueous
+    assert parseAggregateState("Undefined")        == AggregateState.Undefined
+    assert parseAggregateState("XYZ")              == AggregateState.Undefined
+
+    assert identifyAggregateState("XYZ(g)")       == AggregateState.Gas
+    assert identifyAggregateState("XYZ(l)")       == AggregateState.Liquid
+    assert identifyAggregateState("XYZ(s)")       == AggregateState.Solid
+    assert identifyAggregateState("XYZ(s, xyz)")  == AggregateState.Solid
+    assert identifyAggregateState("XYZ(pl)")      == AggregateState.Plasma
+    assert identifyAggregateState("XYZ(cd)")      == AggregateState.CondensedPhase
+    assert identifyAggregateState("XYZ(fl)")      == AggregateState.Fluid
+    assert identifyAggregateState("XYZ(lc)")      == AggregateState.LiquidCrystal
+    assert identifyAggregateState("XYZ(cr)")      == AggregateState.CrystallineSolid
+    assert identifyAggregateState("XYZ(am)")      == AggregateState.AmorphousSolid
+    assert identifyAggregateState("XYZ(vit)")     == AggregateState.Vitreous
+    assert identifyAggregateState("XYZ(ads)")     == AggregateState.Adsorbed
+    assert identifyAggregateState("XYZ(mon)")     == AggregateState.Monomeric
+    assert identifyAggregateState("XYZ(pol)")     == AggregateState.Polymeric
+    assert identifyAggregateState("XYZ(ss)")      == AggregateState.SolidSolution
+    assert identifyAggregateState("XYZ(ex)")      == AggregateState.IonExchange
+    assert identifyAggregateState("XYZ(aq)")      == AggregateState.Aqueous
+    assert identifyAggregateState("XYZ(aq, uvw)") == AggregateState.Aqueous
+
+    assert identifyAggregateState("XYZ-")         == AggregateState.Aqueous
+    assert identifyAggregateState("XYZ--")        == AggregateState.Aqueous
+    assert identifyAggregateState("XYZ---")       == AggregateState.Aqueous
+    assert identifyAggregateState("XYZ-2")        == AggregateState.Aqueous
+    assert identifyAggregateState("XYZ-3")        == AggregateState.Aqueous
+
+    assert identifyAggregateState("XYZ+")         == AggregateState.Aqueous
+    assert identifyAggregateState("XYZ++")        == AggregateState.Aqueous
+    assert identifyAggregateState("XYZ+++")       == AggregateState.Aqueous
+    assert identifyAggregateState("XYZ+2")        == AggregateState.Aqueous
+    assert identifyAggregateState("XYZ+3")        == AggregateState.Aqueous
+    assert identifyAggregateState("XYZ[2-]")      == AggregateState.Aqueous
+
+    assert identifyAggregateState("XYZ-(pl)")     == AggregateState.Plasma
+    assert identifyAggregateState("XYZ--(pl)")    == AggregateState.Plasma
+    assert identifyAggregateState("XYZ---(pl)")   == AggregateState.Plasma
+    assert identifyAggregateState("XYZ-2(pl)")    == AggregateState.Plasma
+    assert identifyAggregateState("XYZ-3(pl)")    == AggregateState.Plasma
+
+    assert identifyAggregateState("XYZ+(pl)")     == AggregateState.Plasma
+    assert identifyAggregateState("XYZ++(pl)")    == AggregateState.Plasma
+    assert identifyAggregateState("XYZ+++(pl)")   == AggregateState.Plasma
+    assert identifyAggregateState("XYZ[3+](pl)")  == AggregateState.Plasma
+    assert identifyAggregateState("XYZ+2(pl)")    == AggregateState.Plasma
+    assert identifyAggregateState("XYZ+3(pl)")    == AggregateState.Plasma
+
+    assert identifyAggregateState("XYZ")          == AggregateState.Undefined
+    assert identifyAggregateState("XYZ(xy)")      == AggregateState.Undefined
