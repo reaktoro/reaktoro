db3516b6b07d70cb38642b97178a0c7207329ed4
alexandrebbruno <alexandrebbruno@gmail.com>
2021-07-08 15:56:20 -0300

    create binds for phaseCompressibilityFactors

--- Reaktoro/Thermodynamics/ThermodynamicsUtils.cpp
+++ Reaktoro/Thermodynamics/ThermodynamicsUtils.cpp
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with this library. If not, see <http://www.gnu.org/licenses/>.
 
-#include "Utils.hpp"
+#include "ThermodynamicsUtils.hpp"
 
 #include <Reaktoro/Common/Constants.hpp>
 
