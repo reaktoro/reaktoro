1b3fcacd3817ca69351740e486a09d0d95fb549a
Allan Leal <allanmulin@gmail.com>
2021-07-09 19:50:31 +0200

    Now CCache.cmake works for both Unix and Windows

--- cmake/CCache.cmake
+++ cmake/CCache.cmake
@@ -1,17 +1,17 @@
 # Speed up repeated and similar compilation operations with this cmake module.
 #
-# This module tries first to find if ccache is installed in the system.
-# If found, the cmake variables CMAKE_C_COMPILER_LAUNCHER and
-# CMAKE_CXX_COMPILER_LAUNCHER are then set to the ccache program.
-# As a result, ccache will be used, together with the chosen
-# (or default) compiler to cache the compilations. Subsequent
-# compilations can then be performed much faster (e.g., changing
+# This module tries first to find if ccache (unix) or clcache (windows) is
+# installed in the system. If found, the cmake variables
+# CMAKE_C_COMPILER_LAUNCHER and CMAKE_CXX_COMPILER_LAUNCHER are then set to the
+# ccache program in Unix. In Windows, the cmake variables CMAKE_C_COMPILER and
+# CMAKE_CXX_COMPILER are set to the clcache program. As a result, ccache or
+# clcahe will be used, together with a compiler to cache the compilations.
+# Subsequent compilations can then be performed much faster (e.g., changing
 # branches).
 #
 # Add the option -DCCACHE_IGNORE=TRUE to ignore ccache speed up.
 #
-# *** IMPORTANT ***
-# To use this module, add:
+# *** IMPORTANT *** To use this module, add:
 #
 #     include(CCache)
 #
@@ -23,17 +23,22 @@
 
 if(WIN32)
     find_program(CCACHE_PROGRAM clcache)
+    if(NOT CCACHE_IGNORE AND CCACHE_PROGRAM)
+        message(STATUS "CCache: Found ${CCACHE_PROGRAM}.")
+        message(STATUS "CCache: Using it to potentially speed up the build operation.")
+        set(CMAKE_C_COMPILER ${CCACHE_PROGRAM})
+        set(CMAKE_CXX_COMPILER ${CCACHE_PROGRAM})
+    else()
+        message(STATUS "CCache: Could not find clcache. Install it to speed up similar build operations.")
+    endif()
 else()
     find_program(CCACHE_PROGRAM ccache)
-endif()
-
-if(NOT CCACHE_IGNORE AND CCACHE_PROGRAM)
-    message(STATUS "CCache: Found ${CCACHE_PROGRAM}.")
-    message(STATUS "CCache: Using it to potentially speed up the build operation.")
-    set(CMAKE_C_COMPILER            ${CCACHE_PROGRAM})
-    set(CMAKE_C_COMPILER_LAUNCHER   ${CCACHE_PROGRAM})
-    set(CMAKE_CXX_COMPILER          ${CCACHE_PROGRAM})
-    set(CMAKE_CXX_COMPILER_LAUNCHER ${CCACHE_PROGRAM})
-else()
-    message(STATUS "CCache: Could not find ccache (unix) or clcache (win). Install it to speed up the build operation.")
+    if(NOT CCACHE_IGNORE AND CCACHE_PROGRAM)
+        message(STATUS "CCache: Found ccache installed.")
+        message(STATUS "CCache: Using ccache to potentially speed up the build operation.")
+        set(CMAKE_C_COMPILER_LAUNCHER ${CCACHE_PROGRAM})
+        set(CMAKE_CXX_COMPILER_LAUNCHER ${CCACHE_PROGRAM})
+    else()
+        message(STATUS "CCache: Could not find ccache. Install it to speed up similar build operations.")
+    endif()
 endif()
