34c34e629f4f685f50aee36d78104562c35b51f3
Allan Leal <allanmulin@gmail.com>
2021-07-14 13:23:02 +0200

    Fix typo and remove alignment on );

--- Reaktoro/Core/Phases.py
+++ Reaktoro/Core/Phases.py
@@ -20,11 +20,12 @@ from reaktoro import *
 import pytest
 
 
-def checkSpeicesInPhase(phase, expected_species):
+def checkSpeciesInPhase(phase, expected_species):
     phase_species = ""
     for species in phase.species():
         phase_species += species.name() + " "
     assert phase_species[0:-1] == expected_species
+
 
 def testPhases():
 
@@ -86,45 +87,44 @@ def testPhases():
     # -----------------------------------------------------------------------------------------------------------------
 
     db = Database()
-    db.addSpecies( Species("H2O(aq)")                            )
-    db.addSpecies( Species("H+")                                 )
-    db.addSpecies( Species("OH-")                                )
-    db.addSpecies( Species("H2(aq)")                             )
-    db.addSpecies( Species("O2(aq)")                             )
-    db.addSpecies( Species("Na+")                                )
-    db.addSpecies( Species("Cl-")                                )
-    db.addSpecies( Species("NaCl(aq)")                           )
-    db.addSpecies( Species("HCl(aq)")                            )
-    db.addSpecies( Species("NaOH(aq)")                           )
-    db.addSpecies( Species("Ca++")                               )
-    db.addSpecies( Species("Mg++")                               )
-    db.addSpecies( Species("CO2(aq)")                            )
-    db.addSpecies( Species("HCO3-")                              )
-    db.addSpecies( Species("CO3--")                              )
-    db.addSpecies( Species("CaCl2(aq)")                          )
-    db.addSpecies( Species("MgCl2(aq)")                          )
-    db.addSpecies( Species("SiO2(aq)")                           )
-    db.addSpecies( Species("NaCl(s)").withName("Halite")         )
-    db.addSpecies( Species("SiO2(s)").withName("Quartz")         )
-    db.addSpecies( Species("CO2(g)")                             )
-    db.addSpecies( Species("O2(g)")                              )
-    db.addSpecies( Species("H2(g)")                              )
-    db.addSpecies( Species("H2O(g)")                             )
-    db.addSpecies( Species("CH4(g)")                             )
-    db.addSpecies( Species("CO(g)")                              )
-
-    db.addSpecies( Species("CaCO3(s)").withName("Calcite").withTags("carbonate")        )
-    db.addSpecies( Species("MgCO3(s)").withName("Magnesite").withTags("carbonate")      )
-    db.addSpecies( Species("CaMg(CO3)2(s)").withName("Dolomite").withTags("carbonate")  )
-    db.addSpecies( Species("C(s)").withName("Graphite")                                 )
-    db.addSpecies( Species("CaO(s)").withName("Lime")                                   )
-    db.addSpecies( Species("N2(g)").withTags("inert")                                   )
-    db.addSpecies( Species("C4H9OH").withTags("organic").withName("1-Butanol(aq)")      )
-    db.addSpecies( Species("C4H8").withTags("organic").withName("1-Butene(aq)")         )
-    db.addSpecies( Species("BaSO4(s)").withName("Barite").withTags("sulfate")           )
-    db.addSpecies( Species("SrSO4(s)").withName("Celestite").withTags("sulfate")        )
-    db.addSpecies( Species("PbSO4(s)").withName("Anglesite").withTags("sulfate")        )
-    db.addSpecies( Species("CaSO4(s)").withName("Anhydrite").withTags("sulfate")        )
+    db.addSpecies( Species("H2O(aq)") )
+    db.addSpecies( Species("H+") )
+    db.addSpecies( Species("OH-") )
+    db.addSpecies( Species("H2(aq)") )
+    db.addSpecies( Species("O2(aq)") )
+    db.addSpecies( Species("Na+") )
+    db.addSpecies( Species("Cl-") )
+    db.addSpecies( Species("NaCl(aq)") )
+    db.addSpecies( Species("HCl(aq)") )
+    db.addSpecies( Species("NaOH(aq)") )
+    db.addSpecies( Species("Ca++") )
+    db.addSpecies( Species("Mg++") )
+    db.addSpecies( Species("CO2(aq)") )
+    db.addSpecies( Species("HCO3-") )
+    db.addSpecies( Species("CO3--") )
+    db.addSpecies( Species("CaCl2(aq)") )
+    db.addSpecies( Species("MgCl2(aq)") )
+    db.addSpecies( Species("SiO2(aq)") )
+    db.addSpecies( Species("NaCl(s)").withName("Halite") )
+    db.addSpecies( Species("SiO2(s)").withName("Quartz") )
+    db.addSpecies( Species("CO2(g)") )
+    db.addSpecies( Species("O2(g)") )
+    db.addSpecies( Species("H2(g)") )
+    db.addSpecies( Species("H2O(g)") )
+    db.addSpecies( Species("CH4(g)") )
+    db.addSpecies( Species("CO(g)") )
+    db.addSpecies( Species("CaCO3(s)").withName("Calcite").withTags("carbonate") )
+    db.addSpecies( Species("MgCO3(s)").withName("Magnesite").withTags("carbonate") )
+    db.addSpecies( Species("CaMg(CO3)2(s)").withName("Dolomite").withTags("carbonate") )
+    db.addSpecies( Species("C(s)").withName("Graphite") )
+    db.addSpecies( Species("CaO(s)").withName("Lime") )
+    db.addSpecies( Species("N2(g)").withTags("inert") )
+    db.addSpecies( Species("C4H9OH").withTags("organic").withName("1-Butanol(aq)") )
+    db.addSpecies( Species("C4H8").withTags("organic").withName("1-Butene(aq)") )
+    db.addSpecies( Species("BaSO4(s)").withName("Barite").withTags("sulfate") )
+    db.addSpecies( Species("SrSO4(s)").withName("Celestite").withTags("sulfate") )
+    db.addSpecies( Species("PbSO4(s)").withName("Anglesite").withTags("sulfate") )
+    db.addSpecies( Species("CaSO4(s)").withName("Anhydrite").withTags("sulfate") )
 
     # -----------------------------------------------------------------------------------------------------------------
     # Testing AqueousPhase with provided speciates
@@ -141,8 +141,8 @@ def testPhases():
     assert phases[0].name() == "AqueousPhase"
     assert phases[1].name() == "GaseousPhase"
 
-    checkSpeicesInPhase(phases[0], "H2O(aq) H+ OH- H2(aq) O2(aq) Na+ Cl- NaCl(aq) HCl(aq) NaOH(aq) CO2(aq) HCO3- CO3-- 1-Butanol(aq) 1-Butene(aq)")
-    checkSpeicesInPhase(phases[1], "CO2(g)")
+    checkSpeciesInPhase(phases[0], "H2O(aq) H+ OH- H2(aq) O2(aq) Na+ Cl- NaCl(aq) HCl(aq) NaOH(aq) CO2(aq) HCO3- CO3-- 1-Butanol(aq) 1-Butene(aq)")
+    checkSpeciesInPhase(phases[1], "CO2(g)")
 
     # -----------------------------------------------------------------------------------------------------------------
     # Testing AqueousPhase with provided speciates and tags, so that species possessing them are excluded
@@ -159,8 +159,8 @@ def testPhases():
     assert phases[0].name() == "AqueousPhase"
     assert phases[1].name() == "GaseousPhase"
 
-    checkSpeicesInPhase(phases[0], "H2O(aq) H+ OH- H2(aq) O2(aq) Na+ Cl- NaCl(aq) HCl(aq) NaOH(aq) CO2(aq) HCO3- CO3--")
-    checkSpeicesInPhase(phases[1], "CO2(g)")
+    checkSpeciesInPhase(phases[0], "H2O(aq) H+ OH- H2(aq) O2(aq) Na+ Cl- NaCl(aq) HCl(aq) NaOH(aq) CO2(aq) HCO3- CO3--")
+    checkSpeciesInPhase(phases[1], "CO2(g)")
 
     # -----------------------------------------------------------------------------------------------------------------
     # AqueousPhase with provided tags, so that species possessing them are excluded
@@ -177,8 +177,8 @@ def testPhases():
     assert phases[0].name() == "AqueousPhase"
     assert phases[1].name() == "GaseousPhase"
 
-    checkSpeicesInPhase(phases[0], "H2O(aq) H+ OH- H2(aq) O2(aq)")
-    checkSpeicesInPhase(phases[1], "CO2(g)")
+    checkSpeciesInPhase(phases[0], "H2O(aq) H+ OH- H2(aq) O2(aq)")
+    checkSpeciesInPhase(phases[1], "CO2(g)")
 
     # -----------------------------------------------------------------------------------------------------------------
     # Testing MineralPhases with provided speciate symbols and tags, so that species possessing them are excluded
@@ -196,9 +196,9 @@ def testPhases():
     assert phases[1].name() == "Graphite"
     assert phases[2].name() == "Lime"
 
-    checkSpeicesInPhase(phases[0], "H2O(aq) H+ OH- H2(aq) O2(aq)")
-    checkSpeicesInPhase(phases[1], "Graphite")
-    checkSpeicesInPhase(phases[2], "Lime")
+    checkSpeciesInPhase(phases[0], "H2O(aq) H+ OH- H2(aq) O2(aq)")
+    checkSpeciesInPhase(phases[1], "Graphite")
+    checkSpeciesInPhase(phases[2], "Lime")
 
     # -----------------------------------------------------------------------------------------------------------------
     # Testing MineralPhases with provided tags, so that species possessing them are excluded
@@ -217,9 +217,9 @@ def testPhases():
     assert phases[1].name() == "GaseousPhase"
     assert phases[2].name() == "Graphite"
 
-    checkSpeicesInPhase(phases[0], "H2O(aq) H+ OH- H2(aq) O2(aq) CO2(aq) HCO3- CO3--")
-    checkSpeicesInPhase(phases[1], "H2O(g) CO2(g)")
-    checkSpeicesInPhase(phases[2], "Graphite")
+    checkSpeciesInPhase(phases[0], "H2O(aq) H+ OH- H2(aq) O2(aq) CO2(aq) HCO3- CO3--")
+    checkSpeciesInPhase(phases[1], "H2O(g) CO2(g)")
+    checkSpeciesInPhase(phases[2], "Graphite")
 
     # -----------------------------------------------------------------------------------------------------------------
     # Testing GaseousPhases with provided speciates and tags, so that species possessing them are excluded
@@ -238,9 +238,9 @@ def testPhases():
     assert phases[1].name() == "GaseousPhase"
     assert phases[2].name() == "Graphite"
 
-    checkSpeicesInPhase(phases[0], "H2O(aq) H+ OH- H2(aq) O2(aq) CO2(aq) HCO3- CO3--")
-    checkSpeicesInPhase(phases[1], "CO2(g) O2(g) H2(g) H2O(g) CH4(g) CO(g)")
-    checkSpeicesInPhase(phases[2], "Graphite")
+    checkSpeciesInPhase(phases[0], "H2O(aq) H+ OH- H2(aq) O2(aq) CO2(aq) HCO3- CO3--")
+    checkSpeciesInPhase(phases[1], "CO2(g) O2(g) H2(g) H2O(g) CH4(g) CO(g)")
+    checkSpeciesInPhase(phases[2], "Graphite")
 
     # -----------------------------------------------------------------------------------------------------------------
     # Testing GaseousPhases with provided tags, so that species possessing them are excluded
@@ -260,7 +260,7 @@ def testPhases():
     assert phases[2].name() == "Halite"
     assert phases[3].name() == "Graphite"
 
-    checkSpeicesInPhase(phases[0], "H2O(aq) H+ OH- H2(aq) O2(aq) Na+ Cl- NaCl(aq) HCl(aq) NaOH(aq) CO2(aq) HCO3- CO3--")
-    checkSpeicesInPhase(phases[1], "CO2(g) O2(g) H2(g) H2O(g) CH4(g) CO(g)")
-    checkSpeicesInPhase(phases[2], "Halite")
-    checkSpeicesInPhase(phases[3], "Graphite")
+    checkSpeciesInPhase(phases[0], "H2O(aq) H+ OH- H2(aq) O2(aq) Na+ Cl- NaCl(aq) HCl(aq) NaOH(aq) CO2(aq) HCO3- CO3--")
+    checkSpeciesInPhase(phases[1], "CO2(g) O2(g) H2(g) H2O(g) CH4(g) CO(g)")
+    checkSpeciesInPhase(phases[2], "Halite")
+    checkSpeciesInPhase(phases[3], "Graphite")
