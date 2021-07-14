513280339b49d5b9adb5d9e91f7f1e6b6a23c8fe
Svetlana Kyas <mtsveta@gmail.com>
2021-07-14 20:05:34 +0200

    Renaming the example of the supcrt-database usage

--- examples/python/ex-equilibrium-supcrt-database.py
+++ examples/python/ex-equilibrium-supcrt-database.py
@@ -15,25 +15,30 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this library. If not, see <http://www.gnu.org/licenses/>.
 
-
+# Import reaktoro python package
 from reaktoro import *
 
+# Initialize a thermodynamic database
 db = SupcrtDatabase("supcrt98.xml")
 
+# Create an aqueous phase automatically selecting all species with provided elements
 aqueousphase = AqueousPhase(speciate("H O C Na Cl"))
 aqueousphase.setActivityModel(ActivityModelHKF())
 
+# Create a gaseous phase
 gaseousphase = GaseousPhase("CO2(g)")
 gaseousphase.setActivityModel(ActivityModelPengRobinson())
 
+# Collecting all above-defined phases
 phases = Phases(db)
 phases.add(aqueousphase)
 phases.add(gaseousphase)
 
+# Construct the chemical system
 system = ChemicalSystem(phases)
 
+# Define initial equilibrium state
 state = ChemicalState(system)
-
 state.setTemperature(25.0, "celsius")
 state.setPressure(1.0, "bar")
 state.setSpeciesMass("H2O(l)", 1.0, "kg")
@@ -41,20 +46,12 @@ state.setSpeciesAmount("CO2(g)", 10.0, "mol")
 state.setSpeciesAmount("Na+", 1.0, "mol")
 state.setSpeciesAmount("Cl-", 1.0, "mol")
 
-options = EquilibriumOptions()
-options.optima.output.active = True
-# options.optima.kkt.method = Optima.SaddlePointMethod.Fullspace
-# options.optima.kkt.method = Optima.SaddlePointMethod.Nullspace
-# options.optima.kkt.method = Optima.SaddlePointMethod.Rangespace
-# options.optima.linesearch.trigger_when_current_error_is_greater_than_initial_error_by_factor = 0.0  # zero to disable line search
-# options.optima.linesearch.trigger_when_current_error_is_greater_than_previous_error_by_factor = 0.0
-
+# Define equilibrium solver and equilibrate given initial state
 solver = EquilibriumSolver(system)
-solver.setOptions(options)
-
 solver.solve(state)
 
+# Obtain species composition from the equilibrated state
 n = state.speciesAmounts()
 
 for i in range(len(n)):
-    print(f"{system.species(i).name()} {n[i]}")
+    print(f"{system.species(i).name():12s} {n[i][0]:6.4e}")
