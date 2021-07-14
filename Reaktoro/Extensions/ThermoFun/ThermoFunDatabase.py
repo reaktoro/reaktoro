f036434c678a76a45df9b225d5a9422fc755811e
Allan Leal <allanmulin@gmail.com>
2021-07-14 12:11:10 +0200

    Implemented python tests for ThermoFunDatabase

--- Reaktoro/Extensions/ThermoFun/ThermoFunDatabase.py
+++ Reaktoro/Extensions/ThermoFun/ThermoFunDatabase.py
@@ -20,6 +20,102 @@ from reaktoro import *
 import pytest
 
 
-# TODO Implement tests for the python bindings of component ThermoFunDatabase in ThermoFunDatabase[test].py
 def testThermoFunDatabase():
-    pass
+    aq17 = ThermoFunDatabase("aq17")
+
+    T = 298.15
+    P = 1.0e5
+
+    #-------------------------------------------------------------------
+    # Testing attributes and thermodynamic properties of H2O@
+    #-------------------------------------------------------------------
+    species = aq17.species().get("H2O@")
+    assert species.formula().equivalent("H2O")
+    assert species.substance() == "Water HGK"
+    assert species.aggregateState() == AggregateState.Aqueous
+    assert species.charge() == 0
+    assert species.molarMass() == pytest.approx(0.0180153)
+
+    props = species.props(T, P)
+    assert props.G0[0]  == pytest.approx(-2.371817e+05)
+    assert props.H0[0]  == pytest.approx(-2.858310e+05)
+    assert props.V0[0]  == pytest.approx( 1.806862e-05)
+    assert props.Cp0[0] == pytest.approx( 7.532758e+01)
+    assert props.Cv0[0] == pytest.approx( 7.453942e+01)
+
+    #-------------------------------------------------------------------
+    # Testing attributes and thermodynamic properties of CO3-2
+    #-------------------------------------------------------------------
+    species = aq17.species().get("CO3-2")
+    assert species.formula().equivalent("CO3-2")
+    assert species.substance() == "CO3-2 carbonate ion"
+    assert species.aggregateState() == AggregateState.Aqueous
+    assert species.charge() == -2
+    assert species.molarMass() == pytest.approx(0.0600100979)
+
+    props = species.props(T, P)
+    assert props.G0[0]  == pytest.approx(-5.279830e+05)
+    assert props.H0[0]  == pytest.approx(-6.752359e+05)
+    assert props.V0[0]  == pytest.approx(-6.063738e-06)
+    assert props.Cp0[0] == pytest.approx(-3.228612e+02)
+    assert props.Cv0[0] == pytest.approx(-3.228612e+02)
+
+    #-------------------------------------------------------------------
+    # Testing attributes and thermodynamic properties of Ca+2
+    #-------------------------------------------------------------------
+    species = aq17.species().get("Ca+2")
+    assert species.formula().equivalent("Ca+2")
+    assert species.substance() == "Ca+2 ion"
+    assert species.aggregateState() == AggregateState.Aqueous
+    assert species.charge() == +2
+    assert species.molarMass() == pytest.approx(0.040076902)
+
+    props = species.props(T, P)
+    assert props.G0[0]  == pytest.approx(-5.528210e+05)
+    assert props.H0[0]  == pytest.approx(-5.431003e+05)
+    assert props.V0[0]  == pytest.approx(-1.844093e-05)
+    assert props.Cp0[0] == pytest.approx(-3.099935e+01)
+    assert props.Cv0[0] == pytest.approx(-3.099935e+01)
+
+    #-------------------------------------------------------------------
+    # Testing attributes and thermodynamic properties of CO2
+    #-------------------------------------------------------------------
+    species = aq17.species().get("CO2")
+    assert species.formula().equivalent("CO2")
+    assert species.substance() == "Carbon dioxide (CO2)"
+    assert species.aggregateState() == AggregateState.Gas
+    assert species.charge() == 0
+    assert species.molarMass() == pytest.approx(0.0440096006)
+
+    props = species.props(T, P)
+    assert props.G0[0]  == pytest.approx(-3.943510e+05)
+    assert props.H0[0]  == pytest.approx(-3.935472e+05)
+    assert props.V0[0]  == pytest.approx( 2.466434e-02)
+    assert props.Cp0[0] == pytest.approx( 3.710812e+01)
+    assert props.Cv0[0] == pytest.approx( 0.000000e+00)
+
+    #-------------------------------------------------------------------
+    # Testing attributes and thermodynamic properties of Calcite
+    #-------------------------------------------------------------------
+    species = aq17.species().get("Calcite")
+    assert species.formula().equivalent("CaCO3")
+    assert species.substance() == "Calcite (cc)"
+    assert species.aggregateState() == AggregateState.CrystallineSolid
+    assert species.charge() == 0
+    assert species.molarMass() == pytest.approx(0.1000869999)
+
+    props = species.props(T, P)
+    assert props.G0[0]  == pytest.approx(-1.129195e+06)
+    assert props.H0[0]  == pytest.approx(-1.207470e+06)
+    assert props.V0[0]  == pytest.approx( 3.689000e-05)
+    assert props.Cp0[0] == pytest.approx( 8.337073e+01)
+    assert props.Cv0[0] == pytest.approx( 0.000000e+00)
+
+    with pytest.raises(RuntimeError):
+        assert ThermoFunDatabase("not-a-valid-file-name")
+
+    with pytest.raises(RuntimeError):
+        assert ThermoFunDatabase.withName("not-a-valid-file-name")
+
+    with pytest.raises(RuntimeError):
+        assert ThermoFunDatabase.fromFile("not-a-valid-file-name")
