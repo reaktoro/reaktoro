33a6a664ac4c90e3a4709d3f6c754e220e1886ae
Svetlana Kyas <mtsveta@gmail.com>
2021-07-16 09:52:10 +0200

    Replacing the examples with old supcrt98.xml database

--- examples/python/ex-equilibrium-halite-solubility-h2o.py
+++ examples/python/ex-equilibrium-halite-solubility-h2o.py
@@ -19,41 +19,35 @@
 from reaktoro import *
 
 # Initialize a thermodynamic database
-db = SupcrtDatabase("supcrt98.xml")
+db = Database("supcrt98.yaml")
 
 # Create an aqueous phase automatically selecting all species with provided elements
-aqueousphase = AqueousPhase(speciate("H O C Na Cl"))
+aqueousphase = AqueousPhase(speciate("H O Na Cl"))
 aqueousphase.setActivityModel(ActivityModelHKF())
 
 # Create a gaseous phase
-gaseousphase = GaseousPhase("CO2(g)")
-gaseousphase.setActivityModel(ActivityModelPengRobinson())
+mineralphase = MineralPhase("Halite")
 
 # Collecting all above-defined phases
 phases = Phases(db)
 phases.add(aqueousphase)
-phases.add(gaseousphase)
+phases.add(mineralphase)
 
 # Construct the chemical system
 system = ChemicalSystem(phases)
+
+# Initial amount of NaCl
+m0Halite = 100.0
 
 # Define initial equilibrium state
 state = ChemicalState(system)
 state.setTemperature(25.0, "celsius")
 state.setPressure(1.0, "bar")
-state.setSpeciesMass("H2O(l)", 1.0, "kg")
-state.setSpeciesAmount("CO2(g)", 10.0, "mol")
-state.setSpeciesAmount("Na+", 1.0, "mol")
-state.setSpeciesAmount("Cl-", 1.0, "mol")
+state.setSpeciesMass("H2O(aq)", 1.0, "kg")
+state.setSpeciesAmount("Halite", m0Halite, "mol")
 
 # Define equilibrium solver and equilibrate given initial state
 solver = EquilibriumSolver(system)
 solver.solve(state)
 
-print(state)
-
-# Obtain species composition from the equilibrated state
-n = state.speciesAmounts()
-
-for i in range(len(n)):
-    print(f"{system.species(i).name():12s} {n[i][0]:6.4e}")
+print(f"Solubility of Halite in water is {(m0Halite - state.speciesAmount('Halite')) / state.speciesMass('H2O(aq)')} molal")
