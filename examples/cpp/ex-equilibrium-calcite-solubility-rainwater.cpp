b25ab9f2d1031a58f6e6048283afd69d24823f1f
Svetlana Kyas <mtsveta@gmail.com>
2021-07-16 09:20:51 +0200

    Correct to 4-space indentation

--- examples/cpp/ex-equilibrium-calcite-solubility-rainwater.cpp
+++ examples/cpp/ex-equilibrium-calcite-solubility-rainwater.cpp
@@ -26,8 +26,8 @@ int main()
     // Create an aqueous phase automatically selecting all species with provided elements
     AqueousPhase aqueousphase(speciate("H O C Ca Mg K Cl Na S N"), exclude("organic"));
     aqueousphase.setActivityModel(chain(
-            ActivityModelHKF(),
-            ActivityModelDrummond("CO2")
+        ActivityModelHKF(),
+        ActivityModelDrummond("CO2")
     ));
 
     // Create a gaseous phase
