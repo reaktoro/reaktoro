426be0f7e9fb614e136c5df0e6bfd0fa5f7149b2
Allan Leal <allanmulin@gmail.com>
2021-08-31 10:06:22 +0200

    [pub] Extend StateOfMatter with Fluid and Condensed options

--- Reaktoro/Core/StateOfMatter.hpp
+++ Reaktoro/Core/StateOfMatter.hpp
@@ -22,10 +22,16 @@
 
 namespace Reaktoro {
 
-/// The list of states of matter.
+/// The list of states of matter for phases.
 enum class StateOfMatter
 {
-    Solid, Liquid, Gas, Plasma
+    Solid,      ///< when the state of matter of a phase is always considered solid
+    Liquid,     ///< when the state of matter of a phase is always considered liquid
+    Gas,        ///< when the state of matter of a phase is always considered gas
+    Plasma,     ///< when the state of matter of a phase is always considered plama
+    Fluid,      ///< when the state of matter of a phase can be either liquid, gas, or plasma
+    Condensed,  ///< when the state of matter of a phase can be either liquid or solid
+    Any         ///< when the state of matter of a phase is any of the above
 };
 
 /// Output a StateOfMatter value.
@@ -33,10 +39,13 @@ inline auto operator<<(std::ostream& out, StateOfMatter option) -> std::ostream&
 {
     switch(option)
     {
-    case StateOfMatter::Solid:  out << "Solid";  return out;
-    case StateOfMatter::Liquid: out << "Liquid"; return out;
-    case StateOfMatter::Gas:    out << "Gas";    return out;
-    case StateOfMatter::Plasma: out << "Plasma"; return out;
+    case StateOfMatter::Solid:      out << "Solid";     return out;
+    case StateOfMatter::Liquid:     out << "Liquid";    return out;
+    case StateOfMatter::Gas:        out << "Gas";       return out;
+    case StateOfMatter::Plasma:     out << "Plasma";    return out;
+    case StateOfMatter::Fluid:      out << "Fluid";     return out;
+    case StateOfMatter::Condensed:  out << "Condensed"; return out;
+    case StateOfMatter::Any:        out << "Any";       return out;
     default: out << "Solid"; return out;
     }
 }
