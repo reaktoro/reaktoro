73c9974ec5ccc8dc6d361fd08bf323d208633c19
Allan Leal <allanmulin@gmail.com>
2021-09-14 11:35:44 +0200

    Implemented StandardThermoModelInterpolation

---
+++ Reaktoro/Models/StandardThermoModelInterpolation.cpp
@@ -0,0 +1,70 @@
+// Reaktoro is a unified framework for modeling chemically reactive systems.
+//
+// Copyright © 2014-2021 Allan Leal
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library. If not, see <http://www.gnu.org/licenses/>.
+
+#include "StandardThermoModelInterpolation.hpp"
+
+// Reaktoro includes
+#include <Reaktoro/Math/BilinearInterpolator.hpp>
+#include <Reaktoro/Serialization/Models.YAML.hpp>
+
+namespace Reaktoro {
+
+/// Return a Vec<Param> object containing all Param objects in @p params.
+auto extractParams(const StandardThermoModelParamsInterpolation& params) -> Vec<Param>
+{
+    return {};
+}
+
+/// Return a ModelSerializer for given model parameters in @p params.
+auto createModelSerializer(const StandardThermoModelParamsInterpolation& params) -> ModelSerializer
+{
+    return [=]()
+    {
+        yaml node;
+        node["Interpolation"] = params;
+        return node;
+    };
+}
+
+auto StandardThermoModelInterpolation(const StandardThermoModelParamsInterpolation& params) -> StandardThermoModel
+{
+    const auto& temperatures = params.temperatures;
+    const auto& pressures = params.pressures;
+
+    BilinearInterpolator iG0(temperatures, pressures, params.G0);
+    BilinearInterpolator iH0(temperatures, pressures, params.H0);
+    BilinearInterpolator iV0(temperatures, pressures, params.V0);
+    BilinearInterpolator iCp0(temperatures, pressures, params.Cp0);
+    BilinearInterpolator iCv0(temperatures, pressures, params.Cv0);
+    BilinearInterpolator iVT0(temperatures, pressures, params.VT0);
+    BilinearInterpolator iVP0(temperatures, pressures, params.VP0);
+
+    auto evalfn = [=](StandardThermoProps& props, real T, real P)
+    {
+        if(!iG0.empty()) props.G0 = iG0(T, P);
+        if(!iH0.empty()) props.H0 = iH0(T, P);
+        if(!iV0.empty()) props.V0 = iV0(T, P);
+        if(!iCp0.empty()) props.Cp0 = iCp0(T, P);
+        if(!iCv0.empty()) props.Cv0 = iCv0(T, P);
+        if(!iVT0.empty()) props.VT0 = iVT0(T, P);
+        if(!iVP0.empty()) props.VP0 = iVP0(T, P);
+    };
+
+    return StandardThermoModel(evalfn, extractParams(params), createModelSerializer(params));
+}
+
+} // namespace Reaktoro
---
+++ Reaktoro/Models/StandardThermoModelInterpolation.hpp
@@ -0,0 +1,60 @@
+// Reaktoro is a unified framework for modeling chemically reactive systems.
+//
+// Copyright © 2014-2021 Allan Leal
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library. If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+// Reaktoro includes
+#include <Reaktoro/Common/Types.hpp>
+#include <Reaktoro/Core/StandardThermoProps.hpp>
+
+namespace Reaktoro {
+
+/// The parameters in the Maier-Kelley model for calculating standard thermodynamic properties of fluid and solid species.
+struct StandardThermoModelParamsInterpolation
+{
+    /// The temperatures at which known thermodynamic data is known (in K).
+    Vec<double> temperatures;
+
+    /// The pressures at which known thermodynamic data is known (in Pa).
+    Vec<double> pressures;
+
+    /// The standard molar Gibbs energies @f$G^{\circ}@f$ of formation of the species (in J/mol).
+    Vec<Vec<double>> G0;
+
+    /// The standard molar enthalpies @f$H^{\circ}@f$ of formation of the species (in J/mol).
+    Vec<Vec<double>> H0;
+
+    /// The standard molar volumes @f$V^{\circ}@f$ of the species (in m³/mol).
+    Vec<Vec<double>> V0;
+
+    /// The standard molar isobaric heat capacities @f$C_{P}^{\circ}@f$ of the species (in J/(mol·K)).
+    Vec<Vec<double>> Cp0;
+
+    /// The standard molar isochoric heat capacities @f$C_{V}^{\circ}@f$ of the species (in J/(mol·K)).
+    Vec<Vec<double>> Cv0;
+
+    /// The temperature derivative of the standard molar volumes @f$\partial V^{\circ}/\partial T@f$ of the species (in m³/(mol·K)).
+    Vec<Vec<double>> VT0;
+
+    /// The pressure derivative of the standard molar volumes @f$\partial V^{\circ}/\partial P@f$ of the species (in m³/(mol·K)).
+    Vec<Vec<double>> VP0;
+};
+
+/// Return a function that calculates thermodynamic properties of a species using the Maier-Kelley model.
+auto StandardThermoModelInterpolation(const StandardThermoModelParamsInterpolation& params) -> StandardThermoModel;
+
+} // namespace Reaktoro
