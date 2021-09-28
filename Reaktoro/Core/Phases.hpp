5c6f150524f079abd0cea7d48261144fecc3b3d2
Allan Leal <allanmulin@gmail.com>
2021-09-28 08:46:52 +0200

    Minor changes

--- Reaktoro/Core/Phases.hpp
+++ Reaktoro/Core/Phases.hpp
@@ -426,7 +426,7 @@ public:
         setName("SolidPhase");
         setStateOfMatter(StateOfMatter::Solid);
         setAggregateState(AggregateState::Solid);
-        setAdditionalAggregateStates({AggregateState::CrystallineSolid}); // to make sure that ThermoFun solids are included
+        setAdditionalAggregateStates({AggregateState::CrystallineSolid});
         setActivityModel(ActivityModelIdealSolution());
         setIdealActivityModel(ActivityModelIdealSolution());
     }
