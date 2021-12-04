5173cd443ea475a7c14de451f815a393474849e1
Diego Tavares Volpatto <diego@esss.co>
2021-12-04 13:35:25 -0300

    Add basic code structure for E-UNIQUAC model
    (STEPS-18)

---
+++ Reaktoro/Thermodynamics/Models/AqueousChemicalModelEUNIQUAC.hpp
@@ -0,0 +1,67 @@
+// Reaktoro is a unified framework for modeling chemically reactive systems.
+//
+// Copyright (C) 2014-2018 Allan Leal
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library. If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef REAKTORO_AQUEOUSCHEMICALMODELEUNIQUAC_HPP
+#define REAKTORO_AQUEOUSCHEMICALMODELEUNIQUAC_HPP
+
+// C++ includes
+#include <map>
+#include <memory>
+
+// Reaktoro includes
+#include <Reaktoro/Thermodynamics/Models/PhaseChemicalModel.hpp>
+
+namespace Reaktoro {
+
+// Forward declarations
+class AqueousMixture;
+class EUNIQUACParams;
+
+/// Return an equation of state for an aqueous phase based on the E-UNIQUAC activity model.
+/// @param mixture The aqueous mixture instance
+/// @param params The parameters for the E-UNIQUAC activity model.
+/// @return The activity model function for the aqueous phase
+/// @see AqueousMixture, EUNIQUACParams, PhaseChemicalModel
+auto aqueousChemicalModelEUNIQUAC(const AqueousMixture& mixture, const EUNIQUACParams& params) -> PhaseChemicalModel;
+
+class EUNIQUACParams
+{
+public:
+    /// Construct a default EUNIQUACParams instance.
+    EUNIQUACParams();
+
+    auto ri(std::string name, double value) -> void;
+
+    auto ri(const std::map<std::string, double>& pairs) -> void;
+
+    auto qi(std::string name, double value) -> void;
+
+    auto qi(const std::map<std::string, double>& pairs) -> void;
+
+    auto setDTUvalues() -> void;
+
+private:
+    struct Impl;
+
+    std::shared_ptr<Impl> pimpl;
+
+    std::unordered_map<std::string, int> bips_species_id_map;
+};
+
+}
+
+#endif //REAKTORO_AQUEOUSCHEMICALMODELEUNIQUAC_HPP
