77b68bbc5de1c0f4380f13b752dd94cf1801b194
Allan Leal <allanmulin@gmail.com>
2022-01-03 16:34:46 +0100

    Using double in NasaThermoParams for exponents q1...17

--- Reaktoro/Extensions/Nasa/NasaThermoParams.hpp
+++ Reaktoro/Extensions/Nasa/NasaThermoParams.hpp
@@ -38,25 +38,25 @@ namespace Reaktoro {
 /// https://doi.org/NASA RP-1311
 struct NasaThermoParams
 {
-    real Tmin = {}; ///< The minimum temperature (in K) for which the coefficients below are valid.
-    real Tmax = {}; ///< The maximum temperature (in K) for which the coefficients below are valid.
-    long qN = {};   ///< The number of exponent coefficients \eq{q_i}, which is always 7.
-    real q1 = {};   ///< The exponent \eq{q_1} in the regression model for \eq{C_{p}^{\circ}}.
-    real q2 = {};   ///< The exponent \eq{q_2} in the regression model for \eq{C_{p}^{\circ}}.
-    real q3 = {};   ///< The exponent \eq{q_3} in the regression model for \eq{C_{p}^{\circ}}.
-    real q4 = {};   ///< The exponent \eq{q_4} in the regression model for \eq{C_{p}^{\circ}}.
-    real q5 = {};   ///< The exponent \eq{q_5} in the regression model for \eq{C_{p}^{\circ}}.
-    real q6 = {};   ///< The exponent \eq{q_6} in the regression model for \eq{C_{p}^{\circ}}.
-    real q7 = {};   ///< The exponent \eq{q_7} in the regression model for \eq{C_{p}^{\circ}}.
-    real a1 = {};   ///< The least-square coefficient \eq{a_1} in the regression model for \eq{C_{p}^{\circ}}.
-    real a2 = {};   ///< The least-square coefficient \eq{a_2} in the regression model for \eq{C_{p}^{\circ}}.
-    real a3 = {};   ///< The least-square coefficient \eq{a_3} in the regression model for \eq{C_{p}^{\circ}}.
-    real a4 = {};   ///< The least-square coefficient \eq{a_4} in the regression model for \eq{C_{p}^{\circ}}.
-    real a5 = {};   ///< The least-square coefficient \eq{a_5} in the regression model for \eq{C_{p}^{\circ}}.
-    real a6 = {};   ///< The least-square coefficient \eq{a_6} in the regression model for \eq{C_{p}^{\circ}}.
-    real a7 = {};   ///< The least-square coefficient \eq{a_7} in the regression model for \eq{C_{p}^{\circ}}.
-    real b1 = {};   ///< The integration constant \eq{b_1} used to compute \eq{H^\circ}.
-    real b2 = {};   ///< The integration constant \eq{b_2} used to compute \eq{S^\circ}.
+    double Tmin = {}; ///< The minimum temperature (in K) for which the coefficients below are valid.
+    double Tmax = {}; ///< The maximum temperature (in K) for which the coefficients below are valid.
+    long qN = {};     ///< The number of exponent coefficients \eq{q_i}, which is always 7.
+    double q1 = {};   ///< The exponent \eq{q_1} in the regression model for \eq{C_{p}^{\circ}}.
+    double q2 = {};   ///< The exponent \eq{q_2} in the regression model for \eq{C_{p}^{\circ}}.
+    double q3 = {};   ///< The exponent \eq{q_3} in the regression model for \eq{C_{p}^{\circ}}.
+    double q4 = {};   ///< The exponent \eq{q_4} in the regression model for \eq{C_{p}^{\circ}}.
+    double q5 = {};   ///< The exponent \eq{q_5} in the regression model for \eq{C_{p}^{\circ}}.
+    double q6 = {};   ///< The exponent \eq{q_6} in the regression model for \eq{C_{p}^{\circ}}.
+    double q7 = {};   ///< The exponent \eq{q_7} in the regression model for \eq{C_{p}^{\circ}}.
+    real a1 = {};     ///< The least-square coefficient \eq{a_1} in the regression model for \eq{C_{p}^{\circ}}.
+    real a2 = {};     ///< The least-square coefficient \eq{a_2} in the regression model for \eq{C_{p}^{\circ}}.
+    real a3 = {};     ///< The least-square coefficient \eq{a_3} in the regression model for \eq{C_{p}^{\circ}}.
+    real a4 = {};     ///< The least-square coefficient \eq{a_4} in the regression model for \eq{C_{p}^{\circ}}.
+    real a5 = {};     ///< The least-square coefficient \eq{a_5} in the regression model for \eq{C_{p}^{\circ}}.
+    real a6 = {};     ///< The least-square coefficient \eq{a_6} in the regression model for \eq{C_{p}^{\circ}}.
+    real a7 = {};     ///< The least-square coefficient \eq{a_7} in the regression model for \eq{C_{p}^{\circ}}.
+    real b1 = {};     ///< The integration constant \eq{b_1} used to compute \eq{H^\circ}.
+    real b2 = {};     ///< The integration constant \eq{b_2} used to compute \eq{S^\circ}.
 };
 
 /// Return true if two NasaThermoParams objects are different.
