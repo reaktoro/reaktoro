b0f295d96ebdcc11341ac95abe288c1905d6f686
Allan Leal <allanmulin@gmail.com>
2022-01-03 16:34:47 +0100

    Using 999'999'999'999 for G0 in StandardThermoModelNasa for species with out-or-range temperature

--- Reaktoro/Models/StandardThermoModelNasa.cpp
+++ Reaktoro/Models/StandardThermoModelNasa.cpp
@@ -81,7 +81,7 @@ auto computeStandardThermoProps(const StandardThermoModelParamsNasa& params, con
     if(params.polynomials.empty())
     {
         StandardThermoProps props;
-        props.G0 = 999'999; // NOTE: This high value for G0 is to ensure the condensed species with limited data is never stable at equilibrium
+        props.G0 = 999'999'999'999; // NOTE: This high value for G0 is to ensure the condensed species with limited data is never stable at equilibrium
         props.H0 = params.H0;
         return props;
     };
@@ -95,7 +95,7 @@ auto computeStandardThermoProps(const StandardThermoModelParamsNasa& params, con
 
     // Otherwise, return standard thermo props whose G0 is high to penalize the species from appearing at equilibrium
     StandardThermoProps props;
-    props.G0 = 999'999; // NOTE: This high value for G0 is to ensure the condensed species with limited data is never stable at equilibrium
+    props.G0 = 999'999'999'999; // NOTE: This high value for G0 is to ensure the condensed species with limited data is never stable at equilibrium
     return props;
 }
 
