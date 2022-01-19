25d4b0cb40f181981d8283a4bb22dacbeecac6fe
Allan Leal <allanmulin@gmail.com>
2022-01-19 12:10:02 +0100

    Physical states are now tested in cuic eos activity model

--- Reaktoro/Thermodynamics/Fluids/ActivityModelCubicEOS.test.cxx
+++ Reaktoro/Thermodynamics/Fluids/ActivityModelCubicEOS.test.cxx
@@ -16,7 +16,7 @@
 // along with this library. If not, see <http://www.gnu.org/licenses/>.
 
 // Catch includes
-#include <catch2/catch.hpp>
+#include <Reaktoro/catch.hxx>
 
 // Reaktoro includes
 #include <Reaktoro/Thermodynamics/Fluids/ActivityModelCubicEOS.hpp>
@@ -42,72 +42,204 @@ TEST_CASE("Testing ActivityModelCubicEOS", "[ActivityModelCubicEOS]")
     const auto T = 300.0;
     const auto P = 12.3e5;
 
-    WHEN("The gases are H2O(g), CO2(g), CH4(g).")
+    //=============================================
+    // FLUID MIXTURE (CO2, H2O, CH4)
+    //=============================================
+    WHEN("The gases are CO2, H2O, CH4")
     {
-        const auto species = SpeciesList("H2O(g) CO2(g) CH4(g)");
+        const auto species = SpeciesList("CO2 H2O CH4");
 
-        const ArrayXr x = ArrayXr{{0.1, 0.6, 0.3}};
+        const ArrayXr x = ArrayXr{{0.90, 0.08, 0.02}};
 
-        // Construct the activity props function with the given gaseous species.
+        // Construct the activity props function with the given species.
         ActivityModel fn = ActivityModelPengRobinson()(species);
 
         // Create the ActivityProps object with the results.
         ActivityProps props = ActivityProps::create(species.size());
 
-        // Evaluate the activity props function
-        fn(props, {T, P, x});
+        WHEN("Conditions correspond to gas state")
+        {
+            const auto T = 25.0 + 273.15; // 25 °C
+            const auto P = 1.0 * 1e5;     // 1 bar
 
-        CHECK( props.Vx == Approx(0.00189912) );
+            fn(props, {T, P, x}); // Evaluate the activity props function
 
-        CHECK( exp(props.ln_g[0]) == Approx(0.84961190475435533) ); // H2O
-        CHECK( exp(props.ln_g[1]) == Approx(0.93520991341973592) ); // CO2
-        CHECK( exp(props.ln_g[2]) == Approx(0.98003282312005890) ); // CH4
+            CHECK( props.Vx == Approx(0.02464) );
 
-        checkActivities(x, P, props);
+            CHECK( exp(props.ln_g[0]) == Approx(0.994555) ); // CO2
+            CHECK( exp(props.ln_g[1]) == Approx(0.986592) ); // H2O
+            CHECK( exp(props.ln_g[2]) == Approx(0.998501) ); // CH4
+
+            CHECK( props.som == StateOfMatter::Gas );
+
+            checkActivities(x, P, props);
+        }
+
+        WHEN("Conditions correspond to liquid state")
+        {
+            const auto T = 10.0 + 273.15; // 10 °C
+            const auto P = 100.0 * 1e5;   // 100 bar
+
+            fn(props, {T, P, x}); // Evaluate the activity props function
+
+            CHECK( props.Vx == Approx(4.21022e-05) );
+
+            CHECK( exp(props.ln_g[0]) == Approx(0.375122) ); // CO2
+            CHECK( exp(props.ln_g[1]) == Approx(0.00662715) ); // H2O
+            CHECK( exp(props.ln_g[2]) == Approx(2.21312) ); // CH4
+
+            CHECK( props.som == StateOfMatter::Liquid );
+
+            checkActivities(x, P, props);
+        }
+
+        WHEN("Conditions correspond to supercritical state")
+        {
+            const auto T = 60.0 + 273.15; // 60 °C
+            const auto P = 100.0 * 1e5;   // 100 bar
+
+            fn(props, {T, P, x}); // Evaluate the activity props function
+
+            CHECK( props.Vx == Approx(9.33293e-05) );
+
+            CHECK( exp(props.ln_g[0]) == Approx(0.676616) ); // CO2
+            CHECK( exp(props.ln_g[1]) == Approx(0.137854) ); // H2O
+            CHECK( exp(props.ln_g[2]) == Approx(1.34433) ); // CH4
+
+            CHECK( props.som == StateOfMatter::Supercritical );
+
+            checkActivities(x, P, props);
+        }
     }
 
-    WHEN("Only H2O(g) and CO2(g) are considered.")
+    //=============================================
+    // FLUID MIXTURE (CO2, H2O)
+    //=============================================
+    WHEN("The gases are CO2 and H2O")
     {
-        const auto species = SpeciesList("H2O(g) CO2(g)");
+        const auto species = SpeciesList("CO2 H2O");
 
-        const ArrayXr x = ArrayXr{{0.1, 0.9}};
+        const ArrayXr x = ArrayXr{{0.9, 0.1}};
 
-        // Construct the activity props function with the given gaseous species.
+        // Construct the activity props function with the given species.
         ActivityModel fn = ActivityModelPengRobinson()(species);
 
         // Create the ActivityProps object with the results.
         ActivityProps props = ActivityProps::create(species.size());
 
-        // Evaluate the activity props function
-        fn(props, {T, P, x});
+        WHEN("Conditions correspond to gas state")
+        {
+            const auto T = 25.0 + 273.15; // 25 °C
+            const auto P = 1.0 * 1e5;     // 1 bar
 
-        CHECK( props.Vx == Approx(0.00186714) );
+            fn(props, {T, P, x}); // Evaluate the activity props function
 
-        CHECK( exp(props.ln_g[0]) == Approx(0.84135538513830999) ); // H2O
-        CHECK( exp(props.ln_g[1]) == Approx(0.93544583990584096) ); // CO2
+            CHECK( props.Vx == Approx(0.024634) );
 
-        checkActivities(x, P, props);
+            CHECK( exp(props.ln_g[0]) == Approx(0.994566) ); // CO2
+            CHECK( exp(props.ln_g[1]) == Approx(0.986475) ); // H2O
+
+            CHECK( props.som == StateOfMatter::Gas );
+
+            checkActivities(x, P, props);
+        }
+
+        WHEN("Conditions correspond to liquid state")
+        {
+            const auto T = 10.0 + 273.15; // 10 °C
+            const auto P = 100.0 * 1e5;   // 100 bar
+
+            fn(props, {T, P, x}); // Evaluate the activity props function
+
+            CHECK( props.Vx == Approx(4.04694e-05) );
+
+            CHECK( exp(props.ln_g[0]) == Approx(0.382635) ); // CO2
+            CHECK( exp(props.ln_g[1]) == Approx(0.00536954) ); // H2O
+
+            CHECK( props.som == StateOfMatter::Liquid );
+
+            checkActivities(x, P, props);
+        }
+
+        WHEN("Conditions correspond to supercritical state")
+        {
+            const auto T = 60.0 + 273.15; // 60 °C
+            const auto P = 100.0 * 1e5;   // 100 bar
+
+            fn(props, {T, P, x}); // Evaluate the activity props function
+
+            CHECK( props.Vx == Approx(6.92881e-05) );
+
+            CHECK( exp(props.ln_g[0]) == Approx(0.707373) ); // CO2
+            CHECK( exp(props.ln_g[1]) == Approx(0.0855991) ); // H2O
+
+            CHECK( props.som == StateOfMatter::Supercritical );
+
+            checkActivities(x, P, props);
+        }
     }
 
-    WHEN("Only CH4(g) is considered.")
+    //=============================================
+    // FLUID MIXTURE (CO2)
+    //=============================================
+    WHEN("There is only CO2")
     {
-        const auto species = SpeciesList("CH4(g)");
+        const auto species = SpeciesList("CO2");
 
         const ArrayXr x = ArrayXr{{1.0}};
 
-        // Construct the activity props function with the given gaseous species.
+        // Construct the activity props function with the given species.
         ActivityModel fn = ActivityModelPengRobinson()(species);
 
         // Create the ActivityProps object with the results.
         ActivityProps props = ActivityProps::create(species.size());
 
-        // Evaluate the activity props function
-        fn(props, {T, P, x});
+        WHEN("Conditions correspond to gas state")
+        {
+            const auto T = 25.0 + 273.15; // 25 °C
+            const auto P = 1.0 * 1e5;     // 1 bar
 
-        CHECK( props.Vx == Approx(0.00197476) );
+            fn(props, {T, P, x}); // Evaluate the activity props function
 
-        CHECK( exp(props.ln_g[0]) == Approx(0.97385868141252452) ); // CH4
+            CHECK( props.Vx == Approx(0.0246538) );
 
-        checkActivities(x, P, props);
+            CHECK( exp(props.ln_g[0]) == Approx(0.994544) ); // CO2
+
+            CHECK( props.som == StateOfMatter::Gas );
+
+            checkActivities(x, P, props);
+        }
+
+        WHEN("Conditions correspond to liquid state")
+        {
+            const auto T = 10.0 + 273.15; // 10 °C
+            const auto P = 100.0 * 1e5;   // 100 bar
+
+            fn(props, {T, P, x}); // Evaluate the activity props function
+
+            CHECK( props.Vx == Approx(4.80345e-05) );
+
+            CHECK( exp(props.ln_g[0]) == Approx(0.36694) ); // CO2
+
+            CHECK( props.som == StateOfMatter::Liquid );
+
+            checkActivities(x, P, props);
+        }
+
+        WHEN("Conditions correspond to supercritical state")
+        {
+            const auto T = 60.0 + 273.15; // 60 °C
+            const auto P = 100.0 * 1e5;   // 100 bar
+
+            fn(props, {T, P, x}); // Evaluate the activity props function
+
+            CHECK( props.Vx == Approx(0.000150429) );
+
+            CHECK( exp(props.ln_g[0]) == Approx(0.657945) ); // CO2
+
+            CHECK( props.som == StateOfMatter::Supercritical );
+
+            checkActivities(x, P, props);
+        }
     }
 }
