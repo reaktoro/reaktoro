7ee332a2c406e2f7f29d87b2d633b33bd72f6868
Allan Leal <allanmulin@gmail.com>
2022-01-20 20:13:30 +0100

    Implemented Reaction::props method

--- Reaktoro/Core/Reaction.cpp
+++ Reaktoro/Core/Reaction.cpp
@@ -18,9 +18,13 @@
 #include "Reaction.hpp"
 
 // Reaktoro includes
+#include <Reaktoro/Common/Constants.hpp>
 #include <Reaktoro/Common/Exception.hpp>
+#include <Reaktoro/Common/Units.hpp>
 
 namespace Reaktoro {
+
+const auto R = universalGasConstant;
 
 struct Reaction::Impl
 {
@@ -30,11 +34,41 @@ struct Reaction::Impl
     /// The equation of the reaction with its species and stoichiometric coefficients.
     ReactionEquation equation;
 
-    /// The function that computes the equilibrium constant of the reaction (in natural log).
-    EquilibriumConstantFn lnKfn;
-
     /// The function that computes the rate of the reaction (in mol/s).
     ReactionRateFn ratefn;
+
+    /// Construct a default Reaction::Impl object
+    Impl()
+    : ratefn([](const ChemicalProps&) { return 0.0; })
+    {
+    }
+
+    /// Calculate the complete set of thermodynamic properties of the reaction.
+    auto props(real T, real P) const -> ReactionProps
+    {
+        ReactionProps rprops;
+        rprops.T = T;
+        rprops.P = P;
+
+        for(const auto& [species, coeff] : equation)
+        {
+            const auto sprops = species.props(T, P);
+            rprops.dG0  += coeff * sprops.G0;
+            rprops.dH0  += coeff * sprops.H0;
+            rprops.dV0  += coeff * sprops.V0;
+            rprops.dVT0 += coeff * sprops.VT0;
+            rprops.dVP0 += coeff * sprops.VP0;
+            rprops.dCp0 += coeff * sprops.Cp0;
+            rprops.dCv0 += coeff * sprops.Cv0;
+            rprops.dU0  += coeff * sprops.U0;
+            rprops.dS0  += coeff * sprops.S0;
+            rprops.dA0  += coeff * sprops.A0;
+        }
+
+        rprops.lgK = -rprops.dG0/(R*T*ln10);
+
+        return rprops;
+    }
 };
 
 Reaction::Reaction()
@@ -62,13 +96,6 @@ auto Reaction::withEquation(const ReactionEquation& equation) const -> Reaction
     return copy;
 }
 
-auto Reaction::withEquilibriumConstantFn(const EquilibriumConstantFn& fn) const -> Reaction
-{
-    Reaction copy = clone();
-    copy.pimpl->lnKfn = fn;
-    return copy;
-}
-
 auto Reaction::withRateFn(const ReactionRateFn& fn) const -> Reaction
 {
     Reaction copy = clone();
@@ -86,14 +113,26 @@ auto Reaction::equation() const -> const ReactionEquation&
     return pimpl->equation;
 }
 
-auto Reaction::equilibriumConstantFn() const -> const EquilibriumConstantFn&
-{
-    return pimpl->lnKfn;
-}
-
 auto Reaction::rateFn() const -> const ReactionRateFn&
 {
     return pimpl->ratefn;
+}
+
+auto Reaction::props(real T, real P) const -> ReactionProps
+{
+    return pimpl->props(T, P);
+}
+
+auto Reaction::props(real T, Chars unitT, real P, Chars unitP) const -> ReactionProps
+{
+    T = units::convert(T, unitT, "K");
+    P = units::convert(P, unitP, "Pa");
+    return props(T, P);
+}
+
+auto Reaction::rate(const ChemicalProps& props) const -> real
+{
+    return pimpl->ratefn(props);
 }
 
 auto operator<(const Reaction& lhs, const Reaction& rhs) -> bool
@@ -103,7 +142,6 @@ auto operator<(const Reaction& lhs, const Reaction& rhs) -> bool
 
 auto operator==(const Reaction& lhs, const Reaction& rhs) -> bool
 {
-
     return lhs.name() == rhs.name();
 }
 
