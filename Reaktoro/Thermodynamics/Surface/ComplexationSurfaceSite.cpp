45ba724568de63b564e9b77f9d9f6ee1a2534ac6
Svetlana Kyas <mtsveta@gmail.com>
2022-01-20 12:58:03 +0100

    Add ComplexationSurfaceSite class

---
+++ Reaktoro/Thermodynamics/Surface/ComplexationSurfaceSite.cpp
@@ -0,0 +1,151 @@
+// Reaktoro is a unified framework for modeling chemically reactive systems.
+//
+// Copyright © 2014-2021 Allan Leal
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library. If not, see <http://www.gnu.org/licenses/>.
+
+#include "ComplexationSurfaceSite.hpp"
+
+// Reaktoro includes
+#include <Reaktoro/Common/Units.hpp>
+
+namespace Reaktoro {
+
+ComplexationSurfaceSite::ComplexationSurfaceSite()
+{}
+
+ComplexationSurfaceSite::ComplexationSurfaceSite(const String& name)
+: site_name(name)
+{}
+
+ComplexationSurfaceSite::ComplexationSurfaceSite(const String& name, const String& tag)
+: site_name(name), site_tag(tag)
+{}
+
+// Get the site name.
+auto ComplexationSurfaceSite::name() const -> String
+{
+    return site_name;
+}
+
+// Get the site tag.
+auto ComplexationSurfaceSite::tag() const -> String
+{
+    return site_tag;
+}
+
+// Get the specific surface site surface area (in m2/kg).
+auto ComplexationSurfaceSite::specificSurfaceArea() const -> real
+{
+    return specific_surface_area;
+}
+
+// Get the mass of the surface site (in kg).
+auto ComplexationSurfaceSite::mass() const -> real
+{
+    return site_mass;
+}
+
+// Get the amount of the surface site (in mol).
+auto ComplexationSurfaceSite::amount() const -> real
+{
+    return site_amount;
+}
+
+// Return sorption species.
+auto ComplexationSurfaceSite::sorptionSpecies() const -> SpeciesList
+{
+    return sorption_species;
+}
+
+// Return indices of the sorption species.
+auto ComplexationSurfaceSite::indicesSorptionSpecies() const -> Indices
+{
+    return sorption_species_indices;
+}
+
+// Set name of the surface site.
+auto ComplexationSurfaceSite::setName(String name) -> ComplexationSurfaceSite&
+{
+    site_name = name;
+
+    // If site's name contain `_` symbol,
+    // then initialize surface name and site's tag
+    auto pos = site_name.find("_");
+    if(pos != std::string::npos)
+    {
+        surface_name = site_name.substr(0, pos);
+        site_tag =  site_name.substr(pos, std::string::npos);
+    }
+    return *this;
+}
+
+// Set name of the surface.
+auto ComplexationSurfaceSite::setSurfaceName(String name) -> ComplexationSurfaceSite&
+{
+    surface_name = name;
+    return *this;
+}
+
+// Set the tag of the surface site.
+auto ComplexationSurfaceSite::setTag(String tag) -> ComplexationSurfaceSite&
+{
+    // Check if the site name contains this tag
+    if(site_name.find(tag) != std::string::npos)
+        site_tag = tag;
+
+    return *this;
+}
+
+// Set the surface site surface area (in m2).
+auto ComplexationSurfaceSite::setAmount(double value, String unit) -> ComplexationSurfaceSite&
+{
+    site_amount = units::convert(value, unit, "mol");
+    return *this;
+}
+
+// Set the specific surface site surface area (in m2/kg).
+auto ComplexationSurfaceSite::setSpecificSurfaceArea(double value, String unit) -> ComplexationSurfaceSite&
+{
+    specific_surface_area = units::convert(value, unit, "m2/kg");
+    return *this;
+}
+
+// Set the mass of the solid (in kg).
+auto ComplexationSurfaceSite::setMass(double value, String unit) -> ComplexationSurfaceSite&
+{
+    site_mass = units::convert(value, unit, "kg");
+    return *this;
+}
+
+// Set the density of the solid (in kg).
+auto ComplexationSurfaceSite::setDensity(double value, String unit) -> ComplexationSurfaceSite&
+{
+    site_mass = units::convert(value, unit, "sites/nm2");
+    return *this;
+}
+
+// Add sorption species on the site.
+auto ComplexationSurfaceSite::addSorptionSpecies(const Species& species, const Index& index) -> void
+{
+    // If added species' name contains the surface name and the site tag,
+    // we add this species and its index to the list of the sorption species and species' indices
+    if(species.name().find(surface_name + site_tag) != std::string::npos)
+    {
+        sorption_species.append(species);
+        sorption_species_indices.emplace_back(index);
+    }
+}
+
+} // namespace Reaktoro
---
+++ Reaktoro/Thermodynamics/Surface/ComplexationSurfaceSite.hpp
@@ -0,0 +1,119 @@
+// Reaktoro is a unified framework for modeling chemically reactive systems.
+//
+// Copyright © 2014-2021 Allan Leal
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library. If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+// Reaktoro includes
+#include <Reaktoro/Common/Types.hpp>
+#include <Reaktoro/Core/SpeciesList.hpp>
+#include <Reaktoro/Common/Matrix.hpp>
+
+namespace Reaktoro {
+
+/// A type used to describe the site of a complexation surface.
+class ComplexationSurfaceSite
+{
+public:
+    /// Default constructor of ComplexationSurfaceSite class
+    ComplexationSurfaceSite();
+
+    /// Construct an ComplexationSurfaceSite instance with given name.
+    ComplexationSurfaceSite(const String& name);
+
+    /// Construct an ComplexationSurfaceSite instance with given name and tag.
+    ComplexationSurfaceSite(const String& name, const String& tag);
+
+    // Get the site name.
+    auto name() const -> String;
+
+    // Get the site tag.
+    auto tag() const -> String;
+
+    // Get the specific surface site surface area (in m2/kg).
+    auto specificSurfaceArea() const -> real;
+
+    // Get the mass of the solid (in kg).
+    auto mass() const -> real;
+
+    /// Return the amount of the surface site (in mol).
+    auto amount() const -> real;
+
+    // Return sorption species.
+    auto sorptionSpecies() const -> SpeciesList;
+
+    // Return indices of the sorption species.
+    auto indicesSorptionSpecies() const -> Indices;
+
+    // Set the name of the surface site.
+    auto setName(String name) -> ComplexationSurfaceSite&;
+
+    // Set name of the surface.
+    auto setSurfaceName(String name) -> ComplexationSurfaceSite&;
+
+    // Set the tag of the surface site.
+    auto setTag(String tag) -> ComplexationSurfaceSite&;
+
+    // Set the amount of surface site.
+    auto setAmount(double value, String unit = "mol") -> ComplexationSurfaceSite&;
+
+    // Set the specific surface site surface area (in m2/kg).
+    auto setSpecificSurfaceArea(double value, String unit = "m2/kg") -> ComplexationSurfaceSite&;
+
+    // Set the mass of the solid (in kg).
+    auto setMass(double value, String unit = "kg") -> ComplexationSurfaceSite&;
+
+    // Set the density of surface site (in sites/m2).
+    auto setDensity(double value, String unit = "sites/m2") -> ComplexationSurfaceSite&;
+
+    // Add sorption species on the site.
+    auto addSorptionSpecies(const Species& species, const Index& index) -> void;
+
+private:
+
+    /// The site amount (mol)
+    real site_amount;
+
+    /// The site density (sites/nm2, 1e9*sites/m2)
+    real site_density;
+
+    /// The specific area (m2/kg), default value is 600 m2/g = 6e5 m2/kg
+    real specific_surface_area;
+
+    /// The solid mass (kg)
+    real site_mass;
+
+    /// The name of the surface site
+    String site_name;
+
+    /// The name of the surface
+    String surface_name;
+
+    /// The tag of the surface site
+    String site_tag;
+
+    /// The species that can be sorbed on this site
+    SpeciesList sorption_species;
+
+    /// The indices of the species that can be sorbed on this site
+    Indices sorption_species_indices;
+
+    // The charges of the species that can be sorbed on this site
+    ArrayXd z;
+
+};
+
+}
