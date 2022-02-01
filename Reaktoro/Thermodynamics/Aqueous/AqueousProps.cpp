aec306f0add71f873591b07901809e03d5a2daf5
Allan Leal <allanmulin@gmail.com>
2022-02-01 16:14:58 +0100

    Implemented methods to compute saturation indices in AqueousProps

--- Reaktoro/Thermodynamics/Aqueous/AqueousProps.cpp
+++ Reaktoro/Thermodynamics/Aqueous/AqueousProps.cpp
@@ -28,15 +28,20 @@ using namespace tabulate;
 #include <Optima/Echelonizer.hpp>
 
 // Reaktoro includes
+#include <Reaktoro/Common/Algorithms.hpp>
 #include <Reaktoro/Common/Constants.hpp>
 #include <Reaktoro/Common/Exception.hpp>
+#include <Reaktoro/Core/ActivityModel.hpp>
 #include <Reaktoro/Core/ChemicalProps.hpp>
 #include <Reaktoro/Core/ChemicalPropsPhase.hpp>
 #include <Reaktoro/Core/ChemicalState.hpp>
 #include <Reaktoro/Core/ChemicalSystem.hpp>
 #include <Reaktoro/Core/Phase.hpp>
+#include <Reaktoro/Core/Species.hpp>
+#include <Reaktoro/Core/SpeciesList.hpp>
 #include <Reaktoro/Core/Utils.hpp>
 #include <Reaktoro/Thermodynamics/Aqueous/AqueousMixture.hpp>
+#include <Reaktoro/Thermodynamics/Fluids/ActivityModelCubicEOS.hpp>
 
 namespace Reaktoro {
 namespace {
@@ -54,6 +59,67 @@ auto indexAqueousPhase(const ChemicalSystem& system) -> Index
         "Could not create an AqueousProps object because there is no "
         "phase in the system with aggregate state value AggregateState::Aqueous.");
     return idx;
+}
+
+// Return a chemical potential function for a fluid species using a given activity model.
+auto chemicalPotentialModelFluidSpecies(Species const& species, ActivityModelGenerator const& generator) -> Fn<real(real, real)>
+{
+    const auto activitymodel = generator({species}); // TODO: Use .withMemoization() here to avoid full recomputations when same T and P are given.
+    const auto R = universalGasConstant;
+    const auto x = ArrayXr{{1.0}}; // mole fraction of species in a pure phase = 1.0
+    auto actprops = ActivityProps::create(1);
+
+    return [=](real T, real P) mutable -> real
+    {
+        activitymodel(actprops, {T, P, x}); // evaluate the activity model
+        const auto G0 = species.standardThermoProps(T, P).G0;
+        const auto ln_a = actprops.ln_a[0];
+        return G0 + R*T*ln_a;
+    };
+}
+
+// Return a default chemical potential function for a fluid species using
+// Peng-Robinson activity model. This chemical potential model assumes the
+// species constitute a pure gas phase (containing just a single gaseous
+// species). For gases, even pure phases cannot be considered as ideal phases.
+// Thus, Peng-Robinson model is assumed instead.
+auto defaultChemicalPotentialModelFluidSpecies(Species const& species) -> Fn<real(real, real)>
+{
+    return chemicalPotentialModelFluidSpecies(species, ActivityModelPengRobinson());
+}
+
+// Return a default chemical potential function for a solid species using ideal
+// activity model for solid solutions. This chemical potential model assumes
+// the species constitute a pure solid phase (containing just a single solid
+// species). In this case, an ideal model coincides with the fact that the
+// activity of a pure solid is one and its chemical potential is identical to
+// its standard chemical potential.
+auto defaultChemicalPotentialModelSolidSpecies(Species const& species) -> Fn<real(real, real)>
+{
+    return [=](real T, real P)
+    {
+        return species.standardThermoProps(T, P).G0;
+    };
+}
+
+// Return a default chemical potential function for a chemical species.
+auto defaultChemicalPotentialModel(Species const& species) -> Fn<real(real, real)>
+{
+    switch(species.aggregateState())
+    {
+    case AggregateState::Gas:
+    case AggregateState::Liquid:
+    case AggregateState::Fluid:
+        return defaultChemicalPotentialModelFluidSpecies(species);
+    default:
+        return defaultChemicalPotentialModelSolidSpecies(species);
+    }
+}
+
+// Return a vector with default chemical potential functions for every given chemical species.
+auto defaultChemicalPotentialModels(SpeciesList const& nonaqueous) -> Vec<Fn<real(real, real)>>
+{
+    return vectorize(nonaqueous, RKT_LAMBDA(x, defaultChemicalPotentialModel(x)));
 }
 
 } // namespace
@@ -84,17 +150,29 @@ struct AqueousProps::Impl
     /// The state of the aqueous solution.
     AqueousMixtureState aqstate;
 
-    /// The formula matrix of the aqueous species.
-    MatrixXd Aaq;
-
     /// The amounts of the species in the aqueous phase (to be used with echelonizer - not for any computation, since it does not have autodiff propagation!).
     VectorXd naq;
 
-    /// The echelon form of the formula matrix of the aqueous species.
+    /// The chemical potentials of the elements in the aqueous phase
+    VectorXr lambda;
+
+    /// The non-aqueous species in the database for which saturation indices are calculated.
+    SpeciesList nonaqueous;
+
+    /// The formula matrix of the aqueous species in the aqueous phase.
+    MatrixXd Aaqs;
+
+    /// The formula matrix of the non-aqueous species for the computation of saturation indices.
+    MatrixXd Anon;
+
+    /// The echelon form of the formula matrix `Aaqs` of the aqueous species.
     Optima::Echelonizer echelonizer;
 
+    // The chemical potential models for the non-aqueous species (as if they were pure phases) for the computation of their saturation indices.
+    Vec<Fn<real(real, real)>> chemical_potential_models;
+
     /// The extra properties and data produced during the evaluation of the aqueous phase activity model.
-    Map<String, Any> m_extra;
+    Map<String, Any> extra;
 
     Impl(const ChemicalSystem& system)
     : system(system),
@@ -117,8 +195,23 @@ struct AqueousProps::Impl
         error(iH >= size, "Cannot create AqueousProps object for phase ", phase.name(), " "
             "because it does not contain a species with formula H+.");
 
-        Aaq = detail::assembleFormulaMatrix(phase.species(), phase.elements());
+        // The symbols of the elements in the aqueous phase
+        const auto symbols = vectorize(phase.elements(), RKT_LAMBDA(x, x.symbol()));
 
+        // Collect the species from the database that contains the elements in the aqueous phase
+        const auto species_same_elements = system.database().species().withElements(symbols);
+
+        // Collect the non-aqueous species from the database that contains the elements in the aqueous phase
+        nonaqueous = remove(species_same_elements, RKT_LAMBDA(x, x.aggregateState() == AggregateState::Aqueous));
+
+        // Assemble the formula matrices of the aqueous and non-aqueous species w.r.t. elements in the aqueous phase
+        Aaqs = detail::assembleFormulaMatrix(phase.species(), phase.elements());
+        Anon = detail::assembleFormulaMatrix(nonaqueous, phase.elements());
+
+        // Initialize the chemical potential models for the non-aqueous species, as if they were pure phases
+        chemical_potential_models = defaultChemicalPotentialModels(nonaqueous);
+
+        // Initialize the aqueous state properties
         aqstate.T = NaN;
         aqstate.P = NaN;
         aqstate.rho = NaN;
@@ -128,7 +221,8 @@ struct AqueousProps::Impl
         aqstate.m.setConstant(size, NaN);
         aqstate.ms.setConstant(size, NaN);
 
-        echelonizer.compute(Aaq); // echelon form of Aaq (columns of A corresponding to aqueous species)
+        // Compute the initial echelon form of formula matrix `Aaqs`
+        echelonizer.compute(Aaqs);
     }
 
     Impl(const ChemicalSystem& system, const ChemicalState& state)
@@ -150,19 +244,36 @@ struct AqueousProps::Impl
         const auto n = state.speciesAmounts();
         const auto ifirst = system.phases().numSpeciesUntilPhase(iphase);
         const auto size = phase.species().size();
-        props.update(T, P, n.segment(ifirst, size), m_extra);
+        props.update(T, P, n.segment(ifirst, size), extra);
         update(props);
     }
 
     auto update(const ChemicalPropsPhase& aqprops) -> void
     {
+        // Auxiliary variables
+        const auto& T = aqprops.temperature();
+        const auto& P = aqprops.pressure();
+        const auto& x = aqprops.speciesMoleFractions();
+
+        // Update the internal properties of the aqueous phase
         props = aqprops;
-        const auto& T = props.temperature();
-        const auto& P = props.pressure();
-        const auto& x = props.speciesMoleFractions();
+
+        // Update the internal aqueous state object
         aqstate = aqsolution.state(T, P, x);
+
+        // Update auxiliary vector naq to be used in the echelonization below
         naq = props.speciesAmounts();
+
+        // Update the echelon form and also the list of basic species
         echelonizer.updateWithPriorityWeights(naq);
+
+        // Compute chemical potentials of the elements in the aqueous phase
+        const auto u = props.speciesChemicalPotentials();
+        const auto ib = echelonizer.indicesBasicVariables();
+        const auto R = echelonizer.R();
+        const auto Rb = R.topRows(ib.size());
+        const VectorXr ub = u(ib);
+        lambda = -Rb.transpose() * ub;
     }
 
     auto update(const ChemicalProps& sysprops) -> void
@@ -184,14 +295,14 @@ struct AqueousProps::Impl
     {
         const auto idx = detail::resolveElementIndex(phase, symbol);
         const auto& m = aqstate.m.matrix();
-        return Aaq.row(idx) * m;
+        return Aaqs.row(idx) * m;
     }
 
     auto elementMolalities() const -> VectorXr
     {
         const auto E = phase.elements().size();
         const auto& m = aqstate.m.matrix();
-        return Aaq.topRows(E) * m;
+        return Aaqs.topRows(E) * m;
     }
 
     auto speciesMolality(const StringOrIndex& name) const -> real
@@ -224,17 +335,10 @@ struct AqueousProps::Impl
     auto pE() const -> real
     {
         const auto T = props.temperature();
-        const auto u = props.speciesChemicalPotentials();
-        const auto ib = echelonizer.indicesBasicVariables();
-        const auto R = echelonizer.R();
-        const auto Rb = R.topRows(ib.size());
-        const VectorXr ub = u(ib);
-        const auto lambda = -Rb.transpose() * ub;
         const auto E = phase.elements().size();
-        const auto lambda_Z = lambda[E];
+        const auto lambdaZ = lambda[E];
         const auto RT = universalGasConstant * T;
-        const auto res = lambda_Z/(RT*ln10);
-
+        const auto res = lambdaZ/(RT*ln10);
         return res;
     }
 
@@ -251,6 +355,33 @@ struct AqueousProps::Impl
     {
         error(true, "AqueousProps::alkalinity has not been implemented yet.");
         return {};
+    }
+
+    auto saturationIndexLn(const StringOrIndex& species) const -> real
+    {
+        const auto i = detail::resolveSpeciesIndex(nonaqueous, species);
+        errorif(i >= nonaqueous.size(), "It was not possible to calculate the "
+            "saturation index of species with name or index `", detail::stringfy(species), "` "
+            "because there is no such species in the list of species returned by method "
+            "AqueousProps::saturationSpecies.");
+        const auto T = temperature();
+        const auto P = pressure();
+        const auto ui = chemical_potential_models[i](T, P);
+        const auto li = Anon.col(i).dot(lambda);
+        const auto lnOmegai = li - ui;
+        return lnOmegai;
+    }
+
+    auto saturationIndicesLn() const -> VectorXr
+    {
+        const auto T = temperature();
+        const auto P = pressure();
+        const auto num_nonaqueous = nonaqueous.size();
+        VectorXr lnOmega(num_nonaqueous);
+        lnOmega = Anon.transpose() * lambda;
+        for(auto i = 0; i < num_nonaqueous; ++i)
+            lnOmega[i] -= chemical_potential_models[i](T, P);
+        return lnOmega;
     }
 };
 
@@ -352,6 +483,31 @@ auto AqueousProps::Eh() const -> real
 auto AqueousProps::alkalinity() const -> real
 {
     return pimpl->alkalinity();
+}
+
+auto AqueousProps::saturationSpecies() const -> SpeciesList
+{
+    return pimpl->nonaqueous;
+}
+
+auto AqueousProps::saturationIndex(const StringOrIndex& species) const -> real
+{
+    return exp(pimpl->saturationIndexLn(species));
+}
+
+auto AqueousProps::saturationIndexLn(const StringOrIndex& species) const -> real
+{
+    return pimpl->saturationIndexLn(species);
+}
+
+auto AqueousProps::saturationIndexLg(const StringOrIndex& species) const -> real
+{
+    return pimpl->saturationIndexLn(species) / ln10;
+}
+
+auto AqueousProps::saturationIndicesLn() const -> VectorXr
+{
+    return pimpl->saturationIndicesLn();
 }
 
 auto AqueousProps::phase() const -> const Phase&
