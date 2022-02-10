b82a26b6afcbb399ae7bc40ce20b5ec8e0018012
Svetlana Kyas <mtsveta@gmail.com>
2022-02-10 16:16:00 +0100

    Export plotting in the RT with kinetics into the plotting script

--- demos/python/demo-rtsolver-kinetic-dolomitization.py
+++ demos/python/demo-rtsolver-kinetic-dolomitization.py
@@ -19,8 +19,7 @@
 from reaktoro import *
 from numpy import *
 import os
-from joblib import Parallel, delayed
-import matplotlib.pyplot as plt
+import plotting
 
 #------------------------------------------------------------------------------#
 # Problems parameters
@@ -66,17 +65,17 @@ output_quantities = """
     phaseVolume(Calcite)
     phaseVolume(Dolomite)
 """.split()
-
+output_quantities_map = {"pH" : 0,
+                         "speciesMolality(H+)" : 1,
+                         "speciesMolality(Ca++)" : 2,
+                         "speciesMolality(Mg++)" : 3,
+                         "speciesMolality(HCO3-)" : 4,
+                         "speciesMolality(CO2(aq))" : 5,
+                         "phaseVolume(Calcite)" : 6,
+                         "phaseVolume(Dolomite)" : 7}
 #------------------------------------------------------------------------------#
 # Auxiliary functions
 #------------------------------------------------------------------------------#
-
-# Step 10: Return a string for the title of a figure in the format Time: #h##m
-def titlestr(t):
-    t = t / minute   # Convert from seconds to minutes
-    h = int(t) / 60  # The number of hours
-    m = int(t) % 60  # The number of remaining minutes
-    return 'Time: {:>3}h{:>2}m'.format(h, str(m).zfill(2))
 
 # Step 6: Creating folders for the results' output
 def make_results_folders():
@@ -85,75 +84,6 @@ def make_results_folders():
     os.system('mkdir -p figures-' + folder + '/aqueous-species')
     os.system('mkdir -p figures-' + folder + '/calcite-dolomite')
     os.system('mkdir -p videos-' + folder)
-
-# Step 8: Plot all result files and generate a video
-def plot():
-    # Plot all result files
-    files = sorted(os.listdir(folder))
-    Parallel(n_jobs=16)(delayed(plotfile)(file) for file in files)
-    # Create videos for the figures
-    ffmpegstr = 'ffmpeg -y -r 30 -i figures-' + folder + '/{0}/%0' + str(ndigits) + 'd.png -codec:v mpeg4 -flags:v +qscale -global_quality:v 0 videos-' + folder + '/{0}.mp4'
-    os.system(ffmpegstr.format('calcite-dolomite'))
-    os.system(ffmpegstr.format('aqueous-species'))
-    os.system(ffmpegstr.format('ph'))
-
-def plotfile(file):
-
-    step = int((file.split('.')[0]).split('-')[1])
-
-    print('Plotting figure', step, '...')
-
-    t = step * dt
-    filearray = loadtxt(folder + '/' + file, skiprows=1)
-    data = filearray.T
-
-    ndigits = len(str(nsteps))
-
-    x = linspace(xl, xr, ncells)
-
-    plt.figure()
-    plt.xlim(left=-0.02, right=0.52)
-    plt.ylim(bottom=2.5, top=10.5)
-    plt.title(titlestr(t))
-    plt.xlabel('Distance [m]')
-    plt.ylabel('pH')
-    plt.plot(x, data[0])
-    plt.tight_layout()
-    plt.savefig('figures-' + folder + '/ph/{}.png'.format(str(step).zfill(ndigits)))
-    plt.close()
-
-    plt.figure()
-    plt.xlim(left=-0.02, right=0.52)
-    #plt.ylim(bottom=44.5, top=45.3)
-    plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
-    plt.title(titlestr(t))
-    plt.xlabel('Distance [m]')
-    plt.ylabel('Mineral Volume [%$_{\mathsf{vol}}$]')
-    plt.plot(x, data[6] * 100, label='Calcite')
-    plt.plot(x, data[7] * 100, label='Dolomite')
-    plt.legend(loc='center right')
-    plt.tight_layout()
-    plt.savefig('figures-' + folder + '/calcite-dolomite/{}.png'.format(str(step).zfill(ndigits)))
-    plt.close()
-
-    plt.figure()
-    plt.yscale('log')
-    plt.xlim(left=-0.02, right=0.52)
-    plt.ylim(bottom=0.5e-5, top=2)
-    plt.title(titlestr(t))
-    plt.xlabel('Distance [m]')
-    plt.ylabel('Concentration [molal]')
-    plt.plot(x, data[2], label='Ca++')
-    plt.plot(x, data[3], label='Mg++')
-    plt.plot(x, data[4], label='HCO3-')
-    plt.plot(x, data[5], label='CO2(aq)')
-    plt.plot(x, data[1], label='H+')
-    plt.legend(loc='lower right')
-    plt.tight_layout()
-    plt.savefig('figures-' + folder + '/aqueous-species/{}.png'.format(str(step).zfill(ndigits)))
-    plt.close()
-
-    plt.close('all')
 
 #------------------------------------------------------------------------------#
 # Reactive transport simulations
@@ -230,6 +160,9 @@ state_bc.scaleVolume(1.0, 'm3')
 # Step 11: Create the mesh for the column
 mesh = Mesh(ncells, xl, xr)
 
+# The interval [xl, xr] split into ncells
+x = linspace(xl, xr, ncells + 1)
+
 # Step 12: Create a chemical field object with every cell having state given by state_ic
 field = ChemicalField(mesh.numCells(), state_ic)
 
@@ -271,4 +204,4 @@ while step <= nsteps:  # step until the number of steps are achieved
     t += dt
     step += 1
 
-plot()
+plotting.plot_rt_kinetic_dolomitization(folder, x, nsteps, dt, output_quantities_map)
