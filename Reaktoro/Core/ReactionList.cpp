e015ecce5d75bc43c0997b05477849dd1e83e63d
Allan Leal <allanmulin@gmail.com>
2022-09-08 15:54:04 +0200

    Implemented ReactionList

---
+++ Reaktoro/Core/ReactionList.cpp
@@ -0,0 +1,117 @@
+// Reaktoro is a unified framework for modeling chemically reactive systems.
+//
+// Copyright © 2014-2022 Allan Leal
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library. If not, see <http://www.gnu.org/licenses/>.
+
+#include "ReactionList.hpp"
+
+// Reaktoro includes
+#include <Reaktoro/Common/Algorithms.hpp>
+#include <Reaktoro/Common/Enumerate.hpp>
+#include <Reaktoro/Common/Exception.hpp>
+#include <Reaktoro/Common/StringList.hpp>
+
+namespace Reaktoro {
+
+ReactionList::ReactionList()
+{}
+
+ReactionList::ReactionList(const Vec<Reaction>& reaction)
+: m_reactions(reaction)
+{}
+
+auto ReactionList::append(const Reaction& reaction) -> void
+{
+    m_reactions.push_back(reaction);
+}
+
+auto ReactionList::data() const -> const Vec<Reaction>&
+{
+    return m_reactions;
+}
+
+auto ReactionList::empty() const -> bool
+{
+    return m_reactions.empty();
+}
+
+auto ReactionList::size() const -> Index
+{
+    return m_reactions.size();
+}
+
+auto ReactionList::operator[](Index i) const -> const Reaction&
+{
+    return m_reactions[i];
+}
+
+auto ReactionList::operator[](Index i) -> Reaction&
+{
+    return m_reactions[i];
+}
+
+auto ReactionList::find(const String& name) const -> Index
+{
+    return findWithName(name);
+}
+
+auto ReactionList::findWithName(const String& name) const -> Index
+{
+    return indexfn(m_reactions, RKT_LAMBDA(p, p.name() == name));
+}
+
+auto ReactionList::index(const String& name) const -> Index
+{
+    return indexWithName(name);
+}
+
+auto ReactionList::indexWithName(const String& name) const -> Index
+{
+    const auto idx = findWithName(name);
+    error(idx >= size(), "Could not find any Reaction object with name ", name, ".");
+    return idx;
+}
+
+auto ReactionList::get(const String& name) const -> const Reaction&
+{
+    return getWithName(name);
+}
+
+auto ReactionList::getWithName(const String& name) const -> const Reaction&
+{
+    return m_reactions[indexWithName(name)];
+}
+
+auto ReactionList::withNames(const StringList& names) const -> ReactionList
+{
+    return vectorize(names, RKT_LAMBDA(name, m_reactions[indexWithName(name)]));
+}
+
+ReactionList::operator Vec<Reaction>&()
+{
+    return m_reactions;
+}
+
+ReactionList::operator Vec<Reaction>const&() const
+{
+    return m_reactions;
+}
+
+auto operator+(const ReactionList& a, const ReactionList& b) -> ReactionList
+{
+    return concatenate(a, b);
+}
+
+} // namespace Reaktoro
---
+++ Reaktoro/Core/ReactionList.hpp
@@ -0,0 +1,117 @@
+// Reaktoro is a unified framework for modeling chemically reactive systems.
+//
+// Copyright © 2014-2022 Allan Leal
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library. If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+// Reaktoro includes
+#include <Reaktoro/Common/Types.hpp>
+#include <Reaktoro/Core/Reaction.hpp>
+#include <Reaktoro/Core/SpeciesList.hpp>
+
+namespace Reaktoro {
+
+/// A type used as a collection of reactions.
+class ReactionList
+{
+public:
+    /// Construct a default ReactionList object.
+    ReactionList();
+
+    /// Construct an ReactionList object with given reaction.
+    ReactionList(const Vec<Reaction>& reaction);
+
+    /// Append a new reaction to the list of reaction.
+    auto append(const Reaction& reaction) -> void;
+
+    /// Return the internal collection of Reaction objects.
+    auto data() const -> const Vec<Reaction>&;
+
+    /// Return true if there are no reactions in the collection.
+    auto empty() const -> bool;
+
+    /// Return the number of reactions in the collection.
+    auto size() const -> Index;
+
+    /// Return the Reaction object with given index.
+    auto operator[](Index i) const -> const Reaction&;
+
+    /// Return the Reaction object with given index.
+    auto operator[](Index i) -> Reaction&;
+
+    /// Return the index of the reaction with given unique name or the number of reactions if not found.
+    auto find(const String& name) const -> Index;
+
+    /// Return the index of the reaction with given unique name or the number of reactions if not found.
+    auto findWithName(const String& name) const -> Index;
+
+    /// Return the index of the reaction with given unique name or throw a runtime error if not found.
+    auto index(const String& name) const -> Index;
+
+    /// Return the index of the reaction with given unique name or throw a runtime error if not found.
+    auto indexWithName(const String& name) const -> Index;
+
+    /// Return the reaction with a given name.
+    auto get(const String& name) const -> const Reaction&;
+
+    /// Return the reaction with a given name.
+    auto getWithName(const String& name) const -> const Reaction&;
+
+    /// Return all reactions with given names.
+    auto withNames(const StringList& names) const -> ReactionList;
+
+    /// Convert this ReactionList object into its Vec<Reaction>.
+    operator Vec<Reaction>&();
+
+    /// Convert this ReactionList object into its Vec<Reaction>.
+    operator Vec<Reaction>const&() const;
+
+private:
+    /// The reactions stored in the list.
+    Vec<Reaction> m_reactions;
+
+public:
+    /// Construct an ReactionList object with given begin and end iterators.
+    template<typename InputIterator>
+    ReactionList(InputIterator begin, InputIterator end) : m_reactions(begin, end) {}
+
+    /// Return begin const iterator of this ReactionList instance (for STL compatibility reasons).
+    auto begin() const { return m_reactions.begin(); }
+
+    /// Return begin iterator of this ReactionList instance (for STL compatibility reasons).
+    auto begin() { return m_reactions.begin(); }
+
+    /// Return end const iterator of this ReactionList instance (for STL compatibility reasons).
+    auto end() const { return m_reactions.end(); }
+
+    /// Return end iterator of this ReactionList instance (for STL compatibility reasons).
+    auto end() { return m_reactions.end(); }
+
+    /// Append a new Reaction at the back of the container (for STL compatibility reasons).
+    auto push_back(const Reaction& species) -> void { append(species); }
+
+    /// Insert a container of Reaction objects into this ReactionList instance (for STL compatibility reasons).
+    template<typename Iterator, typename InputIterator>
+    auto insert(Iterator pos, InputIterator begin, InputIterator end) -> void { m_reactions.insert(pos, begin, end); }
+
+    /// The type of the value stored in a ReactionList (for STL compatibility reasons).
+    using value_type = Reaction;
+};
+
+/// Return the concatenation of two ReactionList objects.
+auto operator+(const ReactionList& a, const ReactionList& b) -> ReactionList;
+
+} // namespace Reaktoro
