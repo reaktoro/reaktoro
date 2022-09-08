1ec7e94b117383f9815b4940366e8a9bb4fc2e6f
Allan Leal <allanmulin@gmail.com>
2022-09-08 15:56:20 +0200

    Using KineticsUtils in KineticsSolver

--- Reaktoro/Kinetics/KineticsSolver.cpp
+++ Reaktoro/Kinetics/KineticsSolver.cpp
@@ -30,71 +30,9 @@
 #include <Reaktoro/Kinetics/KineticsOptions.hpp>
 #include <Reaktoro/Kinetics/KineticsResult.hpp>
 #include <Reaktoro/Kinetics/KineticsSensitivity.hpp>
+#include <Reaktoro/Kinetics/KineticsUtils.hpp>
 
 namespace Reaktoro {
-namespace {
-
-/// Return an EquilibriumSpecs object suitable for chemical kinetics
-/// calculations using reactivity constraints in the equilibrium problem to
-/// model the kinetic reactions.
-auto createEquilibriumSpecsForKinetics(EquilibriumSpecs specs) -> EquilibriumSpecs
-{
-    auto const& system = specs.system();
-    auto const& reactions = system.reactions();
-    auto const& Nr = reactions.size();
-    auto const& K = system.stoichiometricMatrix();
-
-    // Add a p control variable to `specs` for each *extent of reaction change* variable Δξ (one for each reactive constraint, aka, restricted reaction)
-    for(auto const& reaction : reactions)
-    {
-        ControlVariableP pvar;
-        pvar.name = "dxi[" + reaction.name() + "]"; // e.g., Δξ[H2O = H+ + OH-], Δξ[Calcite]
-        specs.addControlVariableP(pvar);
-    }
-
-    // The new number of p control variables in `specs` with the newly added extent of reaction variables Δξ
-    auto const& Np = specs.numControlVariablesP();
-
-    // Add reactivity constraints to `specs` to model the restricted reactions in the equilibrium problem (those that cannot react to full equilibrium but are constrained to a certain extent of reaction progress)
-    ReactivityConstraints rconstraints;
-    rconstraints.ids = vectorize(reactions, RKT_LAMBDA(x, x.name()));
-    rconstraints.Kn = K.transpose();
-    rconstraints.Kp = zeros(Nr, Np); // let p' be the last Nr entries in p; thus p' = Δξ and because tr(K)*n - Δξ = ξ0, we set Kn = tr(K) and Kp = = [0, -I] where I has dims Nr x Nr.
-    rconstraints.Kp.rightCols(Nr) = -identity(Nr, Nr);
-
-    specs.addReactivityConstraints(rconstraints);
-
-    //-------------------------------------------------------------------
-    // NOTE 1! There is no need to add ξ0 as inputs into `specs` for each
-    // kinetically controlled reaction because these go in the b vector
-    // together with element amounts.
-    // NOTE 2! No need to also add surface areas as inputs because there
-    // are added by default in EquilibriumSpecs implementation.
-    //-------------------------------------------------------------------
-
-    // Add Δt as input to the calculation (idt is the index of dt := Δt input in the w argument vector when defining equation constraints)
-    const auto idt = specs.addInput("dt");
-
-    // Compute matrix M = tr(K)*K
-    const MatrixXd M = K.transpose() * K;
-
-    // Add equation constraints to `specs` to model the kinetic rates of the reactions in the equilibrium problem
-    EquationConstraints econstraints;
-    econstraints.ids = rconstraints.ids;
-    econstraints.fn = [=](ChemicalState const& state, VectorXrConstRef p, VectorXrConstRef w) -> VectorXr
-    {
-        auto const& dt = w[idt]; // Δt can be found at the input vector w
-        auto const& dxi = p.tail(Nr); // Δξ = the last Nr added entries in p
-        const VectorXr r = state.props().reactionRates();
-        return dxi - dt * M * r; // Δξ - ΔtMr = 0
-    };
-
-    specs.addConstraints(econstraints);
-
-    return specs;
-}
-
-} // namespace
 
 struct KineticsSolver::Impl
 {
@@ -107,7 +45,7 @@ struct KineticsSolver::Impl
     EquilibriumSolver ksolver;         ///< The equilibrium solver used for the kinetics calculations.
     EquilibriumConditions kconditions; ///< The equilibrium conditions used for the kinetics calculations.
     KineticsOptions koptions;          ///< The options of this kinetics solver.
-    KineticsResult kresult;         ///< The result of the equilibrium calculation with kinetic constraints
+    KineticsResult kresult;            ///< The result of the equilibrium calculation with kinetic constraints
     VectorXr w;                        ///< The auxiliary vector used to set the w input variables of the equilibrium conditions used for the kinetics calculations.
     VectorXd plower;                   ///< The auxiliary vector used to set the lower bounds of p variables of the equilibrium conditions used for the kinetics calculations.
     VectorXd pupper;                   ///< The auxiliary vector used to set the upper bounds of p variables of the equilibrium conditions used for the kinetics calculations.
@@ -117,7 +55,7 @@ struct KineticsSolver::Impl
     : system(especs.system()),
       especs(especs),
       edims(especs),
-      kspecs(createEquilibriumSpecsForKinetics(especs)),
+      kspecs(detail::createEquilibriumSpecsForKinetics(especs)),
       kdims(kspecs),
       idt(kspecs.indexInputVariable("dt")),
       ksolver(kspecs),
