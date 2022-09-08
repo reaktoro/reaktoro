13b5eb8b98612b11ef3c52808ff177946706fcc0
Allan Leal <allanmulin@gmail.com>
2022-09-08 15:54:04 +0200

    Added C++ tests for YAML serialization of ReactionRateModelParamsPalandriKharaka

--- Reaktoro/Serialization/Models.YAML.test.cxx
+++ Reaktoro/Serialization/Models.YAML.test.cxx
@@ -30,6 +30,7 @@ using namespace Catch;
 #include <Reaktoro/Core/Params.hpp>
 #include <Reaktoro/Core/Phase.hpp>
 #include <Reaktoro/Core/Species.hpp>
+#include <Reaktoro/Models/ReactionRateModels/ReactionRateModelPalandriKharaka.hpp>
 #include <Reaktoro/Models/ReactionThermoModels/ReactionThermoModelConstLgK.hpp>
 #include <Reaktoro/Models/ReactionThermoModels/ReactionThermoModelGemsLgK.hpp>
 #include <Reaktoro/Models/ReactionThermoModels/ReactionThermoModelPhreeqcLgK.hpp>
@@ -288,6 +289,39 @@ H0: [[4.0, 5.0, 6.0], [7.0, 8.0, 9.0]]
 //======================================================================
 String params_svm_const = R"xyz(
 V0: 1.23e-5
+)xyz";
+
+//======================================================================
+// Parameters in YAML format for reaction rate models (rrm)
+//======================================================================
+String params_rrm_palandri_kharaka_1 = R"xyz(
+Mineral: Halite
+Mechanisms:
+    Neutral:   { lgk: -0.21, E: 7.4 }
+)xyz";
+
+String params_rrm_palandri_kharaka_2 = R"xyz(
+Mineral: Pyrite
+Mechanisms:
+    Acid:      { lgk: -7.52, E: 56.9, a(H+): -0.500, a(Fe+3): 0.500 }
+    Neutral:   { lgk: -4.55, E: 56.9, a(O2): 0.500 }
+)xyz";
+
+String params_rrm_palandri_kharaka_3 = R"xyz(
+Mineral: Epidote
+Mechanisms:
+    Acid:      { lgk: -10.60, E: 71.1, a(H+):  0.338 }
+    Neutral:   { lgk: -11.99, E: 70.7 }
+    Base:      { lgk: -17.33, E: 79.1, a(H+): -0.556 }
+)xyz";
+
+String params_rrm_palandri_kharaka_4 = R"xyz(
+Mineral: FakeMineral FakeMineralAlternativeName
+Mechanisms:
+    Acid:      { lgk: -0.30, E: 14.4, a(H+): 1.000 }
+    Neutral:   { lgk: -5.81, E: 23.5 }
+    Carbonate: { lgk: -3.48, E: 35.4, P(CO2): 1.000 }
+    Base:      { lgk: -17.33, E: 79.1, a((Ca)(CO3)): -0.556, p: 2.34, q: 1.46 }
 )xyz";
 
 template<typename VecType>
@@ -598,5 +632,119 @@ TEST_CASE("Testing Serialization for StandardVolumeModel types", "[Serialization
         StandardVolumeModelParamsConstant params = yaml::parse(params_svm_const);
 
         CHECK( params.V0 == 1.23e-5 );
+    }
+}
+
+TEST_CASE("Testing Serialization for ReactionRateModel types", "[Serialization]")
+{
+    SECTION("Testing YAML serialization of ReactionRateModelParamsPalandriKharaka")
+    {
+        ReactionRateModelParamsPalandriKharaka params;
+
+        params = yaml::parse(params_rrm_palandri_kharaka_1);
+
+        CHECK( params.names == Strings{"Halite"} );
+        CHECK( params.mechanisms.size() == 1 );
+        CHECK( params.mechanisms[0].name == "Neutral" );
+        CHECK( params.mechanisms[0].lgk == -0.21 );
+        CHECK( params.mechanisms[0].E == 7.40 );
+        CHECK( params.mechanisms[0].p == 1.00 );
+        CHECK( params.mechanisms[0].q == 1.00 );
+        CHECK( params.mechanisms[0].catalysts.size() == 0 );
+
+        params = yaml::parse(params_rrm_palandri_kharaka_2);
+
+        CHECK( params.names == Strings{"Pyrite"} );
+        CHECK( params.mechanisms.size() == 2 );
+        CHECK( params.mechanisms[0].name == "Acid" );
+        CHECK( params.mechanisms[0].lgk == -7.52 );
+        CHECK( params.mechanisms[0].E == 56.9 );
+        CHECK( params.mechanisms[0].p == 1.00 );
+        CHECK( params.mechanisms[0].q == 1.00 );
+        CHECK( params.mechanisms[0].catalysts.size() == 2 );
+        CHECK( params.mechanisms[0].catalysts[0].formula == "H+" );
+        CHECK( params.mechanisms[0].catalysts[0].power == -0.50 );
+        CHECK( params.mechanisms[0].catalysts[0].property == "a" );
+        CHECK( params.mechanisms[0].catalysts[1].formula == "Fe+3" );
+        CHECK( params.mechanisms[0].catalysts[1].power == 0.50 );
+        CHECK( params.mechanisms[0].catalysts[1].property == "a" );
+        CHECK( params.mechanisms[1].name == "Neutral" );
+        CHECK( params.mechanisms[1].lgk == -4.55 );
+        CHECK( params.mechanisms[1].E == 56.9 );
+        CHECK( params.mechanisms[1].p == 1.00 );
+        CHECK( params.mechanisms[1].q == 1.00 );
+        CHECK( params.mechanisms[1].catalysts.size() == 1 );
+        CHECK( params.mechanisms[1].catalysts[0].formula == "O2" );
+        CHECK( params.mechanisms[1].catalysts[0].power == 0.50 );
+        CHECK( params.mechanisms[1].catalysts[0].property == "a" );
+
+        params = yaml::parse(params_rrm_palandri_kharaka_3);
+
+        CHECK( params.names == Strings{"Epidote"} );
+        CHECK( params.mechanisms.size() == 3 );
+        CHECK( params.mechanisms[0].name == "Acid" );
+        CHECK( params.mechanisms[0].lgk == -10.60 );
+        CHECK( params.mechanisms[0].E == 71.1 );
+        CHECK( params.mechanisms[0].p == 1.00 );
+        CHECK( params.mechanisms[0].q == 1.00 );
+        CHECK( params.mechanisms[0].catalysts.size() == 1 );
+        CHECK( params.mechanisms[0].catalysts[0].formula == "H+" );
+        CHECK( params.mechanisms[0].catalysts[0].power == 0.338 );
+        CHECK( params.mechanisms[0].catalysts[0].property == "a" );
+        CHECK( params.mechanisms[1].name == "Neutral" );
+        CHECK( params.mechanisms[1].lgk == -11.99 );
+        CHECK( params.mechanisms[1].E == 70.7 );
+        CHECK( params.mechanisms[1].p == 1.00 );
+        CHECK( params.mechanisms[1].q == 1.00 );
+        CHECK( params.mechanisms[1].catalysts.size() == 0 );
+        CHECK( params.mechanisms[2].name == "Base" );
+        CHECK( params.mechanisms[2].lgk == -17.33 );
+        CHECK( params.mechanisms[2].E == 79.1 );
+        CHECK( params.mechanisms[2].p == 1.00 );
+        CHECK( params.mechanisms[2].q == 1.00 );
+        CHECK( params.mechanisms[2].catalysts.size() == 1 );
+        CHECK( params.mechanisms[2].catalysts[0].formula == "H+" );
+        CHECK( params.mechanisms[2].catalysts[0].power == -0.556 );
+        CHECK( params.mechanisms[2].catalysts[0].property == "a" );
+
+        params = yaml::parse(params_rrm_palandri_kharaka_4);
+
+        CHECK( params.names == Strings{"FakeMineral", "FakeMineralAlternativeName"} );
+        CHECK( params.mechanisms.size() == 4 );
+        CHECK( params.mechanisms[0].name == "Acid" );
+        CHECK( params.mechanisms[0].lgk == -0.30 );
+        CHECK( params.mechanisms[0].E == 14.4 );
+        CHECK( params.mechanisms[0].p == 1.00 );
+        CHECK( params.mechanisms[0].q == 1.00 );
+        CHECK( params.mechanisms[0].catalysts.size() == 1 );
+        CHECK( params.mechanisms[0].catalysts[0].formula == "H+" );
+        CHECK( params.mechanisms[0].catalysts[0].power == 1.0 );
+        CHECK( params.mechanisms[0].catalysts[0].property == "a" );
+        CHECK( params.mechanisms[1].name == "Neutral" );
+        CHECK( params.mechanisms[1].lgk == -5.81 );
+        CHECK( params.mechanisms[1].E == 23.5 );
+        CHECK( params.mechanisms[1].p == 1.00 );
+        CHECK( params.mechanisms[1].q == 1.00 );
+        CHECK( params.mechanisms[1].catalysts.size() == 0 );
+        CHECK( params.mechanisms[2].name == "Carbonate" );
+        CHECK( params.mechanisms[2].lgk == -3.48 );
+        CHECK( params.mechanisms[2].E == 35.4 );
+        CHECK( params.mechanisms[2].p == 1.00 );
+        CHECK( params.mechanisms[2].q == 1.00 );
+        CHECK( params.mechanisms[2].catalysts.size() == 1 );
+        CHECK( params.mechanisms[2].catalysts[0].formula == "CO2" );
+        CHECK( params.mechanisms[2].catalysts[0].power == 1.0 );
+        CHECK( params.mechanisms[2].catalysts[0].property == "P" );
+        CHECK( params.mechanisms[3].name == "Base" );
+        CHECK( params.mechanisms[3].lgk == -17.33 );
+        CHECK( params.mechanisms[3].E == 79.1 );
+        CHECK( params.mechanisms[3].p == 2.34 );
+        CHECK( params.mechanisms[3].q == 1.46 );
+        CHECK( params.mechanisms[3].catalysts.size() == 1 );
+        CHECK( params.mechanisms[3].catalysts[0].formula == "(Ca)(CO3)" );
+        CHECK( params.mechanisms[3].catalysts[0].power == -0.556 );
+        CHECK( params.mechanisms[3].catalysts[0].property == "a" );
+
+
     }
 }
