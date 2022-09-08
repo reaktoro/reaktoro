fd1dfa4f79565188343af45e490b2d622924369f
Allan Leal <allanmulin@gmail.com>
2022-09-08 15:54:48 +0200

    Implemented Data serialization for Common module types

---
+++ Reaktoro/Serialization/Common.Data.hpp
@@ -0,0 +1,27 @@
+// Reaktoro is a unified framework for modeling chemically reactive systems.
+//
+// Copyright © 2014-2022 Allan Leal
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library. If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+// Reaktoro includes
+#include <Reaktoro/Common/Types.hpp>
+#include <Reaktoro/Core/Data.hpp>
+
+namespace Reaktoro {
+
+
+} // namespace YAML
