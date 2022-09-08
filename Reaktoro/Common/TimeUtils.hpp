fc0ddc171d6338bdb191db1a2094abaab18ccc90
Allan Leal <allanmulin@gmail.com>
2022-09-08 15:56:20 +0200

    Implemented Stopwatch in TimeUtils

--- Reaktoro/Common/TimeUtils.hpp
+++ Reaktoro/Common/TimeUtils.hpp
@@ -1,19 +1,3 @@
-// Reaktoro is a unified framework for modeling chemically reactive systems.
-//
-// Copyright © 2014-2022 Allan Leal
-//
-// This library is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public
-// License as published by the Free Software Foundation; either
-// version 2.1 of the License, or (at your option) any later version.
-//
-// This library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-// Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with this library. If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
@@ -32,13 +16,38 @@ auto time() -> Time;
 
 /// Return the elapsed time between two time points (in units of s)
 /// @param end The end time point
-/// @param begin The begin time point
+/// @param end The begin time point
 /// @return The elapsed time between *end* and *begin* in seconds
 auto elapsed(const Time& end, const Time& begin) -> double;
 
 /// Return the elapsed time between a time point and now (in units of s)
-/// @param begin The begin time point
+/// @param end The begin time point
 /// @return The elapsed time between now and *begin* in seconds
 auto elapsed(const Time& begin) -> double;
+
+/// Used for measuring elapsed time since object creation.
+class Stopwatch
+{
+public:
+    /// Construct a Stopwatch object and start measuring time.
+    /// @param reading The variable to store the final measured elapsed time
+    Stopwatch(double& reading) : reading(reading), tstart(time()) {}
+
+    /// Destroy this Stopwatch object and stop measuring time.
+    ~Stopwatch() { stop(); }
+
+    /// Start measuring time.
+    auto start() -> void { tstart = time(); }
+
+    /// Stop measuring time.
+    auto stop() -> void { reading = elapsed(tstart); }
+
+private:
+    /// The auxiliary time variable marking the start of timing.
+    Time tstart;
+
+    /// The reference to the variable that will store the final measured elapsed time.
+    double& reading;
+};
 
 } // namespace Reaktoro
