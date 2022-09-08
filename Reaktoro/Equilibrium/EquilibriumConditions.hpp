75932baf7a9360b6f6ff164e77b4382941850722
Allan Leal <allanmulin@gmail.com>
2022-09-08 15:55:09 +0200

    Implemented methods in EquilibriumConditions to set surface areas

--- Reaktoro/Equilibrium/EquilibriumConditions.hpp
+++ Reaktoro/Equilibrium/EquilibriumConditions.hpp
@@ -31,7 +31,7 @@ class EquilibriumConditions
 {
 public:
     /// Construct an EquilibriumConditions object.
-    explicit EquilibriumConditions(const EquilibriumSpecs& specs);
+    explicit EquilibriumConditions(EquilibriumSpecs const& specs);
 
     //=================================================================================================
     //
@@ -42,91 +42,108 @@ public:
     /// Specify the **temperature** of the system at chemical equilibrium.
     /// @param value The temperature of the system
     /// @param unit The unit of the temperature value (must be convertible to K)
-    auto temperature(real value, String unit="K") -> void;
+    auto temperature(real const& value, String const& unit="K") -> void;
 
     /// Specify the **pressure** of the system at chemical equilibrium.
     /// @param value The pressure of the system
     /// @param unit The unit of the pressure value (must be convertible to Pa)
-    auto pressure(real value, String unit="Pa") -> void;
+    auto pressure(real const& value, String const& unit="Pa") -> void;
 
     /// Specify the **volume** of the system at chemical equilibrium.
     /// @param value The volume of the system
     /// @param unit The unit of the volume value (must be convertible to m@sup{3})
-    auto volume(real value, String unit="m3") -> void;
+    auto volume(real const& value, String const& unit="m3") -> void;
 
     /// Specify the **internal energy** of the system at chemical equilibrium.
     /// @param value The internal energy of the system
     /// @param unit The unit of the internal energy value (must be convertible to J)
-    auto internalEnergy(real value, String unit="J") -> void;
+    auto internalEnergy(real const& value, String const& unit="J") -> void;
 
     /// Specify the **enthalpy** of the system at chemical equilibrium.
     /// @param value The enthalpy of the system
     /// @param unit The unit of the enthalpy value (must be convertible to J)
-    auto enthalpy(real value, String unit="J") -> void;
+    auto enthalpy(real const& value, String const& unit="J") -> void;
 
     /// Specify the **Gibbs energy** of the system at chemical equilibrium.
     /// @param value The Gibbs energy of the system
     /// @param unit The unit of the Gibbs energy value (must be convertible to J)
-    auto gibbsEnergy(real value, String unit="J") -> void;
+    auto gibbsEnergy(real const& value, String const& unit="J") -> void;
 
     /// Specify the **Helmholtz energy** of the system at chemical equilibrium.
     /// @param value The Helmholtz energy of the system
     /// @param unit The unit of the Helmholtz energy value (must be convertible to J)
-    auto helmholtzEnergy(real value, String unit="J") -> void;
+    auto helmholtzEnergy(real const& value, String const& unit="J") -> void;
 
     /// Specify the **entropy** of the system at chemical equilibrium.
     /// @param value The entropy of the system
     /// @param unit The unit of the entropy value (must be convertible to J/K)
-    auto entropy(real value, String unit="J/K") -> void;
+    auto entropy(real const& value, String const& unit="J/K") -> void;
 
     /// Specify the **electric charge** at chemical equilibrium.
     /// @param value The electric charge amount in the system
     /// @param unit The unit of the electric charge value (must be convertible to mol)
-    auto charge(real value, String unit="mol") -> void;
+    auto charge(real const& value, String const& unit="mol") -> void;
 
     /// Specify the **amount of an element** at chemical equilibrium.
     /// @param element The name or index of the element in the system
     /// @param value The amount of an element in the system
     /// @param unit The unit of the element amount value (must be convertible to mol)
-    auto elementAmount(const StringOrIndex& element, real value, String unit="mol") -> void;
+    auto elementAmount(StringOrIndex const& element, real const& value, String const& unit="mol") -> void;
 
     /// Specify the **amount of an element in a phase** at chemical equilibrium.
     /// @param element The name or index of the element in the system
     /// @param phase The name or index of the phase in the system
     /// @param value The amount of an element in a phase
     /// @param unit The unit of the element amount value (must be convertible to mol)
-    auto elementAmountInPhase(const StringOrIndex& element, const StringOrIndex& phase, real value, String unit="mol") -> void;
+    auto elementAmountInPhase(StringOrIndex const& element, StringOrIndex const& phase, real const& value, String const& unit="mol") -> void;
 
     /// Specify the **mass of an element** at chemical equilibrium.
     /// @param element The name or index of the element in the system
     /// @param value The mass of an element in the system
     /// @param unit The unit of the element mass value (must be convertible to kg)
-    auto elementMass(const StringOrIndex& element, real value, String unit="kg") -> void;
+    auto elementMass(StringOrIndex const& element, real const& value, String const& unit="kg") -> void;
 
     /// Specify the **mass of an element in a phase** at chemical equilibrium.
     /// @param element The name or index of the element in the system
     /// @param phase The name or index of the phase in the system
     /// @param value The mass of an element in a phase
     /// @param unit The unit of the element mass value (must be convertible to kg)
-    auto elementMassInPhase(const StringOrIndex& element, const StringOrIndex& phase, real value, String unit="kg") -> void;
+    auto elementMassInPhase(StringOrIndex const& element, StringOrIndex const& phase, real const& value, String const& unit="kg") -> void;
 
     /// Specify the **amount of a phase** at chemical equilibrium.
     /// @param phase The name or index of the phase in the system
     /// @param value The amount of a phase in the system
     /// @param unit The unit of the phase amount value (must be convertible to mol)
-    auto phaseAmount(const StringOrIndex& phase, real value, String unit="mol") -> void;
+    auto phaseAmount(StringOrIndex const& phase, real const& value, String const& unit="mol") -> void;
 
     /// Specify the **mass of a phase** at chemical equilibrium.
     /// @param phase The name or index of the phase in the system
     /// @param value The mass of a phase in the system
     /// @param unit The unit of the phase mass value (must be convertible to kg)
-    auto phaseMass(const StringOrIndex& phase, real value, String unit="kg") -> void;
+    auto phaseMass(StringOrIndex const& phase, real const& value, String const& unit="kg") -> void;
 
     /// Specify the **volume of a phase** at chemical equilibrium.
     /// @param phase The name or index of the phase in the system
     /// @param value The volume of a phase in the system
     /// @param unit The unit of the phase volume value (must be convertible to m3)
-    auto phaseVolume(const StringOrIndex& phase, real value, String unit="m3") -> void;
+    auto phaseVolume(StringOrIndex const& phase, real const& value, String const& unit="m3") -> void;
+
+    //=================================================================================================
+    //
+    // METHODS TO SPECIFY SURFACE AREA CONDITIONS
+    //
+    //=================================================================================================
+
+    /// Specify the **surface areas of all reacting phase interfaces** at chemical equilibrium.
+    /// The surface areas that are unknown in the calculation are ignored in this method.
+    /// @param values The area of all surfaces in the system (in m2)
+    auto surfaceAreas(ArrayXrConstRef const& values) -> void;
+
+    /// Specify the **surface area of a reacting phase interface** at chemical equilibrium.
+    /// @param surface The name or index of the surface in the system
+    /// @param value The area of a surface in the system
+    /// @param unit The unit of the surface area value (must be convertible to m2)
+    auto surfaceArea(StringOrIndex const& surface, real const& value, String const& unit="m2") -> void;
 
     //=================================================================================================
     //
@@ -140,28 +157,28 @@ public:
     /// @param unit The unit for the constrained chemical potential value (must be convertible to J/mol).
     /// @warning An error is thrown if the specifications for the chemical equilibrium calculation
     /// @warning do not consider a chemical potential constraint for the substance.
-    auto chemicalPotential(String substance, real value, String unit="J/mol") -> void;
+    auto chemicalPotential(String const& substance, real const& value, String const& unit="J/mol") -> void;
 
     /// Specify the **ln activity** of a species at chemical equilibrium.
     /// @param species The name of the chemical species.
     /// @param value The constrained ln activity value.
     /// @warning An error is thrown if the specifications for the chemical equilibrium calculation
     /// @warning do not consider an activity constraint for the species.
-    auto lnActivity(String species, real value) -> void;
+    auto lnActivity(String const& species, real const& value) -> void;
 
     /// Specify the **lg activity** of a species at chemical equilibrium.
     /// @param species The name of the chemical species.
     /// @param value The constrained lg activity value.
     /// @warning An error is thrown if the specifications for the chemical equilibrium calculation
     /// @warning do not consider an activity constraint for the species.
-    auto lgActivity(String species, real value) -> void;
+    auto lgActivity(String const& species, real const& value) -> void;
 
     /// Specify the **activity** of a species at chemical equilibrium.
     /// @param species The name of the chemical species.
     /// @param value The constrained activity value.
     /// @warning An error is thrown if the specifications for the chemical equilibrium calculation
     /// @warning do not consider an activity constraint for the species.
-    auto activity(String species, real value) -> void;
+    auto activity(String const& species, real const& value) -> void;
 
     /// Specify the **fugacity** of a gaseous species at chemical equilibrium.
     /// @param species The name of the gaseous species.
@@ -169,32 +186,32 @@ public:
     /// @param unit The unit for the constrained fugacity value (must be convertible to Pa; default is bar).
     /// @warning An error is thrown if the specifications for the chemical equilibrium calculation
     /// @warning do not consider a fugacity constraint for the gas.
-    auto fugacity(String species, real value, String unit="bar") -> void;
+    auto fugacity(String const& species, real const& value, String const& unit="bar") -> void;
 
     /// Specify the *pH* at chemical equilibrium.
     /// @param value The constrained value for pH.
     /// @warning An error is thrown if the specifications for the chemical equilibrium calculation
     /// @warning do not consider a pH constraint.
-    auto pH(real value) -> void;
+    auto pH(real const& value) -> void;
 
     /// Specify the *pMg* at chemical equilibrium.
     /// @param value The constrained value for pMg.
     /// @warning An error is thrown if the specifications for the chemical equilibrium calculation
     /// @warning do not consider a pMg constraint.
-    auto pMg(real value) -> void;
+    auto pMg(real const& value) -> void;
 
     /// Specify the *pE* at chemical equilibrium.
     /// @param value The constrained value for pE.
     /// @warning An error is thrown if the specifications for the chemical equilibrium calculation
     /// @warning do not consider a pE constraint.
-    auto pE(real value) -> void;
+    auto pE(real const& value) -> void;
 
     /// Specify the *Eh* at chemical equilibrium.
     /// @param value The constrained value for Eh.
     /// @param unit The unit of the constrained value for Eh (must be convertible to V).
     /// @warning An error is thrown if the specifications for the chemical equilibrium calculation
     /// @warning do not consider an Eh constraint.
-    auto Eh(real value, String unit="V") -> void;
+    auto Eh(real const& value, String const& unit="V") -> void;
 
     //=================================================================================================
     //
@@ -203,22 +220,22 @@ public:
     //=================================================================================================
 
     /// Set the lower bound for temperature during the equilibrium calculation.
-    auto setLowerBoundTemperature(double value, String unit="K") -> void;
+    auto setLowerBoundTemperature(double value, String const& unit="K") -> void;
 
     /// Set the upper bound for temperature during the equilibrium calculation.
-    auto setUpperBoundTemperature(double value, String unit="K") -> void;
+    auto setUpperBoundTemperature(double value, String const& unit="K") -> void;
 
     /// Set the lower bound for pressure during the equilibrium calculation.
-    auto setLowerBoundPressure(double value, String unit="Pa") -> void;
+    auto setLowerBoundPressure(double value, String const& unit="Pa") -> void;
 
     /// Set the upper bound for pressure during the equilibrium calculation.
-    auto setUpperBoundPressure(double value, String unit="Pa") -> void;
+    auto setUpperBoundPressure(double value, String const& unit="Pa") -> void;
 
     /// Set the lower bound for the amount of a titrant during the equilibrium calculation.
-    auto setLowerBoundTitrant(String substance, double value, String unit="mol") -> void;
+    auto setLowerBoundTitrant(String const& substance, double value, String const& unit="mol") -> void;
 
     /// Set the upper bound for the amount of a titrant during the equilibrium calculation.
-    auto setUpperBoundTitrant(String substance, double value, String unit="mol") -> void;
+    auto setUpperBoundTitrant(String const& substance, double value, String const& unit="mol") -> void;
 
     //=================================================================================================
     //
@@ -228,60 +245,53 @@ public:
 
     /// Set the input variable with name `input` to the value in `val`.
     /// @warning An error is thrown if there are no input variables with name `input`.
-    auto set(const String& input, const real& val) -> void;
+    auto set(String const& input, real const& val) -> void;
 
     /// Set the input variable with name `input` to the value in `val`.
     /// @warning An error is thrown if there are no input variables with name `input`.
-    auto setInputVariable(const String& input, const real& val) -> void;
+    auto setInputVariable(String const& input, real const& val) -> void;
 
     /// Set the input variables with given vector of input values.
-    auto setInputVariables(VectorXrConstRef const& values) -> void;
+    auto setInputVariables(ArrayXrConstRef const& values) -> void;
 
     /// Set the values of the specified lower bounds for the *p* control variables.
-    auto setLowerBoundsControlVariablesP(VectorXdConstRef const& values) -> void;
+    auto setLowerBoundsControlVariablesP(ArrayXdConstRef const& values) -> void;
 
     /// Set the values of the specified upper bounds for the *p* control variables.
-    auto setUpperBoundsControlVariablesP(VectorXdConstRef const& values) -> void;
+    auto setUpperBoundsControlVariablesP(ArrayXdConstRef const& values) -> void;
 
     /// Return the chemical system associated with the equilibrium conditions.
-    auto system() const -> const ChemicalSystem&;
+    auto system() const -> ChemicalSystem const&;
 
     /// Return the names of the input variables associated with the equilibrium conditions.
-    auto inputNames() const -> const Strings&;
+    auto inputNames() const -> Strings const&;
 
     /// Return the values of the input variables associated with the equilibrium conditions.
-    auto inputValues() const -> VectorXrConstRef;
+    auto inputValues() const -> ArrayXrConstRef;
+
+    /// Return the values of the input variables associated with the equilibrium conditions.
+    /// @param name The unique name of the *w* input variable
+    auto inputValue(String const& name) const -> real const&;
 
     /// Return the specified lower bounds for the *p* control variables.
-    auto lowerBoundsControlVariablesP() const -> VectorXdConstRef;
+    auto lowerBoundsControlVariablesP() const -> ArrayXdConstRef;
 
     /// Return the specified upper bounds for the *p* control variables.
-    auto upperBoundsControlVariablesP() const -> VectorXdConstRef;
+    auto upperBoundsControlVariablesP() const -> ArrayXdConstRef;
 
 private:
-    /// The chemical system associated with the equilibrium problem specifications.
-    const ChemicalSystem m_system;
-
-    /// The names of the input variables in the equilibrium problem specifications.
-    const Strings m_inputs;
-
-    /// The names of the *p* control variables variables in the equilibrium problem specifications.
-    const Strings m_control_variables_p;
-
-    /// The index of temperature variable in *p* or -1 if not unknown.
-    const long m_idxT;
-
-    /// The index of pressure variable in *p* or -1 if not unknown.
-    const long m_idxP;
-
-    /// The current values of the input variables.
-    VectorXr m_inputs_values;
-
-    /// The lower bounds for the *p* control variables.
-    VectorXd m_plower;
-
-    /// The upper bounds for the *p* control variables.
-    VectorXd m_pupper;
+    const ChemicalSystem msystem;         ///< The chemical system associated with these equilibrium conditions.
+    const Strings wvars;                  ///< The names of the *w* input variables in the equilibrium problem.
+    const Strings pvars;                  ///< The names of the *p* control variables variables in the equilibrium problem.
+    const Index itemperature_p;           ///< The index of the temperature variable among the *p* control variables.
+    const Index ipressure_p;              ///< The index of the pressure variable among the *p* control variables.
+    const Indices isurface_areas_w;       ///< The indices of the surface area variables among the *w* input variables.
+    const Indices isurface_areas_p;       ///< The indices of the surface area variables among the *p* control variables.
+    const Indices isurface_areas_known;   ///< The indices of the surfaces areas that are known.
+    const Indices isurface_areas_unknown; ///< The indices of the surfaces areas that are unknown.
+    ArrayXr w;                           ///< The values of the *w* input variables.
+    ArrayXd plower;                      ///< The lower bounds for the *p* control variables.
+    ArrayXd pupper;                      ///< The upper bounds for the *p* control variables.
 };
 
 } // namespace Reaktoro
