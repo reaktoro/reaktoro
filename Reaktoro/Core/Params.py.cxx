ebf760b1c9d20e50bd7e51a5a8224518acaf1e5c
Allan Leal <allanmulin@gmail.com>
2022-09-08 15:54:47 +0200

    Implemented py bindings for Params

--- Reaktoro/Core/Params.py.cxx
+++ Reaktoro/Core/Params.py.cxx
@@ -25,10 +25,14 @@ using namespace Reaktoro;
 void exportParams(py::module& m)
 {
     py::class_<Params>(m, "Params")
-        .def(py::init<>())
-        .def("at", &Params::at, return_internal_ref)
-        .def("get", &Params::get, return_internal_ref)
-        .def("set", py::overload_cast<const String&, const Params&>(&Params::set))
-        .def("set", py::overload_cast<const String&, const Param&>(&Params::set))
+        .def(py::init<>(), "Construct a default Params object.")
+        .def(py::init<Data const&>(), "Construct a Params object with given parameters as Data object.")
+        .def_static("embedded", &Params::embedded, "Return parameters with the given file path within the virtual directory of embedded resources.")
+        .def_static("local", &Params::local, "Return parameters with the given local file path.")
+        .def("data", &Params::data, return_internal_ref, "Return the underlying Data object of this Params object.")
+        .def("append", py::overload_cast<Params const&>(&Params::append), return_internal_ref, "Append another Params object into this.")
+        .def("append", py::overload_cast<Data const&>(&Params::append), return_internal_ref, "Append a Data object containing parameters into this Params object.")
+        .def("__iadd__", [](Params& self, Params const& other) { return self.append(other); }, return_internal_ref, "Append another Params object into this.")
+        .def("__getitem__", [](Params& self, String const& name) { return self[name]; }, return_internal_ref, "Return the set of parameters with given name.")
         ;
 }
