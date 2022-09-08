b3a0a8548c1792c12d20aa2f645365f8c8982d8e
Allan Leal <allanmulin@gmail.com>
2022-09-08 15:55:09 +0200

    Implemente KineticsResult

---
+++ Reaktoro/Kinetics/KineticsResult.hpp
@@ -0,0 +1,36 @@
+// Reaktoro is a unified framework for modeling chemically reactive systems.
+//
+// Copyright © 2014-2022 Allan Leal
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library. If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+// Reaktoro includes
+#include <Reaktoro/Equilibrium/EquilibriumResult.hpp>
+
+namespace Reaktoro {
+
+/// Used to describe the result of a chemical kinetics calculation.
+struct KineticsResult : EquilibriumResult
+{
+    /// Construct a default KineticsResult object.
+    KineticsResult() = default;
+
+    /// Construct a  KineticsResult object from a EquilibriumResult one.
+    KineticsResult(EquilibriumResult const& other)
+    : EquilibriumResult(other) {}
+};
+
+} // namespace Reaktoro
