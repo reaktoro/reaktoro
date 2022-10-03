169063ee70c7d1446efd9b4629f16162033690ba
Allan Leal <allanmulin@gmail.com>
2022-10-03 15:10:56 +0200

    Implemented C++ tests for interpolateLinear and interpolateQuadratic

--- Reaktoro/Common/InterpolationUtils.test.cxx
+++ Reaktoro/Common/InterpolationUtils.test.cxx
@@ -24,5 +24,40 @@ using namespace Reaktoro;
 
 TEST_CASE("Testing InterpolationUtils module", "[InterpolationUtils]")
 {
-    // TODO implement tests for InterpolationUtils.
+    SECTION("Checking method interpolateLinear")
+    {
+        const auto a = 2.3; // a in L(x) = a + bx
+        const auto b = 6.7; // b in L(x) = a + bx
+
+        const auto L = [&](auto x) { return a + b*x; };
+
+        const auto x0 = 5.7;
+        const auto x1 = 9.2;
+
+        const auto y0 = L(x0);
+        const auto y1 = L(x1);
+
+        for(auto x : Vec<double>{-12, -8.0, -4.0, 0.0, 4.0, 8.0, 12})
+            CHECK( interpolateLinear(x, x0, x1, y0, y1) == Approx(L(x)) );
+    }
+
+    SECTION("Checking method interpolateQuadratic")
+    {
+        const auto a = 2.3; // a in L(x) = a + bx + cx^2
+        const auto b = 6.7; // b in L(x) = a + bx + cx^2
+        const auto c = 8.4; // x in L(x) = a + bx + cx^2
+
+        const auto Q = [&](auto x) { return a + b*x + c*x*x; };
+
+        const auto x0 = 5.7;
+        const auto x1 = 7.2;
+        const auto x2 = 9.4;
+
+        const auto y0 = Q(x0);
+        const auto y1 = Q(x1);
+        const auto y2 = Q(x2);
+
+        for(auto x : Vec<double>{-12, -8.0, -4.0, 0.0, 4.0, 8.0, 12})
+            CHECK( interpolateQuadratic(x, x0, x1, x2, y0, y1, y2) == Approx(Q(x)) );
+    }
 }
