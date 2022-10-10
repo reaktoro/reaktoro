2fb7a527910795b0bef43e6ce405208867a5f13e
Allan Leal <allanmulin@gmail.com>
2022-10-10 16:55:31 +0200

    Implemented and tested waterThermoPropsWagnerPrussInterp

--- Reaktoro/Water/WaterInterpolation.test.cxx
+++ Reaktoro/Water/WaterInterpolation.test.cxx
@@ -20,33 +20,52 @@
 
 // Reaktoro includes
 #include <Reaktoro/Water/WaterInterpolation.hpp>
+#include <Reaktoro/Water/WaterThermoProps.hpp>
+#include <Reaktoro/Water/WaterThermoPropsUtils.hpp>
 #include <Reaktoro/Water/WaterUtils.hpp>
 using namespace Reaktoro;
 
-#define CHECK_DENSITY_LIQUID(T, PMPa) \
-    CHECK( waterDensityWagnerPrussInterp(T, PMPa*1e6) == Approx(waterDensityWagnerPruss(T, PMPa*1e6)) );
+#define CHECK_WATER_DENSITY_WAGNER_PRUSS_INTERP(T, PMPa, reltol)                          \
+    {                                                                                     \
+        auto expected = waterDensityWagnerPruss(T, PMPa*1e6).val();                       \
+        auto actual = waterDensityWagnerPrussInterp(T, PMPa*1e6).val();                   \
+        CHECK( actual == Approx(expected).epsilon(reltol) );                              \
+    }
 
-#define CHECK_DENSITY_GAS(T, PMPa) \
-    CHECK( waterDensityWagnerPrussInterp(T, PMPa*1e6) == Approx(waterDensityWagnerPruss(T, PMPa*1e6)) );
+#define CHECK_WATER_THERMO_PROPS_WAGNER_PRUSS_INTERP(T, PMPa, reltol)         \
+    {                                                                         \
+        auto expected = waterThermoPropsWagnerPruss(T, PMPa*1e6).D.val();     \
+        auto actual = waterThermoPropsWagnerPrussInterp(T, PMPa*1e6).D.val(); \
+        CHECK( actual == Approx(expected).epsilon(reltol) );                  \
+    }
 
 TEST_CASE("Testing water interpolation methods", "[WaterInterpolation]")
 {
-    const auto MPa = 1e6;
+    CHECK_WATER_DENSITY_WAGNER_PRUSS_INTERP( 300.000,    0.05, 0.01); // permit 1% error deviation
+    CHECK_WATER_DENSITY_WAGNER_PRUSS_INTERP( 354.467,    0.05, 0.01); // permit 1% error deviation
+    CHECK_WATER_DENSITY_WAGNER_PRUSS_INTERP( 354.468,    0.05, 0.01); // permit 1% error deviation
+    CHECK_WATER_DENSITY_WAGNER_PRUSS_INTERP( 400.000,    0.05, 0.01); // permit 1% error deviation
+    CHECK_WATER_DENSITY_WAGNER_PRUSS_INTERP(1273.000,    0.05, 0.01); // permit 1% error deviation
+    CHECK_WATER_DENSITY_WAGNER_PRUSS_INTERP(1273.000, 1000.00, 0.01); // permit 1% error deviation
+    CHECK_WATER_DENSITY_WAGNER_PRUSS_INTERP( 353.000,    2.00, 0.01); // permit 1% error deviation
+    CHECK_WATER_DENSITY_WAGNER_PRUSS_INTERP( 423.000,    8.00, 0.01); // permit 1% error deviation
+    CHECK_WATER_DENSITY_WAGNER_PRUSS_INTERP( 566.000,   14.00, 0.01); // permit 1% error deviation
+    CHECK_WATER_DENSITY_WAGNER_PRUSS_INTERP( 690.000,   87.00, 0.02); // permit 2% error deviation
+    CHECK_WATER_DENSITY_WAGNER_PRUSS_INTERP( 723.000,  125.00, 0.05); // permit 5% error deviation
+    CHECK_WATER_DENSITY_WAGNER_PRUSS_INTERP( 834.000,  345.00, 0.02); // permit 2% error deviation
 
-    CHECK( waterDensityWagnerPrussInterp( 300.000, 0.05*MPa).val()     == Approx(996.534) );      // error 1.093747046920056e-05 % when compared to 996.5338910044 from waterDensityWagnerPruss
-    CHECK( waterDensityWagnerPrussInterp( 354.467, 0.05*MPa).val()     == Approx(970.942) );      // error 1.017359326934713e-05 % when compared to 970.9420987797 from waterDensityWagnerPruss
-    CHECK( waterDensityWagnerPrussInterp( 354.468, 0.05*MPa).val()     == Approx(0.3086359765) ); // error 0.000774660655486% when compared to 0.3086383674 from waterDensityWagnerPruss
-    CHECK( waterDensityWagnerPrussInterp( 400.000, 0.05*MPa).val()     == Approx(0.27229) );      // error 0.001559709779697% when compared to 0.272294247 from waterDensityWagnerPruss
-    CHECK( waterDensityWagnerPrussInterp(1273.000, 0.05*MPa).val()     == Approx(0.08511) );      // error 0.003205366516750% when compared to 0.085107272 from waterDensityWagnerPruss
-    CHECK( waterDensityWagnerPrussInterp(1273.000, 1000*MPa).val()     == Approx(809.28) );       // error 3.282837988918427e-05 % when compared to 809.2802656736 from waterDensityWagnerPruss
-    CHECK( waterDensityWagnerPrussInterp(353.000,   2*MPa).val()       == Approx(972.73352) );    // error 5.064858354630614e-05 % when compared to 972.7330273245 from waterDensityWagnerPruss
-    CHECK( waterDensityWagnerPrussInterp(423.000,   8*MPa).val()       == Approx(921.35355) );    // error 0.000772530481138% when compared to 921.360667792 from waterDensityWagnerPruss
-    CHECK( waterDensityWagnerPrussInterp(566.000,  14*MPa).val()       == Approx(738.0114944) );  // error 0.022461362711453% when compared to 738.1772990806 from waterDensityWagnerPruss
-    CHECK( waterDensityWagnerPrussInterp(690.000,  87*MPa).val()       == Approx(650.7964578) );  // error 1.072531461874359% when compared to 643.8905292933 from waterDensityWagnerPruss
-    CHECK( waterDensityWagnerPrussInterp(723.000, 125*MPa).val()       == Approx(686.10870775) ); // error 4.170730301765396% when compared to 658.6386653549 from waterDensityWagnerPruss
-    CHECK( waterDensityWagnerPrussInterp(834.000, 345*MPa).val()       == Approx(757.17433517) ); // error 0.520612296527317% when compared to 753.2528084254 from waterDensityWagnerPruss
-
-    CHECK( waterDensityWagnerPrussInterp( 370.000, 0.15*MPa).val()     == Approx(waterDensityWagnerPruss( 370.000, 0.15*MPa)) );
+    CHECK_WATER_THERMO_PROPS_WAGNER_PRUSS_INTERP( 300.000,    0.05, 0.01); // permit 1% error deviation
+    CHECK_WATER_THERMO_PROPS_WAGNER_PRUSS_INTERP( 354.467,    0.05, 0.01); // permit 1% error deviation
+    CHECK_WATER_THERMO_PROPS_WAGNER_PRUSS_INTERP( 354.468,    0.05, 0.01); // permit 1% error deviation
+    CHECK_WATER_THERMO_PROPS_WAGNER_PRUSS_INTERP( 400.000,    0.05, 0.01); // permit 1% error deviation
+    CHECK_WATER_THERMO_PROPS_WAGNER_PRUSS_INTERP(1273.000,    0.05, 0.01); // permit 1% error deviation
+    CHECK_WATER_THERMO_PROPS_WAGNER_PRUSS_INTERP(1273.000, 1000.00, 0.01); // permit 1% error deviation
+    CHECK_WATER_THERMO_PROPS_WAGNER_PRUSS_INTERP( 353.000,    2.00, 0.01); // permit 1% error deviation
+    CHECK_WATER_THERMO_PROPS_WAGNER_PRUSS_INTERP( 423.000,    8.00, 0.01); // permit 1% error deviation
+    CHECK_WATER_THERMO_PROPS_WAGNER_PRUSS_INTERP( 566.000,   14.00, 0.01); // permit 1% error deviation
+    CHECK_WATER_THERMO_PROPS_WAGNER_PRUSS_INTERP( 690.000,   87.00, 0.02); // permit 2% error deviation
+    CHECK_WATER_THERMO_PROPS_WAGNER_PRUSS_INTERP( 723.000,  125.00, 0.05); // permit 5% error deviation
+    CHECK_WATER_THERMO_PROPS_WAGNER_PRUSS_INTERP( 834.000,  345.00, 0.02); // permit 2% error deviation
 
     // TODO: To reduce errors, above (note the 4.17% error at 723K and 125MPa), more refinement in the grid is needed.
 }
