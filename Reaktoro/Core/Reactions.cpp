f989c125a1e1e7308e942f1ede5dcc99c4557b66
Allan Leal <allanmulin@gmail.com>
2022-10-14 16:06:52 +0200

    Implemented C++ class GeneralReaction

--- Reaktoro/Core/Reactions.cpp
+++ Reaktoro/Core/Reactions.cpp
@@ -17,7 +17,69 @@
 
 #include "Reactions.hpp"
 
+// Reaktoro includes
+#include <Reaktoro/Core/ReactionEquation.hpp>
+
 namespace Reaktoro {
+
+GeneralReaction::GeneralReaction()
+{}
+
+GeneralReaction::GeneralReaction(String const& equation)
+{
+    setName(equation);
+    setEquation(equation);
+}
+
+auto GeneralReaction::setName(String const& name) -> GeneralReaction&
+{
+    reaction_name = name;
+    return *this;
+}
+
+auto GeneralReaction::setEquation(String const& equation) -> GeneralReaction&
+{
+    reaction_equation = equation;
+    return *this;
+}
+
+auto GeneralReaction::setRateModel(ReactionRateModel const& model) -> GeneralReaction&
+{
+    rate_model = model;
+    return *this;
+}
+
+auto GeneralReaction::set(ReactionRateModel const& model) -> GeneralReaction&
+{
+    return setRateModel(model);
+}
+
+auto GeneralReaction::name() const -> String const&
+{
+    return reaction_name;
+}
+
+auto GeneralReaction::equation() const -> String const&
+{
+    return reaction_equation;
+}
+
+auto GeneralReaction::rateModel() const -> ReactionRateModel const&
+{
+    return rate_model;
+}
+
+auto GeneralReaction::operator()(SpeciesList const& species) const -> Vec<Reaction>
+{
+    errorif(reaction_name.empty(), "Converting a GeneralReaction object to a Reaction object requires a non-empty reaction name. Use method GeneralReaction::setName to resolve this.");
+    errorif(reaction_equation.empty(), "Converting a GeneralReaction object to a Reaction object requires a non-empty reaction equation. Use method GeneralReaction::setEquation to resolve this.");
+    errorif(!rate_model.initialized(), "Converting a GeneralReaction object to a Reaction object requires a non-empty reaction rate model. Use method GeneralReaction::setRateModel to resolve this.");
+
+    return { Reaction()
+        .withName(reaction_name)
+        .withEquation(ReactionEquation(reaction_equation, species))
+        .withRateModel(rate_model) };
+}
 
 Reactions::Reactions()
 {}
@@ -26,7 +88,7 @@ auto Reactions::convert(SpeciesList const& species) const -> Vec<Reaction>
 {
     Vec<Reaction> reactions;
 
-    for(auto const& fn : rgenerators)
+    for(auto const& fn : reaction_generators)
     {
         auto rxns = fn(species);
         reactions.insert(reactions.end(), rxns.begin(), rxns.end());
