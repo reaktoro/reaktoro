61b5fe8d3cd06f66f62aa7e54ad9eb80800bf8be
Allan Leal <allanmulin@gmail.com>
2022-10-14 16:06:52 +0200

    Implemented C++ class GeneralReaction

--- Reaktoro/Core/Reactions.hpp
+++ Reaktoro/Core/Reactions.hpp
@@ -20,14 +20,72 @@
 // Reaktoro includes
 #include <Reaktoro/Common/TraitsUtils.hpp>
 #include <Reaktoro/Common/Types.hpp>
-#include <Reaktoro/Core/PhaseList.hpp>
 #include <Reaktoro/Core/Reaction.hpp>
+#include <Reaktoro/Core/SpeciesList.hpp>
 
 namespace Reaktoro {
 
 /// The function type for the generation of reactions with given species in the chemical system.
 /// @param species The species composing the chemical system where the reactions will take place.
 using ReactionGenerator = Fn<Vec<Reaction>(SpeciesList const& species)>;
+
+/// Used to define a general reaction.
+/// @ingroup Core
+class GeneralReaction
+{
+public:
+    /// Construct a default GeneralReaction object.
+    GeneralReaction();
+
+    /// Construct a GeneralReaction object with given reaction equation as a formatted string.
+    /// @copydetails GeneralReaction::setEquation
+    explicit GeneralReaction(String const& equation);
+
+    /// Set the unique name of the reaction.
+    auto setName(String const& name) -> GeneralReaction&;
+
+    /// Set the equation of the reaction as a formatted string.
+    /// Below are examples of formatted strings representing reaction equations:
+    /// ~~~
+    /// GeneralReaction("CO2(g) + H2O = H+ + HCO3-");
+    /// GeneralReaction("Calcite + H+ = Ca++ + HCO3-");
+    /// GeneralReaction("Dolomite + 2*H+ = Ca++ + Mg++ + 2*HCO3-");
+    /// GeneralReaction("Quartz");
+    /// ~~~
+    /// Note that unity stoichiometric coefficients can be ommited from the equation. The operator
+    /// `*` must be used when this is not the case. Also note that equations with a single reactant
+    /// name is possible, as in the Quartz reaction above. This exists to permit reaction rates to
+    /// be defined for a single species instead of a normal stoichiometrically balanced reaction.
+    auto setEquation(String const& equation) -> GeneralReaction&;
+
+    /// Set the reaction rate model of the reaction.
+    auto setRateModel(ReactionRateModel const& model) -> GeneralReaction&;
+
+    /// Set the reaction rate model of the reaction (equivalent to GeneralReaction::setRateModel).
+    auto set(ReactionRateModel const& model) -> GeneralReaction&;
+
+    /// Return the name of the reaction.
+    auto name() const -> String const&;
+
+    /// Return the reaction equation of the reaction.
+    auto equation() const -> String const&;
+
+    /// Return the reaction rate model of the reaction.
+    auto rateModel() const -> ReactionRateModel const&;
+
+    /// Convert this GeneralReaction object into a vector of a single Reaction object (for compatibility with ReactionGenerator).
+    auto operator()(SpeciesList const& species) const -> Vec<Reaction>;
+
+private:
+    /// The name of the reaction.
+    String reaction_name;
+
+    /// The reaction equation of the reaction as a formatted string.
+    String reaction_equation;
+
+    /// The rate model of the reaction.
+    ReactionRateModel rate_model;
+};
 
 /// The class used to represent a collection of reactions controlled kinetically.
 /// @ingroup Core
@@ -54,13 +112,13 @@ public:
 
         if constexpr(isConvertible<T, ReactionGenerator>)
         {
-            rgenerators.push_back([=](SpeciesList const& species) -> Vec<Reaction> {
+            reaction_generators.push_back([=](SpeciesList const& species) -> Vec<Reaction> {
                 return item(species); // item is a ReactionGenerator object
             });
         }
         else
         {
-            rgenerators.push_back([=](SpeciesList const& species) -> Vec<Reaction> {
+            reaction_generators.push_back([=](SpeciesList const& species) -> Vec<Reaction> {
                 return { Reaction(item) }; // item is convertible to Reaction object
             });
         }
@@ -72,7 +130,7 @@ public:
 
 private:
     /// The ReactionGenerator objects collected so far with each call to Reactions::add method.
-    Vec<ReactionGenerator> rgenerators;
+    Vec<ReactionGenerator> reaction_generators;
 
     /// Add one or more ReactionGenerator or Reaction objects into the Reactions container.
     template<typename Arg, typename... Args>
