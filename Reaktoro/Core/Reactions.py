b4d67b8c9a0f2b31d44056fc26056c51ca1c7f71
Allan Leal <allanmulin@gmail.com>
2022-11-08 15:44:03 +0100

    Improved Python testing for Reactions

--- Reaktoro/Core/Reactions.py
+++ Reaktoro/Core/Reactions.py
@@ -19,12 +19,21 @@
 from reaktoro import *
 import pytest
 
+def generateRateModel(value: float):
+    def ratefn(props: ChemicalProps):
+        return ReactionRate(value)
+    return ReactionRateModel(ratefn)
+
 class ReactionGeneratorUsingClass:
     def __call__(self, phases):
-        return [ Reaction().withEquation("H2O(aq) = H+ + OH-") ]
+        return [ Reaction()
+            .withEquation("H2O(aq) = H+ + OH-")
+            .withRateModel(generateRateModel(5.0)) ]
 
 def ReactionGeneratorUsingFunction(phases):
-    return [ Reaction().withEquation("H2O(aq) = H2(aq) + 0.5*O2(aq)") ]
+    return [ Reaction()
+        .withEquation("H2O(aq) = H2(aq) + 0.5*O2(aq)")
+        .withRateModel(generateRateModel(6.0)) ]
 
 
 def testReactions():
@@ -41,17 +50,54 @@ def testReactions():
 
     phases = PhaseList(phases.convert())
 
-    reactions = Reactions()
+    system = ChemicalSystem(db, phases)
+    props = ChemicalProps(system)
 
-    reactions.add(db.reaction("Halite = Na+ + Cl-"))
-    reactions.add(db.reaction("Calcite"))
-    reactions.add(ReactionGeneratorUsingClass())
-    reactions.add(ReactionGeneratorUsingFunction)
+    reaction1 = db.reaction("Halite = Na+ + Cl-")
+    reaction2 = db.reaction("Calcite")
 
-    converted = reactions.convert(phases.species())
+    generalreaction1 = GeneralReaction("CO2(g) = CO2(aq)")
+    generalreaction2 = GeneralReaction("HCO3- + H+ = CO2(aq) + H2O(aq)")
 
-    assert len(converted) == 4
-    assert str(converted[0].equation()) == "Halite = Na+ + Cl-"
-    assert str(converted[1].equation()) == "Calcite"
-    assert str(converted[2].equation()) == "H2O(aq) = H+ + OH-"
-    assert str(converted[3].equation()) == "H2O(aq) = H2(aq) + 0.5*O2(aq)"
+    reaction1 = reaction1.withRateModel(generateRateModel(1.0))
+    reaction2 = reaction2.withRateModel(generateRateModel(2.0))
+    generalreaction1.setRateModel(generateRateModel(3.0))
+    generalreaction2.setRateModel(generateRateModel(4.0))
+
+    reactionsA = Reactions()
+    reactionsA.add(reaction1)
+    reactionsA.add(reaction2)
+    reactionsA.add(generalreaction1)
+    reactionsA.add(generalreaction2)
+    reactionsA.add(ReactionGeneratorUsingClass())
+    reactionsA.add(ReactionGeneratorUsingFunction)
+
+    reactionsB = Reactions(
+        reaction1,
+        reaction2,
+        generalreaction1,
+        generalreaction2,
+        ReactionGeneratorUsingClass(),
+        ReactionGeneratorUsingFunction)
+
+    def checkReactionsConversion(reactions: Reactions):
+        converted: list[Reaction] = reactions.convert(phases.species())
+
+        assert len(converted) == 6
+
+        assert str(converted[0].equation()) == "Halite = Na+ + Cl-"
+        assert str(converted[1].equation()) == "Calcite"
+        assert str(converted[2].equation()) == "CO2(g) = CO2(aq)"
+        assert str(converted[3].equation()) == "HCO3- + H+ = CO2(aq) + H2O(aq)"
+        assert str(converted[4].equation()) == "H2O(aq) = H+ + OH-"
+        assert str(converted[5].equation()) == "H2O(aq) = H2(aq) + 0.5*O2(aq)"
+
+        assert converted[0].rate(props).val() == 1.0
+        assert converted[1].rate(props).val() == 2.0
+        assert converted[2].rate(props).val() == 3.0
+        assert converted[3].rate(props).val() == 4.0
+        assert converted[4].rate(props).val() == 5.0
+        assert converted[5].rate(props).val() == 6.0
+
+    checkReactionsConversion(reactionsA)
+    checkReactionsConversion(reactionsB)
