2351eab39c88bc4f0b140a93322e53a3988da351
Allan Leal <allanmulin@gmail.com>
2022-11-11 16:04:52 +0100

    Adapted Surfaces with new Surface

--- Reaktoro/Core/Surfaces.hpp
+++ Reaktoro/Core/Surfaces.hpp
@@ -19,6 +19,7 @@
 
 // Reaktoro includes
 #include <Reaktoro/Common/Types.hpp>
+#include <Reaktoro/Core/SurfaceAreaModel.hpp>
 
 namespace Reaktoro {
 
@@ -26,28 +27,103 @@ namespace Reaktoro {
 class PhaseList;
 class Surface;
 
-/// Used to represent the interface surface between two phases in a chemical system.
+/// The function type for the generation of surfaces with given phases in the chemical system.
+/// @param phases The phases composing the chemical system.
+using SurfaceGenerator = Fn<Vec<Surface>(PhaseList const& phases)>;
+
+/// Used to define a general surface.
+/// @ingroup Core
+class GeneralSurface
+{
+public:
+    /// Construct a default GeneralSurface object.
+    GeneralSurface();
+
+    /// Construct a GeneralSurface object with given unique surface name.
+    explicit GeneralSurface(String const& name);
+
+    /// Set the unique name of the surface.
+    auto setName(String const& name) -> GeneralSurface&;
+
+    /// Set the area model of the surface.
+    auto setAreaModel(SurfaceAreaModel const& model) -> GeneralSurface&;
+
+    /// Set the area model of the surface (equivalent to GeneralSurface::setAreaModel).
+    auto set(SurfaceAreaModel const& model) -> GeneralSurface&;
+
+    /// Return the name of the surface.
+    auto name() const -> String const&;
+
+    /// Return the area model of the surface.
+    auto areaModel() const -> SurfaceAreaModel const&;
+
+    /// Convert this GeneralSurface object into a Surface object.
+    auto operator()(PhaseList const& phases) const -> Surface;
+
+private:
+    /// The name of the surface.
+    String surface_name;
+
+    /// The area model of the surface.
+    SurfaceAreaModel area_model;
+};
+
+/// Used to represent a collection of surfaces across which chemical reactions take place.
+/// @ingroup Core
 class Surfaces
 {
 public:
     /// Construct a Surfaces object.
     Surfaces();
 
-    /// Register a new surface between phases with names `phase1` and `phase2`.
-    auto add(String const& phase1, String const& phase2) -> void;
+    /// Construct a Surfaces object with given Surface, GeneralSurface, or SurfaceGenerator objects.
+    /// @param surfaces The objects of type Surface, GeneralSurface, or SurfaceGenerator.
+    template<typename... SurfaceConvertible>
+    explicit Surfaces(SurfaceConvertible const&... surfaces)
+    {
+        static_assert(sizeof...(surfaces) > 0);
+        addAux(surfaces...);
+    }
 
-    /// Register a new surface surrounding phase with name `phase`.
-    auto add(String const& phase) -> void;
+    /// Add a surface generator into the Surfaces container.
+    template<typename T>
+    auto add(T const& item) -> void
+    {
+        static_assert(
+            isConvertible<T, Surface> ||
+            isConvertible<T, GeneralSurface> ||
+            isConvertible<T, SurfaceGenerator>);
 
-    /// Return the pairs of phase names already registered.
-    auto data() const -> Pairs<String, String> const&;
+        if constexpr(isConvertible<T, Surface>)
+        {
+            surface_generators.push_back([=](PhaseList const& phases) -> Vec<Surface> { return { item }; }); // item is a Surface object
+        }
+        else if constexpr(isConvertible<T, GeneralSurface>)
+        {
+            surface_generators.push_back([=](PhaseList const& phases) -> Vec<Surface> { return { item(phases) }; }); // item is a GeneralSurface object; use operator()(PhaseList) to convert to Surface
+        }
+        else
+        {
+            surface_generators.push_back(item); // item is already a SurfaceGenerator
+        }
+    }
 
     /// Convert this Surfaces object into a vector of Surface objects.
+    /// @param phases The phases composing the chemical system.
     auto convert(PhaseList const& phases) const -> Vec<Surface>;
 
 private:
-    /// The registered surfaces.
-    Pairs<String, String> surfaces;
+    /// The SurfaceGenerator objects collected so far with each call to Surfaces::add method.
+    Vec<SurfaceGenerator> surface_generators;
+
+    /// Add one or more SurfaceGenerator or Surface objects into the Surfaces container.
+    template<typename Arg, typename... Args>
+    auto addAux(const Arg& arg, const Args&... args) -> void
+    {
+        add(arg);
+        if constexpr (sizeof...(Args) > 0)
+            addAux(args...);
+    }
 };
 
 } // namespace Reaktoro
