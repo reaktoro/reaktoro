e4bfb8c8a840929f2005783e044d49f345a39648
Allan Leal <allanmulin@gmail.com>
2022-11-14 18:03:56 +0100

    Implemented pybindings for SurfaceAreaModelConstant

---
+++ Reaktoro/Models/SurfaceAreaModels/SurfaceAreaModelConstant.py.cxx
@@ -0,0 +1,30 @@
+// Reaktoro is a unified framework for modeling chemically reactive systems.
+//
+// Copyright © 2014-2022 Allan Leal
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library. If not, see <http://www.gnu.org/licenses/>.
+
+// pybind11 includes
+#include <Reaktoro/pybind11.hxx>
+
+// Reaktoro includes
+#include <Reaktoro/Core/ChemicalProps.hpp>
+#include <Reaktoro/Models/SurfaceAreaModels/SurfaceAreaModelConstant.hpp>
+using namespace Reaktoro;
+
+void exportSurfaceAreaModelConstant(py::module& m)
+{
+    m.def("SurfaceAreaModelConstant", py::overload_cast<Param const&>(SurfaceAreaModelConstant));
+    m.def("SurfaceAreaModelConstant", py::overload_cast<real const&, Chars>(SurfaceAreaModelConstant));
+}
