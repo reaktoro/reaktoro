dfdf1e81e135332bda76b408837f67c869b1e784
Allan Leal <allanmulin@gmail.com>
2022-11-30 19:40:19 +0100

    Removed commented out code no longer needed in `MineralReaction.cpp`.

--- Reaktoro/Utils/MineralReaction.cpp
+++ Reaktoro/Utils/MineralReaction.cpp
@@ -27,51 +27,6 @@
 namespace Reaktoro {
 namespace detail {
 
-// /// Convert a vector of MineralReactionRateModel objects to a vector of ReactionRateModel objects.
-// auto convert(Strings const& minerals, Vec<MineralReactionRateModel> const& models) -> Vec<ReactionRateModel>
-// {
-//     errorif(minerals.size() != models.size(), "Expecting same number of minerals and mineral reaction rate models.");
-
-//     Vec<ReactionRateModel> ratemodels(models.size());
-
-//     for(auto i = 0; i < models.size(); ++i)
-//     {
-//         ratemodels[i] = [=](ChemicalProps const& props) -> ReactionRate
-//         {
-//             auto const& system = props.system();
-//             auto const& aprops = getAqueousProps(system); // get the AqueousProps object corresponding to the ChemicalSystem object in `props`, which will be reused by all mineral reactions!
-
-//             thread_local auto const iminerals = vectorize(minerals, RKT_LAMBDA(x, aprops.saturationSpecies().indexWithName(x)));
-//             thread_local auto const imineralphases = vectorize(minerals, RKT_LAMBDA(x, system.phases().indexWithName(x)));
-//             thread_local auto const imineralsurfaces = vectorize(minerals, RKT_LAMBDA(x, system.surfaces().indexWithName(x)));
-
-//             auto const& T = props.temperature();
-//             auto const& P = props.pressure();
-//             auto const& pH = aprops.pH();
-//             auto const& Omega = aprops.saturationRatio(iminerals[i]);
-//             auto const& area = props.surfaceArea(imineralsurfaces[i]);
-
-//             const auto args = MineralReactionRateModelArgs{ props, aprops, T, P, pH, Omega, area };
-
-//             // Evaluate the mineral reaction rate model and switch sign because
-//             // Reaktoro's convention for reaction rate is positive when the
-//             // reaction proceeds from left to right (and this is how the mineral
-//             // reaction is represented, with the mineral on the left side, its
-//             // dissolution rate, from left to right, should be positive).
-//             return -models[i](args);
-//         };
-//     }
-
-//     // Ensure the first rate model updates AqueousProps object before it is used by every mineral rate model!
-//     ratemodels[0] = [=](ChemicalProps const& props) mutable -> ReactionRate
-//     {
-//         getAqueousProps(props.system()).update(props); // update the AqueousProps object corresponding to the ChemicalSystem object in `props`; note this is done once for the first mineral rate model, and updated state reused by all other minerals for performance reasons!
-//         return ratemodels[0](props);
-//     };
-
-//     return ratemodels;
-// }
-
 /// Convert a MineralReactionRateModel object to a ReactionRateModel object.
 auto convert(String const& mineral, MineralReactionRateModel const& model) -> ReactionRateModel
 {
@@ -126,57 +81,5 @@ auto MineralReaction::mineral() const -> String const&
 {
     return GeneralReaction::name();
 }
-// MineralReaction::MineralReaction(String const& mineral)
-// : m_minerals(minerals), m_mineral_rate_model_generators(m_minerals.size())
-// {
-// }
-
-// auto MineralReaction::setRateModel(MineralReactionRateModelGenerator const& generator) -> MineralReaction&
-// {
-//     const auto size = m_mineral_rate_model_generators.size();
-//     m_mineral_rate_model_generators.assign(size, generator);
-//     return *this;
-// }
-
-// auto MineralReaction::setRateModel(String const& mineral, MineralReactionRateModelGenerator const& generator) -> MineralReaction&
-// {
-//     errorif(!generator, "You are trying to specify a non-initialized mineral reaction rate generator.");
-//     const auto idx = index(m_minerals, mineral);
-//     errorif(idx >= m_minerals.size(), "You did not specify mineral `", mineral, "` in the list of minerals when creating the MineralReaction object, e.g., `MineralReaction(\"Calcite Dolomite Quartz\")`.");
-//     m_mineral_rate_model_generators[idx] = generator;
-//     return *this;
-// }
-
-// auto MineralReaction::operator()(SpeciesList const& species) const -> Vec<Reaction>
-// {
-//     for(auto&& [i, generator] : enumerate(m_mineral_rate_model_generators))
-//     {
-//         const auto imineral = species.findWithName(m_minerals[i]);
-//         errorif(!generator, "You forgot to set a mineral reaction rate model for mineral `", m_minerals[i], "` and maybe for all other minerals as well. Use method MineralReaction::setRateModel to fix this.");
-//         errorif(imineral >= species.size(), "There is no mineral with name `", m_minerals[i], "` in the chemical system.");
-//     }
-
-//     const auto num_minerals = m_minerals.size();
-
-//     Vec<MineralReactionRateModel> mineral_reaction_rate_models(num_minerals);
-//     for(auto&& [i, generator] : enumerate(m_mineral_rate_model_generators))
-//         mineral_reaction_rate_models[i] = generator(m_minerals[i], species);
-
-//     const auto reaction_rate_models = detail::convert(m_minerals, mineral_reaction_rate_models);
-
-//     Vec<Reaction> reactions(num_minerals);
-//     for(auto i = 0; i < num_minerals; ++i)
-//     {
-//         const auto imineral = species.index(m_minerals[i]);
-//         const auto mineralspecies = species[imineral];
-//         reactions[i] = Reaction()
-//             .withName(m_minerals[i])
-//             .withEquation({{ {mineralspecies, -1.0} }})
-//             .withRateModel(reaction_rate_models[i])
-//             ;
-//     }
-
-//     return reactions;
-// }
 
 } // namespace Reaktoro
