347bf32b61f92f384ed858f7e41bc30a206f4b46
Allan Leal <allanmulin@gmail.com>
2023-01-04 16:01:53 +0100

    Implementend C++ tests for `KineticSolver` following fix for non-TP bug

--- Reaktoro/Kinetics/KineticsSolver.test.cxx
+++ Reaktoro/Kinetics/KineticsSolver.test.cxx
@@ -22,10 +22,76 @@
 #include <catch2/catch.hpp>
 
 // Reaktoro includes
-#include <Reaktoro/Kinetics/KineticsSolver.hpp>
+#include <Reaktoro/Core/ChemicalProps.hpp>
+#include <Reaktoro/Core/ChemicalState.hpp>
+#include <Reaktoro/Core/ChemicalSystem.hpp>
+#include <Reaktoro/Extensions/Nasa.hpp>
+#include <Reaktoro/Equilibrium.hpp>
+#include <Reaktoro/Kinetics.hpp>
 using namespace Reaktoro;
 
-#define PRINT_INFO_IF_FAILS(x) INFO(#x " = \n" << std::scientific << std::setprecision(16) << x)
-
 TEST_CASE("Testing KineticsSolver", "[KineticsSolver]")
-{}
+{
+    NasaDatabase db("nasa-cea");
+
+    auto ratefn = [](ChemicalProps const& props)
+    {
+        const auto k0 = 0.01;
+        const auto nc = props.speciesAmount("C(gr)");
+        return k0 * nc;
+    };
+
+    ChemicalSystem system(db,
+        CondensedPhase("C(gr)"),
+        GaseousPhase("O2 CO2"),
+        GeneralReaction("C(gr) + O2 = CO2").setRateModel(ratefn)
+    );
+
+    ChemicalState state(system);
+    state.set("C(gr)", 1.0, "mol");
+    state.set("O2", 1.0, "mol");
+
+    ChemicalProps props(state);
+
+    SECTION("When temperature and pressure are constant")
+    {
+        EquilibriumSpecs specs = EquilibriumSpecs::TP(system);
+
+        KineticsSolver solver(specs);
+
+        KineticsOptions options;
+        options.optima.output.active = true;
+        solver.setOptions(options);
+
+        const auto dt = 1.0;
+
+        auto res = solver.solve(state, dt);
+
+        REQUIRE( res.succeeded() );
+
+        CHECK( state.speciesAmount("C(gr)") == Approx(0.990099) );
+    }
+
+    SECTION("When enthalpy and pressure are constant")
+    {
+        EquilibriumSpecs specs = EquilibriumSpecs::HP(system);
+
+        KineticsSolver solver(specs);
+
+        KineticsOptions options;
+        options.optima.output.active = true;
+        solver.setOptions(options);
+
+        const auto dt = 1.0;
+
+        EquilibriumConditions conditions(specs);
+        conditions.pressure(props.pressure());
+        conditions.enthalpy(props.enthalpy());
+
+        auto res = solver.solve(state, dt, conditions);
+
+        REQUIRE( res.succeeded() );
+
+        CHECK( state.speciesAmount("C(gr)") == Approx(0.990099) );
+    }
+}
