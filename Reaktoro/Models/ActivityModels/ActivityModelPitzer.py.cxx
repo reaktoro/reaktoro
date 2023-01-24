f2b0fa084f1a4c7172beb39c6c08795dd7f6757e
Allan Leal <allanmulin@gmail.com>
2023-01-24 09:29:25 +0100

    Started implementation of a new Pitzer activity model in Reaktoro

---
+++ Reaktoro/Models/ActivityModels/ActivityModelPitzer.py.cxx
@@ -0,0 +1,28 @@
+// Reaktoro is a unified framework for modeling chemically reactive systems.
+//
+// Copyright © 2014-2022 Allan Leal
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library. If not, see <http://www.gnu.org/licenses/>.
+
+// pybind11 includes
+#include <Reaktoro/pybind11.hxx>
+
+// Reaktoro includes
+#include <Reaktoro/Models/ActivityModels/ActivityModelPitzerPHREEQC.hpp>
+using namespace Reaktoro;
+
+void exportActivityModelPitzerPHREEQC(py::module& m)
+{
+    m.def("ActivityModelPitzerPHREEQC", ActivityModelPitzerPHREEQC);
+}
