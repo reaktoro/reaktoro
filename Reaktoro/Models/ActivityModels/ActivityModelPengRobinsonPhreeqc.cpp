c0b168e707dd5c3e58413c6a53dd5132c9cd95f7
Allan Leal <allanmulin@gmail.com>
2023-03-09 12:49:13 +0100

    Implemented class `PengRobinsonPhreeqc`

--- Reaktoro/Models/ActivityModels/ActivityModelPengRobinsonPhreeqc.cpp
+++ Reaktoro/Models/ActivityModels/ActivityModelPengRobinsonPhreeqc.cpp
@@ -26,6 +26,7 @@
 #include <Reaktoro/Common/Enumerate.hpp>
 #include <Reaktoro/Common/Exception.hpp>
 #include <Reaktoro/Common/Real.hpp>
+#include <Reaktoro/Common/StringUtils.hpp>
 #include <Reaktoro/Singletons/CriticalProps.hpp>
 
 namespace Reaktoro {
@@ -33,15 +34,6 @@ namespace {
 
 using std::log;
 using std::sqrt;
-
-/// Returns true if string `str` starts with `sub0`, or `sub1`, etc.
-template<typename... Subs>
-auto startswith(String const& str, const char* sub0, Subs... subs)
-{
-    if constexpr(sizeof...(Subs) > 0)
-        return str.find(sub0) == 0 || startswith(str, subs...);
-    return str.find(sub0) == 0;
-}
 
 /// The arguments for the evaluation of the properties a fluid phase using the Peng-Robinson-Phreeqc equation of state model.
 struct PengRobinsonPhreeqcArgs
@@ -53,19 +45,7 @@ struct PengRobinsonPhreeqcArgs
     real const& Patm;
 
     /// The mole fractions of the species in the fluid phase (in mol/mol).
-    ArrayXrConstRef const& xgases;
-
-    /// The critical temperatures of the species in the fluid phase (in K).
-    ArrayXrConstRef const& Tcr;
-
-    /// The critical pressures of the species in the fluid phase (in atm).
-    ArrayXrConstRef const& Pcr;
-
-    /// The acentric factors of the species in the fluid phase.
-    ArrayXrConstRef const& omega;
-
-    /// The names of the species in the fluid phase.
-    Strings const& gases;
+    ArrayXrConstRef const& x;
 };
 
 /// The properties computed for a fluid phase using the Peng-Robinson-Phreeqc equation of state model.
@@ -78,185 +58,174 @@ struct PengRobinsonPhreeqcProps
     ArrayXr ln_phi;
 };
 
-// Based on LDBLE calc_PR(std::vector<class phase*> phase_ptrs, LDBLE P, LDBLE TK, LDBLE V_m);
-/* ---------------------------------------------------------------------- */
-/*  Calculate fugacity and fugacity coefficient for gas pressures if critical T and P
-    are defined.
-  1) Solve molar volume V_m or total pressure P from Peng-Robinson's EOS:
-  P = R * T / (V_m - b) - a * aa / (V_m^2 + 2 * b * V_m - b^2)
-     a = 0.457235 * (R * T_c)^2 / P_c
-     b = 0.077796 * R * T_c / P_c
-     aa = (1 + kk * (1 - T_r^0.5))^2
-     kk = 0.37464 + 1.54226 * omega - 0.26992 * omega^2
-     T_r = T / T_c
-  multicomponent gas phase:
-     use: b_sum = Sum(x_i * b), x_i is mole-fraction
-          a_aa_sum = Sum_i( Sum_j(x_i * x_j * (a_i * aa_i * a_j * aa_j)^0.5) )
-  2) Find the fugacity coefficient phi for gas i:
-  log(phi_i) = B_ratio * (z - 1) - log(z - B) + A / (2.8284 * B) * (B_ratio - 2 / a_aa_sum * a_aa_sum2) *\
-           log((z + 2.4142 * B) / (z - 0.4142 * B))
-     B_ratio = b_i / b_sum
-     A = a_aa_sum * P / R_TK^2
-     B = b_sum * P / R_TK
-     a_aa_sum2 = Sum_j(x_j * (a_aa_i * a_aa_j)^0.5
-  3) correct the solubility of gas i with:
-  pr_si_f = log10(phi_i) -  Delta_V_i * (P - 1) / (2.303 * R * TK);
-*/
-
-
-void evaluatePengRobinsonPhreeqc(PengRobinsonPhreeqcArgs const& args, PengRobinsonPhreeqcProps& props)
+/// The arguments for the evaluation of the properties a fluid phase using the Peng-Robinson-Phreeqc equation of state model.
+struct PengRobinsonPhreeqc
 {
-    const auto R_LITER_ATM = 0.0820597; // L-atm/deg-mol
-    int i, i1, n_g = args.gases.size();
-    real T_c, P_c;
-    real A, B, B_r, kk, oo, a_aa, T_r;
-    real a_aa_sum2;
-    real phi;
-    real /*R_TK,*/ R = R_LITER_ATM; /* L atm / (K mol) */
-    real r3[4], r3_12, rp, rp3, rq, rz, ri, ri1, one_3 = 0.33333333333333333;
-    real disct, vinit, v1, ddp, dp_dv, dp_dv2;
-    int it;
-    // class phase *phase_ptr, *phase_ptr1;
-    // cxxGasPhase * gas_phase_ptr = use.Get_gas_phase_ptr();
-    bool halved;
-    real P = args.Patm;
-    real TK = args.TK;
-    real R_TK = R * TK;
-    real b_sum;
-    real a_aa_sum;
+    /// The names of the species in the fluid phase.
+    const Strings species;
 
-    auto& V_m = props.V_m;
-    auto& ln_phi = props.ln_phi;
+    /// The critical temperatures of the species in the fluid phase (in K).
+    const ArrayXr Tcr;
 
-    ArrayXr pr_a(n_g);
-    ArrayXr pr_b(n_g);
-    ArrayXr pr_alpha(n_g);
-    ArrayXr pr_aa_sum2(n_g);
+    /// The critical pressures of the species in the fluid phase (in atm).
+    const ArrayXr Pcr;
 
-    for(i = 0; i < n_g; i++)
+    /// The acentric factors of the species in the fluid phase.
+    const ArrayXr omega;
+
+    // Auxiliary working arrays
+
+    ArrayXr pr_a;
+    ArrayXr pr_b;
+    ArrayXr pr_alpha;
+    ArrayXr pr_aa_sum2;
+
+    /// Evaluates the Peng-Robinson model similar to that implemented in PHREEQC.
+    auto evaluate(PengRobinsonPhreeqcArgs const& args, PengRobinsonPhreeqcProps& props)
     {
-        errorif(args.Tcr[i] == 0.0, "Expecting a non-zero critical temperature for gas ", args.gases[i], " in Peng-Robinson model.");
-        errorif(args.Pcr[i] == 0.0, "Expecting a non-zero critical pressure for gas ", args.gases[i], " in Peng-Robinson model.");
+        const auto R_LITER_ATM = 0.0820597; // L-atm/deg-mol
+        const auto R = R_LITER_ATM; // L atm / (K mol)
+        const auto one_3 = 1.0/3.0;
+        const auto n_g = species.size();
 
-        T_c = args.Tcr[i];
-        P_c = args.Pcr[i];
-        pr_a[i] = 0.457235 * R * R * T_c * T_c / P_c;
-        pr_b[i] = 0.077796 * R * T_c / P_c;
-        T_r = TK / T_c;
-        oo = args.omega[i];
-        kk = 0.37464 + oo * (1.54226 - 0.26992 * oo);
-        pr_alpha[i] = pow(1 + kk * (1 - sqrt(T_r)), 2);
-    }
+        real P = args.Patm;
+        real TK = args.TK;
+        real R_TK = R * TK;
 
-    for (i = 0; i < n_g; i++)
-    {
-        a_aa_sum2 = 0.0;
-        b_sum += args.xgases[i] * pr_b[i];
+        real b_sum;
+        real a_aa_sum;
+        real a_aa_sum2;
 
-        for (i1 = 0; i1 < n_g; i1++)
+        pr_a.resize(n_g);
+        pr_b.resize(n_g);
+        pr_alpha.resize(n_g);
+        pr_aa_sum2.resize(n_g);
+
+        auto& V_m = props.V_m;
+        auto& ln_phi = props.ln_phi;
+
+        for(auto i = 0; i < n_g; ++i)
         {
-            if(args.xgases[i1] == 0.0)
+            const auto T_c = Tcr[i];
+            const auto P_c = Pcr[i];
+            const auto oo = omega[i];
+            const auto T_r = TK / T_c;
+            const auto kk = 0.37464 + oo * (1.54226 - 0.26992 * oo);
+            pr_a[i] = 0.457235 * R * R * T_c * T_c / P_c;
+            pr_b[i] = 0.077796 * R * T_c / P_c;
+            pr_alpha[i] = pow(1 + kk * (1 - sqrt(T_r)), 2);
+        }
+
+        for(auto i = 0; i < n_g; ++i)
+        {
+            a_aa_sum2 = 0.0;
+            b_sum += args.x[i] * pr_b[i];
+
+            for(auto i1 = 0; i1 < n_g; ++i1)
+            {
+                if(args.x[i1] == 0.0)
+                    continue;
+
+                auto a_aa = sqrt(pr_a[i]*pr_alpha[i] * pr_a[i1]*pr_alpha[i1]);
+
+                if(startswith(species[i], "H2O"))
+                {
+                    if(startswith(species[i1], "CO2"))
+                        a_aa *= 0.81; // Soreide and Whitson, 1992, FPE 77, 217
+                    else if(startswith(species[i1], "H2S"))
+                        a_aa *= 0.81;
+                    else if(startswith(species[i1], "CH4", "Mtg", "Methane", "METHANE"))
+                        a_aa *= 0.51;
+                    else if(startswith(species[i1], "N2", "Ntg"))
+                        a_aa *= 0.51;
+                    else if(startswith(species[i1], "C2H6", "Ethane", "ETHANE"))
+                        a_aa *= 0.51;
+                    else if(startswith(species[i1], "C3H8", "Propane", "PROPANE"))
+                        a_aa *= 0.45;
+                }
+                if(startswith(species[i1], "H2O"))
+                {
+                    if(startswith(species[i], "CO2"))
+                        a_aa *= 0.81;
+                    else if(startswith(species[i], "H2S"))
+                        a_aa *= 0.81;
+                    else if(startswith(species[i], "CH4", "Mtg", "Methane", "METHANE"))
+                        a_aa *= 0.51;
+                    else if(startswith(species[i], "N2", "Ntg"))
+                        a_aa *= 0.51;
+                    else if(startswith(species[i], "C2H6", "Ethane", "ETHANE"))
+                        a_aa *= 0.51;
+                    else if(startswith(species[i], "C3H8", "Propane", "PROPANE"))
+                        a_aa *= 0.45;
+                }
+                a_aa_sum += args.x[i] * args.x[i1] * a_aa;
+                a_aa_sum2 += args.x[i1] * a_aa;
+            }
+            pr_aa_sum2[i] = a_aa_sum2;
+        }
+
+        real b2 = b_sum * b_sum;
+
+        if(P < 1e-10)
+            P = 1e-10;
+
+        real r3[4];
+
+        r3[1] = b_sum - R_TK / P;
+        r3[2] = -3.0 * b2 + (a_aa_sum - R_TK * 2.0 * b_sum) / P;
+        r3[3] = b2 * b_sum + (R_TK * b2 - b_sum * a_aa_sum) / P;
+
+        // solve t^3 + rp*t + rq = 0.
+        // molar volume V_m = t - r3[1] / 3...
+        const auto r3_12 = r3[1] * r3[1];
+        const auto rp = r3[2] - r3_12 / 3;
+        const auto rp3 = rp * rp * rp;
+        const auto rq = (2.0 * r3_12 * r3[1] - 9.0 * r3[1] * r3[2]) / 27 + r3[3];
+        const auto rz = rq * rq / 4 + rp3 / 27;
+
+        if(rz >= 0) // Cardono's method...
+        {
+            auto ri = sqrt(rz);
+            if(ri + rq / 2 <= 0)
+            {
+                V_m = pow(ri - rq / 2, one_3) + pow(- ri - rq / 2, one_3) - r3[1] / 3;
+            }
+            else
+            {
+                ri = - pow(ri + rq / 2, one_3);
+                V_m = ri - rp / (3.0 * ri) - r3[1] / 3;
+            }
+        }
+        else // use complex plane...
+        {
+            const auto ri = sqrt(- rp3 / 27); // rp < 0
+            const auto ri1 = acos(- rq / 2 / ri);
+            V_m = 2.0 * pow(ri, one_3) * cos(ri1 / 3) - r3[1] / 3;
+        }
+
+        // calculate the fugacity coefficients...
+        for(auto i = 0; i < n_g; ++i)
+        {
+            if(args.x[i] == 0.0)
+            {
+                ln_phi[i] = 0.0;
                 continue;
-
-            a_aa = sqrt(pr_a[i]*pr_alpha[i] * pr_a[i1]*pr_alpha[i1]);
-
-            if(startswith(args.gases[i], "H2O"))
-            {
-                if(startswith(args.gases[i1], "CO2"))
-                    a_aa *= 0.81; // Soreide and Whitson, 1992, FPE 77, 217
-                else if(startswith(args.gases[i1], "H2S"))
-                    a_aa *= 0.81;
-                else if(startswith(args.gases[i1], "CH4", "Mtg", "Methane", "METHANE"))
-                    a_aa *= 0.51;
-                else if(startswith(args.gases[i1], "N2", "Ntg"))
-                    a_aa *= 0.51;
-                else if(startswith(args.gases[i1], "C2H6", "Ethane", "ETHANE"))
-                    a_aa *= 0.51;
-                else if(startswith(args.gases[i1], "C3H8", "Propane", "PROPANE"))
-                    a_aa *= 0.45;
             }
-            if(startswith(args.gases[i1], "H2O"))
-            {
-                if(startswith(args.gases[i], "CO2"))
-                    a_aa *= 0.81;
-                else if(startswith(args.gases[i], "H2S"))
-                    a_aa *= 0.81;
-                else if(startswith(args.gases[i], "CH4", "Mtg", "Methane", "METHANE"))
-                    a_aa *= 0.51;
-                else if(startswith(args.gases[i], "N2", "Ntg"))
-                    a_aa *= 0.51;
-                else if(startswith(args.gases[i], "C2H6", "Ethane", "ETHANE"))
-                    a_aa *= 0.51;
-                else if(startswith(args.gases[i], "C3H8", "Propane", "PROPANE"))
-                    a_aa *= 0.45;
-            }
-            a_aa_sum += args.xgases[i] * args.xgases[i1] * a_aa;
-            a_aa_sum2 += args.xgases[i1] * a_aa;
-        }
-        pr_aa_sum2[i] = a_aa_sum2;
-    }
-
-    real b2 = b_sum * b_sum;
-
-    if(P < 1e-10)
-        P = 1e-10;
-
-    r3[1] = b_sum - R_TK / P;
-    r3_12 = r3[1] * r3[1];
-    r3[2] = -3.0 * b2 + (a_aa_sum - R_TK * 2.0 * b_sum) / P;
-    r3[3] = b2 * b_sum + (R_TK * b2 - b_sum * a_aa_sum) / P;
-    // solve t^3 + rp*t + rq = 0.
-    // molar volume V_m = t - r3[1] / 3...
-    rp = r3[2] - r3_12 / 3;
-    rp3 = rp * rp * rp;
-    rq = (2.0 * r3_12 * r3[1] - 9.0 * r3[1] * r3[2]) / 27 + r3[3];
-    rz = rq * rq / 4 + rp3 / 27;
-    if(rz >= 0) // Cardono's method...
-    {
-        ri = sqrt(rz);
-        if(ri + rq / 2 <= 0)
-        {
-            V_m = pow(ri - rq / 2, one_3) + pow(- ri - rq / 2, one_3) - r3[1] / 3;
-        }
-        else
-        {
-            ri = - pow(ri + rq / 2, one_3);
-            V_m = ri - rp / (3.0 * ri) - r3[1] / 3;
+            const auto rz = P * V_m / R_TK;
+            const auto A = a_aa_sum * P / (R_TK * R_TK);
+            const auto B = b_sum * P / R_TK;
+            const auto B_r = pr_b[i] / b_sum;
+            if(rz > B)
+                ln_phi[i] = B_r * (rz - 1) - log(rz - B) + A / (2.828427 * B) * (B_r - 2.0 * pr_aa_sum2[i] / a_aa_sum) * log((rz + 2.41421356 * B) / (rz - 0.41421356 * B));
+            else ln_phi[i] = -3.0; // fugacity coefficient = 0.05
         }
     }
-    else // use complex plane...
-    {
-        ri = sqrt(- rp3 / 27); // rp < 0
-        ri1 = acos(- rq / 2 / ri);
-        V_m = 2.0 * pow(ri, one_3) * cos(ri1 / 3) - r3[1] / 3;
-    }
-
-    // calculate the fugacity coefficients...
-    for (i = 0; i < n_g; i++)
-    {
-        if(args.xgases[i] == 0.0)
-        {
-            ln_phi[i] = 0.0;
-            continue;
-        }
-        rz = P * V_m / R_TK;
-        A = a_aa_sum * P / (R_TK * R_TK);
-        B = b_sum * P / R_TK;
-        B_r = pr_b[i] / b_sum;
-        if(rz > B)
-            ln_phi[i] = B_r * (rz - 1) - log(rz - B) + A / (2.828427 * B) * (B_r - 2.0 * pr_aa_sum2[i] / a_aa_sum) * log((rz + 2.41421356 * B) / (rz - 0.41421356 * B));
-        else ln_phi[i] = -3.0; // fugacity coefficient = 0.05
-    }
-}
+};
 
 } // namespace anonymous
 
 auto createActivityModelPengRobinsonPhreeqc(SpeciesList const& specieslist) -> ActivityModel
 {
-    // Ensure all species are gas, liquid, or fluid
-    // for(auto species : specieslist)
-    //     errorifnot(oneof(species.aggregateState(), AggregateState::Gas, AggregateState::Liquid, AggregateState::Fluid), "Expecting all species for Peng-Robinson-Phreeqc model to have a gas, liquid, or fluid aggregate state but this is not the case for ", species.name(), ".");
-
     const auto numspecies = specieslist.size();
 
     Strings names(numspecies); // the names of the species
@@ -288,6 +257,8 @@ auto createActivityModelPengRobinsonPhreeqc(SpeciesList const& specieslist) -> A
     PengRobinsonPhreeqcProps prprops;
     prprops.ln_phi.resize(numspecies);
 
+    PengRobinsonPhreeqc prphreeqc{ names, Tcr, Pcr, omega };
+
     ActivityModel fn = [=](ActivityPropsRef props, ActivityModelArgs args) mutable
     {
         // The arguments for the activity model evaluation
@@ -296,11 +267,9 @@ auto createActivityModelPengRobinsonPhreeqc(SpeciesList const& specieslist) -> A
         const auto Patm = P / atmToPascal; // convert from Pa to atm
         const auto Pbar = P / barToPascal; // convert from Pa to bar
 
-        PengRobinsonPhreeqcArgs prargs{ T, Patm, x, Tcr, Pcr, omega, names };
+        prphreeqc.evaluate({ T, Patm, x}, prprops);
 
-        evaluatePengRobinsonPhreeqc(prargs, prprops);
-
-        props.Vx   = prprops.V_m * 1e-3; // convert from liter/mol to m3/mol
+        props.Vx   = prprops.V_m * 0.001; // convert from liter/mol to m3/mol
         props.VxT  = 0.0; // PHREEQC does not compute this property
         props.VxP  = 0.0; // PHREEQC does not compute this property
         props.Gx   = 0.0; // PHREEQC does not compute this property
