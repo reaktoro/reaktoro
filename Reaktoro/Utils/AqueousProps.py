2d040b6cb8eefc4f3d7d6801a11f53b2ae2f9bdb
Diego Tavares Volpatto <diego@esss.co>
2023-11-10 19:27:27 -0300

    Implement test to check Alkalinity calc exactly

--- Reaktoro/Utils/AqueousProps.py
+++ Reaktoro/Utils/AqueousProps.py
@@ -95,3 +95,45 @@ def testWolfGladrowSystemProps(db, composition):
     assert aq_props.pH().val() == pytest.approx(8.2, rel=1e-3)
     # Within 2% of relative deviation compared with the paper
     assert aq_props.alkalinity().val() == pytest.approx(2.33e-3, rel=2e-2)
+
+
+def testAlkalinity():
+    """
+    This test uses the exact species that are considered in the Alkalinity calculation
+    and checks if the expected result of the Total Alkalinity expression in [1]_ is
+    obtained.
+    
+    .. [1] Wolf-Gladrow et al. (2007). See: https://doi.org/10.1016/j.marchem.2007.01.006
+    """
+    db = SupcrtDatabase("supcrt07")
+    water = "H2O(aq) H+ OH-"
+    ions = "Na+ Mg+2 Ca+2 K+ Sr+2 Cl- Br- NO3-"
+    tpo4_cluster = "H3PO4(aq) H2PO4- HPO4-2 PO4-3"
+    tnh3_cluster = "NH3(aq) NH4+"
+    tso4_cluster = "SO4-2 HSO4-"
+    thf_cluster = "F- HF(aq)"
+    thno2_cluster = "NO2- HNO2(aq)"
+    species_names = " ".join(
+        [water, ions, tpo4_cluster, tnh3_cluster, tso4_cluster, thf_cluster, thno2_cluster]
+    )
+    aq_solution = AqueousPhase(species_names)
+    system = ChemicalSystem(db, aq_solution)
+    
+    # Setting a fictional chemical state with known Alkalinity given Wolf-Gladrow formula
+    state = ChemicalState(system)
+    state.temperature(25, "celsius")
+    state.pressure(1, "atm")
+    state.set("H2O(aq)", 1, "kg")
+    all_solute_species = species_names.split()
+    all_solute_species.remove("H2O(aq)")
+    for solute in all_solute_species:
+        state.set(solute, 1, "mol")
+
+    aq_props = AqueousProps(state)
+    props = ChemicalProps(state)
+    volume_in_liter = props.volume().val() * 1e3
+    total_amounts_of_solutes_for_alkalinity = 1 + 2 + 2 + 1 + 2  # Na+ Mg+2 Ca+2 K+ Sr+2 
+    total_amounts_of_solutes_for_alkalinity += -1 - 1 - 1 - 4  # Cl- Br- NO3- TPO4
+    total_amounts_of_solutes_for_alkalinity += 2 - 4 - 2 - 2  # TNH3 TSO4 THF THNO2
+    expected_alkalinity = total_amounts_of_solutes_for_alkalinity / volume_in_liter
+    assert aq_props.alkalinity().val() == pytest.approx(expected_alkalinity)
