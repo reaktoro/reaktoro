1861fc131654b4992620a88447c77d0c8fe98dcc
Allan Leal <allanmulin@gmail.com>
2023-11-13 09:58:56 +0100

    Implemented working version of the eUNIQUAC model - further testing needed

--- Reaktoro/Models/ActivityModels/ActivityModelExtendedUNIQUAC.cpp
+++ Reaktoro/Models/ActivityModels/ActivityModelExtendedUNIQUAC.cpp
@@ -21,8 +21,8 @@
 #include <Reaktoro/Common/Algorithms.hpp>
 #include <Reaktoro/Common/Enumerate.hpp>
 #include <Reaktoro/Common/Exception.hpp>
-#include <Reaktoro/Core/Embedded.hpp>
 #include <Reaktoro/Common/Matrix.hpp>
+#include <Reaktoro/Core/Embedded.hpp>
 #include <Reaktoro/Models/ActivityModels/Support/AqueousMixture.hpp>
 #include <Reaktoro/Serialization/Models/ActivityModels.hpp>
 
@@ -41,13 +41,34 @@ auto createActivityModelExtendedUNIQUAC(SpeciesList const& species, ActivityMode
     const ArrayXd z2 = z * z;
 
     // The index of water in the solution
-    const Index iH2O = solution.indexWater();
+    const Index iw = solution.indexWater();
 
-    Indices iDH; // The indices of the species containing Debye-Huckel contribution
-    Indices iR; // The indices of the species containing the surface area parameter rᵢ
-    Indices iQ; // The indices of the species containing the volume parameter qᵢ
-    Indices iU; // The indices of the species containing energetic binary interaction parameters uᵢⱼ
-    // Indices iUQ; // The indices of the species containing both the volume parameter qᵢ and the energetic binary interaction parameters uᵢⱼ
+    // The molar mass of water (in kg/mol)
+    auto const Mw = species[iw].molarMass();
+
+    // The reference temperature used for the computation of the Debye-Huckel variable A
+    auto const T0 = 273.15;
+
+    // The reference temperature used for the temperature-correction of the energetic UNIQUAC parameters uᵢⱼ
+    auto const Tr = 298.15;
+
+    // The term b ≡ aB in the Debye-Huckel equation (assumed constant in the e-UNIQUAC model)
+    auto const b = 1.5;
+
+    // The co-ordination number z of the UNIQUAC model (Thomasen, 1997, equation 4.2)
+    auto const Z = 10.0;
+
+    // The indices of the species containing Debye-Huckel contribution (the indices of the charged species)
+    Indices const iDH = solution.indicesCharged();
+
+    // The indices of the species containing the surface area parameter rᵢ
+    Indices iR;
+
+    // The indices of the species containing the volume parameter qᵢ
+    Indices iQ;
+
+    // The indices of the species containing energetic binary interaction parameters uᵢⱼ
+    Indices iU;
 
     ArrayXr r = ArrayXr::Zero(species.size()); // TODO: Initialize properly with values from parameters
     ArrayXr q = ArrayXr::Zero(species.size()); // TODO: Initialize properly with values from parameters
@@ -58,16 +79,19 @@ auto createActivityModelExtendedUNIQUAC(SpeciesList const& species, ActivityMode
     u0.diagonal().fill(0.0);
     uT.diagonal().fill(0.0);
 
-    ArrayXr lnGammaDH(species.size());
-    ArrayXr lnGammaCombinatorial(species.size());
-    ArrayXr lnGammaResidual(species.size());
+    ArrayXr ln_gDH(species.size());
+    ArrayXr ln_gC(species.size());
+    ArrayXr ln_gR(species.size());
+
+    ArrayXr ln_gCinf(species.size());
+    ArrayXr ln_gRinf(species.size());
 
     ArrayXr thetapsi;  // The matrix-vector product tr(ψ)θ
 
     for(auto const& [formula, param] : params.r)
     {
         errorif(param <= 0.0, "The surface area parameter rᵢ in the Extended UNIQUAC model cannot be zero or negative: r[", formula, "] = ", param);
-        const auto ispecies = species.findWithFormula(formula);
+        auto const ispecies = species.findWithFormula(formula);
         if(ispecies >= species.size())
             continue;
         iR.push_back(ispecies);
@@ -77,7 +101,7 @@ auto createActivityModelExtendedUNIQUAC(SpeciesList const& species, ActivityMode
     for(auto const& [formula, param] : params.q)
     {
         errorif(param <= 0.0, "The volume parameter qᵢ in the Extended UNIQUAC model cannot be zero or negative: q[", formula, "] = ", param);
-        const auto ispecies = species.findWithFormula(formula);
+        auto const ispecies = species.findWithFormula(formula);
         if(ispecies >= species.size())
             continue;
         iQ.push_back(ispecies);
@@ -86,20 +110,26 @@ auto createActivityModelExtendedUNIQUAC(SpeciesList const& species, ActivityMode
 
     for(auto const& [formula1, formula2, params] : params.u)
     {
-        const auto ispecies1 = species.findWithFormula(formula1);
-        const auto ispecies2 = species.findWithFormula(formula2);
+        auto const ispecies1 = species.findWithFormula(formula1);
+        auto const ispecies2 = species.findWithFormula(formula2);
         if(ispecies1 >= species.size() || ispecies2 >= species.size())
             continue;
         iU.push_back(ispecies1);
         iU.push_back(ispecies2);
-        u0(ispecies2, ispecies1) = params[0];
-        uT(ispecies2, ispecies1) = params[1];
+        u0(ispecies1, ispecies2) = u0(ispecies2, ispecies1) = params[0];
+        uT(ispecies1, ispecies2) = uT(ispecies2, ispecies1) = params[1];
     }
 
     iR = unique(iR);
     iQ = unique(iQ);
     iU = unique(iU);
     // iUQ = intersect(iU, iQ);
+
+    auto const irw = index(iR, iw);
+    auto const iqw = index(iQ, iw);
+
+    errorifnot(irw < iR.size(), "The surface area parameter rᵢ in the Extended UNIQUAC model is expected for H2O.");
+    errorifnot(iqw < iQ.size(), "The volume parameter qᵢ in the Extended UNIQUAC model is expected for H2O.");
 
     // errorif(identical(iR, iQ), "Expecting both rᵢ and qᵢ parameters in the Extended UNIQUAC model, but for some species, only one of these parameters were provided.");
 
@@ -128,21 +158,24 @@ auto createActivityModelExtendedUNIQUAC(SpeciesList const& species, ActivityMode
         // Evaluate the state of the aqueous solution
         auto const& aqstate = solution.state(T, P, x);
 
+        // The ionic strength of the solution and its square root
         auto const& I = aqstate.Ie;
         auto const& sqrtI = sqrt(I);
 
-        // Set the state of matter of the phase
+        // The molalities of the species and their natural log
+        auto const& m = aqstate.m;
+        auto const& ln_m = log(m);
+
+        // Set the state of matter of the phase (always liquid for the e-UNIQUAC model)
         props.som = StateOfMatter::Liquid;
 
-        // The mole fraction of water
-        auto const xw = x[iH2O];
+        // The mole fraction of water and its natural log
+        auto const xw = x[iw];
+        auto const ln_xw = log(xw);
 
         // Calculate the Debye-Huckel A coefficient according to equation (6) of Thomsen (2005)
-        const auto Tr = 273.15;
-        const auto A = 1.131 + 1.335e-3*(T - Tr) + 1.164e-5*(T - Tr)*(T - Tr);
-        const auto b = 1.5; // This is the aB term in the Debye-Huckel contribution (assumed constant in the e-UNIQUAC model)
-        const auto Z = 10.0; // The co-ordination number z of the UNIQUAC model (Thomasen, 1997, equation 4.2)
-        const auto Lambda = 1.0 + b*sqrtI;
+        auto const A = 1.131 + 1.335e-3*(T - T0) + 1.164e-5*(T - T0)*(T - T0);
+        auto const Lambda = 1.0 + b*sqrtI;
         auto const alpha = A*sqrtI/Lambda;
 
         xr = x(iR);
@@ -161,25 +194,46 @@ auto createActivityModelExtendedUNIQUAC(SpeciesList const& species, ActivityMode
 
         thetapsi = tr(psi) * theta.matrix();
 
-        lnGammaDH(iDH) = -z2(iDH)*alpha;
+        // Reset the values of all activity coefficient contributions
+        ln_gDH = 0.0;
+        ln_gC = 0.0;
+        ln_gR = 0.0;
+        ln_gCinf = 0.0;
+        ln_gRinf = 0.0;
 
-        for(auto const& [i, ispecies] : enumerate(iR))
-            lnGammaCombinatorial[ispecies] = log(phi[i]/x[i]) + 1 - phi[i]/x[i] - 0.5*Z*q[i]*(log(phi[i]/theta[i]) + 1 - phi[i]/theta[i]);
+        // Calculate the Debye-Huckel activity coefficients for charged species -- see equation (8) of Thomsen (2005)
+        ln_gDH(iDH) = -z2(iDH)*alpha;
 
-        for(auto const& [i, ispecies] : enumerate(iQ))
-            lnGammaResidual[ispecies] = q[i]*(1 - log(thetapsi[i]) - (theta * ArrayXr(psi.row(i))/thetapsi).sum());
+        // Calculate the Debye-Huckel activity coefficients for water species -- see equation (9) of Thomsen (2005)
+        ln_gDH[iw] = 2*Mw*A*(Lambda - 1/Lambda - 2*log(Lambda))/(b*b*b);
 
-        // Set the activity coefficients of the solutes
-        // props.ln_g = pzstate.ln_gamma;
+        // Calculate the UNIQUAC combinatorial activity coefficients for the species -- see equation (10) of Thomsen (2005) and equation (13) of Hingerl et al. (2014)
+        for(auto const& [i, ispecies] : enumerate(iR)) {
+            ln_gC[ispecies] = log(phi[i]/xr[i]) + 1 - phi[i]/xr[i] - 0.5*Z*q[i]*(log(phi[i]/theta[i]) + 1 - phi[i]/theta[i]);
+            ln_gCinf[ispecies] = log(r[i]/r[irw]) + 1 - r[i]/r[irw] - 0.5*Z*q[i]*(log((r[i]*q[iqw])/(r[irw]*q[i])) + 1 - (r[i]*q[iqw])/(r[irw]*q[i]));
+        }
 
-        // // Set the activities of the solutes
-        // props.ln_a = props.ln_g + log(aqstate.m);
+        // Calculate the UNIQUAC residual activity coefficients for the species -- see equation (16) of Thomsen (2005) and equation (13) of Hingerl et al. (2014)
+        for(auto const& [i, ispecies] : enumerate(iQ)) {
+            ln_gR[ispecies] = q[i]*(1 - log(thetapsi[i]) - (theta * ArrayXr(psi.row(i))/thetapsi).sum());
+            ln_gRinf[ispecies] = q[i]*(1 - log(psi(iqw, i)) - psi(i, iqw)/psi(iqw, iqw)); // Note: Thomsen (2005) always assume ψ(w,w) = 1, but here we don't necessarily; that's why psi(iqw, iqw) is used here.
+        }
 
-        // // Set the activitiy of water
-        // props.ln_a[iH2O] = pzstate.ln_aw;
+        // Set the activity coefficients of the species in the unsymmetrical convention and molal scale
+        props.ln_g = ln_gDH + (ln_gC - ln_gCinf) + (ln_gR - ln_gRinf) + ln_xw;
 
-        // // Set the activity coefficient of water (mole fraction scale)
-        // props.ln_g[iH2O] = pzstate.ln_aw - log(xw);
+        // Set the activity coefficient of water species in the symmetrical convention and mole-fraction scale
+        props.ln_g[iw] = ln_gDH[iw] + ln_gC[iw] + ln_gR[iw];
+
+        // Set the activities of the aqueous species
+        props.ln_a = props.ln_g + ln_m;
+
+        // Set the activitiy of water species
+        props.ln_a[iw] = props.ln_g[iw] + ln_xw;
+
+        // auto const GxDH = -4*xw*Mw*A*(log(Lambda) - Lambda + 1 + 0.5*b*b*I)/(b*b*b);
+        // props.Gx =
+
     };
 
     return fn;
