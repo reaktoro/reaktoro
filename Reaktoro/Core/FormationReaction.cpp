c2231ccfbbcd8fbfcee79ac83af9bb3d33929bfa
Allan Leal <allan.m.m.leal@gmail.com>
2024-01-08 14:12:39 +0100

    Combine parameters from reaction and volume models in reaction standard thermo models in Data object

--- Reaktoro/Core/FormationReaction.cpp
+++ Reaktoro/Core/FormationReaction.cpp
@@ -57,8 +57,10 @@ struct FormationReaction::Impl
 
         const auto num_reactants = reactants.size();
 
-        // Collect Param objects from both rxn_thermo_model and std_volume_model.
-        Vec<Param> params = concatenate(rxn_thermo_model.params(), std_volume_model.params());
+        // Collect parameters from both rxn_thermo_model and std_volume_model.
+        Data params;
+        params.add(rxn_thermo_model.params());
+        params.add(std_volume_model.params());
 
         Vec<StandardThermoProps> reactants_props(num_reactants);
 
