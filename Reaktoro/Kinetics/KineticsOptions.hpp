6a17115261e582eab1bad97362be042c07380717
Allan Leal <allan.m.m.leal@gmail.com>
2024-01-16 23:57:08 +0100

    Adapt according to changesin Optima v0.5

--- Reaktoro/Kinetics/KineticsOptions.hpp
+++ Reaktoro/Kinetics/KineticsOptions.hpp
@@ -27,32 +27,11 @@ struct KineticsOptions : EquilibriumOptions
 {
     /// Construct a default KineticsOptions object.
     KineticsOptions()
-    {
-        // For kinetics, it is crucial that every calculation performs at least
-        // one Newton step, even if the 0th iteration already shows sactisfatory
-        // residuals that pass the convergence test. Without this, we see
-        // simulations in which, for example, a mineral initially absent (with
-        // amount 1e-16) that should precipitate ends up not precipitating
-        // because the calculation already exists at the 0th iteration, without
-        // a Newton step applied.
-        EquilibriumOptions::optima.convergence.requires_at_least_one_iteration = true;
-
-        // TODO: Requiring at least one Newton step in EquilibriumSolver should
-        // be default. This needs Optima to be adjusted, however, so that it
-        // always pick the same set of basic variables that resulted from the
-        // last calculation. Currently, this does not happen, and some
-        // EquilibriumSolver tests fail because it was expecting just an extra
-        // iteration for convergence, but becasue of the change in basic
-        // variables, 3 or 4 iterations become needed instead.
-    }
+    {}
 
     /// Construct a  KineticsOptions object from a EquilibriumOptions one.
     KineticsOptions(EquilibriumOptions const& other)
-    : EquilibriumOptions(other)
-    {
-        // Check reasons above.
-        EquilibriumOptions::optima.convergence.requires_at_least_one_iteration = true;
-    }
+    : EquilibriumOptions(other) {}
 
     /// The time step used for preconditioning the chemical state when performing the very first chemical kinetics step.
     double dt0 = 1e-6;
