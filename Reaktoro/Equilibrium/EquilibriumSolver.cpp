1bead93685f893e96fded90db9ea1eb6545ae069
Allan Leal <allan.m.m.leal@gmail.com>
2024-01-16 21:45:49 +0100

    Create a backup chemica state object before equilibrium calculation

--- Reaktoro/Equilibrium/EquilibriumSolver.cpp
+++ Reaktoro/Equilibrium/EquilibriumSolver.cpp
@@ -360,7 +360,7 @@ struct EquilibriumSolver::Impl
         updateOptProblem(state, conditions, restrictions);
         updateOptState(state);
 
-        auto optstatebkp = optstate;
+        const auto optstatebkp = optstate;
 
         result.optima = optsolver.solve(optproblem, optstate);
 
